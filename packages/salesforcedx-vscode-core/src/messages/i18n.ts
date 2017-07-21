537f7f1cdbf765f697f22bfa36a5c860610bb743
Nick Chen <nchen@salesforce.com>
2017-07-21 12:20:04 -0700

    Add BSD-3 License
    @W-4165821@

--- packages/salesforcedx-vscode-core/src/messages/i18n.ts
+++ packages/salesforcedx-vscode-core/src/messages/i18n.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2017, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 /**
  * Conventions:
  * _message: is for unformatted text that will be shown as-is to
