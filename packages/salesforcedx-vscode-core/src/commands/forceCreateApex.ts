4f7dd80cf3517dfeb82af155f4e6b1493b262397
james.sweetman <james.sweetman@salesforce.com>
2017-08-07 15:22:27 -0700

    Uses the desired flow but slow and choppy because of filewatcher

--- packages/salesforcedx-vscode-core/src/commands/forceCreateApex.ts
+++ packages/salesforcedx-vscode-core/src/commands/forceCreateApex.ts
@@ -5,60 +5,72 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-// import {
-//   CliCommandExecutor,
-//   SfdxCommandBuilder
-// } from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
+import {
+  CliCommandExecutor,
+  SfdxCommandBuilder
+} from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import fs = require('fs');
 // import { channelService } from '../channels';
-// import { nls } from '../messages';
+import { nls } from '../messages';
 // import { notificationService } from '../notifications';
 // import { CancellableStatusBar, taskViewService } from '../statuses';
 
 export async function forceCreateApex(dir?: any) {
   console.log('create apex class extension activated');
+  console.log('dir: ' + dir);
   vscode.window
-    .showQuickPick(['class', 'test class', 'exception', 'email'])
+    .showQuickPick([
+      'DefaultApexClass',
+      'ApexException',
+      'ApexUnitTest',
+      'InboundEmailService'
+    ])
     .then(selection => {
       if (!selection) {
         return;
       }
 
-      const inputOptions = <vscode.InputBoxOptions>{
-        prompt: 'Please enter desired filename',
-        value: selection
-      };
+      vscode.commands
+        .executeCommand('explorer.newFile', function(x: any) {
+          console.log(x);
+        })
+        .then(y => {
+          console.log(y);
+          const fswatcher = vscode.workspace.createFileSystemWatcher(
+            '**',
+            false,
+            true,
+            true
+          );
+          fswatcher.onDidCreate(e => {
+            console.log(e);
+            const fileName = path.basename(e.fsPath);
+            const directoryName = path.dirname(e.fsPath);
+            fs.unlinkSync(e.fsPath);
+            fswatcher.dispose();
 
-      vscode.window.showInputBox(inputOptions).then(fileName => {
-        const fileContents = fs.readFileSync(
-          '/Users/james.sweetman/Documents/sfdx-dreamhouse-internal/README.md'
-        );
-        const rootpath = vscode.workspace.rootPath
-          ? vscode.workspace.rootPath
-          : '';
-        if (fileName == undefined) {
-          fileName = '';
-        }
-        fs.writeFile(path.join(rootpath, fileName), fileContents, function(
-          err
-        ) {
-          if (err) {
-            vscode.window.showErrorMessage(err.message);
-          }
-          vscode.window.showInformationMessage(fileName + ' created');
+            const cancellationTokenSource = new vscode.CancellationTokenSource();
+            const cancellationToken = cancellationTokenSource.token;
+            const execution = new CliCommandExecutor(
+              new SfdxCommandBuilder()
+                .withDescription(nls.localize('force_create_apex_text'))
+                .withArg('force:apex:class:create')
+                .withFlag('--classname', fileName)
+                .withFlag('--template', selection)
+                .withFlag('--outputdir', directoryName)
+                .build(),
+              { cwd: vscode.workspace.rootPath }
+            ).execute(cancellationToken);
+            console.log(execution.command.toCommand());
+            execution.stdoutSubject.subscribe(data => {
+              console.log(data.toString());
+            });
+            execution.stderrSubject.subscribe(data => {
+              console.log(data.toString());
+            });
+          });
         });
-      });
     });
-  // const uri = vscode.Uri.parse(
-  //   'file:///Users/james.sweetman/Documents/sfdx-dreamhouse-internal/README.md'
-  // );
-  // const position = new vscode.Position(2, 1);
-  // const workspaceEdit = await vscode.commands.executeCommand(
-  //   'vscode.executeDocumentRenameProvider',
-  //   uri,
-  //   position,
-  //   'help'
-  // );
 }
