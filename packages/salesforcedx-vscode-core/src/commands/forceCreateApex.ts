aee98d9afd0c20d5a65cb843894d2c085b6d00a2
james.sweetman <james.sweetman@salesforce.com>
2017-08-08 16:58:06 -0700

    implement 2nd flow for apex create

--- packages/salesforcedx-vscode-core/src/commands/forceCreateApex.ts
+++ packages/salesforcedx-vscode-core/src/commands/forceCreateApex.ts
@@ -32,45 +32,102 @@ export async function forceCreateApex(dir?: any) {
         return;
       }
 
-      vscode.commands
-        .executeCommand('explorer.newFile', function(x: any) {
-          console.log(x);
-        })
-        .then(y => {
-          console.log(y);
-          const fswatcher = vscode.workspace.createFileSystemWatcher(
-            '**',
-            false,
-            true,
-            true
-          );
-          fswatcher.onDidCreate(e => {
-            console.log(e);
-            const fileName = path.basename(e.fsPath);
-            const directoryName = path.dirname(e.fsPath);
-            fs.unlinkSync(e.fsPath);
-            fswatcher.dispose();
+      // input boxes for template/filename/path
+      const fileNameInputOptions = <vscode.InputBoxOptions>{
+        prompt: 'Please enter desired filename',
+        value: selection
+      };
 
-            const cancellationTokenSource = new vscode.CancellationTokenSource();
-            const cancellationToken = cancellationTokenSource.token;
-            const execution = new CliCommandExecutor(
-              new SfdxCommandBuilder()
-                .withDescription(nls.localize('force_create_apex_text'))
-                .withArg('force:apex:class:create')
-                .withFlag('--classname', fileName)
-                .withFlag('--template', selection)
-                .withFlag('--outputdir', directoryName)
-                .build(),
-              { cwd: vscode.workspace.rootPath }
-            ).execute(cancellationToken);
-            console.log(execution.command.toCommand());
-            execution.stdoutSubject.subscribe(data => {
-              console.log(data.toString());
-            });
-            execution.stderrSubject.subscribe(data => {
-              console.log(data.toString());
-            });
+      vscode.window.showInputBox(fileNameInputOptions).then(fileName => {
+        if (!fileName || fileName === '') {
+          return;
+        }
+
+        const editor = vscode.window.activeTextEditor;
+        const rootPath = vscode.workspace.rootPath
+          ? vscode.workspace.rootPath
+          : '';
+        let directoryName;
+        if (editor === undefined) {
+          directoryName = '';
+        } else {
+          directoryName = path.relative(
+            rootPath,
+            path.dirname(editor.document.fileName)
+          );
+        }
+
+        const dirInputOptions = <vscode.InputBoxOptions>{
+          prompt:
+            'Please enter desired directory (default is top level workspace directory)',
+          value: directoryName
+        };
+
+        vscode.window.showInputBox(dirInputOptions).then(dirName => {
+          if (dirName === undefined) {
+            return;
+          }
+          const cancellationTokenSource = new vscode.CancellationTokenSource();
+          const cancellationToken = cancellationTokenSource.token;
+          const execution = new CliCommandExecutor(
+            new SfdxCommandBuilder()
+              .withDescription(nls.localize('force_create_apex_text'))
+              .withArg('force:apex:class:create')
+              .withFlag('--classname', fileName)
+              .withFlag('--template', selection)
+              .withFlag('--outputdir', !dirName ? '.' : dirName)
+              .build(),
+            { cwd: vscode.workspace.rootPath }
+          ).execute(cancellationToken);
+          console.log(execution.command.toCommand());
+          execution.stdoutSubject.subscribe(data => {
+            console.log(data.toString());
+          });
+          execution.stderrSubject.subscribe(data => {
+            console.log(data.toString());
           });
         });
+      });
+
+      // vscode.commands
+      //   .executeCommand('explorer.newFile', function(x: any) {
+      //     console.log(x);
+      //   })
+      //   .then(y => {
+      //     console.log(y);
+      //     const fswatcher = vscode.workspace.createFileSystemWatcher(
+      //       '**',
+      //       false,
+      //       true,
+      //       true
+      //     );
+      //     fswatcher.onDidCreate(e => {
+      //       console.log(e);
+      //       const fileName = path.basename(e.fsPath);
+      //       const directoryName = path.dirname(e.fsPath);
+      //       fs.rename(e.fsPath, e.fsPath + '.cls');
+      //       fswatcher.dispose();
+
+      //       const cancellationTokenSource = new vscode.CancellationTokenSource();
+      //       const cancellationToken = cancellationTokenSource.token;
+      //       const execution = new CliCommandExecutor(
+      //         new SfdxCommandBuilder()
+      //           .withDescription(nls.localize('force_create_apex_text'))
+      //           .withArg('force:apex:class:create')
+      //           .withFlag('--classname', fileName)
+      //           .withFlag('--template', selection)
+      //           .withFlag('--outputdir', directoryName)
+      //           .build(),
+      //         { cwd: vscode.workspace.rootPath }
+      //       ).execute(cancellationToken);
+      //       console.log(execution.command.toCommand());
+      //       execution.stdoutSubject.subscribe(data => {
+      //         console.log(data.toString());
+      //       });
+      //       execution.stderrSubject.subscribe(data => {
+      //         console.log(data.toString());
+      //       });
+      //     });
+      //   });
     });
 }
