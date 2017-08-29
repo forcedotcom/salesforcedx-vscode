c7155da5f7d5e891b8ccea88ae7095d96fc0ccad
Winston <wchow@salesforce.com>
2017-08-29 15:25:31 -0700

    Refactor commandOutput into common class in utils

--- packages/salesforcedx-apex-debugger/src/core/streamingService.ts
+++ packages/salesforcedx-apex-debugger/src/core/streamingService.ts
@@ -5,6 +5,15 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+<<<<<<< HEAD
+=======
+import {
+  CliCommandExecutor,
+  CommandExecution,
+  CommandOutput,
+  SfdxCommandBuilder
+} from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
+>>>>>>> Refactor commandOutput into common class in utils
 import { StreamingClient, StreamingClientInfo } from './streamingClient';
 
 export class StreamingService {
@@ -68,4 +77,57 @@ export class StreamingService {
     }
     return false;
   }
+<<<<<<< HEAD
+=======
+
+  public async getOrgInfo(projectPath: string): Promise<OrgInfo> {
+    const execution = new CliCommandExecutor(
+      new SfdxCommandBuilder()
+        .withArg('force:org:display')
+        .withArg('--json')
+        .build(),
+      { cwd: projectPath }
+    ).execute();
+
+    const output = new CommandOutput();
+    return output.getCmdResult(execution);
+  }
+
+  private async getCmdResult(execution: CommandExecution): Promise<OrgInfo> {
+    const outputHolder = new CommandOutput();
+    execution.stderrSubject.subscribe(data =>
+      outputHolder.setStdErr(data.toString())
+    );
+    execution.stdoutSubject.subscribe(data =>
+      outputHolder.setStdOut(data.toString())
+    );
+
+    return new Promise<
+      OrgInfo
+    >(
+      (
+        resolve: (result: OrgInfo) => void,
+        reject: (reason: string) => void
+      ) => {
+        execution.processExitSubject.subscribe(data => {
+          if (data != undefined && data.toString() === '0') {
+            try {
+              const cmdResult = JSON.parse(outputHolder.getStdOut());
+              if (cmdResult && cmdResult.result) {
+                const orgInfo = cmdResult.result as OrgInfo;
+                return resolve(orgInfo);
+              } else {
+                return reject(outputHolder.getStdOut());
+              }
+            } catch (e) {
+              return reject(outputHolder.getStdOut());
+            }
+          } else {
+            return reject(outputHolder.getStdErr());
+          }
+        });
+      }
+    );
+  }
+>>>>>>> Refactor commandOutput into common class in utils
 }
--- packages/salesforcedx-apex-debugger/test/unit/core/sessionService.test.ts
+++ packages/salesforcedx-apex-debugger/test/unit/core/sessionService.test.ts
@@ -5,7 +5,10 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { SfdxCommandBuilder } from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
+import {
+  CommandOutput,
+  SfdxCommandBuilder
+} from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import { SessionService } from '../../../src/core/sessionService';
