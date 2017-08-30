7a492c04f62d964c5792271525215425eedec90a
Winston <wchow@salesforce.com>
2017-08-30 11:19:40 -0700

    Remove unused imports

--- packages/salesforcedx-apex-debugger/test/unit/adapter/apexDebug.test.ts
+++ packages/salesforcedx-apex-debugger/test/unit/adapter/apexDebug.test.ts
@@ -5,7 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { CommandOutput } from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import {
