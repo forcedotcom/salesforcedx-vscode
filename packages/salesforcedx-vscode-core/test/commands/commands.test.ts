7f0d079cb2ef43be4b22a363d02f1209f441368a
Nick Chen <nchen@salesforce.com>
2017-09-11 17:29:22 -0700

    Fix lint error

--- packages/salesforcedx-vscode-core/test/commands/commands.test.ts
+++ packages/salesforcedx-vscode-core/test/commands/commands.test.ts
@@ -22,7 +22,7 @@ import {
 // tslint:disable:no-unused-expression
 describe('Command Utilities', () => {
   const WORKSPACE_NAME = 'sfdx-simple';
-  const SFDX_SIMPLE_NUM_OF_DIRS = 11;
+
   describe('EmptyParametersGatherer', () => {
     it('Should always return continue with empty object as data', async () => {
       const gatherer = new EmptyParametersGatherer();
