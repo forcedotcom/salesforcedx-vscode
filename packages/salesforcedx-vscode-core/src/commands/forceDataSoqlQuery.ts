1bc0aa5804b90d422304b4d1478629b424e0b92c
muenzpraeger <rwinkelmeyer@salesforce.com>
2017-09-18 14:01:18 +0200

    Updated SOQL query implementation

--- packages/salesforcedx-vscode-core/src/commands/forceDataSoqlQuery.ts
+++ packages/salesforcedx-vscode-core/src/commands/forceDataSoqlQuery.ts
@@ -9,29 +9,65 @@ import {
   Command,
   SfdxCommandBuilder
 } from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
+import * as vscode from 'vscode';
 import { nls } from '../messages';
 import {
-  GetUserInput,
+  CancelResponse,
+  ContinueResponse,
+  ParametersGatherer,
   SfdxCommandlet,
   SfdxCommandletExecutor,
-  SfdxWorkspaceChecker,
-  UserInput
+  SfdxWorkspaceChecker
 } from './commands';
 
 class ForceDataSoqlQueryExecutor extends SfdxCommandletExecutor<{}> {
-  public build(data: UserInput): Command {
+  public build(data: QueryInput): Command {
     return new SfdxCommandBuilder()
-      .withDescription(nls.localize('force_data_soql_query_text'))
+      .withDescription(nls.localize('force_data_soql_query_input_text'))
       .withArg('force:data:soql:query')
       .withFlag('--query', `${data.input}`)
       .build();
   }
 }
 
+export type QueryInput = {
+  input: string;
+};
+
+export class GetQueryInput implements ParametersGatherer<{ input: string }> {
+  public async gather(): Promise<
+    CancelResponse | ContinueResponse<{ input: string }>
+  > {
+    const editor = await vscode.window.activeTextEditor;
+
+    let input;
+
+    if (!editor) {
+      const userInputOptions = <vscode.InputBoxOptions>{
+        prompt: nls.localize('parameter_gatherer_enter_soql_query')
+      };
+      input = await vscode.window.showInputBox(userInputOptions);
+    } else {
+      const document = editor!.document;
+      if (editor!.selection.isEmpty) {
+        const userInputOptions = <vscode.InputBoxOptions>{
+          prompt: nls.localize('parameter_gatherer_enter_soql_query')
+        };
+        input = await vscode.window.showInputBox(userInputOptions);
+      } else {
+        input = document.getText(editor!.selection);
+      }
+    }
+
+    input = input!.replace('[', '').replace(']', '');
+    return input ? { type: 'CONTINUE', data: { input } } : { type: 'CANCEL' };
+  }
+}
+
 const workspaceChecker = new SfdxWorkspaceChecker();
-const parameterGatherer = new GetUserInput();
 
 export async function forceDataSoqlQuery(explorerDir?: any) {
+  const parameterGatherer = new GetQueryInput();
   const commandlet = new SfdxCommandlet(
     workspaceChecker,
     parameterGatherer,
