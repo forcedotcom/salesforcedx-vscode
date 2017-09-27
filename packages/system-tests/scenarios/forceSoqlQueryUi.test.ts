819366bd0ddb6a897b32b0a17316b8a82e339b39
james-sweetman <james.sweetman@salesforce.com>
2017-09-27 01:46:57 -0700

    Select all text in platform agnostic way, add streaming flag to test commands

--- packages/system-tests/scenarios/forceSoqlQueryUi.test.ts
+++ packages/system-tests/scenarios/forceSoqlQueryUi.test.ts
@@ -76,7 +76,7 @@ describe(TITLE, () => {
     await app.wait();
 
     // Select all text in the current window
-    await app.client.keys(['Meta', 'a', 'NULL'], false);
+    await app.command('editor.action.selectAll');
 
     // Invoke SFDX: Execute SOQL Query command by name
     await app.command('workbench.action.quickOpen');
