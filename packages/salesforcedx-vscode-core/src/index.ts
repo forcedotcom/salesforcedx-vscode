c001954741517e40b50d95085f44581f17e8f546
james.sweetman <james.sweetman@salesforce.com>
2017-11-16 13:40:26 -0800

    Fix linting errors

--- packages/salesforcedx-vscode-core/src/index.ts
+++ packages/salesforcedx-vscode-core/src/index.ts
@@ -5,7 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import * as path from 'path';
 import * as vscode from 'vscode';
 import {
   forceAliasList,
