e0926a1a185615f5bd96de5fcae61a8c705ef7a6
Jonathan <jwidjaja@salesforce.com>
2017-11-16 11:04:28 -0800

    Add tests to verify exception type quick list shows enabled exceptions at the top

--- packages/salesforcedx-vscode-apex-debugger/test/index.test.ts
+++ packages/salesforcedx-vscode-apex-debugger/test/index.test.ts
@@ -1,6 +1,15 @@
+import {
+  EXCEPTION_BREAKPOINT_BREAK_MODE_ALWAYS,
+  EXCEPTION_BREAKPOINT_BREAK_MODE_NEVER
+} from '@salesforce/salesforcedx-apex-debugger/out/src';
 import { expect } from 'chai';
 import * as vscode from 'vscode';
-import { ApexDebuggerConfigurationProvider } from '../src/index';
+import {
+  ApexDebuggerConfigurationProvider,
+  ExceptionBreakpointItem,
+  mergeExceptionBreakpointInfos
+} from '../src/index';
+import { nls } from '../src/messages';
 
 describe('Extension Setup', () => {
   describe('Configuration provider', () => {
@@ -47,6 +56,99 @@ describe('Extension Setup', () => {
       const configs = provider.provideDebugConfigurations(undefined);
 
       expect(configs).to.deep.equal([expectedConfig]);
+    });
+  });
+
+  describe('Exception breakpoint', () => {
+    describe('Merge breakpoint infos', () => {
+      let breakpointInfos: ExceptionBreakpointItem[] = [];
+
+      beforeEach(() => {
+        breakpointInfos = [
+          {
+            typeref: 'barexception',
+            label: 'barexception',
+            uri: 'file:///barexception.cls',
+            breakMode: EXCEPTION_BREAKPOINT_BREAK_MODE_NEVER,
+            description: ''
+          },
+          {
+            typeref: 'fooexception',
+            label: 'fooexception',
+            uri: 'file:///fooexception.cls',
+            breakMode: EXCEPTION_BREAKPOINT_BREAK_MODE_NEVER,
+            description: ''
+          },
+          {
+            typeref: 'com/salesforce/api/exception/NullPointerException',
+            label: 'System.NullPointerException',
+            breakMode: EXCEPTION_BREAKPOINT_BREAK_MODE_NEVER,
+            description: ''
+          }
+        ];
+      });
+      it('Should order breakpoints by enabled first', () => {
+        const enabledBreakpoints = {
+          typerefs: ['com/salesforce/api/exception/NullPointerException']
+        };
+
+        const exceptionBreakpointQuickPicks = mergeExceptionBreakpointInfos(
+          breakpointInfos,
+          enabledBreakpoints
+        );
+
+        expect(exceptionBreakpointQuickPicks.length).to.equal(3);
+        expect(exceptionBreakpointQuickPicks[0].typeref).to.equal(
+          'com/salesforce/api/exception/NullPointerException'
+        );
+        expect(exceptionBreakpointQuickPicks[0].breakMode).to.equal(
+          EXCEPTION_BREAKPOINT_BREAK_MODE_ALWAYS
+        );
+        expect(exceptionBreakpointQuickPicks[0].description).to.equal(
+          `$(stop) ${nls.localize('always_break_text')}`
+        );
+        expect(exceptionBreakpointQuickPicks[1].typeref).to.equal(
+          'barexception'
+        );
+        expect(exceptionBreakpointQuickPicks[1].breakMode).to.equal(
+          EXCEPTION_BREAKPOINT_BREAK_MODE_NEVER
+        );
+        expect(exceptionBreakpointQuickPicks[2].typeref).to.equal(
+          'fooexception'
+        );
+        expect(exceptionBreakpointQuickPicks[2].breakMode).to.equal(
+          EXCEPTION_BREAKPOINT_BREAK_MODE_NEVER
+        );
+      });
+
+      it('Should not modify breakpoint infos if there is no enabled breakpoints', () => {
+        const exceptionBreakpointQuickPicks = mergeExceptionBreakpointInfos(
+          breakpointInfos,
+          undefined
+        );
+
+        expect(exceptionBreakpointQuickPicks).to.deep.equal(breakpointInfos);
+      });
+
+      it('Should not modify breakpoint infos if enabled breakpoints has undefined typerefs', () => {
+        const exceptionBreakpointQuickPicks = mergeExceptionBreakpointInfos(
+          breakpointInfos,
+          {}
+        );
+
+        expect(exceptionBreakpointQuickPicks).to.deep.equal(breakpointInfos);
+      });
+
+      it('Should not modify breakpoint infos if enabled breakpoints has empty typerefs', () => {
+        const exceptionBreakpointQuickPicks = mergeExceptionBreakpointInfos(
+          breakpointInfos,
+          {
+            typerefs: []
+          }
+        );
+
+        expect(exceptionBreakpointQuickPicks).to.deep.equal(breakpointInfos);
+      });
     });
   });
 });
