d959d05c901a3dcf4b5fdfe4f0cccec894846821
Nick Chen <nchen@salesforce.com>
2018-01-12 11:28:06 -0800

    Fix tslint error

--- packages/salesforcedx-vscode-lwc/src/index.ts
+++ packages/salesforcedx-vscode-lwc/src/index.ts
@@ -5,7 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import * as fs from 'fs';
 import * as lwcLanguageServer from 'lwc-language-server';
 import * as path from 'path';
 import { ExtensionContext, workspace } from 'vscode';
