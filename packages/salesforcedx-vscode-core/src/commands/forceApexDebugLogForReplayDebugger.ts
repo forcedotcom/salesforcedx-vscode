2372d4cb277bc49b1236ce822fc8943c851510d9
James <james.sweetman@salesforce.com>
2018-01-15 16:04:30 -0800

    Add command to set debuglevel for traceflag, does not set start/expiration date nor restore levels

--- packages/salesforcedx-vscode-core/src/commands/forceApexDebugLogForReplayDebugger.ts
+++ packages/salesforcedx-vscode-core/src/commands/forceApexDebugLogForReplayDebugger.ts
@@ -6,25 +6,85 @@
  */
 
 import {
+  CliCommandExecutor,
   Command,
+  CommandOutput,
   SfdxCommandBuilder
 } from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
+import * as vscode from 'vscode';
 import { nls } from '../messages';
 import {
+  CancelResponse,
+  ContinueResponse,
   EmptyParametersGatherer,
+  ParametersGatherer,
   SfdxCommandlet,
   SfdxCommandletExecutor,
   SfdxWorkspaceChecker
 } from './commands';
 
-export class ForceApexDebugLogForReplayDebuggerExecutor extends SfdxCommandletExecutor<{}> {
-  public build(data: {}): Command {
-    return new SfdxCommandBuilder().build();
+export class ForceApexDebugLogForReplayDebuggerExecutor extends SfdxCommandletExecutor<
+  ApexDebugLevelInfo
+> {
+  public build(data: ApexDebugLevelInfo): Command {
+    return new SfdxCommandBuilder()
+      .withDescription(
+        nls.localize('force_apex_debug_log_replay_debugger_text')
+      )
+      .withArg('force:data:record:update')
+      .withFlag('--sobjecttype', 'DebugLevel')
+      .withFlag('--sobjectid', data.debugLevelId)
+      .withFlag('--values', 'ApexCode=Finest Visualforce=Finest')
+      .withArg('--usetoolingapi')
+      .build();
+  }
+}
+
+export type ApexDebugLevelInfo = {
+  debugLevelId: string;
+  apexCodeDebugLevel: string;
+  vfCodeDebugLevel: string;
+};
+class ApexDebugLevelGatherer implements ParametersGatherer<ApexDebugLevelInfo> {
+  public async gather(): Promise<
+    CancelResponse | ContinueResponse<ApexDebugLevelInfo>
+  > {
+    const execution = new CliCommandExecutor(
+      new SfdxCommandBuilder()
+        .withArg('force:data:soql:query')
+        .withFlag(
+          '--query',
+          'select id, logtype, startdate, expirationdate, debuglevelid, debuglevel.apexcode, debuglevel.visualforce from traceflag'
+        )
+        .withArg('--usetoolingapi')
+        .withArg('--json')
+        .build(),
+      {
+        cwd: vscode.workspace.rootPath!
+      }
+    ).execute();
+
+    const cmdOutput = new CommandOutput();
+    const result = await cmdOutput.getCmdResult(execution);
+    const resultJson = JSON.parse(result);
+    console.log(resultJson);
+    const apexCodeDebugLevel = resultJson.result.records[0].DebugLevel.ApexCode;
+    const vfCodeDebugLevel =
+      resultJson.result.records[0].DebugLevel.Visualforce;
+    const debugLevelId = resultJson.result.records[0].DebugLevelId;
+    return {
+      type: 'CONTINUE',
+      data: {
+        debugLevelId,
+        apexCodeDebugLevel,
+        vfCodeDebugLevel
+      }
+    };
   }
 }
 
 const workspaceChecker = new SfdxWorkspaceChecker();
-const parameterGatherer = new EmptyParametersGatherer();
+const parameterGatherer = new ApexDebugLevelGatherer();
 
 export function forceApexDebugLogForReplayDebugger() {
   // tslint:disable-next-line:no-invalid-this
