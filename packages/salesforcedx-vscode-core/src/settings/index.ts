f5c0ff099bbc2f72e6f0f6b54cc054787f35e83a
lcampos <lcamposguajardo@gmail.com>
2018-01-16 17:25:26 -0800

    Add extension setting to control showing cli success messages via inf… (#259)
    * Add extension setting to control showing cli success messages via information messages or status bar.

---
+++ packages/salesforcedx-vscode-core/src/settings/index.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2017, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { SfdxCoreSettings } from './sfdxCoreSettings';
+
+export const sfdxCoreSettings = SfdxCoreSettings.getInstance();
