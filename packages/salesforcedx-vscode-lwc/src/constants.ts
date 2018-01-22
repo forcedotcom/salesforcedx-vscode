f7e333b4a8b48d9b5393b1a2c33fd4575f97bf76
Nick Chen <nchen.dev@mac.com>
2018-01-22 11:52:22 -0800

    Bundle eslint and eslint-plugin-lwc (#270)
    * Bundle eslint and eslint-plugin-lwc
    * Fix eslint
    * Only activate for SFDX projects
    
    @W-4615981@

--- packages/salesforcedx-vscode-lwc/src/constants.ts
+++ packages/salesforcedx-vscode-lwc/src/constants.ts
@@ -5,11 +5,5 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { expect } from 'chai';
-
-describe('LWC Extension Tests', () => {
-  it('Simple sample', () => {
-    expect([1, 2, 3].indexOf(5)).to.equal(-1);
-    expect([1, 2, 3].indexOf(0)).to.equal(-1);
-  });
-});
+export const ESLINT_NODEPATH_CONFIG = 'eslint.nodePath';
+export const LWC_EXTENSION_NAME = 'salesforcedx-vscode-lwc';
