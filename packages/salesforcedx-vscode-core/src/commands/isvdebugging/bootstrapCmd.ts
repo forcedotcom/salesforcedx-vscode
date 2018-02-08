2de07e77412f057cfdbf9f1ec8c2f1cdcda4917c
Gunnar Wagenknecht <gwagenknecht@salesforce.com>
2018-02-08 14:54:46 +0100

    Use async/await pattern instead of Promise.then chaining

--- packages/salesforcedx-vscode-core/src/commands/isvdebugging/bootstrapCmd.ts
+++ packages/salesforcedx-vscode-core/src/commands/isvdebugging/bootstrapCmd.ts
@@ -120,36 +120,32 @@ export class IsvDebugBootstrapExecutor extends SfdxCommandletExecutor<{}> {
       this.relativeApexPackageXmlPath
     );
 
-    await Promise.resolve()
-      .then(
-        // 1: create project
-        this.executeCommand(
-          this.buildCreateProjectCommand(response.data),
-          {
-            cwd: projectParentPath
-          },
-          cancellationTokenSource,
-          cancellationToken
-        )
-      )
-      .then(
-        // 2: configure project
-        this.executeCommand(
-          this.buildConfigureProjectCommand(response.data),
-          {
-            cwd: projectPath
-          },
-          cancellationTokenSource,
-          cancellationToken
-        )
-      )
-      .then(
-        // 3a: create package.xml for downloading org apex
-        async () => {
-          this.mkdirSyncRecursive(projectMetadataTempPath);
-          fs.writeFileSync(
-            apexRetrievePackageXmlPath,
-            `<?xml version="1.0" encoding="UTF-8"?>
+    // 1: create project
+    await this.executeCommand(
+      this.buildCreateProjectCommand(response.data),
+      {
+        cwd: projectParentPath
+      },
+      cancellationTokenSource,
+      cancellationToken
+    );
+
+    // 2: configure project
+    await this.executeCommand(
+      this.buildConfigureProjectCommand(response.data),
+      {
+        cwd: projectPath
+      },
+      cancellationTokenSource,
+      cancellationToken
+    );
+
+    // 3a: create package.xml for downloading org apex
+    try {
+      this.mkdirSyncRecursive(projectMetadataTempPath);
+      fs.writeFileSync(
+        apexRetrievePackageXmlPath,
+        `<?xml version="1.0" encoding="UTF-8"?>
 <Package xmlns="http://soap.sforce.com/2006/04/metadata">
   <version>41.0</version>
   <types>
@@ -161,78 +157,75 @@ export class IsvDebugBootstrapExecutor extends SfdxCommandletExecutor<{}> {
     <name>ApexTrigger</name>
   </types>
 </Package>`
-          );
-        }
-      )
-      .then(
-        // 3b: retrieve unmanged org source
-        this.executeCommand(
-          this.buildRetrieveOrgSourceCommand(response.data),
-          {
-            cwd: projectPath
-          },
-          cancellationTokenSource,
-          cancellationToken
-        )
-      )
-      .then(
-        // 4a: unzip retrieved source
-        () => {
-          const zip = new AdmZip(
-            path.join(projectMetadataTempPath, 'unpackaged.zip')
-          );
-          zip.extractAllTo(projectMetadataTempPath, true);
-        }
-      )
-      .then(
-        // 4b: convert org source
-        this.executeCommand(
-          this.buildMetadataApiConvertCommand(response.data),
-          {
-            cwd: projectPath
-          },
-          cancellationTokenSource,
-          cancellationToken
-        )
-      )
-      .then(async () => {
-        // last step: open the folder in VS Code
-        await vscode.commands.executeCommand(
-          'vscode.openFolder',
-          vscode.Uri.parse(projectPath)
-        );
-      })
-      .catch(error => {
-        channelService.appendLine(
-          nls.localize('error_creating_packagexml', error.toString())
-        );
-        notificationService.showErrorMessage(
-          nls.localize('error_creating_packagexml', error.toString())
-        );
-      });
+      );
+    } catch (error) {
+      channelService.appendLine(
+        nls.localize('error_creating_packagexml', error.toString())
+      );
+      notificationService.showErrorMessage(
+        nls.localize('error_creating_packagexml', error.toString())
+      );
+      return;
+    }
+
+    // 3b: retrieve unmanged org source
+    this.executeCommand(
+      this.buildRetrieveOrgSourceCommand(response.data),
+      {
+        cwd: projectPath
+      },
+      cancellationTokenSource,
+      cancellationToken
+    );
+
+    // 4a: unzip retrieved source
+    try {
+      const zip = new AdmZip(
+        path.join(projectMetadataTempPath, 'unpackaged.zip')
+      );
+      zip.extractAllTo(projectMetadataTempPath, true);
+    } catch (error) {
+      channelService.appendLine(
+        nls.localize('error_extracting_org_source', error.toString())
+      );
+      notificationService.showErrorMessage(
+        nls.localize('error_extracting_org_source', error.toString())
+      );
+      return;
+    }
+
+    // 4b: convert org source
+    await this.executeCommand(
+      this.buildMetadataApiConvertCommand(response.data),
+      {
+        cwd: projectPath
+      },
+      cancellationTokenSource,
+      cancellationToken
+    );
+
+    // last step: open the folder in VS Code
+    await vscode.commands.executeCommand(
+      'vscode.openFolder',
+      vscode.Uri.parse(projectPath)
+    );
   }
 
-  public executeCommand(
+  public async executeCommand(
     command: Command,
     options: ExecOptions,
     cancellationTokenSource: vscode.CancellationTokenSource,
     cancellationToken: vscode.CancellationToken
-  ): () => Promise<string> {
-    return () => {
-      const execution = new CliCommandExecutor(command, options).execute(
-        cancellationToken
-      );
+  ): Promise<string> {
+    const execution = new CliCommandExecutor(command, options).execute(
+      cancellationToken
+    );
 
-      const result = new CommandOutput().getCmdResult(execution);
+    const result = new CommandOutput().getCmdResult(execution);
 
-      this.attachExecution(
-        execution,
-        cancellationTokenSource,
-        cancellationToken
-      );
+    this.attachExecution(execution, cancellationTokenSource, cancellationToken);
 
-      return result;
-    };
+    return result;
   }
 
   protected attachExecution(
