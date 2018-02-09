81bbbd7310be02ac4bcbdd82db99d86c5b860ef6
JAMES <jsuplizio@salesforce.com>
2018-02-09 14:56:01 -0800

    Updates based upon feedback pt2

--- packages/salesforcedx-vscode-replay-debugger/src/states/frameExitState.ts
+++ packages/salesforcedx-vscode-replay-debugger/src/states/frameExitState.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2018, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt icensefile in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
 import { LogContext } from '../core/logContext';
