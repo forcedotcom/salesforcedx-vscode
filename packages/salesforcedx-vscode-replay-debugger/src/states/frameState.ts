6da6f9ee67e2c787d94e86bfc4b942b260b3aa1d
JimSuplizio <jsuplizio@salesforce.com>
2018-02-13 18:40:34 -0800

    Stackframe name fixes (#300)
    * Stackframe name fixes
    * remove the unused constants from FrameStateUtil that TSLint decided not to complain about until I submitted.
    * Updates based upon feedback
    * Updates based upon feedback pt2
    
    @W-4684891, W-4685026@

--- packages/salesforcedx-vscode-replay-debugger/src/states/frameState.ts
+++ packages/salesforcedx-vscode-replay-debugger/src/states/frameState.ts
@@ -4,19 +4,14 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import { FrameStateUtil } from './frameStateUtil';
 
-import { SFDC_TRIGGER } from '../constants';
-
-export class FrameState {
-  protected readonly signature: string;
-  protected readonly frameName: string;
+export abstract class FrameState {
+  protected readonly _signature: string;
+  protected readonly _frameName: string;
 
   constructor(fields: string[]) {
-    this.signature = fields[fields.length - 1];
-    if (this.signature.startsWith(SFDC_TRIGGER)) {
-      this.frameName = this.signature.substring(SFDC_TRIGGER.length);
-    } else {
-      this.frameName = this.signature;
-    }
+    this._signature = fields[fields.length - 1];
+    this._frameName = FrameStateUtil.computeFrameName(fields);
   }
 }
