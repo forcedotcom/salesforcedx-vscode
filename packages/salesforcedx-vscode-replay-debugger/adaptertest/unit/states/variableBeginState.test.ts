b36d277ac600c544d817277163d4e17549bc9cc2
James <james.sweetman@salesforce.com>
2018-03-12 16:11:11 -0700

    Add test for begin scope for static variables

--- packages/salesforcedx-vscode-replay-debugger/adaptertest/unit/states/variableBeginState.test.ts
+++ packages/salesforcedx-vscode-replay-debugger/adaptertest/unit/states/variableBeginState.test.ts
@@ -15,7 +15,7 @@ import {
   LaunchRequestArguments
 } from '../../../src/adapter/apexReplayDebug';
 import { LogContext } from '../../../src/core';
-import { FrameEntryState } from '../../../src/states';
+import { FrameEntryState, VariableBeginState } from '../../../src/states';
 
 // tslint:disable:no-unused-expression
 describe('Variable begin scope event', () => {
@@ -27,15 +27,17 @@ describe('Variable begin scope event', () => {
     logFile: logFilePath,
     trace: true
   };
+  const staticVarLogLine =
+    'fakeTime|VARIABLE_SCOPE_BEGIN|[38]|fakeClass.staticInteger|5|false|true';
+  const newStaticVarLogLine =
+    'fakeTime|VARIABLE_SCOPE_BEGIN|[38]|anotherFakeClass.staticInteger|5|false|true';
+  const localVarLogLine =
+    'fakeTime|VARIABLE_SCOPE_BEGIN|[38]|localInteger|10|false|false';
   let map: Map<String, Map<String, ApexVariable>>;
 
   beforeEach(() => {
     map = new Map<String, Map<String, ApexVariable>>();
-    map.set('previousClass', new Map<String, ApexVariable>());
-    map.get('previousClass')!.set(
-      'var1',
-      new ApexVariable('var1', '0', 'Integer')
-    );
+    map.set('fakeClass', new Map<String, ApexVariable>());
     getStaticMapStub = sinon
       .stub(LogContext.prototype, 'getStaticVariablesClassMap')
       .returns(map);
@@ -46,14 +48,53 @@ describe('Variable begin scope event', () => {
   });
 
   it('Should add static variable to frame', () => {
-    //
+    const entryState = new VariableBeginState(staticVarLogLine.split('|'));
+    const context = new LogContext(launchRequestArgs, new ApexReplayDebug());
+    context
+      .getFrames()
+      .push({ id: 0, name: 'execute_anonymous_apex' } as StackFrame);
+
+    expect(entryState.handle(context)).to.be.false;
+    expect(context.getStaticVariablesClassMap().has('fakeClass')).to.be.true;
+    expect(
+      context.getStaticVariablesClassMap().get('fakeClass')!.size
+    ).to.equal(1);
+    expect(context.getStaticVariablesClassMap().get('fakeClass')).to.have.key(
+      'fakeClass.staticInteger'
+    );
   });
 
   it('Should add local variable to frame', () => {
-    //
+    const entryState = new VariableBeginState(localVarLogLine.split('|'));
+    const context = new LogContext(launchRequestArgs, new ApexReplayDebug());
+    context
+      .getFrames()
+      .push({ id: 0, name: 'execute_anonymous_apex' } as StackFrame);
+
+    expect(entryState.handle(context)).to.be.false;
+    expect(context.getStaticVariablesClassMap().has('fakeClass')).to.be.true;
+    expect(
+      context.getStaticVariablesClassMap().get('fakeClass')!.size
+    ).to.equal(1);
+    expect(context.getStaticVariablesClassMap().get('fakeClass')).to.have.key(
+      'fakeClass.localInteger'
+    );
   });
 
   it('Should create class entry in static variable map when class has not been seen before', () => {
-    //
+    const entryState = new VariableBeginState(staticVarLogLine.split('|'));
+    const context = new LogContext(launchRequestArgs, new ApexReplayDebug());
+    context
+      .getFrames()
+      .push({ id: 0, name: 'execute_anonymous_apex' } as StackFrame);
+
+    expect(entryState.handle(context)).to.be.false;
+    expect(context.getStaticVariablesClassMap().has('fakeClass')).to.be.true;
+    expect(
+      context.getStaticVariablesClassMap().get('fakeClass')!.size
+    ).to.equal(1);
+    expect(context.getStaticVariablesClassMap().get('fakeClass')).to.have.key(
+      'fakeClass.staticInteger'
+    );
   });
 });
