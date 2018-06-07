8e3c7ca6da308aec777defb008a4c14ba7d15794
James <james.sweetman@salesforce.com>
2018-06-07 12:40:15 -0700

    Add config.yml, aggregateJUnit script

--- scripts/aggregate-junit-xml.js
+++ scripts/aggregate-junit-xml.js
@@ -1,12 +1,27 @@
 const process = require('process');
 const fs = require('fs-extra');
 const path = require('path');
+const shell = require('shelljs');
 
 const currDir = process.cwd();
-console.log(currDir);
 const packagesDir = path.join(currDir, 'packages');
-console.log(packagesDir);
 const dirs = fs.readdirSync(packagesDir).filter(function(file) {
   return fs.statSync(packagesDir + '/' + file).isDirectory();
 });
-console.log(dirs);
+const junitDirs = dirs.filter(dir => {
+  return fs.existsSync(path.join(packagesDir, dir, 'junit-custom.xml'));
+});
+
+const actualDirs = junitDirs.map(dir => {
+  return path.join(process.cwd(), 'packages', dir, 'junit-custom.xml');
+});
+
+shell.mkdir(path.join(process.cwd(), 'junit-aggregate'));
+let i = 0;
+actualDirs.map(junitPath => {
+  shell.cp(
+    junitPath,
+    path.join(process.cwd(), `junit-aggregate/junit-custom${i}.xml`)
+  );
+  i++;
+});
