7c3a9e4c4c167750828c946fbbd8cdf49096a5b2
James <james.sweetman@salesforce.com>
2018-06-12 11:50:22 -0700

    Activate extension if not activated yet and remove sleep

--- packages/salesforcedx-vscode-lightning/test/deprecatedClassName.test.ts
+++ packages/salesforcedx-vscode-lightning/test/deprecatedClassName.test.ts
@@ -14,21 +14,22 @@ describe('SLDS Deprecated Class Name', () => {
 
   before(async () => {
     if (vscode.workspace.rootPath) {
+      const lightningExtension = vscode.extensions.getExtension(
+        'salesforce.salesforcedx-vscode-lightning'
+      ) as vscode.Extension<any>;
+      if (lightningExtension && !lightningExtension.isActive) {
+        await lightningExtension.activate();
+      }
       res = await vscode.workspace.findFiles(
         path.join('**', 'DemoComponent.cmp')
       );
       await vscode.workspace
         .openTextDocument(res[0])
         .then(document => vscode.window.showTextDocument(document));
-      const lightningExtension = vscode.extensions.getExtension(
-        'salesforcedx-vscode-lightning'
-      ) as vscode.Extension<any>;
-      await lightningExtension.activate();
     }
   });
 
   it('Should create SFDX fix deprecated class command', async () => {
-    // await new Promise(resolve => setTimeout(resolve, 3000));
     const commandList = await vscode.commands.getCommands(true);
     expect(commandList).to.include(
       'sfdx.force.lightning.slds.fix.deprecated.class'
