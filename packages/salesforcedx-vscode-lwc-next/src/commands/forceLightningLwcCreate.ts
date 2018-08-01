e50b82f5d4a7174d135cd5cc840eae3783932f50
Nick Chen <nchen.dev@mac.com>
2018-08-01 11:23:02 -0700

    Change 'web' to 'lwc' for LWC (#541)
    @W-5267747@

--- packages/salesforcedx-vscode-lwc-next/src/commands/forceLightningLwcCreate.ts
+++ packages/salesforcedx-vscode-lwc-next/src/commands/forceLightningLwcCreate.ts
@@ -79,7 +79,7 @@ class ForceLightningLwcCreateExecutor extends (SfdxCommandletExecutor as {
     return new SfdxCommandBuilder()
       .withDescription(nls.localize('force_lightning_lwc_create_text'))
       .withArg('force:lightning:component:create')
-      .withFlag('--type', 'web')
+      .withFlag('--type', 'lwc')
       .withFlag('--componentname', data.fileName)
       .withFlag('--outputdir', data.outputdir)
       .build();
