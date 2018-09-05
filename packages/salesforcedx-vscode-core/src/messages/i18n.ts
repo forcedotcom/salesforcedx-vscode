d57d998943ab54f786e1c1b6597a3e24a0667fe1
Ananya <a.jha@salesforce.com>
2018-09-05 14:16:08 -0700

    Edited message

--- packages/salesforcedx-vscode-core/src/messages/i18n.ts
+++ packages/salesforcedx-vscode-core/src/messages/i18n.ts
@@ -50,7 +50,7 @@ export const messages = {
     'Enter desired directory (Press Enter to confirm or Esc to cancel)',
   parameter_gatherer_enter_username_name: 'Enter target username',
   parameter_gatherer_enter_alias_name:
-    'Enter a scratch org alias or use default alias',
+    'Enter an org alias or use default alias',
   parameter_gatherer_enter_project_name: 'Enter project name',
   parameter_gatherer_paste_forceide_url: 'Paste forceide:// URL from Setup',
   parameter_gatherer_paste_forceide_url_placeholder:
@@ -61,7 +61,7 @@ export const messages = {
   force_org_create_default_scratch_org_text:
     'SFDX: Create a Default Scratch Org...',
 
-  force_org_open_default_scratch_org_text: 'SFDX: Open Default Scratch Org',
+  force_org_open_default_scratch_org_text: 'SFDX: Open Default Org',
 
   force_source_pull_default_scratch_org_text:
     'SFDX: Pull Source from Default Scratch Org',
