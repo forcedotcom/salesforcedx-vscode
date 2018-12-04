c0212b34a9a4f3ff2db33220bf5267759637ae8a
Min Idzelis <midzelis@salesforce.com>
2018-12-04 18:30:47 -0500

    Add initial Aura LSP Client

---
+++ packages/salesforcedx-vscode-aura/src/index.ts
@@ -0,0 +1,90 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import { workspace, ExtensionContext, Uri, commands } from 'vscode';
+import { window, QuickPickItem } from 'vscode';
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind
+} from 'vscode-languageclient';
+
+import { logger } from './utils';
+
+let client: LanguageClient;
+
+// See https://github.com/Microsoft/vscode-languageserver-node/issues/105
+export function code2ProtocolConverter(value: Uri): string {
+  if (/^win32/.test(process.platform)) {
+    // The *first* : is also being encoded which is not the standard for URI on Windows
+    // Here we transform it back to the standard way
+    return value.toString().replace('%3A', ':');
+  } else {
+    return value.toString();
+  }
+}
+
+function protocol2CodeConverter(value: string): Uri {
+  return Uri.parse(value);
+}
+
+async function timeout(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function activate(context: ExtensionContext) {
+  debugger;
+  let serverModule = context.asAbsolutePath(
+    path.join('node_modules', 'aura-language-server', 'lib', 'server.js')
+  );
+
+  // The debug options for the server
+  let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+  // let debugOptions = { };
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  let serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions
+    }
+  };
+
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: ['html', 'javascript'],
+    synchronize: {
+      fileEvents: [
+        workspace.createFileSystemWatcher('**/*.resource'),
+        workspace.createFileSystemWatcher(
+          '**/labels/CustomLabels.labels-meta.xml'
+        ),
+        workspace.createFileSystemWatcher('**/lightningcomponents/**/*.js'),
+        // need to watch for directory deletions as no events are created for contents or deleted directories
+        workspace.createFileSystemWatcher('**/', true, true, false)
+      ]
+    },
+    uriConverters: {
+      code2Protocol: code2ProtocolConverter,
+      protocol2Code: protocol2CodeConverter
+    }
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    'auraLanguageServer',
+    'Lightning Language Server',
+    serverOptions,
+    clientOptions
+  );
+  client.start();
+}
+
+export function deactivate(): Thenable<void> {
+  if (!client) {
+    return Promise.resolve(undefined);
+  }
+  return client.stop();
+}
