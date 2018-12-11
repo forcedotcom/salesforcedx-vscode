d3856bc58fdbed22fa52e31535227d107228c0fc
brpowell <powell.bryan@outlook.com>
2018-12-11 15:04:17 -0800

    update labels based on text status and go to first error in ApexGroupTestNode

--- packages/salesforcedx-vscode-apex/src/index.ts
+++ packages/salesforcedx-vscode-apex/src/index.ts
@@ -98,6 +98,13 @@ async function registerTestView(
       testRunner.showErrorMessage(test)
     )
   );
+  // Show Definition command
+  testViewItems.push(
+    vscode.commands.registerCommand(
+      'sfdx.force.test.view.showDefinition',
+      test => testRunner.showErrorMessage(test)
+    )
+  );
   // Run Class Tests command
   testViewItems.push(
     vscode.commands.registerCommand(
--- packages/salesforcedx-vscode-apex/src/views/testRunner.ts
+++ packages/salesforcedx-vscode-apex/src/views/testRunner.ts
@@ -12,6 +12,7 @@ import * as vscode from 'vscode';
 import { nls } from '../messages';
 import { ReadableApexTestRunExecutor } from './readableApexTestRunExecutor';
 import {
+  ApexTestGroupNode,
   ApexTestNode,
   ApexTestOutlineProvider,
   TestNode
@@ -34,11 +35,22 @@ export class ApexTestRunner {
   }
 
   public async showErrorMessage(test: TestNode) {
+    let testNode = test;
     let position: vscode.Range | number = test.location!.range;
-    if (test instanceof ApexTestNode) {
-      const errorMessage = test.errorMessage;
+    if (testNode instanceof ApexTestGroupNode) {
+      if (test.contextValue === 'apexTestGroup_Fail') {
+        const failedTest = test.children.find(
+          testCase => testCase.contextValue === 'apexTest_Fail'
+        );
+        if (failedTest) {
+          testNode = failedTest;
+        }
+      }
+    }
+    if (testNode instanceof ApexTestNode) {
+      const errorMessage = testNode.errorMessage;
       if (errorMessage && errorMessage !== '') {
-        const stackTrace = test.stackTrace;
+        const stackTrace = testNode.stackTrace;
         position =
           parseInt(
             stackTrace.substring(
@@ -54,8 +66,8 @@ export class ApexTestRunner {
         channelService.showChannelOutput();
       }
     }
-    if (test.location) {
-      vscode.window.showTextDocument(test.location.uri).then(() => {
+    if (testNode.location) {
+      vscode.window.showTextDocument(testNode.location.uri).then(() => {
         this.eventsEmitter.emit('sfdx:update_selection', position);
       });
     }
