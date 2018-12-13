a31cdab78d1ba1a83ef639293bb02863c56fa988
Alli <allison.leong@salesforce.com>
2018-12-13 15:02:50 -0700

    Update unit tests for deploy/retrieve

---
+++ packages/salesforcedx-vscode-core/test/commands/forceSourceRetrieveSourcePath.test.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { expect } from 'chai';
+import * as path from 'path';
+import { ForceSourceRetrieveSourcePathExecutor } from './../../src/commands/forceSourceRetrieveSourcePath';
+
+import { nls } from '../../src/messages';
+
+describe('Force Source Retrieve with Sourcepath Option', () => {
+  it('Should build the source retrieve command', () => {
+    const sourcePath = path.join('path', 'to', 'sourceFile');
+    const sourceRetrieve = new ForceSourceRetrieveSourcePathExecutor();
+    const sourceRetrieveCommand = sourceRetrieve.build(sourcePath);
+    expect(sourceRetrieveCommand.toCommand()).to.equal(
+      `sfdx force:source:retrieve --sourcepath ${sourcePath}`
+    );
+    expect(sourceRetrieveCommand.description).to.equal(
+      nls.localize('force_source_retrieve_text')
+    );
+  });
+});
