180135b02806aa848dd344862a7a8ad403b29d49
Benj <benj.kamm@gmail.com>
2018-12-18 16:15:46 -0600

    Allow multiline queries in 'Execute SOQL query'

--- packages/salesforcedx-vscode-core/src/commands/forceDataSoqlQuery.ts
+++ packages/salesforcedx-vscode-core/src/commands/forceDataSoqlQuery.ts
@@ -63,7 +63,7 @@ export class GetQueryAndApiInputs
         query = document.getText(editor.selection);
       }
     }
-    query = query!.replace('[', '').replace(']', '').replace('\n', ' ');
+    query = query!.replace('[', '').replace(']', '').replace(/(\r\n|\n)/g, " ");
 
     if (!query) {
       return { type: 'CANCEL' };
