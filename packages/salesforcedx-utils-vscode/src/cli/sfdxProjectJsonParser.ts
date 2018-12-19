2ec8378884d3d2b13f3d79073007363da4f12dd1
Bryan <b.powell@salesforce.com>
2018-12-19 11:31:21 -0800

    using project json util to read login url

---
+++ packages/salesforcedx-utils-vscode/src/cli/sfdxProjectJsonParser.ts
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { SfdxProject } from '@salesforce/core';
+import { JsonArray, JsonMap } from '@salesforce/ts-types';
+
+import * as path from 'path';
+
+export class SfdxProjectJsonParser {
+  public async getValue(workspacePath: string, key: string) {
+    const sfdxProject = await SfdxProject.resolve(workspacePath);
+    const sfdxProjectJson = await sfdxProject.resolveProjectConfig();
+    return sfdxProjectJson[key];
+  }
+
+  public async getPackageDirectoryPaths(
+    workspacePath: string
+  ): Promise<string[]> {
+    const sfdxProject = await SfdxProject.resolve(workspacePath);
+    const sfdxProjectJson = await sfdxProject.resolveProjectConfig();
+    const packageDirectories = sfdxProjectJson.packageDirectories as JsonArray;
+    if (packageDirectories) {
+      const packageDirectoryPaths: string[] = [];
+      packageDirectories.forEach(packageDir => {
+        if (packageDir) {
+          const packageDirectory = packageDir as JsonMap;
+          if (packageDirectory.path) {
+            let dirPath = packageDirectory.path as string;
+            dirPath = dirPath.trim();
+            if (dirPath.startsWith(path.sep)) {
+              dirPath = dirPath.substring(1);
+            }
+            packageDirectoryPaths.push(dirPath);
+          }
+        }
+      });
+      if (packageDirectoryPaths.length === 0) {
+        const error = new Error();
+        error.name = 'NoPackageDirectoryPathsFound';
+        throw error;
+      }
+      return Promise.resolve(packageDirectoryPaths);
+    } else {
+      const error = new Error();
+      error.name = 'NoPackageDirectoriesFound';
+      throw error;
+    }
+  }
+}
