7861b0619dcea14a33f6dd28b7bd94d814bb015a
Nathan Totten <ntotten@salesforce.com>
2019-02-11 10:20:39 -0500

    Fixed oss header

--- packages/salesforcedx-visualforce-language-server/test/pathCompletionFixtures/.foo.js
+++ packages/salesforcedx-visualforce-language-server/test/pathCompletionFixtures/.foo.js
@@ -1,4 +1,4 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Licensed under the MIT License. See OSSREADME.json in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
