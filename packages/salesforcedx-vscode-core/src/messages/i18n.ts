d6208d13394e3f84066a664370070a1d70deaa64
Luis Campos <lcamposguajardo@salesforce.com>
2019-02-26 14:38:07 -0800

    Fix error labels

--- packages/salesforcedx-vscode-core/src/messages/i18n.ts
+++ packages/salesforcedx-vscode-core/src/messages/i18n.ts
@@ -20,7 +20,7 @@ export const messages = {
   channel_starting_message: 'Starting ',
   channel_end_with_exit_code: 'ended with exit code %s',
   channel_end_with_sfdx_not_found:
-    'The Salesforce CLI is not installed. Install it from https://developer.salesforce.com/tools/sfdxcli',
+    'Salesforce CLI is not installed. Install it from https://developer.salesforce.com/tools/sfdxcli',
   channel_end_with_error: 'ended with error %s',
   channel_end: 'ended',
 
@@ -225,7 +225,7 @@ export const messages = {
   error_parsing_sfdx_project_file:
     "Couldn't parse sfdx-project.json file (%s). Parse error: %s",
   sfdx_cli_not_found:
-    'The Salesforce CLI is not installed. Install it from [%s](%s)',
+    'Salesforce CLI is not installed. Install it from [%s](%s)',
   table_header_errors: 'ERRORS',
   table_header_project_path: 'PROJECT PATH',
   table_header_type: 'TYPE',
