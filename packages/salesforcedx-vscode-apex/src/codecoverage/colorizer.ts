a3d642ed7ce0d58ab4f8d72a438907b49f2cd6fd
Luis Campos <lcamposguajardo@salesforce.com>
2019-02-28 19:14:57 -0800

    Add error handling and messaging when code coverage data is not present

--- packages/salesforcedx-vscode-apex/src/codecoverage/colorizer.ts
+++ packages/salesforcedx-vscode-apex/src/codecoverage/colorizer.ts
@@ -13,10 +13,15 @@ import {
 } from './decorations';
 import { StatusBarToggle } from './statusBarToggle';
 
-export function getLineRange(
-  document: TextDocument,
-  lineNumber: number
-): Range {
+const apexDirPath = path.join(
+  workspace!.workspaceFolders![0].uri.fsPath,
+  '.sfdx',
+  'tools',
+  'testresults',
+  'apex'
+);
+
+function getLineRange(document: TextDocument, lineNumber: number): Range {
   const adjustedLineNumber = lineNumber - 1;
   const firstLine = document.lineAt(adjustedLineNumber);
 
@@ -28,34 +33,53 @@ export function getLineRange(
   );
 }
 
-const apexDirPath = path.join(
-  workspace!.workspaceFolders![0].uri.fsPath,
-  '.sfdx',
-  'tools',
-  'testresults',
-  'apex'
-);
+export type CoverageTestResult = {
+  coverage: {
+    coverage: CoverageItem[];
+  };
+};
 
-function getCoverageData() {
+export type CoverageItem = {
+  id: string;
+  name: string;
+  totalLines: number;
+  lines: { [key: string]: number };
+};
+
+function getCoverageData(): CoverageItem[] {
   const testRunIdFile = path.join(apexDirPath, 'test-run-id.txt');
-  const testRunId = fs.readFileSync(testRunIdFile);
+  if (!fs.existsSync(testRunIdFile)) {
+    throw new Error('No test run information was found for this project.');
+  }
+  const testRunId = fs.readFileSync(testRunIdFile, 'utf8');
   const testResultFilePath = path.join(
     apexDirPath,
-    'test-result-' + testRunId + '.json'
+    `test-result-${testRunId}.json`
   );
 
+  if (!fs.existsSync(testResultFilePath)) {
+    throw new Error(
+      `No code coverage information was found test run ${testRunId}`
+    );
+  }
   const testResultOutput = fs.readFileSync(testResultFilePath, 'utf8');
-  return JSON.parse(testResultOutput);
+  const codeCoverage = JSON.parse(testResultOutput) as CoverageTestResult;
+  return codeCoverage.coverage.coverage;
+}
+
+function isApexMetadata(filePath: string): boolean {
+  return filePath.endsWith('.cls') || filePath.endsWith('.trigger');
 }
 
 function getApexMemberName(filePath: string): string {
-  if (filePath.endsWith('.cls') || filePath.endsWith('.trigger')) {
-    const filePathWithOutType = filePath.replace('.cls', '');
+  if (isApexMetadata(filePath)) {
+    const filePathWithOutType = filePath.replace(/.cls|.trigger/g, '');
     const indexOfLastFolder = filePathWithOutType.lastIndexOf('/');
     return filePathWithOutType.substring(indexOfLastFolder + 1);
   }
   return '';
 }
+
 export class CodeCoverage {
   private statusBar: StatusBarToggle;
 
@@ -68,14 +92,13 @@ export class CodeCoverage {
 
   public onDidChangeActiveTextEditor(editor?: TextEditor) {
     if (editor && this.statusBar.isHighlightingEnabled) {
-      getApexMemberName(editor.document.uri.fsPath);
       this.colorizer(editor);
     }
   }
 
   public showCoverage() {
-    this.statusBar.toggle(true);
     this.colorizer(window.activeTextEditor);
+    this.statusBar.toggle(true);
   }
 
   public hideCoverage() {
@@ -88,29 +111,38 @@ export class CodeCoverage {
   }
 
   private colorizer(editor?: TextEditor) {
-    if (editor) {
-      const jsonSummary = getCoverageData();
-      const coveredLines = Array<Range>();
-      const uncoveredLines = Array<Range>();
-      const convArray = jsonSummary.coverage.coverage;
+    try {
+      if (editor && isApexMetadata(editor.document.uri.fsPath)) {
+        const codeCovArray = getCoverageData();
+        const coveredLines = Array<Range>();
+        const uncoveredLines = Array<Range>();
+        const codeCovItem = codeCovArray.find(
+          covItem =>
+            covItem.name === getApexMemberName(editor.document.uri.fsPath)
+        );
 
-      convArray.forEach((classCov: any) => {
-        // can this be replaced by a find or map ?
-        if (classCov.name === getApexMemberName(editor.document.uri.fsPath)) {
-          for (const key in classCov.lines) {
-            if (classCov.lines.hasOwnProperty(key)) {
-              if (classCov.lines[key] === 1) {
+        if (codeCovItem) {
+          for (const key in codeCovItem.lines) {
+            if (codeCovItem.lines.hasOwnProperty(key)) {
+              if (codeCovItem.lines[key] === 1) {
                 coveredLines.push(getLineRange(editor.document, Number(key)));
               } else {
                 uncoveredLines.push(getLineRange(editor.document, Number(key)));
               }
             }
           }
-        }
-      });
 
-      editor.setDecorations(coveredLinesDecorationType, coveredLines);
-      editor.setDecorations(uncoveredLinesDecorationType, uncoveredLines);
+          editor.setDecorations(coveredLinesDecorationType, coveredLines);
+          editor.setDecorations(uncoveredLinesDecorationType, uncoveredLines);
+        } else {
+          window.showInformationMessage(
+            'Code coverage was not found for the current file.'
+          );
+        }
+      }
+    } catch (e) {
+      // telemetry
+      window.showWarningMessage(e.message);
     }
   }
 }
