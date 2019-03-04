4d830ee3a534dba8a4813be7b8a20fcd163d3b9d
Ruth Sears-Blazej <rsears@salesforce.com>
2019-03-04 10:04:28 -0800

    Change "go to" to "see"

--- packages/salesforcedx-vscode-apex/src/messages/i18n.ts
+++ packages/salesforcedx-vscode-apex/src/messages/i18n.ts
@@ -21,7 +21,7 @@ export const messages = {
   source_jdk_home_env_var_text: 'The JDK_HOME environment variable',
   source_java_home_env_var_text: 'The JAVA_HOME environment variable',
   source_missing_text:
-    '%s points to a missing folder. For more information on how to setup the Salesforce Apex extension, go to [Set Your Java Version](%s).',
+    '%s points to a missing folder. For more information on how to setup the Salesforce Apex extension, see [Set Your Java Version](%s).',
   java_runtime_missing_text:
     'Java runtime could not be located. Set one using the salesforcedx-vscode-apex.java.home VS Code setting. For more information, go to [Set Your Java Version](%s).',
   force_apex_test_run_description_text: 'Run Apex test(s)',
@@ -31,7 +31,7 @@ export const messages = {
   force_test_view_no_tests_description:
     "Your project doesn't contain any Apex test methods. To run Apex tests, open a project that contains methods with @istest annotations or the testMethod keyword",
   wrong_java_version_text:
-    'Java 8 is required to run. Download and install it from https://java.com/en/download/. For more information, go to [Set Your Java Version](%s).',
+    'Java 8 is required to run. Download and install it from https://java.com/en/download/. For more information, see [Set Your Java Version](%s).',
 
   client_name: 'Apex Language Server',
   cannot_determine_workspace:
