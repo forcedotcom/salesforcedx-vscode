67caf9442c208f2631cbfe1c2d52c3ebe7e07441
Luis Campos <lcamposguajardo@salesforce.com>
2019-03-06 14:11:10 -0800

    Adjust mocha config

--- packages/salesforcedx-vscode-core/test/vscode-integration/index.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/index.ts
@@ -25,7 +25,7 @@ const testRunner = require('@salesforce/salesforcedx-utils-vscode/out/src/test/t
 testRunner.configure({
   ui: 'bdd', // the TDD UI is being used in extension.test.ts (suite, test, etc.)
   useColors: true, // colored output from test results
-  timeout: 15000,
+  timeout: 20000,
   fullTrace: true,
   slow: 0
 });
