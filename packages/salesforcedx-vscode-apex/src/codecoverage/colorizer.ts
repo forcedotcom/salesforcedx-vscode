483d8de37e9f7cf409e4817219d996f9d99f8f6d
Luis Campos <lcamposguajardo@salesforce.com>
2019-03-12 17:47:03 -0700

    Add missing copyright header

--- packages/salesforcedx-vscode-apex/src/codecoverage/colorizer.ts
+++ packages/salesforcedx-vscode-apex/src/codecoverage/colorizer.ts
@@ -4,6 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
 import * as fs from 'fs';
 import * as path from 'path';
 import { Range, TextDocument, TextEditor, window, workspace } from 'vscode';
