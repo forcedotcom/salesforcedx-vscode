be06b6499011c9372f02611cbbebc4f86ae99be3
lcampos <lcamposguajardo@gmail.com>
2019-03-15 19:11:17 -0700

    Visual display of apex code coverage (#1145)
    @W-5951754@

---
+++ packages/salesforcedx-vscode-apex/src/codecoverage/statusBarToggle.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2019, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { Disposable, StatusBarItem, window } from 'vscode';
+import { nls } from '../messages';
+
+export class StatusBarToggle implements Disposable {
+  private static readonly toggleCodeCovCommand =
+    'sfdx.force.apex.toggle.colorizer';
+  private static readonly showIcon = '$(tasklist)';
+  private static readonly hideIcon = '$(three-bars)';
+  private static readonly toolTip = nls.localize(
+    'colorizer_statusbar_hover_text'
+  );
+  private isEnabled: boolean;
+  private statusBarItem: StatusBarItem;
+
+  constructor() {
+    this.statusBarItem = window.createStatusBarItem();
+    this.statusBarItem.command = StatusBarToggle.toggleCodeCovCommand;
+    this.statusBarItem.text = StatusBarToggle.showIcon;
+    this.statusBarItem.tooltip = StatusBarToggle.toolTip;
+    this.statusBarItem.show();
+    this.isEnabled = false;
+  }
+
+  public get isHighlightingEnabled(): boolean {
+    return this.isEnabled;
+  }
+
+  public toggle(active: boolean) {
+    if (active) {
+      this.statusBarItem.text = StatusBarToggle.hideIcon;
+      this.isEnabled = true;
+    } else {
+      this.statusBarItem.text = StatusBarToggle.showIcon;
+      this.isEnabled = false;
+    }
+  }
+
+  public dispose() {
+    this.statusBarItem.dispose();
+  }
+}
