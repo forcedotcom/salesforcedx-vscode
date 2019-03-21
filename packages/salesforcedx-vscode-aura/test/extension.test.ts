ff3e876789f99a9a4448a2dc78c22036bd00497c
Nicolas Kruk <nkruk@salesforce.com>
2019-03-21 00:59:17 -0400

    update copyrights to 2019

--- packages/salesforcedx-vscode-aura/test/extension.test.ts
+++ packages/salesforcedx-vscode-aura/test/extension.test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, salesforce.com, inc.
+ * Copyright (c) 2019, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
@@ -11,70 +11,78 @@ import * as vscode from 'vscode';
 const PERFECT_MATCH = 10;
 
 async function matchExtensionAsHtml(extension: string) {
-    const doc = await vscode.workspace.openTextDocument(vscode.Uri.parse(`untitled:fake/path/doc.${extension}`));
-    expect(vscode.languages.match({ language: 'html' }, doc)).to.equal(PERFECT_MATCH);
+  const doc = await vscode.workspace.openTextDocument(
+    vscode.Uri.parse(`untitled:fake/path/doc.${extension}`)
+  );
+  expect(vscode.languages.match({ language: 'html' }, doc)).to.equal(
+    PERFECT_MATCH
+  );
 }
 
 describe('Lightning file association', () => {
-    it('Should support .app association', async () => {
-        await matchExtensionAsHtml('.app');
-    });
+  it('Should support .app association', async () => {
+    await matchExtensionAsHtml('.app');
+  });
 
-    it('Should support .cmp association', async () => {
-        await matchExtensionAsHtml('.cmp');
-    });
+  it('Should support .cmp association', async () => {
+    await matchExtensionAsHtml('.cmp');
+  });
 
-    it('Should support .design association', async () => {
-        await matchExtensionAsHtml('.design');
-    });
+  it('Should support .design association', async () => {
+    await matchExtensionAsHtml('.design');
+  });
 
-    it('Should support .evt association', async () => {
-        await matchExtensionAsHtml('.evt');
-    });
+  it('Should support .evt association', async () => {
+    await matchExtensionAsHtml('.evt');
+  });
 
-    it('Should support.intf association', async () => {
-        await matchExtensionAsHtml('.intf');
-    });
+  it('Should support.intf association', async () => {
+    await matchExtensionAsHtml('.intf');
+  });
 
-    it('Should support .auradoc association', async () => {
-        await matchExtensionAsHtml('.auradoc');
-    });
+  it('Should support .auradoc association', async () => {
+    await matchExtensionAsHtml('.auradoc');
+  });
 
-    it('Should support .tokens association', async () => {
-        await matchExtensionAsHtml('.tokens');
-    });
+  it('Should support .tokens association', async () => {
+    await matchExtensionAsHtml('.tokens');
+  });
 });
 
 describe('Test commands', () => {
-    let coreExtension: vscode.Extension<any>;
-    let auraExtension: vscode.Extension<any>;
+  let coreExtension: vscode.Extension<any>;
+  let auraExtension: vscode.Extension<any>;
 
-    before(async () => {
-        if (vscode.workspace.rootPath) {
-            coreExtension = vscode.extensions.getExtension('salesforce.salesforcedx-vscode-core') as vscode.Extension<any>;
+  before(async () => {
+    if (vscode.workspace.rootPath) {
+      coreExtension = vscode.extensions.getExtension(
+        'salesforce.salesforcedx-vscode-core'
+      ) as vscode.Extension<any>;
 
-            auraExtension = vscode.extensions.getExtension('salesforce.salesforcedx-vscode-aura') as vscode.Extension<any>;
-        }
-    });
+      auraExtension = vscode.extensions.getExtension(
+        'salesforce.salesforcedx-vscode-aura'
+      ) as vscode.Extension<any>;
+    }
+  });
 
-    it('coreExtension activation', async function() {
-        // tslint:disable-next-line:no-invalid-this
-        this.timeout(10000);
-        await coreExtension.activate();
-        expect(coreExtension.isActive);
-    });
+  it('coreExtension activation', async function() {
+    // tslint:disable-next-line:no-invalid-this
+    this.timeout(10000);
+    await coreExtension.activate();
+    expect(coreExtension.isActive);
+  });
 
-    it('aura activation', async () => {
-        // tslint:disable-next-line:no-invalid-this
-        this.timeout(10000);
-        await auraExtension.activate();
-        expect(auraExtension.isActive);
-    });
+  it('aura activation', async () => {
+    // tslint:disable-next-line:no-invalid-this
+    this.timeout(10000);
+    await auraExtension.activate();
+    expect(auraExtension.isActive);
+  });
 
-    it('quick open command', async () => {
-        expect(auraExtension.isActive);
+  it('quick open command', async () => {
+    expect(auraExtension.isActive);
 
-        const commandList = await vscode.commands.getCommands(true);
-        expect(commandList).to.include('salesforce-lightning-quickopen');
-    });
+    const commandList = await vscode.commands.getCommands(true);
+    expect(commandList).to.include('salesforce-lightning-quickopen');
+  });
 });
