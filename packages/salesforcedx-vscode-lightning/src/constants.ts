e2b0d8dfa686ba39396d3f513436b7f2014b9314
Nicolas Kruk <nicolaskruk@gmail.com>
2019-03-22 18:29:06 -0700

    Aura Language Server (#1183)
    * Update SHA256.md
    
    * Add initial Aura LSP Client
    
    * update debug port
    
    * wip
    
    * output channel logging
    
    * fix npm install
    
    * wip
    
    * Add link-lsp
    
    * link script
    
    * Explorer view, quick pick, core support
    
    * Async wait for DX-core. Make optional. Don't activate on non-DX workspaces
    
    * Fix LWC changes to Explorer, and index
    
    * update package.json
    
    * Indexing status
    
    * Tests (#2)
    
    * tests
    
    * Add integration tests
    
    * Reverse system test change
    
    * (Really) reverse settings.json change
    
    * fix package.json versions
    
    * update language server dependencies
    
    * part 1 - initial  merge of aura / lightning packages
    
    * update tsconfig / remove prettierrc
    
    * fix sha256
    
    * update license copyright
    
    * update copyrights to 2019
    
    * fix more formatting junk
    
    * store a copy of the old lightning index.ts
    
    * rename salesforcedx-vscode-aura package to -lightning / remove old lightning package
    
    * update remaining references for -aura packages -> -lightning
    
    * this fixes compile issues, but Im not entirely sure why
    
    * fix linting errors
    
    * use vscode as devDependency
    
    * update lsp to 2.0.7
    
    * fix paths for windows
    
    * add constants file
    
    * add telemetry to aura extension
    
    * telemetry for lightning package / startup timing /  add back core dependency
    
    * remove remaining references to slds-linter extension
    
    * formatting / copyrights
    
    * add i18n to aura package
    
    * telemetry / logging
    
    * update LSPs to 2.0.8
    
    * add back eslint dependency
    
    * temporarily comment out lightning creation test to see if its the problem
    
    * try commenting out other test

--- packages/salesforcedx-vscode-lightning/src/constants.ts
+++ packages/salesforcedx-vscode-lightning/src/constants.ts
@@ -4,6 +4,5 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { TelemetryService } from './telemetry';
-
-export const telemetryService = TelemetryService.getInstance();
+export const COMPONENT_LIBRARY_BUNDLE_LINK =
+  'https://developer.salesforce.com/docs/component-library/bundle/';
--- packages/salesforcedx-vscode-lightning/test/index.ts
+++ packages/salesforcedx-vscode-lightning/test/index.ts
@@ -6,16 +6,14 @@
  */
 
 // tslint:disable-next-line:no-var-requires
-let testRunner = require('@salesforce/salesforcedx-utils-vscode/out/src/test/testrunner');
+testRunner = require('@salesforce/salesforcedx-utils-vscode/out/src/test/testrunner');
 
 // You can directly control Mocha options by uncommenting the following lines
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
 testRunner.configure({
   ui: 'bdd', // the TDD UI is being used in extension.test.ts (suite, test, etc.)
   useColors: true, // colored output from test results
-  retries: 2,
-  fullTrace: true,
-  slow: 0
+  retries: 2
 });
 
 module.exports = testRunner;
