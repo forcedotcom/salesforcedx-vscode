5fc1337393ec960de9b28d377f8797df49868263
lcampos <lcamposguajardo@gmail.com>
2019-05-24 07:09:25 +0200

    Internal development mode for lightning & lwc (#1350)
    @W-6151369@

---
+++ packages/salesforcedx-vscode-core/src/commands/templates/internalCommandUtils.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2019, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import {
+  CancelResponse,
+  ContinueResponse,
+  ParametersGatherer,
+  PreconditionChecker
+} from '@salesforce/salesforcedx-utils-vscode/out/src/types';
+import * as fs from 'fs';
+import { Uri } from 'vscode';
+import { sfdxCoreSettings } from '../../settings';
+
+export class InternalDevWorkspaceChecker implements PreconditionChecker {
+  public check(): boolean {
+    return sfdxCoreSettings.getInternalDev();
+  }
+}
+
+export class FileInternalPathGatherer
+  implements ParametersGatherer<{ outputdir: string }> {
+  private filePath: string;
+  public constructor(uri: Uri) {
+    this.filePath = uri.fsPath;
+  }
+
+  public async gather(): Promise<
+    CancelResponse | ContinueResponse<{ outputdir: string }>
+  > {
+    const outputdir = this.filePath;
+    const isDir =
+      fs.existsSync(outputdir) && fs.lstatSync(outputdir).isDirectory();
+
+    if (isDir) {
+      return { type: 'CONTINUE', data: { outputdir } };
+    }
+
+    return { type: 'CANCEL' };
+  }
+}
