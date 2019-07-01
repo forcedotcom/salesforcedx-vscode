457d4f46fa7ec11d7b4ca0c168d1d2ac854fc00f
Jonny Hork <jhork@salesforce.com>
2019-07-01 10:28:46 -0600

    isolate the org URL from CLI output

--- packages/salesforcedx-vscode-core/src/commands/forceOrgOpen.ts
+++ packages/salesforcedx-vscode-core/src/commands/forceOrgOpen.ts
@@ -7,9 +7,13 @@
 
 import {
   Command,
+  CliCommandExecutor,
   SfdxCommandBuilder
 } from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
+import { ContinueResponse } from '@salesforce/salesforcedx-utils-vscode/out/src/types';
+import * as vscode from 'vscode';
 import { nls } from '../messages';
+import { getRootWorkspacePath } from '../util';
 import {
   EmptyParametersGatherer,
   SfdxCommandlet,
@@ -19,13 +23,33 @@ import {
 
 class ForceOrgOpenExecutor extends SfdxCommandletExecutor<{}> {
   public build(data: {}): Command {
-    return new SfdxCommandBuilder()
+    const builder = new SfdxCommandBuilder()
       .withDescription(nls.localize('force_org_open_default_scratch_org_text'))
       .withArg('force:org:open')
-      .withLogName('force_org_open_default_scratch_org')
-      .build();
+      .withLogName('force_org_open_default_scratch_org');
+    if (process.env.SFDX_CONTAINER_MODE) {
+      builder.withArg('-r').withJson();
+    }
+    return builder.build();
+  }
+
+  public execute(response: ContinueResponse<string>): void {
+    const cancellationTokenSource = new vscode.CancellationTokenSource();
+    const cancellationToken = cancellationTokenSource.token;
+
+    const execution = new CliCommandExecutor(this.build(response.data), {
+      cwd: getRootWorkspacePath()
+    }).execute(cancellationToken);
+
+    execution.stdoutSubject.subscribe(cliResponsedata => {
+      console.log('CLI DATA: ', JSON.parse(cliResponsedata.toString()));
+      const orgData = JSON.parse(cliResponsedata.toString()).result;
+      console.log('\nURL==>', orgData.result.url);
+    });
   }
 }
+
+console.log('Activation CONTAINER MODE==>', process.env.SFDX_CONTAINER_MODE);
 
 const workspaceChecker = new SfdxWorkspaceChecker();
 const parameterGatherer = new EmptyParametersGatherer();
@@ -37,5 +61,13 @@ const commandlet = new SfdxCommandlet(
 );
 
 export async function forceOrgOpen() {
+  console.log(
+    'forceOrgOpen CONTAINER MODE==>',
+    process.env.SFDX_CONTAINER_MODE
+  );
+
+  if (process.env.SFDX_CONTAINER_MODE) {
+    vscode.env.openExternal(vscode.Uri.parse('https://code.visualstudio.com'));
+  }
   await commandlet.run();
 }
