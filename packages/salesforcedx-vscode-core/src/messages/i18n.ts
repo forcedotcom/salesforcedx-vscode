97393571f630c03af937ec66ed9cc4fec3482f87
lcampos <lcamposguajardo@gmail.com>
2019-07-11 10:13:29 -0700

    Update error message label for org create parse issue (#1467)
    @W-6289619@

--- packages/salesforcedx-vscode-core/src/messages/i18n.ts
+++ packages/salesforcedx-vscode-core/src/messages/i18n.ts
@@ -69,7 +69,7 @@ export const messages = {
   force_org_create_default_scratch_org_text:
     'SFDX: Create a Default Scratch Org...',
   force_org_create_result_parsing_error:
-    'There was an unexpected error when processing the org create response.',
+    'An unexpected error occurred while processing the org create response.',
 
   force_org_open_default_scratch_org_text: 'SFDX: Open Default Org',
 
