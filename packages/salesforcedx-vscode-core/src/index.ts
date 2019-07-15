e95ed5cea110ae624551a372436f5ae20b347a5f
AnanyaJha <a.jha@salesforce.com>
2019-07-15 16:06:37 -0700

    Added flag to enable org browser (#1473)

--- packages/salesforcedx-vscode-core/src/index.ts
+++ packages/salesforcedx-vscode-core/src/index.ts
@@ -497,8 +497,8 @@ export async function activate(context: vscode.ExtensionContext) {
   orgList.displayDefaultUsername(defaultUsernameorAlias);
   context.subscriptions.push(registerOrgPickerCommands(orgList));
 
-  // await setupOrgBrowser(context, defaultUsernameorAlias);
-  vscode.commands.executeCommand('setContext', 'sfdx:display_tree_view', false);
+  await setupOrgBrowser(context, defaultUsernameorAlias);
+
   if (isCLIInstalled()) {
     // Set context for defaultusername org
     await setupWorkspaceOrgType(defaultUsernameorAlias);
