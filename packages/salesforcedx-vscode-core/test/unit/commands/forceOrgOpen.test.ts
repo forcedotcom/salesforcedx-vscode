8bfd989f57da66ad9b580ebd0a3bf2a4e81fe171
Jonny Hork <jhork@salesforce.com>
2019-07-25 16:26:01 -0600

    set up unit testing in core package

---
+++ packages/salesforcedx-vscode-core/test/unit/commands/forceOrgOpen.test.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import {
+  forceOrgOpen,
+  ForceOrgOpenContainerExecutor,
+  ForceOrgOpenExecutor
+} from '../../../src/commands/forceOrgOpen';
+
+// tslint:disable:no-unused-expression
+describe.only('Force Org Open', () => {
+  it('should use the correct class based on env', () => {
+    expect(true).to.be.true;
+  });
+});
