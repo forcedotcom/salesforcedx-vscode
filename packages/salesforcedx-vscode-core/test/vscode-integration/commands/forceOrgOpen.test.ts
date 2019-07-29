4cd7cda59d46f6cea04c1d975e277b1f67d4c70e
Jonny Hork <jhork@salesforce.com>
2019-07-29 12:58:45 -0600

    move test to vscode-integration directory to handle dependencies

---
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/forceOrgOpen.test.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import {
+  forceOrgOpen,
+  ForceOrgOpenContainerExecutor,
+  ForceOrgOpenExecutor
+} from '../../../src/commands/forceOrgOpen';
+
+// tslint:disable:no-unused-expression
+describe.only('Force Org Open', () => {
+  it('should build the command for use in a container', () => {
+    expect(true).to.be.true;
+  });
+});
