6e79d3ff2b51aa04f386855c777fdc9597ecea0c
cademay <c.may@salesforce.com>
2019-08-06 15:05:23 -0700

    Consolidate JSON string sanitize & parse into a utility (#1534)
    @W-6406028@

--- packages/salesforcedx-utils-vscode/src/helpers/index.ts
+++ packages/salesforcedx-utils-vscode/src/helpers/index.ts
@@ -5,4 +5,4 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-export { isNullOrUndefined } from './utils';
+export { isNullOrUndefined, extractJsonObject } from './utils';
