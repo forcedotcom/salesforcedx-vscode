097d56efd0fcc33a569f66f34bbcb8b7b59249a7
Shelby Holden <shelby.holden@salesforce.com>
2019-08-28 09:22:17 -0600

    Refactored and added parsing of commit results

--- scripts/change-log-generator.js
+++ scripts/change-log-generator.js
@@ -1,34 +1,46 @@
-const process = require('process');
-const path = require('path');
-const shell = require('shelljs');
-const readlineS = require('readline-sync');
-
 /*
+ * Automates the process of building the change log for releases.
+ *
  * Assumptions:
  * 1. You have shelljs installed globally using `npm install -g shelljs`.
  * 2. The release branch in question has already been cut.
  * 3. readline-sync is installed using 'npm install readline-sync'
  */
 
-// Commands
-const BRANCH_SEARCH_COMMAND =
-  "git branch --remotes --list --sort='-creatordate' 'origin/release/v*'";
-const diffCommand = 'git log --oneline';
+// Imports
+const process = require('process');
+const path = require('path');
+const shell = require('shelljs');
+const readline = require('readline-sync');
 
 // Constants
-const changeLogPath = path.join(
+const CHANGE_LOG_PATH = path.join(
   process.cwd(),
   'packages',
   'salesforce-vscode',
   'CHANGELOG.md'
 );
+const CHANGE_LOG_BRANCH = 'changeLog-v';
+const RELEASE_REGEX = new RegExp(/^\d{2}\.\d{1,2}\.\d/);
+
+// Map Keys
+const PR_NUM = 'PR NUM';
+const COMMIT = 'COMMIT';
+const MESSAGE = 'MESSAGE';
+const FILES = 'FILES';
 
 /**
- * Gets the latest release branch. Assumes that the branch has already been cut.
+ * Gets the latest release branch from git. Retrieves all remote branches
+ * with the name 'origin/release/v*', sorts them in reverse order based on
+ * creation date, and truncates this result so that only the latest branch
+ * is returned.
  */
 function getLatestReleaseBranch() {
   var latestReleaseBranch = shell
-    .exec(BRANCH_SEARCH_COMMAND, { silent: true })
+    .exec(
+      "git branch --remotes --list --sort='-creatordate' 'origin/release/v*'",
+      { silent: true }
+    )
     .stdout.trim()
     .replace('origin/release/v', '')
     .split(' ', 1);
@@ -37,53 +49,104 @@ function getLatestReleaseBranch() {
 }
 
 /**
- * Determines if the user's input is 'y' or 'n'.
+ * Gets the release branch. Allows user to override the default value.
  */
-function isUserInputYorN(input) {
-  return input && (input.toUpperCase() == 'Y' || input.toUpperCase() == 'N');
+function getReleaseBranch() {
+  var releaseBranch = getLatestReleaseBranch();
+  var input = readline.question('Is this branch correct? (Y/n): ');
+  if (!input) {
+    exitWithError('Invalid input. Expected Input: Y/n.');
+  } else if (input.toUpperCase() != 'Y' && input.toUpperCase() != 'N') {
+    exitWithError('Expected Input: Y/n. Unknown user input: ' + input);
+  } else if (input.toUpperCase() == 'N') {
+    releaseBranch = readline.question('Enter release branch (xx.yy.z): ');
+  }
+  return releaseBranch;
 }
 
 /**
- * Verifies the release input from the user. If the release appears to be valid,
- * return it. Otherwise error with the expected format.
+ * Verifies the release branch is in the correct format.
  */
-function verifyUserReleaseBranch(input) {
-  if (input && input.match(/^\d{2}\.\d\.\d/)) {
-    return input;
-  } else {
-    printError("Invalid release '" + input + "'. Expected format [xx.y.z].");
+function validateReleaseBranch(releaseBranch) {
+  if (!(releaseBranch && RELEASE_REGEX.exec(releaseBranch))) {
+    printError(
+      "Invalid release '" + releaseBranch + "'. Expected format [xx.yy.z]."
+    );
   }
+  process.stdout.write('Using release branch: ' + releaseBranch);
+}
+
+/**
+ * Checks out the release branch specified. Builds a new change log
+ * branch using the release branch as its base.
+ */
+function getNewChangeLogBranch(releaseBranch) {
+  var changeLogBranch = CHANGE_LOG_BRANCH + releaseBranch;
+  shell.exec('git checkout ' + path.join('release', 'v' + releaseBranch));
+  shell.exec('git branch ' + changeLogBranch);
+  shell.exec('git checkout ' + changeLogBranch);
+}
+
+/**
+ * At this point our new branch should mirror the release branch. Now when
+ * we review the log we can retrieve all of the commits specific to this
+ * release. Parse this info and return it as a list of hashmaps.
+ */
+function getCommitsAsListOfMaps() {
+  var commits = shell
+    .exec('git log --oneline', { silent: true })
+    .stdout.trim()
+    .split(new RegExp(/\n\d+ Updated SHA256/), 1)
+    .toString()
+    .split('\n');
+  var result = [];
+  for (var i = 0; i < commits.length; i++) {
+    var commitMap = buildMapFromCommit(commits[i]);
+    if (commitMap && Object.keys(commitMap).length > 0) {
+      result.push(buildMapFromCommit(commits[i]));
+    }
+  }
+  return result;
+}
+
+function buildMapFromCommit(commit) {
+  var map = {};
+  if (commit && !commit.includes('Merge branch')) {
+    var pr = new RegExp(/(\(#\d+\))/).exec(commit);
+    var commitNum = new RegExp(/^([\da-zA-Z]+)/).exec(commit);
+    if (pr && commitNum) {
+      var message = commit.replace(commitNum[0], '').replace(pr[0], '');
+      map[PR_NUM] = pr[0];
+      map[COMMIT] = commitNum[0];
+      map[MESSAGE] = message.trim();
+      map[FILES] = getFilesChangedForCommit(commitNum[0]);
+    }
+  }
+  return map;
+}
+
+function getFilesChangedForCommit(commitNumber) {
+  var packageHeader = '';
+  var filesChanged = shell
+    .exec('git show --pretty="" --name-only ' + commitNumber, { silent: true })
+    .stdout.trim()
+    .toString()
+    .replace('\n', ',');
+  packageHeader = filesChanged;
+  return packageHeader;
 }
 
 /**
  * Prints the message to the error stream and exits the script.
  */
-function printError(errorMessage) {
+function exitWithError(errorMessage) {
   process.stderr.write(errorMessage + '\n');
   process.exit(-1);
 }
 
-var latestReleaseBranch = getLatestReleaseBranch();
-var input = readlineS.question('Is this the correct release Branch? (Y/n): ');
+var releaseBranch = getReleaseBranch();
+validateReleaseBranch(releaseBranch);
+getNewChangeLogBranch(releaseBranch);
 
-if (!isUserInputYorN(input)) {
-  printError('Expected Input: Y/n. Unknown user input: ' + input);
-} else if (input.toUpperCase() == 'N') {
-  input = readlineS.question('Enter release branch in format xx.y.z: ');
-  latestReleaseBranch = verifyUserReleaseBranch(input);
-}
-
-// TODO - verify branch doesn't already exist.
-var changeLogBranch = 'changeLogGenerated-v' + latestReleaseBranch;
-process.stdout.write('Release branch result: ' + latestReleaseBranch);
-shell.exec('git checkout ' + path.join('release', 'v' + latestReleaseBranch));
-shell.exec('git branch ' + changeLogBranch);
-shell.exec('git checkout ' + changeLogBranch);
-
-// At this point, our new branch for the change log should mirror the release branch.
-// Now we need to check the log for all of the commits that were added for this new release.
-var result = shell.exec('git log --oneline'); //.stdout.split('Updated SHA256', 1);
-console.log('Results from log: ' + result);
-
-// TODO - get all files for each of the commits.
-//shell.exec('git show --pretty="" --name-only <commit>');
+var commitsAsArrayOfMaps = getCommitsAsListOfMaps();
+console.log(commitsAsArrayOfMaps);
