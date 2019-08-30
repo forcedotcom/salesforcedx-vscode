ae409b2ee0774787b1b6ee637504d8b14ec43f64
Shelby Holden <shelby.holden@salesforce.com>
2019-08-29 19:30:20 -0600

    Refactored and added changelog injection

--- scripts/change-log-generator.js
+++ scripts/change-log-generator.js
@@ -10,37 +10,47 @@
  * 1. External contributions
  * 2. Duplicates (ex file changes made in both salesforcedx-apex-debugger and salesforcedx-apex-replay-debugger)
  * 3. Non-salesforce package contributions aside from doc updates
- * 4. Situations where we should not be adding the entry in the changelog
- * 5. Adding vs. Fixed
+ * 4. Adding vs. Fixed vs. Ignore in change log
  */
 
-// Imports
 const process = require('process');
 const path = require('path');
 const shell = require('shelljs');
 const readline = require('readline-sync');
 const fs = require('fs');
+const util = require('util');
 
 // Constants
 const CHANGE_LOG_PATH = path.join(
   process.cwd(),
   'packages',
-  'salesforce-vscode',
+  'salesforcedx-vscode',
   'CHANGELOG.md'
 );
 const CHANGE_LOG_BRANCH = 'changeLog-v';
-const RELEASE_REGEX = new RegExp(/^\d{2}\.\d{1,2}\.\d/);
-const GIT_HUB_URL = 'https://github.com/forcedotcom/salesforcedx-vscode/pull/';
 
-// Map Keys
+// Change log values
+const LOG_HEADER =
+  '%s - (INSERT RELEASE DATE [Month Day, Year])\n' +
+  '\n## Fixed\nMOVE ENTRIES FROM BELOW\n\n## Added\nMOVE ENTRIES FROM BELOW\n\n';
+const SECTION_HEADER = '\n#### %s\n';
+const MESSAGE_FORMAT =
+  '\n- %s ([PR #%s](https://github.com/forcedotcom/salesforcedx-vscode/pull/%s))\n';
+
+// Commit Map Keys
 const PR_NUM = 'PR NUM';
 const COMMIT = 'COMMIT';
 const MESSAGE = 'MESSAGE';
 const FILES = 'FILES';
 const PACKAGES = 'PACKAGES';
 
+// Regex
+const RELEASE_REGEX = new RegExp(/^\d{2}\.\d{1,2}\.\d/);
+const PR_REGEX = new RegExp(/(\(#\d+\))/);
+const COMMIT_REGEX = new RegExp(/^([\da-zA-Z]+)/);
+
 /**
- * Gets the latest release branch from git. Retrieves all remote branches
+ * Gets the latest release branch using git. Retrieves all remote branches
  * with the name 'origin/release/v*', sorts them in reverse order based on
  * creation date, and truncates this result so that only the latest branch
  * is returned.
@@ -53,17 +63,16 @@ function getLatestReleaseBranch() {
     )
     .stdout.trim()
     .replace('origin/release/v', '')
-    .split(' ', 1);
+    .split(' ', 1)
+    .toString()
+    .replace('\n', '');
   process.stdout.write('Latest Release Branch: ' + latestReleaseBranch);
   return latestReleaseBranch;
 }
 
-/**
- * Gets the release branch. Allows user to override the default value.
- */
 function getReleaseBranch() {
   var releaseBranch = getLatestReleaseBranch();
-  var input = readline.question('Is this branch correct? (Y/n): ');
+  var input = readline.question('\nIs this branch correct? (Y/n): ');
   if (!input) {
     exitWithError('Invalid input. Expected Input: Y/n.');
   } else if (input.toUpperCase() != 'Y' && input.toUpperCase() != 'N') {
@@ -74,9 +83,6 @@ function getReleaseBranch() {
   return releaseBranch;
 }
 
-/**
- * Verifies the release branch is in the correct format.
- */
 function validateReleaseBranch(releaseBranch) {
   if (!(releaseBranch && RELEASE_REGEX.exec(releaseBranch))) {
     printError(
@@ -95,47 +101,75 @@ function getNewChangeLogBranch(releaseBranch) {
   );
 }
 
+function getChangeLogText(releaseBranch) {
+  var changeLogText = util.format(LOG_HEADER, releaseBranch.toString());
+  var messagesByPackage = getMessagesPerPackage();
+  Object.keys(messagesByPackage).forEach(function(packageName) {
+    changeLogText += util.format(SECTION_HEADER, packageName);
+    messagesByPackage[packageName].forEach(function(message) {
+      changeLogText += message;
+    });
+  });
+  return changeLogText;
+}
+
 /**
- * At this point our new branch should mirror the release branch. Now when
- * we review the log we can retrieve all of the commits specific to this
+ * Groups all messages per package header so they can be displayed under
+ * the same package header subsection. Returns a map of lists.
+ */
+function getMessagesPerPackage() {
+  var messagesByPackage = {};
+  parseCommits().forEach(function(commitMap) {
+    var pr = commitMap[PR_NUM].replace(/[^\d]/g, '');
+    commitMap[PACKAGES].forEach(function(packageName) {
+      messagesByPackage[packageName] = messagesByPackage[packageName] || [];
+      messagesByPackage[packageName].push(
+        util.format(MESSAGE_FORMAT, commitMap[MESSAGE], pr, pr)
+      );
+    });
+  });
+  return messagesByPackage;
+}
+
+/**
+ * Review the log and retrieve all of the commits specific to this
  * release. Parse this info and return it as a list of hashmaps.
  */
-function getCommitsAsListOfMaps() {
+function parseCommits() {
+  var results = [];
   var commits = shell
     .exec('git log --oneline', { silent: true })
     .stdout.trim()
     .split('Updated SHA256', 1)
     .toString()
     .split('\n');
-  console.log('commits: ' + commits);
-  var result = [];
   for (var i = 0; i < commits.length; i++) {
     var commitMap = buildMapFromCommit(commits[i]);
     if (commitMap && Object.keys(commitMap).length > 0) {
-      result.push(buildMapFromCommit(commits[i]));
+      results.push(buildMapFromCommit(commits[i]));
     }
   }
-  return result;
+  return results;
 }
 
 function buildMapFromCommit(commit) {
   var map = {};
   if (commit && !commit.includes('Merge branch')) {
-    var pr = new RegExp(/(\(#\d+\))/).exec(commit);
-    var commitNum = new RegExp(/^([\da-zA-Z]+)/).exec(commit);
+    var pr = PR_REGEX.exec(commit);
+    var commitNum = COMMIT_REGEX.exec(commit);
     if (pr && commitNum) {
       var message = commit.replace(commitNum[0], '').replace(pr[0], '');
       map[PR_NUM] = pr[0];
       map[COMMIT] = commitNum[0];
       map[MESSAGE] = message.trim();
-      map[FILES] = getFilesChangedForCommit(map[COMMIT]);
+      map[FILES] = getFilesChanged(map[COMMIT]);
       map[PACKAGES] = getPackageHeaders(map[FILES]);
     }
   }
   return map;
 }
 
-function getFilesChangedForCommit(commitNumber) {
+function getFilesChanged(commitNumber) {
   var packageHeader = '';
   var filesChanged = shell
     .exec('git show --pretty="" --name-only ' + commitNumber, { silent: true })
@@ -146,75 +180,51 @@ function getFilesChangedForCommit(commitNumber) {
   return packageHeader;
 }
 
-// TODO - breakup into smaller pieces
 function getPackageHeaders(filesChanged) {
   var packageHeaders = new Set();
   filesChanged.split(',').forEach(function(filePath) {
-    if (
-      filePath &&
-      !filePath.includes('/images/') &&
-      !filePath.includes('/test/')
-    ) {
-      if (filePath.includes('docs/')) {
-        packageHeaders.add('docs');
-      } else {
-        var packageName = filePath.replace('packages/', '').split('/')[0];
-        if (packageName.startsWith('salesforce')) {
-          packageHeaders.add(packageName);
-        }
-      }
+    var packageName = getPackageName(filePath);
+    if (packageName) {
+      packageHeaders.add(packageName);
     }
   });
+  return filterPackageNames(packageHeaders);
+}
+
+function getPackageName(filePath) {
+  if (
+    filePath &&
+    !filePath.includes('/images/') &&
+    !filePath.includes('/test/')
+  ) {
+    var packageName = filePath.replace('packages/', '').split('/')[0];
+    return packageName.startsWith('salesforce') ||
+      packageName.startsWith('docs')
+      ? packageName
+      : null;
+  }
+  return null;
+}
+
+function filterPackageNames(packageHeaders) {
+  var filteredHeaders = new Set(packageHeaders);
   if (packageHeaders.has('salesforcedx-vscode-core')) {
     packageHeaders.forEach(function(packageName) {
       if (packageName != 'salesforcedx-vscode-core' && packageName != 'docs') {
-        packageHeaders.delete(packageName);
+        filteredHeaders.delete(packageName);
       }
     });
   }
-  return packageHeaders;
+  return filteredHeaders;
 }
 
-// TODO - would like to change this to something similar to how messages are generated.
-// This way we're only replacing pieces of a standard format.
-// TODO - documentation
-function getChangeLog(releaseBranch, commitsAsArrayOfMaps) {
-  var text =
-    releaseBranch.toString().replace('\n', '') +
-    ' - (INSERT RELEASE DATE [Month Day, Year])\n' +
-    '\n## Fixed\nINSERT ENTRIES\n\n## Added\nINSERT ENTRIES\n\n';
-  var messagesByPackage = {};
-
-  Object.values(commitsAsArrayOfMaps).forEach(function(commitMap) {
-    var message = buildChangeLogLine(commitMap);
-    commitMap[PACKAGES].forEach(function(packageName) {
-      messagesByPackage[packageName] = messagesByPackage[packageName] || [];
-      messagesByPackage[packageName].push(message);
-    });
-  });
-
-  Object.keys(messagesByPackage).forEach(function(packageName) {
-    text += '\n#### ' + packageName + '\n';
-    Object.values(messagesByPackage[packageName]).forEach(function(message) {
-      text += message;
-    });
-  });
-  return text;
-}
-
-function buildChangeLogLine(map) {
-  var pr = map[PR_NUM].replace(/[^\d]/g, '');
-  return (
-    '- ' +
-    map[MESSAGE].trim() +
-    ' ([PR #' +
-    pr +
-    ']' +
-    '(' +
-    GIT_HUB_URL +
-    pr +
-    '))\n'
-  );
+function writeChangeLog(changelogText) {
+  var data = fs.readFileSync(CHANGE_LOG_PATH);
+  var fd = fs.openSync(CHANGE_LOG_PATH, 'w+');
+  var buffer = Buffer.from(changelogText.toString());
+  fs.writeSync(fd, buffer, 0, buffer.length, 0); //write new data
+  fs.writeSync(fd, data, 0, data.length, buffer.length); //append old data
+  fs.closeSync(fd);
 }
 
 /**
@@ -229,8 +239,13 @@ var releaseBranch = getReleaseBranch();
 validateReleaseBranch(releaseBranch);
 getNewChangeLogBranch(releaseBranch);
 
-var commitsAsArrayOfMaps = getCommitsAsListOfMaps();
-var result = getChangeLog(releaseBranch, commitsAsArrayOfMaps);
+var result = getChangeLogText(releaseBranch);
+writeChangeLog(result);
+
+console.log('\nChange log has been written to ' + CHANGE_LOG_PATH + '\n');
+console.log('Next Steps:');
+console.log("  1) Remove entries that shouldn't be included in the release.");
 console.log(
-  '\nInsert the following changes into ' + CHANGE_LOG_PATH + ':\n' + result
+  "  2) Move entries to the correlating 'Added' or 'Fixed' section header."
 );
+console.log('  3) Commit, push, and open your PR for team review.');
