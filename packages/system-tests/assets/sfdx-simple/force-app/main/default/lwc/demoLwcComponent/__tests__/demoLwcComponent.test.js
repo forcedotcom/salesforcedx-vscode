6f6395743c7a6d78efdf8257acdeb3716d6457d1
Xiaoyi Chen <xchen@salesforce.com>
2019-10-10 01:41:18 -0700

    tests for CodeLens

---
+++ packages/system-tests/assets/sfdx-simple/force-app/main/default/lwc/demoLwcComponent/__tests__/demoLwcComponent.test.js
@@ -0,0 +1,20 @@
+import { createElement } from 'lwc';
+import DemoLwcComponent from 'c/demoLwcComponent';
+
+describe('Demo Lwc Component', () => {
+  afterEach(() => {
+    // The jsdom instance is shared across test cases in a single file so reset the DOM
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  it('Displays greeting', () => {
+    const element = createElement('demo-lwc-component', {
+      is: DemoLwcComponent
+    });
+    document.body.appendChild(element);
+    const div = element.shadowRoot.querySelector('div');
+    expect(div.textContent).toBe('Hello, World!');
+  });
+});
