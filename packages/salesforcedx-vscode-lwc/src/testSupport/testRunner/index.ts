6ea029802260587bf4b8469f6dcfbd101435409a
Xiaoyi Chen <xchen@salesforce.com>
2019-10-10 13:37:37 -0700

    license notice

--- packages/salesforcedx-vscode-lwc/src/testSupport/testRunner/index.ts
+++ packages/salesforcedx-vscode-lwc/src/testSupport/testRunner/index.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 import { PreconditionChecker } from '@salesforce/salesforcedx-utils-vscode/out/src/types';
 import * as fs from 'fs';
 import * as path from 'path';
