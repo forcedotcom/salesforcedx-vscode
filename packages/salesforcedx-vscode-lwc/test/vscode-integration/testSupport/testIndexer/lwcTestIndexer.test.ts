aab1e5af9ab1e5ca37efe8bcd7a61c062ff6a1cd
Xiaoyi Chen <xchen@salesforce.com>
2019-10-25 02:17:16 -0700

    licensing notice

--- packages/salesforcedx-vscode-lwc/test/vscode-integration/testSupport/testIndexer/lwcTestIndexer.test.ts
+++ packages/salesforcedx-vscode-lwc/test/vscode-integration/testSupport/testIndexer/lwcTestIndexer.test.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 import { expect } from 'chai';
 import * as jestTestSupport from 'jest-editor-support';
 import { SinonStub, stub } from 'sinon';
