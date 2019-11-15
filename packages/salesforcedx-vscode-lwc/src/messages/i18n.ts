96aa06da63870772826ddddaf0230437e9dcf954
Nathan McWilliams <nmcwilliams@salesforce.com>
2019-11-15 12:56:15 -0500

    doc

--- packages/salesforcedx-vscode-lwc/src/messages/i18n.ts
+++ packages/salesforcedx-vscode-lwc/src/messages/i18n.ts
@@ -16,29 +16,26 @@
  * If ommitted, we will assume _message.
  */
 export const messages = {
-  salesforcedx_vscode_core_not_installed_text:
-    'salesforce.salesforcedx-vscode-lwc failed to activate. Ensure that you have the latest version of salesforce.salesforcedx-vscode-core installed and activated',
   command_failure: '%s failed to run',
   command_canceled: '%s was canceled',
   force_lightning_lwc_start_text: 'SFDX: Start Local Development Server',
   force_lightning_lwc_start_not_found:
-    'This command requires installing the @salesforce/lwc-dev-server plugin. For detailed instructions go to [https://developer.salesforce.com/tools/vscode/en/lwc/localdev](https://developer.salesforce.com/tools/vscode/en/lwc/localdev)',
+    'This command requires installing the @salesforce/lwc-dev-server plugin. For detailed instructions go to [https://developer.salesforce.com/docs/component-library/documentation/lwc/lwc.get_started_local_dev](https://developer.salesforce.com/docs/component-library/documentation/lwc/lwc.get_started_local_dev)',
   force_lightning_lwc_start_failed:
     'The local development server was not able to start',
   force_lightning_lwc_start_exited:
     'The local development server exited unexpectedly with code %s',
   force_lightning_lwc_start_already_running:
     'The local development server is already running',
-  force_lightning_lwc_start_stopping: 'Stopping the local development server',
   force_lightning_lwc_stop_text: 'SFDX: Stop Local Development Server',
   force_lightning_lwc_stop_not_running:
     'The local development server is not running',
   force_lightning_lwc_stop_in_progress: 'Stopping local development server',
   force_lightning_lwc_preview_text: 'SFDX: Preview Component Locally',
   force_lightning_lwc_preview_no_file:
-    'The LWC module is not specified or does not exist: %s',
+    "Can't find the Lightning Web Components module %s. It is either not specified or does not exist.",
   force_lightning_lwc_preview_unsupported:
-    "'%s' is not a recognizable LWC module",
+    "The local development server doesn't recognize the Lightning Web Components module '%s'",
   force_lightning_lwc_open_text:
     'SFDX: Open Local Development Server in Browser',
   prompt_option_open_browser: 'Open Browser',
