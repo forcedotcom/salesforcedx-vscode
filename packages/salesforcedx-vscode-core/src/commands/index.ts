bf5b91b7729cb9cabc950b73a185078ba7ae708b
Reid Elliott <reid_elliott@yahoo.com>
2020-01-14 15:34:27 -0700

    Add command to scaffold analytics templates  (#1857)
    @W-6820310@

--- packages/salesforcedx-vscode-core/src/commands/index.ts
+++ packages/salesforcedx-vscode-core/src/commands/index.ts
@@ -24,6 +24,7 @@ export { forceSourceRetrieveManifest } from './forceSourceRetrieveManifest';
 export { forceSourceStatus } from './forceSourceStatus';
 export { forceTaskStop } from './forceTaskStop';
 export {
+  forceAnalyticsTemplateCreate,
   forceApexClassCreate,
   forceApexTriggerCreate,
   forceLightningAppCreate,
