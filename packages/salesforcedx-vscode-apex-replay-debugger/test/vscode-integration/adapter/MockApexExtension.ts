aa82bf3c5da10b13d7cc5aaf170fb7e271cfc1c7
Luis Campos <lcamposguajardo@salesforce.com>
2020-01-17 10:20:47 -0800

    Changes to fix compiling the extensions

--- packages/salesforcedx-vscode-apex-replay-debugger/test/vscode-integration/adapter/MockApexExtension.ts
+++ packages/salesforcedx-vscode-apex-replay-debugger/test/vscode-integration/adapter/MockApexExtension.ts
@@ -4,8 +4,10 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { Extension } from 'vscode';
+import { Extension, ExtensionKind } from 'vscode';
 export class MockApexExtension implements Extension<any> {
+  extensionKind = ExtensionKind.Workspace;
+
   constructor() {
     this.id = 'salesforce.salesforcedx-vscode-apex';
     this.extensionPath = 'extension/local/path';
