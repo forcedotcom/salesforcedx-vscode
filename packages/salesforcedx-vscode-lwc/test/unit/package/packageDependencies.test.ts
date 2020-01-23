f50deb892d4bbbed57f6fd6f4b1a9aa7a62ecba4
Nathan McWilliams <nmcwilliams@salesforce.com>
2020-01-23 15:55:42 -0500

    unit tests for lwc/aura lsp dependency versions

---
+++ packages/salesforcedx-vscode-lwc/test/unit/package/packageDependencies.test.ts
@@ -0,0 +1,67 @@
+import { expect } from 'chai';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const checkedPackagePatterns: RegExp[] = [
+  /lightning-lsp-common/i,
+  /aura-language-server/i,
+  /lwc-language-server/i,
+  /^@salesforce\/salesforcedx/i
+];
+
+function readJsonFile(jsonFilePath: string) {
+  try {
+    return JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+  } catch (e) {
+    throw new Error(`Error reading json file from ${jsonFilePath}: ${e}`);
+  }
+}
+
+const packageJsonPath = path.join(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  '..',
+  'package.json'
+);
+const packageJson = readJsonFile(packageJsonPath);
+
+describe(`package.json dependencies for ${packageJson.name}`, () => {
+  const { dependencies, devDependencies } = packageJson;
+  let testMatchFound = false;
+
+  Object.keys(dependencies).forEach(name => {
+    const versionRange = dependencies[name];
+    checkedPackagePatterns.forEach(pattern => {
+      if (pattern.test(name)) {
+        it(`should use a strict version for dependency ${name}`, () => {
+          expect(versionRange.trim()).to.not.include('^');
+          expect(versionRange.trim()).to.not.include('~');
+          expect(versionRange.trim()).to.not.include('>');
+          expect(versionRange.trim()).to.not.include('<');
+        });
+        testMatchFound = true;
+      }
+    });
+  });
+
+  Object.keys(devDependencies).forEach(name => {
+    const versionRange = devDependencies[name];
+    checkedPackagePatterns.forEach(pattern => {
+      if (pattern.test(name)) {
+        it(`should use a strict version for devDependency ${name}`, () => {
+          expect(versionRange.trim()).to.not.include('^');
+          expect(versionRange.trim()).to.not.include('~');
+          expect(versionRange.trim()).to.not.include('>');
+          expect(versionRange.trim()).to.not.include('<');
+        });
+        testMatchFound = true;
+      }
+    });
+  });
+
+  if (!testMatchFound) {
+    it('does not have any matching dependencies', () => {});
+  }
+});
