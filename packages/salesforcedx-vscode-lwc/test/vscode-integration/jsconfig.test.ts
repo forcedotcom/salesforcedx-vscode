99a93269bb042e402b1bf03f93be5f08914e335c
Brian Buchanan <bbuchanan@salesforce.com>
2020-01-28 15:36:57 -0500

    add debug logs for jsconfig

--- packages/salesforcedx-vscode-lwc/test/vscode-integration/jsconfig.test.ts
+++ packages/salesforcedx-vscode-lwc/test/vscode-integration/jsconfig.test.ts
@@ -9,8 +9,10 @@ import { expect } from 'chai';
 import * as fs from 'fs';
 import * as path from 'path';
 import {
+  Extension,
   FileSystemWatcher,
   Position,
+  extensions,
   window,
   workspace,
   WorkspaceEdit
@@ -22,56 +24,77 @@ const TEST_COMPONENT_NAME = 'testComponent';
 const CREATE_COMPONENT_NAME = 'createComponent';
 
 describe('jsconfig Test Suite', function() {
-  let modulesDir: string;
+  let lwcDir: string;
+
   let configPath: string;
   let config: object;
 
-  before(function() {
-    modulesDir = path.join(
+  before(async function() {
+    lwcDir = path.join(
       workspace.workspaceFolders![0].uri.fsPath,
       'force-app',
       'main',
       'default',
       'lwc'
     );
-    configPath = path.join(modulesDir, CONFIG_FILENAME);
+
+    configPath = path.join(lwcDir, CONFIG_FILENAME);
   });
 
   beforeEach(async function() {
-    this.timeout(15000);
+    this.timeout(30000);
 
-    await createComponent(TEST_COMPONENT_NAME, modulesDir);
+    await createComponent(TEST_COMPONENT_NAME, lwcDir);
     await waitForConfigUpdate(configPath);
     config = await parseConfig(configPath);
+
+    console.log('before each');
+    console.log(JSON.stringify(config));
+
+    console.log(
+      JSON.stringify(await workspace.fs.readDirectory(URI.file(lwcDir)))
+    );
   });
 
   afterEach(async function() {
-    this.timeout(15000);
+    this.timeout(30000);
 
-    if (fs.existsSync(path.join(modulesDir, TEST_COMPONENT_NAME))) {
+    console.log('after each: pre-cleanup');
+    console.log(JSON.stringify(await parseConfig(configPath)));
+    console.log(
+      JSON.stringify(await workspace.fs.readDirectory(URI.file(lwcDir)))
+    );
+
+    if (fs.existsSync(path.join(lwcDir, TEST_COMPONENT_NAME))) {
       await workspace.fs.delete(
-        URI.file(path.join(modulesDir, TEST_COMPONENT_NAME)),
+        URI.file(path.join(lwcDir, TEST_COMPONENT_NAME)),
         { recursive: true }
       );
       await waitForConfigUpdate(configPath);
     }
 
-    if (fs.existsSync(path.join(modulesDir, CREATE_COMPONENT_NAME))) {
+    if (fs.existsSync(path.join(lwcDir, CREATE_COMPONENT_NAME))) {
       await workspace.fs.delete(
-        URI.file(path.join(modulesDir, CREATE_COMPONENT_NAME)),
+        URI.file(path.join(lwcDir, CREATE_COMPONENT_NAME)),
         { recursive: true }
       );
       await waitForConfigUpdate(configPath);
     }
+
+    console.log('after each: post-cleanup');
+    console.log(JSON.stringify(await parseConfig(configPath)));
+    console.log(
+      JSON.stringify(await workspace.fs.readDirectory(URI.file(lwcDir)))
+    );
   });
 
   it('Should add the newly created component to the jsconfig compilerOptions paths map', async function() {
-    this.timeout(15000);
+    this.timeout(30000);
 
-    await createComponent(CREATE_COMPONENT_NAME, modulesDir);
+    await createComponent(CREATE_COMPONENT_NAME, lwcDir);
 
     const didUpdate = await waitForConfigUpdate(configPath);
-    expect(didUpdate).to.equal(true);
+    expect(didUpdate, 'config should be updated').to.be.true;
 
     const newConfig = await parseConfig(configPath);
     expect(config).not.to.eql(newConfig);
@@ -81,15 +104,15 @@ describe('jsconfig Test Suite', function() {
   });
 
   it('Should remove the deleted component from the jsconfig compilerOptions paths map', async function() {
-    this.timeout(15000);
+    this.timeout(30000);
 
     await workspace.fs.delete(
-      URI.file(path.join(modulesDir, TEST_COMPONENT_NAME)),
+      URI.file(path.join(lwcDir, TEST_COMPONENT_NAME)),
       { recursive: true }
     );
 
     const didUpdate = await waitForConfigUpdate(configPath);
-    expect(didUpdate).to.equal(true);
+    expect(didUpdate, 'config should be updated').to.be.true;
 
     const newConfig = await parseConfig(configPath);
     expect(config).not.to.eql(newConfig);
@@ -99,26 +122,26 @@ describe('jsconfig Test Suite', function() {
   });
 
   it('Should not update jsconfig.json when a component is saved', async function() {
-    this.timeout(15000);
+    this.timeout(30000);
 
     const document = await workspace.openTextDocument(
-      path.join(modulesDir, TEST_COMPONENT_NAME, `${TEST_COMPONENT_NAME}.js`)
+      path.join(lwcDir, TEST_COMPONENT_NAME, `${TEST_COMPONENT_NAME}.js`)
     );
 
     await document.save();
 
     const didUpdate = await waitForConfigUpdate(configPath);
-    expect(didUpdate).to.equal(false);
+    expect(didUpdate, 'config should not be updated').to.be.false;
 
     const newConfig = await parseConfig(configPath);
     expect(config).to.eql(newConfig);
   });
 
   it('Should not update jsconfig.json on keystrokes in a component file', async function() {
-    this.timeout(15000);
+    this.timeout(30000);
 
     const document = await workspace.openTextDocument(
-      path.join(modulesDir, TEST_COMPONENT_NAME, `${TEST_COMPONENT_NAME}.js`)
+      path.join(lwcDir, TEST_COMPONENT_NAME, `${TEST_COMPONENT_NAME}.js`)
     );
     const editor = await window.showTextDocument(document);
 
@@ -127,7 +150,7 @@ describe('jsconfig Test Suite', function() {
     });
 
     const didUpdate = await waitForConfigUpdate(configPath);
-    expect(didUpdate).to.equal(false);
+    expect(didUpdate, 'config should not be updated').to.be.false;
 
     const newConfig = await parseConfig(configPath);
     expect(config).to.eql(newConfig);
@@ -151,8 +174,8 @@ async function parseConfig(configPath: string) {
  * @param configPath - path to jsconfig.json
  * @returns value representing whether the config updated or the wait timed out
  */
-async function waitForConfigUpdate(configPath: string): Promise<boolean> {
-  return await new Promise<boolean>(resolve => {
+function waitForConfigUpdate(configPath: string): Promise<boolean> {
+  return new Promise<boolean>(resolve => {
     const watcher = workspace.createFileSystemWatcher(
       configPath,
       true,
@@ -170,7 +193,7 @@ async function waitForConfigUpdate(configPath: string): Promise<boolean> {
     timer = setTimeout(() => {
       watcher.dispose();
       resolve(false);
-    }, 5000);
+    }, 10000);
   });
 }
 
@@ -179,18 +202,18 @@ async function waitForConfigUpdate(configPath: string): Promise<boolean> {
  * Ideally, this should be replaced with invoking `sfdx.force.lightning.lwc.create`
  *
  * @param name - name of the new component
- * @param modulesDir - path for where to create the component
+ * @param lwcDir - path for where to create the component
  */
-async function createComponent(name: string, modulesDir: string) {
-  await workspace.fs.createDirectory(URI.file(path.join(modulesDir, name)));
+async function createComponent(name: string, lwcDir: string) {
+  await workspace.fs.createDirectory(URI.file(path.join(lwcDir, name)));
 
   const htmlEdit = new WorkspaceEdit();
   const jsEdit = new WorkspaceEdit();
   const xmlEdit = new WorkspaceEdit();
 
-  htmlEdit.createFile(URI.file(path.join(modulesDir, name, `${name}.html`)));
-  jsEdit.createFile(URI.file(path.join(modulesDir, name, `${name}.js`)));
-  xmlEdit.createFile(URI.file(path.join(modulesDir, name, `${name}-meta.xml`)));
+  htmlEdit.createFile(URI.file(path.join(lwcDir, name, `${name}.html`)));
+  jsEdit.createFile(URI.file(path.join(lwcDir, name, `${name}.js`)));
+  xmlEdit.createFile(URI.file(path.join(lwcDir, name, `${name}-meta.xml`)));
 
   await workspace.applyEdit(htmlEdit);
   await workspace.applyEdit(jsEdit);
@@ -199,7 +222,7 @@ async function createComponent(name: string, modulesDir: string) {
   const encoder = new TextEncoder();
 
   await workspace.fs.writeFile(
-    URI.file(path.join(modulesDir, name, `${name}.html`)),
+    URI.file(path.join(lwcDir, name, `${name}.html`)),
     encoder.encode(`<template>
     <div>
       Hello, World!
@@ -208,7 +231,7 @@ async function createComponent(name: string, modulesDir: string) {
   );
 
   await workspace.fs.writeFile(
-    URI.file(path.join(modulesDir, name, `${name}.js`)),
+    URI.file(path.join(lwcDir, name, `${name}.js`)),
     encoder.encode(
       `import { LightningElement } from 'lwc';
 export default class ${name} extends LightningElement {}`
@@ -216,7 +239,7 @@ export default class ${name} extends LightningElement {}`
   );
 
   await workspace.fs.writeFile(
-    URI.file(path.join(modulesDir, name, `${name}-meta.xml`)),
+    URI.file(path.join(lwcDir, name, `${name}-meta.xml`)),
     encoder.encode(`<?xml version="1.0" encoding="UTF-8"?>
   <LightningComponentBundle xmlns="http://soap.sforce.com/2006/04/metadata" fqn="${name}">
       <apiVersion>46.0</apiVersion>
