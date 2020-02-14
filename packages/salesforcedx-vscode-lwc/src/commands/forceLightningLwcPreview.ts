4c623e39f5b67cda03a871cdf1436fc89fbf02a0
Nicolas Kruk <nkruk@salesforce.com>
2020-02-14 01:07:45 -0500

    fix lints

--- packages/salesforcedx-vscode-lwc/src/commands/forceLightningLwcPreview.ts
+++ packages/salesforcedx-vscode-lwc/src/commands/forceLightningLwcPreview.ts
@@ -5,8 +5,8 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import * as fs from 'fs';
 import { componentUtil } from '@salesforce/lightning-lsp-common';
+import * as fs from 'fs';
 import * as vscode from 'vscode';
 import { nls } from '../messages';
 import { DevServerService } from '../service/devServerService';
