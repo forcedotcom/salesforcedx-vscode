73ef652d3a55c4ee4c36a89697ebdc039ad652a8
Nicolas Kruk <nkruk@salesforce.com>
2020-02-18 12:42:06 -0500

    lint fixes / telemetry fixes

--- packages/salesforcedx-vscode-lwc/src/index.ts
+++ packages/salesforcedx-vscode-lwc/src/index.ts
@@ -5,8 +5,8 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import * as fs from 'fs';
 import { shared as lspCommon } from '@salesforce/lightning-lsp-common';
+import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import {
