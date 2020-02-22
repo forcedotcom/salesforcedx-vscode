2cac16e799016fcc1ce10a7ef940affb3f9a2482
Xiaoyi Chen <xchen@salesforce.com>
2020-02-21 18:53:37 -0800

    Update lightning-lsp-common reference

--- packages/salesforcedx-vscode-lwc/src/testSupport/workspace/workspaceService.ts
+++ packages/salesforcedx-vscode-lwc/src/testSupport/workspace/workspaceService.ts
@@ -4,7 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { shared as lspCommon } from 'lightning-lsp-common';
+import { shared as lspCommon } from '@salesforce/lightning-lsp-common';
 import { workspace } from 'vscode';
 import * as vscode from 'vscode';
 
