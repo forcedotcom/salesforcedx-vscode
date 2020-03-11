98f4becc85a9667aa2067140b51f7f3f1a01b8a9
AnanyaJha <a.jha@salesforce.com>
2020-03-10 22:53:27 -0700

    Single Apex Class file deploys through Tooling API (#2052)
    @W-6531270@

---
+++ packages/salesforcedx-vscode-core/src/deploys/toolingDeployResultParser.ts
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import {
+  Row,
+  Table
+} from '@salesforce/salesforcedx-utils-vscode/out/src/output';
+import { nls } from '../messages';
+import {
+  DeployResult,
+  DeployStatusEnum,
+  ToolingRetrieveResult
+} from './deployUtil';
+
+export class ToolingDeployParser {
+  public result: ToolingRetrieveResult;
+
+  constructor(deployResult: ToolingRetrieveResult) {
+    this.result = deployResult;
+  }
+
+  public buildSuccesses(componentSuccess: DeployResult) {
+    const mdState =
+      componentSuccess.changed && !componentSuccess.created
+        ? 'Updated'
+        : 'Created';
+    const success = [
+      {
+        state: mdState,
+        fullName: componentSuccess.fullName,
+        type: componentSuccess.componentType,
+        filePath: componentSuccess.fileName
+      },
+      {
+        state: mdState,
+        fullName: componentSuccess.fullName,
+        type: componentSuccess.componentType,
+        filePath: `${componentSuccess.fileName}-meta.xml`
+      }
+    ];
+    return success;
+  }
+
+  public buildErrors(componentErrors: DeployResult[]) {
+    const failures = [];
+    for (const err of componentErrors) {
+      if (err.columnNumber && err.lineNumber) {
+        err.problem = `${err.problem} (${err.lineNumber}:${err.columnNumber})`;
+      }
+      failures.push({
+        filePath: err.fileName,
+        error: err.problem
+      });
+    }
+    return failures;
+  }
+
+  public async outputResult(sourceUri?: string): Promise<string> {
+    let outputResult: string;
+    const table = new Table();
+    let title: string;
+    switch (this.result.State) {
+      case DeployStatusEnum.Completed:
+        title = nls.localize(`table_title_deployed_source`);
+        const successRows = this.buildSuccesses(
+          this.result.DeployDetails!.componentSuccesses[0]
+        );
+        outputResult = table.createTable(
+          (successRows as unknown) as Row[],
+          [
+            { key: 'state', label: nls.localize('table_header_state') },
+            { key: 'fullName', label: nls.localize('table_header_full_name') },
+            { key: 'type', label: nls.localize('table_header_type') },
+            {
+              key: 'filePath',
+              label: nls.localize('table_header_project_path')
+            }
+          ],
+          title
+        );
+        break;
+      case DeployStatusEnum.Failed:
+        const failedErrorRows = this.buildErrors(
+          this.result.DeployDetails!.componentFailures
+        );
+        outputResult = table.createTable(
+          (failedErrorRows as unknown) as Row[],
+          [
+            {
+              key: 'filePath',
+              label: nls.localize('table_header_project_path')
+            },
+            { key: 'error', label: nls.localize('table_header_errors') }
+          ],
+          nls.localize(`table_title_deploy_errors`)
+        );
+        break;
+      case DeployStatusEnum.Queued:
+        outputResult = nls.localize('beta_tapi_queue_status');
+        break;
+      case DeployStatusEnum.Error:
+        const error = this.result.ErrorMsg!;
+        const errorRows = [{ filePath: sourceUri, error }];
+        outputResult = table.createTable(
+          (errorRows as unknown) as Row[],
+          [
+            {
+              key: 'filePath',
+              label: nls.localize('table_header_project_path')
+            },
+            { key: 'error', label: nls.localize('table_header_errors') }
+          ],
+          nls.localize(`table_title_deploy_errors`)
+        );
+        break;
+      default:
+        outputResult = '';
+    }
+    return outputResult;
+  }
+}
