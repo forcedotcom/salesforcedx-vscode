2e0301489d7b91cd6036563a40a8433022285cb8
AnanyaJha <a.jha@salesforce.com>
2020-03-10 22:53:27 -0700

    Single Apex Class file deploys through Tooling API (#2052)
    @W-6531270@

--- packages/salesforcedx-vscode-core/src/commands/forceSourceDeploySourcePath.ts
+++ packages/salesforcedx-vscode-core/src/commands/forceSourceDeploySourcePath.ts
@@ -7,19 +7,30 @@
 
 import {
   Command,
+  CompositeCliCommandExecutor,
   SfdxCommandBuilder
 } from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
 import {
   ContinueResponse,
   ParametersGatherer
 } from '@salesforce/salesforcedx-utils-vscode/out/src/types';
+import * as path from 'path';
 import * as vscode from 'vscode';
 import { channelService } from '../channels';
+import {
+  DeployStatusEnum,
+  ToolingDeploy,
+  ToolingDeployParser,
+  ToolingRetrieveResult
+} from '../deploys';
 import { nls } from '../messages';
 import { notificationService } from '../notifications';
+import { DeployQueue, sfdxCoreSettings } from '../settings';
 import { telemetryService } from '../telemetry';
+import { OrgAuthInfo } from '../util';
 import { BaseDeployExecutor, DeployType } from './baseDeployCommand';
 import { SourcePathChecker } from './forceSourceRetrieveSourcePath';
+import { APEX_CLASS_EXTENSION } from './templates/metadataTypeConstants';
 import { FilePathGatherer, SfdxCommandlet, SfdxWorkspaceChecker } from './util';
 
 export class ForceSourceDeploySourcePathExecutor extends BaseDeployExecutor {
@@ -31,6 +42,73 @@ export class ForceSourceDeploySourcePathExecutor extends BaseDeployExecutor {
       .withFlag('--sourcepath', sourcePath)
       .withJson();
     return commandBuilder.build();
+  }
+
+  public async execute(response: ContinueResponse<string>): Promise<void> {
+    const betaDeployRetrieve = sfdxCoreSettings.getBetaDeployRetrieve();
+    // this supported types logic is temporary until we have a way of generating the metadata type from the path
+    // once we have the metadata type we can check to see if it is a toolingsupportedtype from that util
+    const supportedType =
+      path.extname(response.data) === APEX_CLASS_EXTENSION ||
+      response.data.includes(`${APEX_CLASS_EXTENSION}-meta.xml`);
+    const multipleSourcePaths = response.data.includes(',');
+
+    if (betaDeployRetrieve && supportedType && !multipleSourcePaths) {
+      const startTime = process.hrtime();
+      const cancellationTokenSource = new vscode.CancellationTokenSource();
+      const cancellationToken = cancellationTokenSource.token;
+
+      const executionWrapper = new CompositeCliCommandExecutor(
+        // TODO: Build command for non-cli execution like ForceGenerateFauxClassesExecutor
+        this.build(response.data)
+      ).execute(cancellationToken);
+      this.attachExecution(
+        executionWrapper,
+        cancellationTokenSource,
+        cancellationToken
+      );
+      executionWrapper.processExitSubject.subscribe(() => {
+        this.logMetric('force_source_deploy_with_sourcepath_beta', startTime);
+      });
+
+      try {
+        const usernameOrAlias = await OrgAuthInfo.getDefaultUsernameOrAlias(
+          true
+        );
+        if (!usernameOrAlias) {
+          throw new Error(nls.localize('error_no_default_username'));
+        }
+        const orgConnection = await OrgAuthInfo.getConnection(usernameOrAlias);
+        const deployLibrary = new ToolingDeploy(orgConnection);
+        const deployOutput = await deployLibrary.deploy(response.data);
+
+        const parser = new ToolingDeployParser(deployOutput);
+        const outputResult = await parser.outputResult();
+        channelService.appendLine(outputResult);
+        if (deployOutput.State === DeployStatusEnum.Completed) {
+          executionWrapper.successfulExit();
+        } else {
+          executionWrapper.failureExit();
+        }
+      } catch (e) {
+        telemetryService.sendException(
+          'force_source_deploy_with_sourcepath_beta',
+          e.message
+        );
+        const deployOutput = {
+          State: 'Error',
+          ErrorMsg: e.message
+        } as ToolingRetrieveResult;
+        const parser = new ToolingDeployParser(deployOutput);
+        const errorResult = await parser.outputResult(response.data);
+        channelService.appendLine(errorResult);
+        executionWrapper.failureExit();
+      } finally {
+        await DeployQueue.get().unlock();
+      }
+    } else {
+      super.execute(response);
+    }
   }
 
   protected getDeployType() {
