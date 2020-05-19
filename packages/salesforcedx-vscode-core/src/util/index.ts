442480b6659f613e3bac091d872d753dd0bee30e
rcoringrato-sfdc <54118325+rcoringrato-sfdc@users.noreply.github.com>
2020-05-18 18:36:58 -0600

    Honor CLI telemetry disablement setting (#2193)
    * Honor CLI telemetry disablement setting
    
    * Update telemetry docs
    
    * Simplified setting check, fixed tests.
    
    * Updated docs
    
    * Moved cli telemetry check into telemetry service
    
    * Disregard dev mode for CLI telemetry.
    
    * Added ts-sinon for integration tests

--- packages/salesforcedx-vscode-core/src/util/index.ts
+++ packages/salesforcedx-vscode-core/src/util/index.ts
@@ -7,13 +7,15 @@
 
 export { OrgAuthInfo } from './authInfo';
 export {
+  disableCLITelemetry,
+  isCLIInstalled,
+  isCLITelemetryAllowed,
+  isSFDXContainerMode,
+  showCLINotInstalledMessage
+} from './cliConfiguration';
+export { ConfigSource, ConfigUtil } from './configUtil';
+export {
   getRootWorkspace,
   getRootWorkspacePath,
   hasRootWorkspace
 } from './rootWorkspace';
-export {
-  isCLIInstalled,
-  showCLINotInstalledMessage,
-  isSFDXContainerMode
-} from './cliConfiguration';
-export { ConfigSource, ConfigUtil } from './configUtil';
