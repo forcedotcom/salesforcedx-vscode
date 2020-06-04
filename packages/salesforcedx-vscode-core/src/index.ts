8298f9089300cba6d29e4d1719e23b855819f786
Gabriel Barker <gabrielbarker@gmail.com>
2020-06-04 12:33:01 +0100

    Fix the cosmetic issues e.g. trailing commas

--- packages/salesforcedx-vscode-core/src/index.ts
+++ packages/salesforcedx-vscode-core/src/index.ts
@@ -54,7 +54,7 @@ import {
   forceTaskStop,
   forceVisualforceComponentCreate,
   forceVisualforcePageCreate,
-  turnOffLogging,
+  turnOffLogging
 } from './commands';
 import { RetrieveMetadataTrigger } from './commands/forceSourceRetrieveMetadata';
 import { getUserId } from './commands/forceStartApexDebugLogging';
@@ -66,7 +66,7 @@ import {
   SelectOutputDir,
   SfdxCommandlet,
   SfdxCommandletExecutor,
-  SfdxWorkspaceChecker,
+  SfdxWorkspaceChecker
 } from './commands/util';
 import { registerConflictView, setupConflictView } from './conflict';
 import { getDefaultUsernameOrAlias, setupWorkspaceOrgType } from './context';
@@ -83,7 +83,7 @@ import {
   hasRootWorkspace,
   isCLIInstalled,
   isCLITelemetryAllowed,
-  showCLINotInstalledMessage,
+  showCLINotInstalledMessage
 } from './util';
 import { OrgAuthInfo } from './util/authInfo';
 
@@ -434,14 +434,14 @@ async function setupOrgBrowser(
 
   vscode.commands.registerCommand(
     'sfdx.force.metadata.view.type.refresh',
-    async (node) => {
+    async node => {
       await orgBrowser.refreshAndExpand(node);
     }
   );
 
   vscode.commands.registerCommand(
     'sfdx.force.metadata.view.component.refresh',
-    async (node) => {
+    async node => {
       await orgBrowser.refreshAndExpand(node);
     }
   );
@@ -494,7 +494,7 @@ export async function activate(context: vscode.ExtensionContext) {
       SfdxCommandletExecutor,
       sfdxCoreSettings,
       SfdxWorkspaceChecker,
-      telemetryService,
+      telemetryService
     };
 
     if (!isCLIInstalled()) {
@@ -596,7 +596,7 @@ export async function activate(context: vscode.ExtensionContext) {
     sfdxCoreSettings,
     SfdxWorkspaceChecker,
     taskViewService,
-    telemetryService,
+    telemetryService
   };
 
   telemetryService.sendExtensionActivationEvent(extensionHRStart);
