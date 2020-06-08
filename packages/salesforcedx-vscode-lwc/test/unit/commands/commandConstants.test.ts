964dbbbfef57c593bb8d52b5d525e742df127e33
Jag Jayaprakash <58611665+jag-sfdc@users.noreply.github.com>
2020-06-08 11:28:17 -0700

    @W-7665177@ VSCode open preview component using the new path and correct port (#2239)
    * @W-7665177@ VSCode open preview component using the new path and correct port
    
    * @W-7665177@ Fixing code review comments - Mostly refactoring and test fixes
    
    * @W-7665177@ Fixing code review comments 2 - Mostly refactoring and test fixes
    
    * @W-7665177@ Fixing code review comments 3 - Test fix

--- packages/salesforcedx-vscode-lwc/test/unit/commands/commandConstants.test.ts
+++ packages/salesforcedx-vscode-lwc/test/unit/commands/commandConstants.test.ts
@@ -1,19 +1,13 @@
 import { expect } from 'chai';
 import {
-  DEV_SERVER_BASE_URL,
+  DEV_SERVER_DEFAULT_BASE_URL,
   DEV_SERVER_PREVIEW_ROUTE
 } from '../../../src/commands/commandConstants';
 
 describe('force:lightning:lwc:start constants', () => {
   describe('base url', () => {
     it('should include localhost', async () => {
-      expect(DEV_SERVER_BASE_URL).to.include('localhost');
-    });
-  });
-
-  describe('preview route', () => {
-    it('should include the base url', async () => {
-      expect(DEV_SERVER_PREVIEW_ROUTE).to.include(DEV_SERVER_BASE_URL);
+      expect(DEV_SERVER_DEFAULT_BASE_URL).to.include('localhost');
     });
   });
 });
