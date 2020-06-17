05c44df00af68134d222e6edec6e301123bb47e3
amandastern99 <35906675+amandastern99@users.noreply.github.com>
2020-06-17 14:42:29 -0400

    As/tool tip feature (#2272)

--- packages/salesforcedx-vscode-core/src/messages/i18n.ts
+++ packages/salesforcedx-vscode-core/src/messages/i18n.ts
@@ -42,6 +42,8 @@ export const messages = {
 
   status_bar_text: `$(x) %s`,
   status_bar_tooltip: 'Click to cancel the command',
+  status_bar_open_org_tooltip: 'Open Org',
+  status_bar_org_picker_tooltip: 'Change Default Org',
 
   force_auth_web_login_authorize_dev_hub_text: 'SFDX: Authorize a Dev Hub',
   force_auth_web_login_authorize_org_text: 'SFDX: Authorize an Org',
