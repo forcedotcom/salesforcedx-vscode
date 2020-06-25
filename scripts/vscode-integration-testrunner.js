6b7a313f09af4fff81ae94fe9ae087b6a0fd4694
Xiaoyi Chen <xchen@salesforce.com>
2020-06-25 14:40:50 -0700

    Migrate to vscode-test (#2290)
    Update vscode-test to ^1.4.0. (Note: We were originally using vscode-test to download VS code, but not to launch tests.)
    
    Update test scripts to use runTests from vscode-test module: (npm scripts  test:vscode-integration invoke the test scripts)
    
    * scripts/run-vscode-integration-tests-with-top-level-extensions.js
    * scripts/run-tests-with-recipes.js
    * scripts/run-vscode-integration-tests.js
    
    Update docs:
    
    * Update Running through the CLI section to deprecate CODE_DOWNLOAD_URL and default values for env vars.
    * Remove mentions of vscode/bin/test

---
+++ scripts/vscode-integration-testrunner.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+const { runTests } = require('vscode-test');
+
+/**
+ * A wrapper utlity for running VS Code integration tests. If a correspondent env variable is specified,
+ * it will override the parameters passed in.
+ * @param {string} version Version of VS Code to run the tests against
+ * @param {string} extensionDevelopmentPath Location of the extensions to load
+ * @param {string} extensionTestsPath Location of the tests to execute
+ * @param {string} testWorkspace Location of a workspace to open for the test instance
+ */
+function runIntegrationTests({
+  version,
+  extensionDevelopmentPath,
+  extensionTestsPath,
+  testWorkspace
+}) {
+  try {
+    const {
+      CODE_VERSION,
+      CODE_TESTS_PATH,
+      CODE_EXTENSIONS_PATH,
+      CODE_TESTS_WORKSPACE
+    } = process.env;
+
+    const _version = CODE_VERSION || version;
+    const _extensionDevelopmentPath =
+      CODE_EXTENSIONS_PATH || extensionDevelopmentPath;
+    const _extensionTestsPath = CODE_TESTS_PATH || extensionTestsPath;
+    const _testWorkspace = CODE_TESTS_WORKSPACE || testWorkspace;
+    const launchArgs = _testWorkspace && [_testWorkspace];
+    runTests({
+      version: _version,
+      extensionDevelopmentPath: _extensionDevelopmentPath,
+      extensionTestsPath: _extensionTestsPath,
+      launchArgs
+    });
+  } catch (error) {
+    console.error('Test run failed with error:', error);
+    console.log('Tests exist with error code: 1 when a test fails.');
+    process.exit(1);
+  }
+}
+
+module.exports = {
+  runIntegrationTests
+};
