181ae65839278f2b8e503967649d74543e1d19d4
Xiaoyi Chen <xchen@salesforce.com>
2020-06-24 17:14:42 -0700

    refactor test scripts

--- scripts/run-vscode-integration-tests-with-top-level-extensions.js
+++ scripts/run-vscode-integration-tests-with-top-level-extensions.js
@@ -1,36 +1,18 @@
 #!/usr/bin/env node
 
 const path = require('path');
-const { runTests } = require('vscode-test');
+const { runIntegrationTests } = require('./vscode-integration-testrunner');
 
-function main() {
-  try {
-    const cwd = process.cwd();
-    const extensionDevelopmentPath = path.join(__dirname, '..', 'packages');
-    const testWorkspace = path.join(
-      __dirname,
-      '..',
-      'packages',
-      'system-tests',
-      'assets',
-      'sfdx-simple'
-    );
-    const extensionTestsPath = path.join(
-      cwd,
-      'out',
-      'test',
-      'vscode-integration'
-    );
-    runTests({
-      extensionDevelopmentPath,
-      extensionTestsPath,
-      launchArgs: [testWorkspace]
-    });
-  } catch (error) {
-    console.error('Test run failed with error:', error);
-    console.log('Tests exist with error code: 1 when a test fails.');
-    process.exit(1);
-  }
-}
-
-main();
+const cwd = process.cwd();
+runIntegrationTests({
+  extensionDevelopmentPath: path.join(__dirname, '..', 'packages'),
+  extensionTestsPath: path.join(cwd, 'out', 'test', 'vscode-integration'),
+  testWorkspace: path.join(
+    __dirname,
+    '..',
+    'packages',
+    'system-tests',
+    'assets',
+    'sfdx-simple'
+  )
+});
--- scripts/run-vscode-integration-tests.js
+++ scripts/run-vscode-integration-tests.js
@@ -1,38 +1,10 @@
 #!/usr/bin/env node
 
 const path = require('path');
-const { runTests } = require('vscode-test');
+const { runIntegrationTests } = require('./vscode-integration-testrunner');
 
-function main() {
-  try {
-    const {
-      CODE_VERSION,
-      CODE_TESTS_PATH,
-      CODE_EXTENSIONS_PATH,
-      CODE_TESTS_WORKSPACE
-    } = process.env;
-
-    const cwd = process.cwd();
-    const version = CODE_VERSION;
-    const extensionDevelopmentPath = CODE_EXTENSIONS_PATH
-      ? CODE_EXTENSIONS_PATH
-      : cwd;
-    const extensionTestsPath = CODE_TESTS_PATH
-      ? CODE_TESTS_PATH
-      : path.join(cwd, 'out', 'test', 'vscode-integration');
-    const testWorkspace = CODE_TESTS_WORKSPACE;
-    const launchArgs = testWorkspace ? [testWorkspace] : undefined;
-    runTests({
-      version,
-      extensionDevelopmentPath,
-      extensionTestsPath,
-      launchArgs
-    });
-  } catch (error) {
-    console.error('Test run failed with error:', error);
-    console.log('Tests exist with error code: 1 when a test fails.');
-    process.exit(1);
-  }
-}
-
-main();
+const cwd = process.cwd();
+runIntegrationTests({
+  extensionDevelopmentPath: cwd,
+  extensionTestsPath: path.join(cwd, 'out', 'test', 'vscode-integration')
+});
