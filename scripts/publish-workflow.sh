75cf81064935ecdbc670e6b7e276c08a22e36bd7
Shelby Holden <shelby.holden@salesforce.com>
2020-07-08 10:19:50 -0600

    Porting changes from fork

---
+++ scripts/publish-workflow.sh
@@ -0,0 +1,7 @@
+CircleCIToken=$1
+curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
+  "branch": "master",
+  "parameters": {
+    "publish": true
+  }
+}' https://circleci.com/api/v2/project/gh/forcedotcom/salesforcedx-vscode/pipeline
