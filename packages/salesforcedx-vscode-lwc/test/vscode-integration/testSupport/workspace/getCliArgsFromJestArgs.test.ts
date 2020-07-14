57df98fbae24a15910a840dd749e2213067e755e
Xiaoyi Chen <xchen@salesforce.com>
2020-07-13 19:00:36 -0700

    fix lwc debugging with preview javascript debugger

--- packages/salesforcedx-vscode-lwc/test/vscode-integration/testSupport/workspace/getCliArgsFromJestArgs.test.ts
+++ packages/salesforcedx-vscode-lwc/test/vscode-integration/testSupport/workspace/getCliArgsFromJestArgs.test.ts
@@ -5,12 +5,9 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { expect } from 'chai';
-import { shared as lspCommon } from '@salesforce/lightning-lsp-common';
 import { SinonStub, stub } from 'sinon';
-import {
-  getCliArgsFromJestArgs,
-  workspaceService
-} from '../../../../src/testSupport/workspace';
+import { workspace, Uri, WorkspaceConfiguration } from 'vscode';
+import { getCliArgsFromJestArgs } from '../../../../src/testSupport/workspace';
 import { TestRunType } from '../../../../src/testSupport/testRunner/testRunner';
 
 describe('getCliArgsFromJestArgs Unit Tests', () => {
@@ -22,86 +19,69 @@ describe('getCliArgsFromJestArgs Unit Tests', () => {
     '--runTestsByPath',
     'mockTestPath'
   ];
-  let getCurrentWorkspaceTypeStub: SinonStub<[], lspCommon.WorkspaceType>;
+  let getConfigurationStub: SinonStub<
+    [string?, (Uri | null)?],
+    WorkspaceConfiguration
+  >;
+  let mockBaseConfiguration = {
+    has(section: string) {
+      return true;
+    },
+    inspect(section: string) {
+      return undefined;
+    },
+    async update(section: string, value: any) {}
+  };
+  let mockPreviewJavaScriptDebuggerEnabledConfiguration: WorkspaceConfiguration = {
+    ...mockBaseConfiguration,
+    get(section: string) {
+      if (section === 'javascript.usePreview') return true;
+    }
+  };
+  let mockPreviewJavaScriptDebuggerDisabledConfiguration: WorkspaceConfiguration = {
+    ...mockBaseConfiguration,
+    get(section: string) {
+      if (section === 'javascript.usePreview') return false;
+    }
+  };
+
   beforeEach(() => {
-    getCurrentWorkspaceTypeStub = stub(
-      workspaceService,
-      'getCurrentWorkspaceType'
+    getConfigurationStub = stub(workspace, 'getConfiguration');
+    getConfigurationStub.returns(
+      mockPreviewJavaScriptDebuggerEnabledConfiguration
     );
   });
   afterEach(() => {
-    getCurrentWorkspaceTypeStub.restore();
+    getConfigurationStub.restore();
   });
 
-  describe('SFDX Workspace', () => {
-    beforeEach(() => {
-      getCurrentWorkspaceTypeStub.returns(lspCommon.WorkspaceType.SFDX);
-    });
-
-    it('Should return Cli args for run mode', () => {
-      const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.RUN);
-      const expectedCliArgs = ['--', ...mockJestArgs];
-      expect(cliArgs).to.eql(expectedCliArgs);
-    });
-
-    it('Should return Cli args for watch mode', () => {
-      const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.WATCH);
-      const expectedCliArgs = ['--', ...mockJestArgs];
-      expect(cliArgs).to.eql(expectedCliArgs);
-    });
-
-    it('Should return Cli args for debug mode', () => {
-      const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.DEBUG);
-      const expectedCliArgs = ['--debug', '--', ...mockJestArgs];
-      expect(cliArgs).to.eql(expectedCliArgs);
-    });
+  it('Should return Cli args for run mode', () => {
+    const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.RUN);
+    const expectedCliArgs = ['--', ...mockJestArgs];
+    expect(cliArgs).to.eql(expectedCliArgs);
   });
 
-  describe('Internal Dev Workspace', () => {
-    beforeEach(() => {
-      getCurrentWorkspaceTypeStub.returns(lspCommon.WorkspaceType.CORE_PARTIAL);
-    });
-
-    it('Should return Cli args for run mode', () => {
-      const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.RUN);
-      const expectedCliArgs = ['--', ...mockJestArgs];
-      expect(cliArgs).to.eql(expectedCliArgs);
-    });
-
-    it('Should return Cli args for watch mode', () => {
-      const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.WATCH);
-      const expectedCliArgs = ['--', ...mockJestArgs];
-      expect(cliArgs).to.eql(expectedCliArgs);
-    });
-
-    it('Should return Cli args for debug mode', () => {
-      const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.DEBUG);
-      const expectedCliArgs = ['--debug', '--', ...mockJestArgs];
-      expect(cliArgs).to.eql(expectedCliArgs);
-    });
+  it('Should return Cli args for watch mode', () => {
+    const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.WATCH);
+    const expectedCliArgs = ['--', ...mockJestArgs];
+    expect(cliArgs).to.eql(expectedCliArgs);
   });
 
-  describe('Unknown Workspace', () => {
-    beforeEach(() => {
-      getCurrentWorkspaceTypeStub.returns(lspCommon.WorkspaceType.UNKNOWN);
-    });
+  it('Should return Cli args for debug mode', () => {
+    getConfigurationStub.returns(
+      mockPreviewJavaScriptDebuggerDisabledConfiguration
+    );
+    const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.DEBUG);
+    const expectedCliArgs = ['--debug', '--', ...mockJestArgs];
+    expect(cliArgs).to.eql(expectedCliArgs);
+  });
 
-    it('Should return Cli args for run mode', () => {
-      const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.RUN);
-      const expectedCliArgs = ['--', ...mockJestArgs];
-      expect(cliArgs).to.eql(expectedCliArgs);
-    });
-
-    it('Should return Cli args for watch mode', () => {
-      const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.WATCH);
-      const expectedCliArgs = ['--', ...mockJestArgs];
-      expect(cliArgs).to.eql(expectedCliArgs);
-    });
-
-    it('Should return Cli args for debug mode', () => {
-      const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.DEBUG);
-      const expectedCliArgs = ['--debug', '--', ...mockJestArgs];
-      expect(cliArgs).to.eql(expectedCliArgs);
-    });
+  it('Should return Cli args for debug mode if using preview JavaScript debugger', () => {
+    getConfigurationStub.returns(
+      mockPreviewJavaScriptDebuggerEnabledConfiguration
+    );
+    const cliArgs = getCliArgsFromJestArgs(mockJestArgs, TestRunType.DEBUG);
+    const expectedCliArgs = ['--', ...mockJestArgs];
+    expect(cliArgs).to.eql(expectedCliArgs);
   });
 });
