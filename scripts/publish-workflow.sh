3f39c5b87e1a9c5a74f0e2ce542f9086202e1bf1
Shelby Holden <45604118+sfsholden@users.noreply.github.com>
2020-07-13 18:26:28 -0600

    CircleCi Config Improvements @W-7721208@ (#2309)

---
+++ scripts/publish-workflow.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+CircleCIToken=$1
+curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
+  "branch": "master",
+  "parameters": {
+    "publish": true
+  }
+}' https://circleci.com/api/v2/project/gh/forcedotcom/salesforcedx-vscode/pipeline
