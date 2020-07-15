0f6d9bf6c31c27af2e4d4cc268c5851ffcfdd222
Ananya <a.jha@salesforce.com>
2020-07-15 14:43:01 -0500

    rearranged constants

--- packages/salesforcedx-vscode-core/src/constants.ts
+++ packages/salesforcedx-vscode-core/src/constants.ts
@@ -8,24 +8,26 @@
 export const SFDX_PROJECT_FILE = 'sfdx-project.json';
 export const SFDX_CONFIG_FILE = 'sfdx-config.json';
 export const CLIENT_ID = 'sfdx-vscode';
-export const SFDX_CORE_CONFIGURATION_NAME = 'salesforcedx-vscode-core';
 export const STATUS_BAR_MSG_TIMEOUT_MS = 5000;
-export const SHOW_CLI_SUCCESS_INFO_MSG = 'show-cli-success-msg';
 export const APEX_CODE_DEBUG_LEVEL = 'FINEST';
 export const VISUALFORCE_DEBUG_LEVEL = 'FINER';
-export const TELEMETRY_ENABLED = 'telemetry.enabled';
 export const SFDX_CONFIG_DISABLE_TELEMETRY = 'disableTelemetry';
 export const ENV_SFDX_DISABLE_TELEMETRY = 'SFDX_DISABLE_TELEMETRY';
-export const INTERNAL_DEVELOPMENT_FLAG = 'internal-development';
 export const TELEMETRY_OPT_OUT_LINK =
   'https://forcedotcom.github.io/salesforcedx-vscode/articles/faq/telemetry';
-export const PUSH_OR_DEPLOY_ON_SAVE_ENABLED = 'push-or-deploy-on-save.enabled';
-export const RETRIEVE_TEST_CODE_COVERAGE = 'retrieve-test-code-coverage';
 export const SFDX_CLI_DOWNLOAD_LINK =
   'https://developer.salesforce.com/tools/sfdxcli';
 export const DEFAULT_USERNAME_KEY = 'defaultusername';
 export const DEFAULT_DEV_HUB_USERNAME_KEY = 'defaultdevhubusername';
-export const CONFLICT_DETECTION_ENABLED = 'detectConflictsAtSync';
-export const BETA_DEPLOY_RETRIEVE = 'experimental.deployRetrieve';
 export const PKG_ID_PREFIX = '04t';
+
+// sfdxCore setting config values
+export const BETA_DEPLOY_RETRIEVE = 'experimental.deployRetrieve';
+export const CONFLICT_DETECTION_ENABLED = 'detectConflictsAtSync';
+export const INTERNAL_DEVELOPMENT_FLAG = 'internal-development';
+export const PUSH_OR_DEPLOY_ON_SAVE_ENABLED = 'push-or-deploy-on-save.enabled';
+export const RETRIEVE_TEST_CODE_COVERAGE = 'retrieve-test-code-coverage';
+export const SFDX_CORE_CONFIGURATION_NAME = 'salesforcedx-vscode-core';
+export const SHOW_CLI_SUCCESS_INFO_MSG = 'show-cli-success-msg';
+export const TELEMETRY_ENABLED = 'telemetry.enabled';
 export const USE_APEX_LIBRARY = 'experimental.useApexLibrary';
