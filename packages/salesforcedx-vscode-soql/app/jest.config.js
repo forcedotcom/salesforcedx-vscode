8150043d3873026e366505b5abf0bff080106f78
Jonny Hork <jhork@salesforce.com>
2020-07-27 15:54:47 -0600

    remove lwc app

--- packages/salesforcedx-vscode-soql/app/jest.config.js
+++
@@ -1,9 +0,0 @@
-// You only need this file
-// - if you want to customize your Jest environment
-// - if you want to use Jest i. e. from a Visual Studio Code extension
-const { jestConfig } = require('lwc-services/lib/config/jestConfig');
-
-module.exports = {
-    ...jestConfig
-    // Add your custom Jest configuration
-};
--- packages/salesforcedx-vscode-soql/app/src/index.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import { createElement } from 'lwc';
-// @ts-ignore
-import App from 'qb/app';
-
-const app = createElement('qb-app', { is: App });
-// @ts-ignore
-document.querySelector('#main').appendChild(app);
