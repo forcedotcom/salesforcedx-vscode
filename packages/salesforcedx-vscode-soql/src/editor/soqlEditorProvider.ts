ec4831704209de2a588f4141bc3a0117e4db71da
Dehru Cromer <dehru.cromer@salesforce.com>
2020-08-07 15:55:44 -0600

    update script tags dynamically

--- packages/salesforcedx-vscode-soql/src/editor/soqlEditorProvider.ts
+++ packages/salesforcedx-vscode-soql/src/editor/soqlEditorProvider.ts
@@ -50,12 +50,30 @@ export class SOQLEditorProvider implements vscode.CustomTextEditorProvider {
     );
     const pathToHtml = path.join(pathToLwcDist, 'index.html');
     let html = fs.readFileSync(pathToHtml).toString();
-    const scriptUri = webview.asWebviewUri(
-      vscode.Uri.file(path.join(pathToLwcDist, 'app.js'))
-    );
-    const zeroDotScriptUri = webview.asWebviewUri(
-      vscode.Uri.file(path.join(pathToLwcDist, '0.app.js'))
-    );
+    /**
+     * This section replaces the relative file paths that are produced by
+     * webpack in the build in the dist folder with the protocol that
+     * vscode uses internally.
+     *
+     * <script src="./app.js"> becomes <script src="vscode-webview-resource:app.js">
+     *
+     * Since we don't know how many bundles webpack will produce, we regex match and
+     * replace them in a while loop.
+     */
+    const rgx = /script\ssrc=\"\.\/(?<app>[^\"]*app.js)\"/g;
+    let matches;
+    let newScriptSrc;
+    // tslint:disable-next-line:no-conditional-assignment
+    while ((matches = rgx.exec(html)) !== null) {
+      newScriptSrc = webview.asWebviewUri(
+        vscode.Uri.file(path.join(pathToLwcDist, matches[1]))
+      );
+      html = html.replace(`./${matches[1]}`, newScriptSrc.toString());
+    }
+
+    /**
+     * The content security policy for running in vscode.
+     */
     const cspMetaTag = `<meta
       http-equiv="Content-Security-Policy"
       content="default-src 'none';
@@ -65,9 +83,6 @@ export class SOQLEditorProvider implements vscode.CustomTextEditorProvider {
     />`;
 
     html = html.replace('<!-- CSP TAG -->', cspMetaTag);
-    html = html.replace('./0.app.js', `${zeroDotScriptUri}`);
-    html = html.replace('./app.js', `${scriptUri}`);
-
     return html;
   }
 }
