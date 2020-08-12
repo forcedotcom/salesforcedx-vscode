840b2f4adf83edff540f007dff00af4dfdb5cf74
Shelby Holden <shelby.holden@salesforce.com>
2020-08-12 14:02:19 -0600

    Adding slack integration and publish version control

--- packages/salesforcedx-apex/scripts/publish-workflow.sh
+++ packages/salesforcedx-apex/scripts/publish-workflow.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
+
+# For publishing with a different version type, add a parameter like the example below:
+# "publish-type": "minor"
+
 CircleCIToken=$1
+PublishType=$2
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "main",
   "parameters": {
-    "publish": true
+    "publish": true,
+    "publish-type": "'"${PublishType}"'"
   }
 }' https://circleci.com/api/v2/project/gh/forcedotcom/salesforcedx-apex/pipeline
+
+# open the release pipe line url
+open "https://circleci.com/api/v2/project/gh/forcedotcom/salesforcedx-apex/pipeline"
