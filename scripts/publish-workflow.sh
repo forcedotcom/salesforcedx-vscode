bf2a372a50d09205690347ab52685eb13114b0be
Shelby Holden <45604118+sfsholden@users.noreply.github.com>
2020-08-27 11:40:17 -0600

    chore: Update scripts to use main instead of master (#2457)

--- scripts/publish-workflow.sh
+++ scripts/publish-workflow.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 CircleCIToken=$1
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
-  "branch": "master",
+  "branch": "main",
   "parameters": {
     "publish": true
   }
