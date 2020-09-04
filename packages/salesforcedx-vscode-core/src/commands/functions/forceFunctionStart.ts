158adec8b6707f71cf425fedcd41e488b44a821b
Xiaoyi Chen <xchen@salesforce.com>
2020-09-03 17:37:39 -0700

    feat: scaffolding function start/stop/invoke commands

---
+++ packages/salesforcedx-vscode-core/src/commands/functions/forceFunctionStart.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import {
+  Command,
+  SfdxCommandBuilder
+} from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
+import { Uri } from 'vscode';
+import { nls } from '../../messages';
+import {
+  CommandletExecutor,
+  FilePathGatherer,
+  SfdxCommandlet,
+  SfdxCommandletExecutor,
+  SfdxWorkspaceChecker
+} from '../util';
+
+export class ForceFunctionStart extends SfdxCommandletExecutor<string> {
+  public build(sourceFsPath: string): Command {
+    this.executionCwd = sourceFsPath;
+    return new SfdxCommandBuilder()
+      .withDescription(nls.localize('force_function_start_text'))
+      .withArg('evergreen:function:start')
+      .withLogName('force_function_start')
+      .build();
+  }
+}
+
+/**
+ * Executes sfdx evergreen:function:start --verbose
+ * @param sourceUri
+ */
+export async function forceFunctionStart(sourceUri: Uri) {}
