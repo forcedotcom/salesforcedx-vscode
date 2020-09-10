792e889b97c9c14bb10d2fcd6d458f7b23e35d4c
Marco Zeuli <maaaaarco89@gmail.com>
2020-09-10 15:07:53 +0200

    refactor: add missing copyright header

--- packages/salesforcedx-vscode-core/test/vscode-integration/commands/forceOrgList.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/forceOrgList.test.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2017, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import { expect } from 'chai';
 import { ForceOrgListExecutor } from '../../../src/commands/forceOrgList';
 import { nls } from '../../../src/messages';
