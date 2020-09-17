77172d4238c7122144b2d2c5b52611ea5a5f4fe0
Xiaoyi Chen <xchen@salesforce.com>
2020-09-16 18:44:57 -0700

    Function start/stop commands (#2498)
    * feat: function start/stop impl
    
    * feat: start function from file path
    
    * test: function start tests
    
    * test: more tests for functions start
    
    * feat: function stop, add function start to editor/context
    
    * test: function start tests for active editor
    
    * test: function stop tests
    
    * docs: add comments for getFunctionDir
    
    * test: update function invoke codelens provider test
    
    * fix: review feedback, exclude some json files from command context
    
    * refactor: move streamCommandOutputWithoutColor into streamCommandOutput
    
    * refactor: limit getFunctionDir at root workspace path
    
    * docs: text feedback

--- packages/salesforcedx-vscode-core/src/commands/functions/forceFunctionStart.ts
+++ packages/salesforcedx-vscode-core/src/commands/functions/forceFunctionStart.ts
@@ -5,22 +5,35 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import {
+  CliCommandExecutor,
   Command,
   SfdxCommandBuilder
 } from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
-import { Uri } from 'vscode';
+import { ContinueResponse } from '@salesforce/salesforcedx-utils-vscode/out/src/types';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import * as vscode from 'vscode';
+import { channelService } from '../../channels';
 import { nls } from '../../messages';
+import { notificationService, ProgressNotification } from '../../notifications';
+import { taskViewService } from '../../statuses';
+import { telemetryService } from '../../telemetry';
+import { getRootWorkspacePath } from '../../util';
 import {
-  CommandletExecutor,
   FilePathGatherer,
   SfdxCommandlet,
   SfdxCommandletExecutor,
   SfdxWorkspaceChecker
 } from '../util';
 
-export class ForceFunctionStart extends SfdxCommandletExecutor<string> {
-  public build(sourceFsPath: string): Command {
-    this.executionCwd = sourceFsPath;
+import { Uri, window } from 'vscode';
+import { FunctionService } from './functionService';
+
+export class ForceFunctionStartExecutor extends SfdxCommandletExecutor<string> {
+  public build(functionDirPath: string): Command {
+    this.executionCwd = functionDirPath;
     return new SfdxCommandBuilder()
       .withDescription(nls.localize('force_function_start_text'))
       .withArg('evergreen:function:start')
@@ -28,17 +41,119 @@ export class ForceFunctionStart extends SfdxCommandletExecutor<string> {
       .withLogName('force_function_start')
       .build();
   }
+
+  /**
+   * Locate the directory that has function.toml.
+   * If sourceFsPath is the function folder that has function.toml, or a subdirectory
+   * or file within that folder, this method returns the function folder by recursively looking up.
+   * Otherwise, it returns undefined.
+   * @param sourceFsPath path to start function from
+   */
+  public static getFunctionDir(sourceFsPath: string) {
+    let current = fs.lstatSync(sourceFsPath).isDirectory()
+      ? sourceFsPath
+      : path.dirname(sourceFsPath);
+    const { root } = path.parse(sourceFsPath);
+    const rootWorkspacePath = getRootWorkspacePath();
+    while (current !== rootWorkspacePath && current !== root) {
+      const tomlPath = path.join(current, 'function.toml');
+      if (fs.existsSync(tomlPath)) {
+        return current;
+      }
+      current = path.dirname(current);
+    }
+    return undefined;
+  }
+
+  public execute(response: ContinueResponse<string>) {
+    const startTime = process.hrtime();
+    const cancellationTokenSource = new vscode.CancellationTokenSource();
+    const cancellationToken = cancellationTokenSource.token;
+    const sourceFsPath = response.data;
+    const functionDirPath = ForceFunctionStartExecutor.getFunctionDir(
+      sourceFsPath
+    );
+    if (!functionDirPath) {
+      notificationService.showWarningMessage(
+        nls.localize('force_function_start_warning_no_toml')
+      );
+      telemetryService.sendException(
+        'force_function_start',
+        'force_function_start_no_toml'
+      );
+      return;
+    }
+    const execution = new CliCommandExecutor(this.build(functionDirPath), {
+      cwd: this.executionCwd,
+      env: { SFDX_JSON_TO_STDOUT: 'true' }
+    }).execute(cancellationToken);
+    const executionName = execution.command.toString();
+
+    FunctionService.instance.registerStartedFunction({
+      terminate: () => {
+        return execution.killExecution('SIGTERM');
+      }
+    });
+
+    channelService.streamCommandOutput(execution);
+    channelService.showChannelOutput();
+
+    const progress = new Subject();
+    ProgressNotification.show(
+      execution,
+      cancellationTokenSource,
+      vscode.ProgressLocation.Notification,
+      progress.asObservable() as Observable<number>
+    );
+    const task = taskViewService.addCommandExecution(
+      execution,
+      cancellationTokenSource
+    );
+
+    execution.stdoutSubject.subscribe(data => {
+      if (data.toString().includes('Ready to process signals')) {
+        progress.complete();
+        taskViewService.removeTask(task);
+        notificationService
+          .showSuccessfulExecution(executionName)
+          .catch(() => {});
+        this.logMetric(execution.command.logName, startTime);
+      }
+    });
+
+    execution.processExitSubject.subscribe(async exitCode => {});
+
+    notificationService.reportCommandExecutionStatus(
+      execution,
+      cancellationToken
+    );
+  }
 }
 
 /**
  * Executes sfdx evergreen:function:start --verbose
  * @param sourceUri
  */
-export async function forceFunctionStart(sourceUri: Uri) {
+export async function forceFunctionStart(sourceUri?: Uri) {
+  if (!sourceUri) {
+    // Try to start function from current active editor, if running SFDX: start function from command palette
+    sourceUri = window.activeTextEditor?.document.uri!;
+  }
+  if (!sourceUri) {
+    notificationService.showWarningMessage(
+      nls.localize('force_function_start_warning_not_in_function_folder')
+    );
+    telemetryService.sendException(
+      'force_function_start',
+      'force_function_start_not_in_function_folder'
+    );
+    return;
+  }
+
   const commandlet = new SfdxCommandlet(
     new SfdxWorkspaceChecker(),
     new FilePathGatherer(sourceUri),
-    new ForceFunctionStart()
+    new ForceFunctionStartExecutor()
   );
   await commandlet.run();
 }
