43a3bc6b7a3a6a4e4c9cc4cae56c497855194c64
Jonny Hork <jhork@salesforce.com>
2020-10-01 11:26:45 -0600

    feat(soql): add save button to data view

--- packages/salesforcedx-vscode-soql/src/queryResultsView/queryDataHtml.ts
+++ packages/salesforcedx-vscode-soql/src/queryResultsView/queryDataHtml.ts
@@ -28,10 +28,13 @@ export function html(assets: { [index: string]: Uri }): string {
   </head>
   <body>
     <div>
-      <div>
-        <h3 id="webview-title"></h3>
-        <p id="total-records-size"></p>
-      </div>
+      <header>
+        <div>
+          <h3 id="webview-title"></h3>
+          <p id="total-records-size"></p>
+        </div>
+        <button class="save-btn">Save Records (csv)</button>
+      </header>
       <div id="data-table"></div>
     </div>
   </body>
