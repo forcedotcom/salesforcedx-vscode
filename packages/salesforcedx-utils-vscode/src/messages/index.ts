af2ef94f8f46a11a577875a892b6726db96aae8e
Luis Campos <lcamposguajardo@gmail.com>
2020-10-12 14:38:25 -0700

    fix: add messages module to utils package

---
+++ packages/salesforcedx-utils-vscode/src/messages/index.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import {
+  BASE_FILE_EXTENSION,
+  BASE_FILE_NAME,
+  Config,
+  DEFAULT_LOCALE,
+  Localization,
+  Message
+} from '../i18n';
+import * as i18n from './i18n';
+
+function loadMessageBundle(config?: Config): Message {
+  function resolveFileName(locale: string): string {
+    return locale === DEFAULT_LOCALE
+      ? `${BASE_FILE_NAME}.${BASE_FILE_EXTENSION}`
+      : `${BASE_FILE_NAME}.${locale}.${BASE_FILE_EXTENSION}`;
+  }
+
+  const base = new Message(i18n.messages);
+
+  if (config && config.locale && config.locale !== DEFAULT_LOCALE) {
+    try {
+      const layer = new Message(
+        require(`./${resolveFileName(config.locale)}`).messages,
+        base
+      );
+      return layer;
+    } catch (e) {
+      console.error(`Cannot find ${config.locale}, defaulting to en`);
+      return base;
+    }
+  } else {
+    return base;
+  }
+}
+
+function getNlsConfig(): Config | undefined {
+  const procNlsConfig = process.env.VSCODE_NLS_CONFIG;
+  return procNlsConfig ? JSON.parse(procNlsConfig) : null;
+}
+
+export const nls = new Localization(loadMessageBundle(getNlsConfig()));
