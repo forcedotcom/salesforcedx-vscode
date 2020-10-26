222cea8af3d8fd1d9791320a57ff946f7d7c3fa9
Shelby <shelby.holden@salesforce.com>
2020-10-26 09:42:27 -0600

    chore: modify constants file

--- scripts/change-log-constants.js
+++ scripts/change-log-constants.js
@@ -13,15 +13,9 @@ module.exports = Object.freeze({
   CHANGE_LOG_BRANCH: 'changeLog-v',
   RELEASE_BRANCH_PREFIX: 'origin/release/v',
 
-  // Map Keys
-  PR_NUM: 'PR_NUM',
-  COMMIT: 'COMMIT',
-  MESSAGE: 'MESSAGE',
-  FILES_CHANGED: 'FILES_CHANGED',
-  PACKAGES: 'PACKAGES',
-
   // Regex
   RELEASE_REGEX: new RegExp(/^origin\/release\/v\d{2}\.\d{1,2}\.\d/),
   PR_REGEX: new RegExp(/(\(#\d+\))/),
-  COMMIT_REGEX: new RegExp(/^([\da-zA-Z]+)/)
+  COMMIT_REGEX: new RegExp(/^([\da-zA-Z]+)/),
+  TYPE_REGEX: new RegExp(/([a-zA-Z]+)(?:\([a-zA-Z]+\))?:/)
 });
