f4515a439f737546e740ce8db749ffb6bfca5eec
Xiaoyi Chen <xchen@salesforce.com>
2020-10-29 11:57:02 -0700

    refactor: add notifications in utils (#2660)
    * refactor: add notifications in utils
    
    * refactor: notification i18n text
    
    * test: add tests for progress notification, add i18 text
    
    * refactor: move notificationService to commands
    
    * test: test for notification service
    
    * chore: add missing index exports
    
    * refactor: refactor lwc extension to use utils notification service
    
    * refactor: merge imports

--- packages/salesforcedx-vscode-lwc/src/commands/commandUtils.ts
+++ packages/salesforcedx-vscode-lwc/src/commands/commandUtils.ts
@@ -5,14 +5,17 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { channelService } from '@salesforce/salesforcedx-utils-vscode/out/src/commands';
+import {
+  channelService,
+  notificationService
+} from '@salesforce/salesforcedx-utils-vscode/out/src/commands';
 import * as vscode from 'vscode';
 import { nls } from '../messages';
 
 const sfdxCoreExports = vscode.extensions.getExtension(
   'salesforce.salesforcedx-vscode-core'
 )!.exports;
-const { notificationService, telemetryService } = sfdxCoreExports;
+const { telemetryService } = sfdxCoreExports;
 
 export function showError(e: Error, logName: string, commandName: string) {
   telemetryService.sendException(`${logName}_error`, e.message);
