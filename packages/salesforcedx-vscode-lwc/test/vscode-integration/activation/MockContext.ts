3eb8d521957687150d46bce0ac496e3ac148c760
Luis Campos <lcamposguajardo@gmail.com>
2020-11-09 11:59:58 -0800

    chore: update vscode engine to 1.46.0

--- packages/salesforcedx-vscode-lwc/test/vscode-integration/activation/MockContext.ts
+++ packages/salesforcedx-vscode-lwc/test/vscode-integration/activation/MockContext.ts
@@ -5,7 +5,13 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import * as path from 'path';
-import { ExtensionContext, Memento } from 'vscode';
+import {
+  EnvironmentVariableCollection,
+  EnvironmentVariableMutator,
+  ExtensionContext,
+  Memento,
+  Uri
+} from 'vscode';
 
 class MockMemento implements Memento {
   private telemetryGS: boolean;
@@ -26,10 +32,45 @@ class MockMemento implements Memento {
   }
 }
 
+class MockEnvironmentVariableCollection
+  implements EnvironmentVariableCollection {
+  public persistent = true;
+  public replace(variable: string, value: string): void {
+    throw new Error('Method not implemented.');
+  }
+  public append(variable: string, value: string): void {
+    throw new Error('Method not implemented.');
+  }
+  public prepend(variable: string, value: string): void {
+    throw new Error('Method not implemented.');
+  }
+  public get(variable: string): EnvironmentVariableMutator | undefined {
+    throw new Error('Method not implemented.');
+  }
+  public forEach(
+    callback: (
+      variable: string,
+      mutator: EnvironmentVariableMutator,
+      collection: EnvironmentVariableCollection
+    ) => any,
+    thisArg?: any
+  ): void {
+    throw new Error('Method not implemented.');
+  }
+  public delete(variable: string): void {
+    throw new Error('Method not implemented.');
+  }
+  public clear(): void {
+    throw new Error('Method not implemented.');
+  }
+}
+
 export class MockContext implements ExtensionContext {
   constructor(mm: boolean) {
     this.globalState = new MockMemento(mm);
   }
+  public extensionUri = Uri.parse('file://test');
+  public environmentVariableCollection = new MockEnvironmentVariableCollection();
   public subscriptions: Array<{ dispose(): any }> = [];
   public workspaceState!: Memento;
   public globalState: Memento;
@@ -37,7 +78,7 @@ export class MockContext implements ExtensionContext {
   public globalStoragePath = 'globalStatePath';
   public logPath = 'logPath';
   public asAbsolutePath(relativePath: string): string {
-    return path.join('../../package.json'); // this should point to the src/package.json
+    return path.join('../../../package.json'); // this should point to the src/package.json
   }
   public storagePath: string = 'myStoragePath';
 }
