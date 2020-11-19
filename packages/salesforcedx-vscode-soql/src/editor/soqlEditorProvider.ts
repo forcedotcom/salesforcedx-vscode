182e4e9a7dae44c270d03651aca7b41eec6a63a6
jgellin-sf <55159130+jgellin-sf@users.noreply.github.com>
2020-11-19 17:46:34 -0500

    Remove core extension as dependency on SOQL extension part 1 (#2743)
    * feat(soql): refactor telemetry, channel services
    * fix(soql): channel service = singleton per channel

--- packages/salesforcedx-vscode-soql/src/editor/soqlEditorProvider.ts
+++ packages/salesforcedx-vscode-soql/src/editor/soqlEditorProvider.ts
@@ -14,6 +14,7 @@ import {
   SOQL_BUILDER_UI_PATH,
   SOQL_BUILDER_WEB_ASSETS_PATH
 } from '../constants';
+import { channelService } from '../channel';
 import { HtmlUtils } from './htmlUtils';
 import { SOQLEditorInstance } from './soqlEditorInstance';
 
@@ -23,7 +24,7 @@ const sfdxCoreExtension = vscode.extensions.getExtension(
 const sfdxCoreExports = sfdxCoreExtension
   ? sfdxCoreExtension.exports
   : undefined;
-const { channelService, workspaceContext } = sfdxCoreExports;
+const { workspaceContext } = sfdxCoreExports;
 
 export class SOQLEditorProvider implements vscode.CustomTextEditorProvider {
   public static register(context: vscode.ExtensionContext): vscode.Disposable {
---
+++ packages/salesforcedx-vscode-soql/src/messages/index.ts
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import {
+  BASE_FILE_EXTENSION,
+  BASE_FILE_NAME,
+  Config,
+  DEFAULT_LOCALE,
+  Localization,
+  Message
+} from '@salesforce/salesforcedx-utils-vscode/out/src/i18n';
+
+function loadMessageBundle(config?: Config): Message {
+  function resolveFileName(locale: string): string {
+    return locale === DEFAULT_LOCALE
+      ? `${BASE_FILE_NAME}.${BASE_FILE_EXTENSION}`
+      : `${BASE_FILE_NAME}.${locale}.${BASE_FILE_EXTENSION}`;
+  }
+
+  const base = new Message(
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require(`./${resolveFileName(DEFAULT_LOCALE)}`).messages
+  );
+
+  if (config && config.locale && config.locale !== DEFAULT_LOCALE) {
+    try {
+      const layer = new Message(
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        require(`./${resolveFileName(config.locale)}`).messages,
+        base
+      );
+      return layer;
+    } catch (e) {
+      console.error(`Cannot find ${config.locale}, defaulting to en`);
+      return base;
+    }
+  } else {
+    return base;
+  }
+}
+
+export const nls = new Localization(
+  loadMessageBundle(
+    process.env.VSCODE_NLS_CONFIG
+      ? JSON.parse(process.env.VSCODE_NLS_CONFIG!)
+      : undefined
+  )
+);
