ae676a371c9156ce3b8e29604fefb004d939bcee
Bryan Powell <b.powell@salesforce.com>
2020-12-03 13:45:16 -0800

    feat: deploy manifest using library

--- packages/salesforcedx-vscode-core/test/vscode-integration/commands/forceSourceDeployManifest.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/forceSourceDeployManifest.test.ts
@@ -5,12 +5,26 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import { AuthInfo, Connection } from '@salesforce/core';
+import { MockTestOrgData, testSetup } from '@salesforce/core/lib/testSetup';
+import { ComponentSet, DeployStatus, SourceDeployResult } from '@salesforce/source-deploy-retrieve';
 import { expect } from 'chai';
 import * as path from 'path';
+import { createSandbox, SinonStub } from 'sinon';
+import { channelService } from '../../../src/channels';
 
 import { ForceSourceDeployManifestExecutor } from '../../../src/commands';
+import { LibrarySourceDeployManifestExecutor } from '../../../src/commands/forceSourceDeployManifest';
+import { LibraryDeployResultParser } from '../../../src/commands/util';
+import { workspaceContext } from '../../../src/context';
 
 import { nls } from '../../../src/messages';
+import { notificationService } from '../../../src/notifications';
+import { SfdxPackageDirectories } from '../../../src/sfdxProject';
+import { getRootWorkspacePath } from '../../../src/util';
+
+const env = createSandbox();
+const $$ = testSetup();
 
 describe('Force Source Deploy Using Manifest Option', () => {
   it('Should build the source deploy command', () => {
@@ -23,5 +37,76 @@ describe('Force Source Deploy Using Manifest Option', () => {
     expect(sourceDeployCommand.description).to.equal(
       nls.localize('force_source_deploy_text')
     );
+  });
+
+  describe('Library Beta', () => {
+    const manifestPath = 'package.xml';
+    const packageDirFullPaths = ['p1', 'p2'].map(p => path.join(getRootWorkspacePath(), p));
+    const mockComponents = new ComponentSet([{ fullName: 'Test', type: 'apexclass'}, {fullName: 'Test2', type: 'layout' }]);
+
+    let mockConnection: Connection;
+    let deployStub: SinonStub;
+
+    const executor = new LibrarySourceDeployManifestExecutor();
+
+    beforeEach(async () => {
+      const testData = new MockTestOrgData();
+      $$.setConfigStubContents('AuthInfoConfig', {
+        contents: await testData.getConfig()
+      });
+      mockConnection = await Connection.create({
+        authInfo: await AuthInfo.create({
+          username: testData.username
+        })
+      });
+
+      env.stub(SfdxPackageDirectories, 'getPackageDirectoryFullPaths').resolves(packageDirFullPaths);
+      env.stub(workspaceContext, 'getConnection').resolves(mockConnection);
+      env.stub(ComponentSet, 'fromManifestFile')
+        .withArgs(manifestPath, { resolve: packageDirFullPaths })
+        .returns(mockComponents);
+      deployStub = env
+        .stub(mockComponents, 'deploy')
+        .withArgs(mockConnection);
+    });
+
+    afterEach(() => {
+      env.restore();
+      $$.SANDBOX.restore();
+    });
+
+    it('Should correctly report success', async () => {
+      const deployResult: SourceDeployResult = {
+        id: 'abcd',
+        status: DeployStatus.Succeeded,
+        success: true,
+        components: []
+      };
+      deployStub.resolves(deployResult);
+      const notificationStub = env.stub(notificationService, 'showSuccessfulExecution');
+      const channelServiceStub = env.stub(channelService, 'appendLine');
+
+      await executor.execute({ data: manifestPath, type: 'CONTINUE' });
+
+      expect(notificationStub.calledOnce).to.equal(true);
+      expect(channelServiceStub.calledWith(new LibraryDeployResultParser(deployResult).resultParser(deployResult)));
+    });
+
+    it('Should correctly report failure', async () => {
+      const deployResult: SourceDeployResult = {
+        id: 'abcd',
+        status: DeployStatus.Failed,
+        success: true,
+        components: []
+      };
+      deployStub.resolves(deployResult);
+      const notificationStub = env.stub(notificationService, 'showFailedExecution');
+      const channelServiceStub = env.stub(channelService, 'appendLine');
+
+      await executor.execute({ data: manifestPath, type: 'CONTINUE' });
+
+      expect(notificationStub.calledOnce).to.equal(true);
+      expect(channelServiceStub.calledWith(new LibraryDeployResultParser(deployResult).resultParser(deployResult)));
+    });
   });
 });
