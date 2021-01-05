955944de2c7460e579133659ef3fa725a2b0c13e
Xiaoyi Chen <xchen@salesforce.com>
2021-01-04 16:19:37 -0800

    refactor: update telemetry service index

--- packages/salesforcedx-utils-vscode/src/telemetry/index.ts
+++ packages/salesforcedx-utils-vscode/src/telemetry/index.ts
@@ -4,3 +4,5 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
+export { TelemetryService, TelemetryBuilder, TelemetryData } from './telemetry';
