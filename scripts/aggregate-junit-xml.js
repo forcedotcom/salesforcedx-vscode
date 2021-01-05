1daef5e3d501112e0bedc6bebfe9d2667b8de425
Bryan Powell <b.powell@salesforce.com>
2021-01-05 11:54:17 -0500

    chore: test verification in autobuilds

--- scripts/aggregate-junit-xml.js
+++ scripts/aggregate-junit-xml.js
@@ -1,30 +1,75 @@
 #!/usr/bin/env node
+
 const process = require('process');
 const fs = require('fs-extra');
 const path = require('path');
 const shell = require('shelljs');
 
-const junitFilesToCheck = [
-  'junit-custom.xml',
-  'junit-custom-unitTests.xml',
-  'junit-custom-integrationTests.xml',
-  'junit-custom-vscodeIntegrationTests.xml'
-];
-const currDir = process.cwd();
-const packagesDir = path.join(currDir, 'packages');
-const dirs = fs.readdirSync(packagesDir).filter(function(file) {
-  return fs.statSync(path.join(packagesDir, file)).isDirectory();
-});
+const cwd = process.cwd();
+const packagesDir = path.join(cwd, 'packages');
+const flagsToFiles = {
+  '--vscode-integration': 'junit-custom-vscodeIntegrationTests.xml',
+  '--integration': 'junit-custom-integrationTests.xml',
+  '--unit': 'junit-custom-unitTests.xml',
+  '--system': 'junit-custom.xml',
+}
 
-shell.mkdir(path.join(process.cwd(), 'junit-aggregate'));
-dirs.filter(dir => {
-  for (const junitFile of junitFilesToCheck) {
-    const fullFilePath = path.join(packagesDir, dir, junitFile);
-    if (fs.existsSync(fullFilePath)) {
-      shell.cp(
-        fullFilePath,
-        path.join(process.cwd(), 'junit-aggregate', `${dir}-${junitFile}`)
-      );
+let flags = new Set(process.argv.slice(2));
+if (flags.size === 0) {
+  flags = new Set(Object.keys(flagsToFiles));
+}
+
+// copy junit results to aggregate folder and identify packages missing test results
+shell.mkdir(path.join(cwd, 'junit-aggregate'));
+
+// const missingResults = [];
+const missingResults = {
+  'vscode-integration': [],
+  'integration': [],
+  'unit': [],
+  'system': []
+}
+
+for (const entry of fs.readdirSync(packagesDir)) {
+  const packagePath = path.join(packagesDir, entry);
+  if (fs.statSync(packagePath).isDirectory()) {
+    const testDir = path.join(packagePath, 'test');
+    if (fs.existsSync(testDir) && fs.statSync(testDir).isDirectory()) {
+      for (const testEntry of fs.readdirSync(testDir)) {
+        const flag = `--${testEntry}`;
+        if (flags.has(flag)) {
+          const junitFilePath = path.join(packagePath, flagsToFiles[flag]);
+          if (fs.existsSync(junitFilePath)) {
+            shell.cp(
+              junitFilePath,
+              path.join(cwd, 'junit-aggregate', `${entry}-${flagsToFiles[flag]}`)
+            );
+            foundResults = true;
+          } else {
+            missingResults[testEntry].push(entry);
+          }
+        }
+      }
     }
   }
-});
+}
+
+let missingMessage;
+
+for (const [testType, pkgs] of Object.entries(missingResults)) {
+  if (pkgs.length > 0) {
+    if (!missingMessage) {
+      missingMessage = 'Missing junit results for the following packages:\n';
+    }
+    missingMessage += `\n* ${testType}:`
+    missingMessage = pkgs.reduce((previous, current) => `${previous}\n\t- ${current}`, missingMessage);
+  }
+}
+
+if (missingMessage) {
+  missingMessage += '\n\nPossible Issues:\n\n'
+  missingMessage += "1) Tests in the expected suite categories haven't run yet (unit, integration, etc.)\n";
+  missingMessage += '2) An unexpected test runner or reporter failure while running tests. Sometimes extension activation issues or issues in the tests can silently fail.';
+  console.error(missingMessage);
+  process.exit(1);
+}
