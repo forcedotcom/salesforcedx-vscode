e1ef2f442c9bb8a1494a389326b475aff082882f
jgellin-sf <55159130+jgellin-sf@users.noreply.github.com>
2021-01-12 18:27:54 -0500

    fix(soql): fix local test environment (#2857)
    Co-authored-by: Prakash B <pbalasubramanian@salesforce.com>

--- packages/salesforcedx-vscode-soql/src/index.ts
+++ packages/salesforcedx-vscode-soql/src/index.ts
@@ -5,21 +5,19 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { WorkspaceContextUtil } from '@salesforce/salesforcedx-utils-vscode/out/src/context';
 import * as vscode from 'vscode';
 import { startLanguageClient, stopLanguageClient } from './client/client';
 import { soqlBuilderToggle, soqlOpenNew } from './commands';
 import { SOQLEditorProvider } from './editor/soqlEditorProvider';
 import { QueryDataViewService } from './queryDataView/queryDataViewService';
+import { workspaceContext } from './sfdx';
 import { startTelemetry, stopTelemetry } from './telemetry';
 
-export async function activate(
-  context: vscode.ExtensionContext
-): Promise<void> {
+export async function activate(context: vscode.ExtensionContext): Promise<any> {
   const extensionHRStart = process.hrtime();
   context.subscriptions.push(SOQLEditorProvider.register(context));
   QueryDataViewService.register(context);
-  await WorkspaceContextUtil.getInstance().initialize(context);
+  await workspaceContext.initialize(context);
 
   context.subscriptions.push(
     vscode.commands.registerCommand('soql.builder.open.new', soqlOpenNew)
@@ -31,6 +29,7 @@ export async function activate(
   await startLanguageClient(context);
   startTelemetry(context, extensionHRStart).catch();
   console.log('SOQL Extension Activated');
+  return { workspaceContext };
 }
 
 export function deactivate(): Thenable<void> | undefined {
