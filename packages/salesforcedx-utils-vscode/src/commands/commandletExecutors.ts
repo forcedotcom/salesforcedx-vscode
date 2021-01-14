ab6ddf4bcf7900d7b7efe2e3948e6d0bab0f66d5
Bryan Powell <b.powell@salesforce.com>
2021-01-14 13:42:05 -0500

    Update packages/salesforcedx-utils-vscode/src/commands/commandletExecutors.ts

--- packages/salesforcedx-utils-vscode/src/commands/commandletExecutors.ts
+++ packages/salesforcedx-utils-vscode/src/commands/commandletExecutors.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import * as vscode from 'vscode';
-import { notificationService, ProgressNotification } from '.';
+import { notificationService, ProgressNotification } from './index';
 import { CliCommandExecutor, Command, CommandExecution } from '../cli';
 import {
   Measurements,
