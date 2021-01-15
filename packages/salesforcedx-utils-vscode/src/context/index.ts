f220fbd96be601dc145a0b780a0269734cc8d61b
Ananya <a.jha@salesforce.com>
2021-01-15 17:50:35 -0600

    fix: remove unnecessary change

--- packages/salesforcedx-utils-vscode/src/context/index.ts
+++ packages/salesforcedx-utils-vscode/src/context/index.ts
@@ -4,7 +4,4 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
-import { WorkspaceContextUtil } from './workspaceContextUtil';
-export const workspaceContext = WorkspaceContextUtil.getInstance();
 export { OrgInfo, WorkspaceContextUtil } from './workspaceContextUtil';
