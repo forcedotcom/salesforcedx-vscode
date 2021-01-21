a78808029f7f0fa4ff51b815f23bd1188d258e3d
Xiaoyi Chen <xchen@salesforce.com>
2021-01-21 11:49:43 -0800

    Decouple LWC extension from core extension (#2876)
    * refactor: remove core dependencies in lwc extension
    
    * refactor: refactor tests

--- packages/salesforcedx-vscode-lwc/test/vscode-integration/commands/forceLightningLwcPreview.test.ts
+++ packages/salesforcedx-vscode-lwc/test/vscode-integration/commands/forceLightningLwcPreview.test.ts
@@ -5,6 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import { SfdxCommandlet } from '@salesforce/salesforcedx-utils-vscode/out/src';
 import {
   CliCommandExecution,
   CliCommandExecutor,
@@ -44,10 +45,6 @@ import {
   notificationService
 } from '@salesforce/salesforcedx-utils-vscode/out/src/commands';
 
-const sfdxCoreExports = vscode.extensions.getExtension(
-  'salesforce.salesforcedx-vscode-core'
-)!.exports;
-const { SfdxCommandlet } = sfdxCoreExports;
 const sfdxDeviceListCommand = 'force:lightning:local:device:list';
 const sfdxMobilePreviewCommand = 'force:lightning:lwc:preview';
 const rememberDeviceKey = 'preview.rememberDevice';
