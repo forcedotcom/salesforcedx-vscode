3aa817a48a071592c08d6b21d1122d51e101f68f
Ananya <a.jha@salesforce.com>
2021-02-18 17:00:55 -0800

    fix: tslint

--- packages/salesforcedx-utils-vscode/test/unit/commands/mocks.ts
+++ packages/salesforcedx-utils-vscode/test/unit/commands/mocks.ts
@@ -5,8 +5,8 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { EOL } from "os";
-import { stub } from "sinon";
+import { EOL } from 'os';
+import { stub } from 'sinon';
 
 export class MockChannel {
   public readonly name = 'MockChannel';
