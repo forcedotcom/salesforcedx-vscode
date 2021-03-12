127b88d4a6354db199397893eb6167c0a6814fa6
Jonny Hork <jhork@salesforce.com>
2021-03-12 12:13:04 -0700

    chore: add test project folder

--- packages/salesforcedx-vscode-soql/test/ui-test/soql-ui-smoke.test.ts
+++ packages/salesforcedx-vscode-soql/test/ui-test/soql-ui-smoke.test.ts
@@ -1,15 +1,22 @@
 import { expect } from 'chai';
 import * as path from 'path';
-import { EditorView, InputBox, Workbench } from 'vscode-extension-tester';
+import {
+  EditorView,
+  InputBox,
+  TextEditor,
+  Workbench
+} from 'vscode-extension-tester';
 
 // tslint:disable-next-line:only-arrow-functions
 describe('Hello World SOQL UI Tests', function() {
   this.timeout(55000);
   let editorView: EditorView;
-  const filePath = path.resolve(__dirname, 'example.soql');
+  let editor: TextEditor;
+  const folderPath = path.resolve(__dirname, 'resources', 'sfdx-test-project');
 
   before(async () => {
     editorView = await new EditorView();
+    editor = await new TextEditor(editorView);
   });
 
   after(async () => {
@@ -20,20 +27,19 @@ describe('Hello World SOQL UI Tests', function() {
     await new Promise(res => {
       setTimeout(res, 5000);
     });
-    console.log('====>', filePath);
+    console.log('Folder Path====>', folderPath);
     await editorView.closeAllEditors();
-    await new Workbench().executeCommand('File: Open file...');
+    await new Workbench().executeCommand('Extest: Open Folder');
     const input = await InputBox.create();
     await new Promise(res => setTimeout(res, 5000));
-    await input.setText(filePath);
+    await input.setText(folderPath);
     await new Promise(res => setTimeout(res, 5000));
     await input.confirm();
     await new Promise(res => setTimeout(res, 1000));
 
-    const editor = await editorView.openEditor('example.soql');
-    const title = await editor.getTitle();
-    console.log('HEY JONNY====> ', title);
-    expect(title).equal('example.soql');
+    // await editorView.openEditor('example.soql');
+    // const title = await editor.getTitle();
+    // expect(title).equal('example.soql');
     done();
   });
 });
