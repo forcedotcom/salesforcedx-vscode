78949e87d8d684d8d776ddd04f4cc3e1691ff8d5
Jonny Hork <jhork@salesforce.com>
2021-03-12 11:43:39 -0700

    chore: add start of UI test

--- packages/salesforcedx-vscode-soql/test/ui-test/soql-ui-smoke.test.ts
+++ packages/salesforcedx-vscode-soql/test/ui-test/soql-ui-smoke.test.ts
@@ -1,11 +1,39 @@
 import { expect } from 'chai';
-import { Workbench } from 'vscode-extension-tester';
+import * as path from 'path';
+import { EditorView, InputBox, Workbench } from 'vscode-extension-tester';
 
-describe('Hello World SOQL UI Tests', () => {
-  it('Command shows a notification with the correct text', async () => {
-    const workbench = new Workbench();
-    await workbench.executeCommand('soql.builder.open.new');
+// tslint:disable-next-line:only-arrow-functions
+describe('Hello World SOQL UI Tests', function() {
+  this.timeout(55000);
+  let editorView: EditorView;
+  const filePath = path.resolve(__dirname, 'example.soql');
 
-    expect(true).to.equal(true);
+  before(async () => {
+    editorView = await new EditorView();
+  });
+
+  after(async () => {
+    await editorView.closeAllEditors();
+  });
+
+  it('openEditor works with text editor', async done => {
+    await new Promise(res => {
+      setTimeout(res, 5000);
+    });
+    console.log('====>', filePath);
+    await editorView.closeAllEditors();
+    await new Workbench().executeCommand('File: Open file...');
+    const input = await InputBox.create();
+    await new Promise(res => setTimeout(res, 5000));
+    await input.setText(filePath);
+    await new Promise(res => setTimeout(res, 5000));
+    await input.confirm();
+    await new Promise(res => setTimeout(res, 1000));
+
+    const editor = await editorView.openEditor('example.soql');
+    const title = await editor.getTitle();
+    console.log('HEY JONNY====> ', title);
+    expect(title).equal('example.soql');
+    done();
   });
 });
