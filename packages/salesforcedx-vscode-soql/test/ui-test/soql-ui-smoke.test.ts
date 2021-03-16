956467faf38d7df5dd59d3ef4b6c1029c07d1641
Jonathan Gellin <jgellin@salesforce.com>
2021-03-16 16:12:47 -0400

    chore(soql): webdriver e2e test to check render

--- packages/salesforcedx-vscode-soql/test/ui-test/soql-ui-smoke.test.ts
+++ packages/salesforcedx-vscode-soql/test/ui-test/soql-ui-smoke.test.ts
@@ -1,45 +1,130 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import { expect } from 'chai';
 import * as path from 'path';
 import {
+  By,
+  Editor,
   EditorView,
   InputBox,
   TextEditor,
+  VSBrowser,
+  WebDriver,
+  WebView,
   Workbench
 } from 'vscode-extension-tester';
 
 // tslint:disable-next-line:only-arrow-functions
-describe('Hello World SOQL UI Tests', function() {
+describe('In project folder, SOQL files should', function () {
   this.timeout(55000);
+  let browser: VSBrowser;
+  let driver: WebDriver;
+  let webviewEditor: Editor;
+  let webview: WebView;
   let editorView: EditorView;
   let editor: TextEditor;
-  const folderPath = path.resolve(__dirname, 'resources', 'sfdx-test-project');
+  const folderPath = path.resolve(__dirname, '..', '..', '..', 'test', 'ui-test', 'resources', 'sfdx-test-project') + path.sep;
+  const filename = 'example.soql';
+
+  const openFolder = async (folder: string) => {
+    const workbench = new Workbench();
+    const cmdTitle = 'File: Open...';
+    const commandInput = await workbench.openCommandPrompt();
+    await commandInput.setText('>' + cmdTitle);
+    const qp = await commandInput.findQuickPick(cmdTitle);
+    qp?.click();
+    await pause(500);
+    const input = await InputBox.create();
+    await input.setText(folder);
+    await input.confirm();
+    await pause(3000);
+  }
+
+  const openSoqlFile = async (folder: string, fn: string) => {
+    const workbench = new Workbench();
+    const openEditors = await new EditorView().getOpenEditorTitles();
+    if (!openEditors.find(title => title === fn)) {
+      // not open yet
+      await workbench.executeCommand('File > Open File...');
+      const input = await InputBox.create();
+      await input.setText(path.resolve(folder, fn));
+      await input.confirm();
+      await pause(1000);
+    }
+  };
+
+  const toggleEditor = async (editorTitle: string) => {
+    const editorView = new EditorView();
+    await editorView.openEditor(editorTitle);
+
+    // find toggle action and click
+    const actionToolbar = await editorView.findElement(By.className('editor-actions'));
+    const actions = await actionToolbar.findElements(By.className('codicon'));
+    let toggle = undefined;
+    for (let i = 0; i < actions.length; i++) {
+      const action = actions[i];
+      if (await action.getAttribute('title') === 'Switch Between SOQL Builder and Text Editor') {
+        toggle = action;
+        break;
+      }
+    }
+    expect(toggle).is.not.undefined;
+    await toggle?.click();
+
+    await pause(10000);
+  };
+
+  const pause = async (msec: number) => {
+    await new Promise(res => setTimeout(res, msec));
+  };
 
   before(async () => {
-    editorView = await new EditorView();
-    editor = await new TextEditor(editorView);
+    browser = VSBrowser.instance;
+    driver = browser.driver;
+    await browser.waitForWorkbench();
+    await openFolder(folderPath);
   });
 
   after(async () => {
-    await editorView.closeAllEditors();
+
   });
 
-  it('openEditor works with text editor', async done => {
-    await new Promise(res => {
-      setTimeout(res, 5000);
-    });
-    console.log('Folder Path====>', folderPath);
+  beforeEach(async () => {
+    const editorView = new EditorView();
     await editorView.closeAllEditors();
-    await new Workbench().executeCommand('Extest: Open Folder');
-    const input = await InputBox.create();
-    await new Promise(res => setTimeout(res, 5000));
-    await input.setText(folderPath);
-    await new Promise(res => setTimeout(res, 5000));
-    await input.confirm();
-    await new Promise(res => setTimeout(res, 1000));
-
-    // await editorView.openEditor('example.soql');
-    // const title = await editor.getTitle();
-    // expect(title).equal('example.soql');
-    done();
+    await pause(500);
   });
+
+  it('pass end to end SOQL Builder test', async () => {
+    // everything in one test to save time
+
+    // should open in Text Editor by default
+    await openSoqlFile(folderPath, filename);
+    const textEditor = await new EditorView().openEditor(filename);
+    expect(textEditor.constructor === TextEditor).to.be.true;
+
+    // toggle editor should open in SOQL Builder
+    await toggleEditor(filename);
+    const webviewEditor = await new EditorView().openEditor(filename);
+    expect(webviewEditor.constructor === WebView).to.be.true;
+
+    // UI should render
+    const webview = webviewEditor as WebView;
+    await webview.switchToFrame();
+    const qbApp = await webview.findWebElement(By.css('querybuilder-app'));
+    expect(qbApp).is.not.undefined;
+    // TODO: do we need to check for individual form elements? 
+    //       they are in the shadow DOM and harder to find with webdriver
+    await webview.switchBack();
+
+    // TODO: check query results render
+
+    return Promise.resolve();
+  });
+
 });
