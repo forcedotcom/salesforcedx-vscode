40c7fae2b00c7d070b0ac6a7ccbbe384f9af48f0
Bryan Powell <b.powell@salesforce.com>
2021-04-07 10:35:07 -0500

    fix: better exception handling for type inference errors (#3127)

--- packages/salesforcedx-vscode-core/test/vscode-integration/commands/util/betaDeployRetrieve.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/util/betaDeployRetrieve.test.ts
@@ -10,10 +10,7 @@ import { SourceComponent } from '@salesforce/source-deploy-retrieve';
 import { expect } from 'chai';
 import { createSandbox, SinonStub } from 'sinon';
 import * as vscode from 'vscode';
-import {
-  createComponentCount,
-  useBetaDeployRetrieve
-} from '../../../../src/commands/util';
+import { createComponentCount } from '../../../../src/commands/util';
 import { SfdxCoreSettings } from '../../../../src/settings/sfdxCoreSettings';
 
 const env = createSandbox();
@@ -35,71 +32,6 @@ describe('Deploy/Retrieve Performance Beta Utils', () => {
       []
     )
   ];
-
-  describe('useBetaDeployRetrieve', () => {
-    let settingStub: SinonStub;
-    let registryStub: SinonStub;
-
-    const uriOne = vscode.Uri.file('classes/foo.cls');
-    const uriTwo = vscode.Uri.parse(
-      'channelLayouts/bar.channelLayout-meta.xml'
-    );
-
-    beforeEach(() => {
-      settingStub = env
-        .stub(SfdxCoreSettings.prototype, 'getBetaDeployRetrieve')
-        .returns(true);
-      const wsOne = new ComponentSet();
-      wsOne.add(testComponents[0]);
-      const wsTwo = new ComponentSet();
-      wsTwo.add(testComponents[1]);
-      registryStub = env
-        .stub(ComponentSet.prototype, 'resolveSourceComponents')
-        .withArgs(uriOne.fsPath)
-        .returns(wsOne)
-        .withArgs(uriTwo.fsPath)
-        .returns(wsTwo);
-    });
-
-    afterEach(() => {
-      env.restore();
-    });
-
-    it('should return true when beta configuration is enabled', () => {
-      expect(useBetaDeployRetrieve([uriOne])).to.equal(true);
-      expect(settingStub.calledBefore(registryStub)).to.equal(true);
-    });
-
-    it('should return false when beta configuration is disabled', () => {
-      settingStub.returns(false);
-
-      expect(useBetaDeployRetrieve([uriOne])).to.equal(false);
-      expect(registryStub.notCalled).to.equal(true);
-    });
-
-    it("should return true if a component's type is marked as supported", () => {
-      expect(
-        useBetaDeployRetrieve([uriOne], [registryData.types.apexclass])
-      ).to.equal(true);
-    });
-
-    it("should return false if a component's type is not marked as supported", () => {
-      expect(
-        useBetaDeployRetrieve([uriTwo], [registryData.types.apexclass])
-      ).to.equal(false);
-    });
-
-    it('should return as expected for multiple uris', () => {
-      const permittedTypes = [
-        registryData.types.apexclass,
-        registryData.types.channellayout
-      ];
-      expect(useBetaDeployRetrieve([uriOne, uriTwo], permittedTypes)).to.equal(
-        true
-      );
-      expect(useBetaDeployRetrieve([uriOne, uriTwo], [])).to.equal(false);
-    });
-  });
 
   describe('createComponentCount', () => {
     it('should correctly generate rows for telemetry', () => {
