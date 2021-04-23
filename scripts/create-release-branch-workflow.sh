4aa201def1f80564bec9e17a42e2b761c752902e
Shelby Holden <shelby.holden@salesforce.com>
2021-04-23 11:22:08 -0600

    chore: add circleci configuration

--- scripts/create-release-branch-workflow.sh
+++ scripts/create-release-branch-workflow.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 CircleCIToken=$1
+PublishType=$2
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "develop",
   "parameters": {
-    "create-release": true
+    "create-release": true,
+    "publish-type": "'"${PublishType}"'"
   }
 }' https://circleci.com/api/v2/project/gh/forcedotcom/salesforcedx-vscode/pipeline
