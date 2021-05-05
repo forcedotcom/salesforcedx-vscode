3d634d3ffe5882202335f089ea34a2d5a83c374b
Shelby Holden <shelby.holden@salesforce.com>
2021-05-05 14:40:03 -0600

    chore: simplify

--- scripts/publish-workflow.sh
+++ scripts/publish-workflow.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 CircleCIToken=$1
-ScheduledBuild=$2
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "main",
   "parameters": {
-    "publish": true,
-    "scheduled-build": "'"${ScheduledBuild}"'"
+    "publish": true
   }
 }' https://circleci.com/api/v2/project/gh/forcedotcom/salesforcedx-vscode/pipeline
