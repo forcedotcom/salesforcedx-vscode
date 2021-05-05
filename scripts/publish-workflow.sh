f72b1084e67cd5e50d3bfa40836d6ee97b581236
Shelby Holden <shelby.holden@salesforce.com>
2021-05-05 14:15:30 -0600

    chore: add change case management

--- scripts/publish-workflow.sh
+++ scripts/publish-workflow.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 CircleCIToken=$1
+ScheduledBuild=$2
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "main",
   "parameters": {
-    "publish": true
+    "publish": true,
+    "scheduled-build": "'"${ScheduledBuild}"'"
   }
 }' https://circleci.com/api/v2/project/gh/forcedotcom/salesforcedx-vscode/pipeline
