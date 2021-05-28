ca193c98b86fbd99e8012841137021efa4e103dc
Yousef Haggy <yhaggy@yhaggy-ltmq2wt.internal.salesforce.com>
2021-05-28 08:55:42 -0400

    Add onDidFinishExecutionEvent emitter to LibraryCommandletExecutor

--- packages/salesforcedx-vscode-core/src/commands/functions/forceFunctionInvoke.ts
+++ packages/salesforcedx-vscode-core/src/commands/functions/forceFunctionInvoke.ts
@@ -8,10 +8,6 @@
 /**
  * Executes sfdx run:function --url http://localhost:8080 --payload=@functions/MyFunction/payload.json
  */
-import {
-  Command,
-  SfdxCommandBuilder
-} from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
 import { CancellationToken, Progress, Uri } from 'vscode';
 import { nls } from '../../messages';
 import { notificationService } from '../../notifications';
@@ -27,32 +23,36 @@ import { OrgAuthInfo } from '../../util';
 import { OUTPUT_CHANNEL } from '../../channels';
 
 import { RunFunction } from '@salesforce/functions-core';
-import {
-  streamFunctionCommandOutput,
-  
-} from './functionsCoreHelpers';
+import { streamFunctionCommandOutput } from './functionsCoreHelpers';
 import { LibraryCommandletExecutor } from '@salesforce/salesforcedx-utils-vscode/out/src';
 import { ContinueResponse } from '@salesforce/salesforcedx-utils-vscode/out/src/types';
 export class ForceFunctionInvoke extends LibraryCommandletExecutor<string> {
-  constructor(){
+  constructor() {
     super(
       nls.localize('force_function_invoke_text'),
       'force_function_invoke_library',
       OUTPUT_CHANNEL
     );
   }
-  async run(response: ContinueResponse<string>, progress?: Progress<{ message?: string | undefined; increment?: number | undefined; }>, token?: CancellationToken): Promise<boolean> {
-        const defaultUsername = await OrgAuthInfo.getDefaultUsernameOrAlias(false);
-        const commandName = nls.localize('force_function_invoke_text');
-    
-        const runFunction = new RunFunction();
-        const execution = runFunction.execute({
-          url: 'http://localhost:8080',
-          payload: `${response.data}`,
-          targetusername: defaultUsername
-        })
-        streamFunctionCommandOutput(commandName, runFunction);
-        return await execution;
+  async run(
+    response: ContinueResponse<string>,
+    progress?: Progress<{
+      message?: string | undefined;
+      increment?: number | undefined;
+    }>,
+    token?: CancellationToken
+  ): Promise<boolean> {
+    const defaultUsername = await OrgAuthInfo.getDefaultUsernameOrAlias(false);
+    const commandName = nls.localize('force_function_invoke_text');
+
+    const runFunction = new RunFunction();
+    const execution = runFunction.execute({
+      url: 'http://localhost:8080',
+      payload: `${response.data}`,
+      targetusername: defaultUsername
+    });
+    streamFunctionCommandOutput(commandName, runFunction);
+    return await execution;
   }
 }
 
@@ -87,12 +87,12 @@ export async function forceFunctionDebugInvoke(sourceUri: Uri) {
   await commandlet.run();
 
   if (commandlet.onDidFinishExecution) {
-    commandlet.onDidFinishExecution(async startTime => {
-      await FunctionService.instance.stopDebuggingFunction(localRoot);
-      telemetryService.sendCommandEvent(
-        'force_function_debug_invoke',
-        startTime
-      );
-    });
-  }
+      commandlet.onDidFinishExecution(async startTime => {
+         await FunctionService.instance.stopDebuggingFunction(localRoot);
+        telemetryService.sendCommandEvent(
+          'force_function_debug_invoke',
+          startTime
+        );
+       });
+     }
 }
---
+++ packages/system-tests/assets/lwc-recipes/force-app/main/default/lwc/createComponent/createComponent.js
@@ -0,0 +1,2 @@
+import { LightningElement } from 'lwc';
+export default class createComponent extends LightningElement {}
