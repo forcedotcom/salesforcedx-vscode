be26754e5fa80ba277158bb49d12b2cd6da13d59
Yousef Haggy <yhaggy@yhaggy-ltmq2wt.internal.salesforce.com>
2021-06-02 17:04:49 -0400

    Stop function tests

--- packages/salesforcedx-vscode-core/test/vscode-integration/commands/functions/forceFunctionStop.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/functions/forceFunctionStop.test.ts
@@ -1,132 +1,131 @@
-// /*
-//  * Copyright (c) 2020, salesforce.com, inc.
-//  * All rights reserved.
-//  * Licensed under the BSD 3-Clause license.
-//  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
-//  */
-// import { CliCommandExecutor } from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
-// import * as path from 'path';
-// import { assert, createSandbox, SinonSandbox, SinonStub } from 'sinon';
-// import { Uri } from 'vscode';
-// import { channelService } from '../../../../src/channels';
-// import {
-//   forceFunctionStart,
-//   ForceFunctionStartExecutor
-// } from '../../../../src/commands/functions/forceFunctionStart';
-// import { forceFunctionStop } from '../../../../src/commands/functions/forceFunctionStop';
-// import { nls } from '../../../../src/messages';
-// import { notificationService } from '../../../../src/notifications';
-// import { telemetryService } from '../../../../src/telemetry';
-// import { getRootWorkspacePath } from '../../../../src/util';
-// import { MockExecution } from './mockExecution';
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { StartFunction } from '@salesforce/functions-core';
+import * as path from 'path';
+import { assert, createSandbox, SinonSandbox, SinonStub } from 'sinon';
+import { Uri } from 'vscode';
+import { channelService } from '../../../../src/channels';
+import { forceFunctionStart } from '../../../../src/commands/functions/forceFunctionStart';
+import { forceFunctionStop } from '../../../../src/commands/functions/forceFunctionStop';
+import { nls } from '../../../../src/messages';
+import { notificationService } from '../../../../src/notifications';
+import { telemetryService } from '../../../../src/telemetry';
+import { getRootWorkspacePath } from '../../../../src/util';
 
-// describe('Force Function Stop', () => {
-//   let sandbox: SinonSandbox;
-//   let cliCommandExecutorStub: SinonStub;
-//   const channelServiceStubs: {
-//     [key: string]: SinonStub;
-//   } = {};
-//   const notificationServiceStubs: {
-//     [key: string]: SinonStub;
-//   } = {};
-//   const telemetryServiceStubs: {
-//     [key: string]: SinonStub;
-//   } = {};
-//   let hrtimeStub: SinonStub;
-//   beforeEach(() => {
-//     sandbox = createSandbox();
+describe('Force Function Stop', () => {
+  let sandbox: SinonSandbox;
+  const startFunctionLibraryStub: {
+    [key: string]: SinonStub;
+  } = {};
 
-//     cliCommandExecutorStub = sandbox.stub(
-//       CliCommandExecutor.prototype,
-//       'execute'
-//     );
-//     channelServiceStubs.appendLineStub = sandbox.stub(
-//       channelService,
-//       'appendLine'
-//     );
-//     notificationServiceStubs.showSuccessfulExecutionStub = sandbox.stub(
-//       notificationService,
-//       'showSuccessfulExecution'
-//     );
-//     notificationServiceStubs.showSuccessfulExecutionStub.returns(
-//       Promise.resolve()
-//     );
-//     notificationServiceStubs.showWarningMessageStub = sandbox.stub(
-//       notificationService,
-//       'showWarningMessage'
-//     );
-//     telemetryServiceStubs.sendCommandEventStub = sandbox.stub(
-//       telemetryService,
-//       'sendCommandEvent'
-//     );
-//     hrtimeStub = sandbox.stub(process, 'hrtime');
-//   });
+  const channelServiceStubs: {
+    [key: string]: SinonStub;
+  } = {};
+  const notificationServiceStubs: {
+    [key: string]: SinonStub;
+  } = {};
+  const telemetryServiceStubs: {
+    [key: string]: SinonStub;
+  } = {};
+  let hrtimeStub: SinonStub;
+  beforeEach(() => {
+    sandbox = createSandbox();
 
-//   afterEach(() => {
-//     sandbox.restore();
-//   });
+    startFunctionLibraryStub.executeStub = sandbox.stub(
+      StartFunction.prototype,
+      'execute'
+    );
+    startFunctionLibraryStub.executeStub.returns(true);
+    startFunctionLibraryStub.cancelStub = sandbox.stub(
+      StartFunction.prototype,
+      'cancel'
+    );
+    channelServiceStubs.appendLineStub = sandbox.stub(
+      channelService,
+      'appendLine'
+    );
+    notificationServiceStubs.showSuccessfulExecutionStub = sandbox.stub(
+      notificationService,
+      'showSuccessfulExecution'
+    );
+    notificationServiceStubs.showSuccessfulExecutionStub.returns(
+      Promise.resolve()
+    );
+    notificationServiceStubs.showWarningMessageStub = sandbox.stub(
+      notificationService,
+      'showWarningMessage'
+    );
+    telemetryServiceStubs.sendCommandEventStub = sandbox.stub(
+      telemetryService,
+      'sendCommandEvent'
+    );
+    hrtimeStub = sandbox.stub(process, 'hrtime');
+  });
 
-//   it('Should stop function, show notification and send telemetry', async () => {
-//     const srcUri = Uri.file(
-//       path.join(getRootWorkspacePath(), 'functions', 'demoJavaScriptFunction')
-//     );
-//     const executor = new ForceFunctionStartExecutor();
-//     const mockExecution = new MockExecution(executor.build(srcUri.fsPath));
-//     const killExecutionStub = sandbox.stub(mockExecution, 'killExecution');
-//     cliCommandExecutorStub.returns(mockExecution);
-//     await forceFunctionStart(srcUri);
+  afterEach(() => {
+    sandbox.restore();
+  });
 
-//     const mockStartTime = [1234, 5678];
-//     hrtimeStub.returns(mockStartTime);
-//     await forceFunctionStop();
+  it('Should stop function, show notification and send telemetry', async () => {
+    const srcUri = Uri.file(
+      path.join(getRootWorkspacePath(), 'functions', 'demoJavaScriptFunction')
+    );
 
-//     assert.calledOnce(killExecutionStub);
-//     assert.calledOnce(channelServiceStubs.appendLineStub);
-//     assert.calledWith(
-//       channelServiceStubs.appendLineStub,
-//       nls.localize('force_function_stop_in_progress')
-//     );
-//     assert.calledOnce(notificationServiceStubs.showSuccessfulExecutionStub);
-//     assert.calledWith(
-//       notificationServiceStubs.showSuccessfulExecutionStub,
-//       nls.localize('force_function_stop_text')
-//     );
-//     assert.calledOnce(telemetryServiceStubs.sendCommandEventStub);
-//     assert.calledWith(
-//       telemetryServiceStubs.sendCommandEventStub,
-//       'force_function_stop',
-//       mockStartTime
-//     );
-//   });
+    await forceFunctionStart(srcUri);
 
-//   it('Should show warning message if function is not started', async () => {
-//     await forceFunctionStop();
+    const mockStartTime = [1234, 5678];
+    hrtimeStub.returns(mockStartTime);
+    await forceFunctionStop();
 
-//     assert.calledOnce(notificationServiceStubs.showWarningMessageStub);
-//     assert.calledWith(
-//       notificationServiceStubs.showWarningMessageStub,
-//       nls.localize('force_function_stop_not_started')
-//     );
-//   });
+    assert.calledOnce(startFunctionLibraryStub.cancelStub);
+    assert.calledOnce(channelServiceStubs.appendLineStub);
+    assert.calledWith(
+      channelServiceStubs.appendLineStub,
+      nls.localize('force_function_stop_in_progress')
+    );
+    assert.calledOnce(notificationServiceStubs.showSuccessfulExecutionStub);
+    assert.calledWith(
+      notificationServiceStubs.showSuccessfulExecutionStub,
+      nls.localize('force_function_stop_text')
+    );
+    assert.calledTwice(telemetryServiceStubs.sendCommandEventStub);
+    assert.calledWith(
+      telemetryServiceStubs.sendCommandEventStub,
+      'force_function_stop',
+      mockStartTime
+    );
+  });
 
-//   it('Should show warning message if already stopped function', async () => {
-//     const srcUri = Uri.file(
-//       path.join(getRootWorkspacePath(), 'functions', 'demoJavaScriptFunction')
-//     );
-//     const executor = new ForceFunctionStartExecutor();
-//     const mockExecution = new MockExecution(executor.build(srcUri.fsPath));
-//     cliCommandExecutorStub.returns(mockExecution);
-//     await forceFunctionStart(srcUri);
+  it('Should show warning message if function is not started', async () => {
+    await forceFunctionStop();
 
-//     const mockStartTime = [1234, 5678];
-//     hrtimeStub.returns(mockStartTime);
-//     await forceFunctionStop();
-//     await forceFunctionStop();
+    assert.calledOnce(notificationServiceStubs.showWarningMessageStub);
+    assert.calledWith(
+      notificationServiceStubs.showWarningMessageStub,
+      nls.localize('force_function_stop_not_started')
+    );
+  });
 
-//     assert.calledOnce(notificationServiceStubs.showWarningMessageStub);
-//     assert.calledWith(
-//       notificationServiceStubs.showWarningMessageStub,
-//       nls.localize('force_function_stop_not_started')
-//     );
-//   });
-// });
+  it('Should show warning message if already stopped function', async () => {
+    const srcUri = Uri.file(
+      path.join(getRootWorkspacePath(), 'functions', 'demoJavaScriptFunction')
+    );
+
+    await forceFunctionStart(srcUri);
+
+    const mockStartTime = [1234, 5678];
+    hrtimeStub.returns(mockStartTime);
+    await forceFunctionStop();
+    await forceFunctionStop();
+
+    assert.calledOnce(notificationServiceStubs.showWarningMessageStub);
+    assert.calledWith(
+      notificationServiceStubs.showWarningMessageStub,
+      nls.localize('force_function_stop_not_started')
+    );
+  });
+});
