ab50d9b78e5956ec296a875bec39aa89ee3d6b4e
Max Camp-Oberhauser <c.max@wustl.edu>
2021-06-14 09:06:03 -0600

    fix: reverting source path retrieve for now

--- packages/salesforcedx-vscode-core/src/commands/forceSourceRetrieveSourcePath.ts
+++ packages/salesforcedx-vscode-core/src/commands/forceSourceRetrieveSourcePath.ts
@@ -72,17 +72,7 @@ export class SourcePathChecker implements PostconditionChecker<string> {
           sourcePath
         );
         if (isInSfdxPackageDirectory) {
-          const messages: ConflictDetectionMessages = {
-            warningMessageKey: 'conflict_detect_conflicts_during_deploy',
-            commandHint: input => {
-              return new SfdxCommandBuilder()
-                .withArg('force:source:retrieve')
-                .withFlag('--sourcepath', input)
-                .build()
-                .toString();
-            }
-          };
-          return await new CacheConflictChecker(false, messages).check(inputs);
+          return inputs;
         }
       } catch (error) {
         telemetryService.sendException(
