3d0789c703e9c2ccfeb273cc6053e389e8c374c9
Max Camp-Oberhauser <c.max@wustl.edu>
2021-06-25 14:41:32 -0600

    fix: unlock deploy queue when conflcits detected

--- packages/salesforcedx-vscode-core/src/commands/util/postconditionCheckers.ts
+++ packages/salesforcedx-vscode-core/src/commands/util/postconditionCheckers.ts
@@ -24,7 +24,7 @@ import { TimestampConflictDetector } from '../../conflict/timestampConflictDetec
 import { workspaceContext } from '../../context';
 import { nls } from '../../messages';
 import { notificationService } from '../../notifications';
-import { sfdxCoreSettings } from '../../settings';
+import { DeployQueue, sfdxCoreSettings } from '../../settings';
 import { telemetryService } from '../../telemetry';
 import { getRootWorkspacePath, MetadataDictionary } from '../../util';
 import { PathStrategyFactory } from './sourcePathStrategies';
@@ -406,6 +406,7 @@ export class TimestampConflictChecker implements PostconditionChecker<string> {
           results
         );
 
+        await DeployQueue.get().unlock();
         return { type: 'CANCEL' };
       }
     }
