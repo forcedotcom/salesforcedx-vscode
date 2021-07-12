27a5177f3221efcd4585eb4d332c153e52d9c9c6
Violet Yao <vyao@salesforce.com>
2021-07-12 10:28:24 -0700

    fix: typo

--- packages/salesforcedx-vscode-apex-replay-debugger/src/breakpoints/index.ts
+++ packages/salesforcedx-vscode-apex-replay-debugger/src/breakpoints/index.ts
@@ -5,4 +5,4 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-export { sfdxCreateCheckpoints } from './checkPointService';
+export { sfdxCreateCheckpoints } from './checkpointService';
