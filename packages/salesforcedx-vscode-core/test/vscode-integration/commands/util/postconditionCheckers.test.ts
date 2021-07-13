fc61ebd7e579dd507c5a4123c5feceb3ce2aee82
Max Camp-Oberhauser <c.max@wustl.edu>
2021-07-13 17:34:03 -0600

    chore: fix import

--- packages/salesforcedx-vscode-core/test/vscode-integration/commands/util/postconditionCheckers.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/util/postconditionCheckers.test.ts
@@ -28,7 +28,6 @@ import {
   TimestampConflictChecker
 } from '../../../../src/commands/util/postconditionCheckers';
 import {
-  conflictDetector,
   conflictView,
   DirectoryDiffResults
 } from '../../../../src/conflict';
