71e8bebbaab471a0d10f5f5b85fcb591a349a025
Xiaoyi Chen <xchen@salesforce.com>
2021-09-28 16:27:21 -0700

    feat: custom templates for vscode

--- packages/salesforcedx-vscode-core/test/vscode-integration/commands/templates/forceApexClassCreate.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/templates/forceApexClassCreate.test.ts
@@ -18,7 +18,10 @@ import { forceApexClassCreate } from '../../../../src/commands/templates/forceAp
 import { nls } from '../../../../src/messages';
 import { notificationService } from '../../../../src/notifications';
 import { telemetryService } from '../../../../src/telemetry';
-import { getRootWorkspacePath } from '../../../../src/util';
+import { ConfigUtil, getRootWorkspacePath } from '../../../../src/util';
+
+const TEST_CUSTOM_TEMPLATES_REPO =
+  'https://github.com/forcedotcom/salesforcedx-templates/tree/develop/packages/templates/test/custom-templates';
 
 // tslint:disable:no-unused-expression
 describe('Force Apex Class Create', () => {
@@ -30,6 +33,7 @@ describe('Force Apex Class Create', () => {
   let openTextDocumentStub: SinonStub;
   let sendCommandEventStub: SinonStub;
   let sendExceptionStub: SinonStub;
+  let getConfigValue: SinonStub;
 
   beforeEach(() => {
     showInputBoxStub = stub(vscode.window, 'showInputBox');
@@ -44,6 +48,8 @@ describe('Force Apex Class Create', () => {
     openTextDocumentStub = stub(vscode.workspace, 'openTextDocument');
     sendCommandEventStub = stub(telemetryService, 'sendCommandEvent');
     sendExceptionStub = stub(telemetryService, 'sendException');
+    getConfigValue = stub(ConfigUtil, 'getConfigValue');
+    getConfigValue.returns(undefined);
   });
 
   afterEach(() => {
@@ -55,6 +61,7 @@ describe('Force Apex Class Create', () => {
     openTextDocumentStub.restore();
     sendCommandEventStub.restore();
     sendExceptionStub.restore();
+    getConfigValue.restore();
   });
 
   it('Should create Apex Class', async () => {
@@ -138,4 +145,62 @@ describe('Force Apex Class Create', () => {
       errorMessage
     );
   });
+
+  it('Should create Apex Class with custom templates', async () => {
+    // arrange
+    getConfigValue.returns(TEST_CUSTOM_TEMPLATES_REPO);
+    const outputPath = 'force-app/main/default/classes';
+    const apexClassPath = path.join(
+      getRootWorkspacePath(),
+      outputPath,
+      'TestApexClass.cls'
+    );
+    const apexClassMetaPath = path.join(
+      getRootWorkspacePath(),
+      outputPath,
+      'TestApexClass.cls-meta.xml'
+    );
+    shell.rm('-f', apexClassPath);
+    shell.rm('-f', apexClassMetaPath);
+    assert.noFile([apexClassPath, apexClassMetaPath]);
+    showInputBoxStub.returns('TestApexClass');
+    quickPickStub.returns(outputPath);
+
+    // act
+    await forceApexClassCreate();
+
+    // assert
+    const defaultApiVersion = TemplateService.getDefaultApiVersion();
+    assert.file([apexClassPath, apexClassMetaPath]);
+    assert.fileContent(
+      apexClassPath,
+      'public with sharing class CustomTestApexClass'
+    );
+    assert.fileContent(
+      apexClassMetaPath,
+      `<?xml version="1.0" encoding="UTF-8"?>
+<ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">
+    <apiVersion>${defaultApiVersion}</apiVersion>
+    <status>Inactive</status>
+</ApexClass>`
+    );
+    sinon.assert.calledOnce(openTextDocumentStub);
+    sinon.assert.calledWith(openTextDocumentStub, apexClassPath);
+
+    sinon.assert.calledOnce(sendCommandEventStub);
+    sinon.assert.calledWith(
+      sendCommandEventStub,
+      'force_apex_class_create',
+      sinon.match.array,
+      {
+        dirType: 'defaultDir',
+        commandExecutor: 'library',
+        isUsingCustomOrgMetadataTemplates: 'true'
+      }
+    );
+
+    // clean up
+    shell.rm('-f', apexClassPath);
+    shell.rm('-f', apexClassMetaPath);
+  }).timeout(20000);
 });
