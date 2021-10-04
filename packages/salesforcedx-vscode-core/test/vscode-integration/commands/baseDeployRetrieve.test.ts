19a951e642577d2b8b8d59433d1e5968fc9dfb3a
jbeeghly <46358907+jeffb-sfdc@users.noreply.github.com>
2021-10-04 09:51:16 -0700

    fix: detect-conflict-at-sync-doesnt-work (#3556)
    * fix: detect-conflict-at-sync-doesnt-work

--- packages/salesforcedx-vscode-core/test/vscode-integration/commands/baseDeployRetrieve.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/baseDeployRetrieve.test.ts
@@ -253,7 +253,6 @@ describe('Base Deploy Retrieve Commands', () => {
       public pollStatusStub: SinonStub;
       public deployStub: SinonStub;
       public cancellationStub = sb.stub();
-      public cacheSpy: SinonSpy;
 
       constructor(toDeploy = new ComponentSet()) {
         super('test', 'testlog');
@@ -262,7 +261,6 @@ describe('Base Deploy Retrieve Commands', () => {
         this.deployStub = sb
           .stub(this.components, 'deploy')
           .returns({ pollStatus: this.pollStatusStub });
-        this.cacheSpy = sb.spy(PersistentStorageService.getInstance(), 'setPropertiesForFilesDeploy');
       }
 
       protected async getComponents(
@@ -299,57 +297,6 @@ describe('Base Deploy Retrieve Commands', () => {
       expect(executor.pollStatusStub.calledOnce).to.equal(true);
     });
 
-    it('should store properties in metadata cache on successful deploy', async () => {
-      const executor = new TestDeploy();
-      const deployPropsOne = {
-        name: 'One',
-        fullName: 'One',
-        type: registry.types.apexclass,
-        content: join('project', 'classes', 'One.cls'),
-        xml: join('project', 'classes', 'One.cls-meta.xml')
-      };
-      const deployComponentOne = SourceComponent.createVirtualComponent(deployPropsOne,
-        [{
-          dirPath: dirname(deployPropsOne.content),
-          children: [basename(deployPropsOne.content), basename(deployPropsOne.xml)]
-        }
-      ]);
-      const deployPropsTwo = {
-        name: 'Two',
-        fullName: 'Two',
-        type: registry.types.customobject,
-        content: join('project', 'classes', 'Two.cls'),
-        xml: join('project', 'classes', 'Two.cls-meta.xml')
-      };
-      const deployComponentTwo = SourceComponent.createVirtualComponent(deployPropsTwo,
-        [{
-          dirPath: dirname(deployPropsTwo.content),
-          children: [basename(deployPropsTwo.content), basename(deployPropsTwo.xml)]
-        }
-      ]);
-      const mockDeployResult = new DeployResult(
-        {
-          status: RequestStatus.Succeeded,
-          lastModifiedDate: 'Yesterday'
-        } as MetadataApiDeployStatus,
-        new ComponentSet([
-          deployComponentOne,
-          deployComponentTwo
-        ])
-      );
-      const fileResponses: any[] = [];
-      const cache = PersistentStorageService.getInstance();
-      sb.stub(mockDeployResult, 'getFileResponses').returns(fileResponses);
-      executor.pollStatusStub.resolves(mockDeployResult);
-
-      await executor.run({data: {}, type: 'CONTINUE' });
-
-      expect(executor.cacheSpy.callCount).to.equal(1);
-      expect(executor.cacheSpy.args[0][0].components.size).to.equal(2);
-      expect(cache.getPropertiesForFile(cache.makeKey('ApexClass', 'One'))?.lastModifiedDate).to.equal('Yesterday');
-      expect(cache.getPropertiesForFile(cache.makeKey('CustomObject', 'Two'))?.lastModifiedDate).to.equal('Yesterday');
-    });
-
     it('should not store any properties in metadata cache on failed deploy', async () => {
       const executor = new TestDeploy();
       const mockDeployResult = new DeployResult(
@@ -364,8 +311,6 @@ describe('Base Deploy Retrieve Commands', () => {
       const success = await executor.run({ data: {}, type: 'CONTINUE' });
 
       expect(success).to.equal(false);
-      expect(executor.cacheSpy.callCount).to.equal(1);
-      expect(executor.cacheSpy.args[0][0].components.size).to.equal(0);
     });
 
     describe('Result Output', () => {
