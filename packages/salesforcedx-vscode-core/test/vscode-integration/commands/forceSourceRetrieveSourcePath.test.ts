07b7278e3b104add69bff8261a7ecd50d6985c5d
Jeff Beeghly <jbeeghly@salesforce.com>
2021-11-12 19:14:08 -0800

    feat: updated integration tests

--- packages/salesforcedx-vscode-core/test/vscode-integration/commands/forceSourceRetrieveSourcePath.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/forceSourceRetrieveSourcePath.test.ts
@@ -8,6 +8,7 @@
 import { AuthInfo, Connection } from '@salesforce/core';
 import { MockTestOrgData, testSetup } from '@salesforce/core/lib/testSetup';
 import {
+  CancelResponse,
   ContinueResponse
 } from '@salesforce/salesforcedx-utils-vscode/out/src/types/index';
 import {
@@ -20,7 +21,10 @@ import * as path from 'path';
 import { createSandbox, SinonSandbox, SinonStub } from 'sinon';
 import * as vscode from 'vscode';
 import { channelService } from '../../../src/channels';
-import { LibraryRetrieveSourcePathExecutor } from '../../../src/commands';
+import {
+  LibraryRetrieveSourcePathExecutor,
+  SourcePathChecker
+} from '../../../src/commands';
 import * as forceSourceRetrieveSourcePath from '../../../src/commands/forceSourceRetrieveSourcePath';
 import { workspaceContext } from '../../../src/context';
 import { nls } from '../../../src/messages';
@@ -214,16 +218,16 @@ describe('SourcePathChecker', () => {
     const isInPackageDirectoryStub = sandboxStub
       .stub(SfdxPackageDirectories, 'isInPackageDirectory')
       .returns(true);
-    // const pathChecker = new SourcePathChecker();
+    const pathChecker = new SourcePathChecker();
     const sourcePath = path.join(workspacePath, 'package');
-    // const continueResponse = (await pathChecker.check({
-    //   type: 'CONTINUE',
-    //   data: sourcePath
-    // })) as ContinueResponse<string>;
+    const continueResponse = (await pathChecker.check({
+      type: 'CONTINUE',
+      data: sourcePath
+    })) as ContinueResponse<string>;
 
     expect(isInPackageDirectoryStub.getCall(0).args[0]).to.equal(sourcePath);
-    // expect(continueResponse.type).to.equal('CONTINUE');
-    // expect(continueResponse.data).to.equal(sourcePath);
+    expect(continueResponse.type).to.equal('CONTINUE');
+    expect(continueResponse.data).to.equal(sourcePath);
 
     isInPackageDirectoryStub.restore();
   });
@@ -232,18 +236,18 @@ describe('SourcePathChecker', () => {
     const isInPackageDirectoryStub = sandboxStub
       .stub(SfdxPackageDirectories, 'isInPackageDirectory')
       .returns(false);
-    // const pathChecker = new SourcePathChecker();
-    // const cancelResponse = (await pathChecker.check({
-    //   type: 'CONTINUE',
-    //   data: path.join('not', 'in', 'package', 'directory')
-    // })) as CancelResponse;
+    const pathChecker = new SourcePathChecker();
+    const cancelResponse = (await pathChecker.check({
+      type: 'CONTINUE',
+      data: path.join('not', 'in', 'package', 'directory')
+    })) as CancelResponse;
 
     const errorMessage = nls.localize(
       'error_source_path_not_in_package_directory_text'
     );
     expect(appendLineSpy.getCall(0).args[0]).to.equal(errorMessage);
     expect(showErrorMessageSpy.getCall(0).args[0]).to.equal(errorMessage);
-    // expect(cancelResponse.type).to.equal('CANCEL');
+    expect(cancelResponse.type).to.equal('CANCEL');
     isInPackageDirectoryStub.restore();
   });
 
@@ -251,18 +255,18 @@ describe('SourcePathChecker', () => {
     const isInPackageDirectoryStub = sandboxStub
       .stub(SfdxPackageDirectories, 'isInPackageDirectory')
       .throws(new Error());
-    // const pathChecker = new SourcePathChecker();
-    // const cancelResponse = (await pathChecker.check({
-    //   type: 'CONTINUE',
-    //   data: 'test/path'
-    // })) as CancelResponse;
+    const pathChecker = new SourcePathChecker();
+    const cancelResponse = (await pathChecker.check({
+      type: 'CONTINUE',
+      data: 'test/path'
+    })) as CancelResponse;
 
     const errorMessage = nls.localize(
       'error_source_path_not_in_package_directory_text'
     );
     expect(appendLineSpy.getCall(0).args[0]).to.equal(errorMessage);
     expect(showErrorMessageSpy.getCall(0).args[0]).to.equal(errorMessage);
-    // expect(cancelResponse.type).to.equal('CANCEL');
+    expect(cancelResponse.type).to.equal('CANCEL');
     isInPackageDirectoryStub.restore();
   });
 });
