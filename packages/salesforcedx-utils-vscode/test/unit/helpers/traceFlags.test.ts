8ea1e2a11c445046267b29022db47b5c95b899fd
Jeff Beeghly <jbeeghly@salesforce.com>
2022-02-09 09:17:33 -0800

    feat: added unit test

--- packages/salesforcedx-utils-vscode/test/unit/helpers/traceFlags.test.ts
+++ packages/salesforcedx-utils-vscode/test/unit/helpers/traceFlags.test.ts
@@ -155,6 +155,84 @@ describe('Trace Flags', () => {
     expect(expDate.getTime() - currDate).to.be.greaterThan(60000 * 29);
   });
 
+  it('should verify that createTraceFlag() is called when a trace flag doesn\'t exist', async () => {
+    queryStub = sb.stub(mockConnection, 'query');
+    toolingCreateStub = sb.stub(mockConnection.tooling, 'create');
+    toolingQueryStub = sb.stub(mockConnection.tooling, 'query');
+    toolingUpdateStub = sb.stub(mockConnection.tooling, 'update');
+
+    const traceFlags = new TraceFlags(mockConnection);
+    const createTraceFlagSpy = sb.spy(TraceFlags.prototype, 'createTraceFlag');
+
+    queryStub
+      .onFirstCall()
+      .resolves({
+        done: true,
+        totalSize: 1,
+        records: [{ Id: USER_ID }]
+      });
+    toolingQueryStub
+      .onFirstCall()
+      .resolves({
+        done: true,
+        totalSize: 0,
+        records: []
+      });
+    toolingCreateStub
+      .onFirstCall()
+      .resolves({ success: true, id: 'aBcDeF' })
+      .onSecondCall()
+      .resolves({ success: true, id: '01020304' });
+
+    await traceFlags.ensureTraceFlags();
+    expect(createTraceFlagSpy.callCount).to.equal(1);
+  });
+
+  it('should verify that createTraceFlag() is not called when a trace flag exist', async () => {
+    queryStub = sb.stub(mockConnection, 'query');
+    toolingCreateStub = sb.stub(mockConnection.tooling, 'create');
+    toolingQueryStub = sb.stub(mockConnection.tooling, 'query');
+    toolingUpdateStub = sb.stub(mockConnection.tooling, 'update');
+
+    const traceFlags = new TraceFlags(mockConnection);
+    const createTraceFlagSpy = sb.spy(TraceFlags.prototype, 'createTraceFlag');
+
+    queryStub
+      .onFirstCall()
+      .resolves({
+        done: true,
+        totalSize: 1,
+        records: [{ Id: USER_ID }]
+    });
+    toolingQueryStub
+      .onFirstCall()
+      .resolves({
+        done: true,
+        totalSize: 1,
+        records: [
+          {
+            Id: '1234',
+            DebugLevelId: '00A123456',
+            LogType: 'developer_log',
+            StartDate: null,
+            ExpirationDate: null,
+            DebugLevel: {
+              ApexCode: '',
+              VisualForce: ''
+            }
+          }
+        ]
+      });
+    toolingUpdateStub
+      .onFirstCall()
+      .resolves({ success: true })
+      .onSecondCall()
+      .resolves({ success: true });
+
+    await traceFlags.ensureTraceFlags();
+    expect(createTraceFlagSpy.callCount).to.equal(0);
+  });
+
   it('should raise error for missing username', async () => {
     const traceFlags = new TraceFlags(mockConnection);
     sb.stub(mockConnection, 'getUsername').returns(undefined);
