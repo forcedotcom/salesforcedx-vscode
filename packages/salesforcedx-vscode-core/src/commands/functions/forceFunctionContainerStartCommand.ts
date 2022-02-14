6cc1410aea123924c259306b1a6beddc1f52d3ab
Gordon Bockus <gbockus@salesforce.com>
2022-02-14 15:43:04 -0600

    feat: add copyright to all the new files

--- packages/salesforcedx-vscode-core/src/commands/functions/forceFunctionContainerStartCommand.ts
+++ packages/salesforcedx-vscode-core/src/commands/functions/forceFunctionContainerStartCommand.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, salesforce.com, inc.
+ * Copyright (c) 2022, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
