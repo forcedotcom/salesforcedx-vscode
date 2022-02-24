e0c366b74a57b986d481e94b23348c225f430c62
Jeff Beeghly <jbeeghly@salesforce.com>
2022-02-24 13:39:41 -0800

    feat: added support for Java 17

--- packages/salesforcedx-vscode-apex/test/vscode-integration/requirements.test.ts
+++ packages/salesforcedx-vscode-apex/test/vscode-integration/requirements.test.ts
@@ -5,6 +5,8 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+// tslint:disable:no-unused-expression
+
 import { fail } from 'assert';
 import { expect } from 'chai';
 import { createSandbox, SinonSandbox, SinonStub } from 'sinon';
