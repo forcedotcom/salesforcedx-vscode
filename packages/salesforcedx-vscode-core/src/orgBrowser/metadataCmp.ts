f746b1d908aed475ff0e5e9d930151a275be7d03
Ritam Agrawal <ritam.kumari.27@gmail.com>
2022-03-02 16:38:23 -0800

    chore: refactor load components

--- packages/salesforcedx-vscode-core/src/orgBrowser/metadataCmp.ts
+++ packages/salesforcedx-vscode-core/src/orgBrowser/metadataCmp.ts
@@ -157,24 +157,17 @@ export class ComponentUtils {
       defaultOrg,
       folderName
     );
-
     let componentsList: string[];
+    const freshFetch = (forceRefresh || !fs.existsSync(componentsPath));
     const connection = await workspaceContext.getConnection();
-
     if (metadataType === 'CustomObject' && folderName) {
-      if (forceRefresh || !fs.existsSync(componentsPath)) {
+      if (freshFetch) {
         componentsList = await this.fetchCustomObjectsFields(connection, componentsPath, folderName);
       } else {
         componentsList = this.fetchExistingCustomObjectsFields(componentsPath);
       }
-    } else if (metadataType !== 'CustomObject' && folderName) {
-      if (forceRefresh || !fs.existsSync(componentsPath)) {
-        componentsList = await this.fetchMetadataComponents(metadataType, connection, componentsPath, folderName);
-      } else {
-        componentsList = this.fetchExistingMetadataComponents(metadataType, componentsPath);
-      }
     } else {
-      if (forceRefresh || !fs.existsSync(componentsPath)) {
+      if (freshFetch) {
         componentsList = await this.fetchMetadataComponents(metadataType, connection, componentsPath, folderName);
       } else {
         componentsList = this.fetchExistingMetadataComponents(metadataType, componentsPath);
