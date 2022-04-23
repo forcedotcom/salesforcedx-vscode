b27f02c95efb73ff9f30ca1b0c1c1e308c306d44
mohanraj-r <mrajamanickam@salesforce.com>
2022-04-22 18:56:37 -0700

    fix(soql): add tsconfig for webpack / ts-loader

--- packages/salesforcedx-vscode-soql/webpack.config.js
+++ packages/salesforcedx-vscode-soql/webpack.config.js
@@ -47,7 +47,10 @@ module.exports = {
         ],
         use: [
           {
-            loader: 'ts-loader'
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.webpack.json'
+            }
           }
         ]
       }
