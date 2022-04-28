b5c528c98551c3f16bb2c59fd02c1c3e84d766fb
Jonny Hork <jhork@salesforce.com>
2022-04-28 14:41:33 -0600

    feat: open browser window for device auth flow

--- packages/salesforcedx-vscode-core/src/commands/auth/forceAuthWebLogin.ts
+++ packages/salesforcedx-vscode-core/src/commands/auth/forceAuthWebLogin.ts
@@ -13,6 +13,7 @@ import { CommandOutput } from '@salesforce/salesforcedx-utils-vscode/out/src/cli
 import { CliCommandExecutor } from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
 import { ContinueResponse } from '@salesforce/salesforcedx-utils-vscode/out/src/types';
 import { Observable } from 'rxjs/Observable';
+import * as vscode from 'vscode';
 import { CancellationTokenSource } from 'vscode';
 import { channelService } from '../../channels/index';
 import { nls } from '../../messages';
@@ -22,6 +23,7 @@ import {
   ProgressNotification
 } from '../../notifications/index';
 import { taskViewService } from '../../statuses/index';
+import { telemetryService } from '../../telemetry';
 import { getRootWorkspacePath, isSFDXContainerMode } from '../../util';
 import {
   DemoModePromptGatherer,
@@ -29,32 +31,118 @@ import {
   SfdxCommandletExecutor,
   SfdxWorkspaceChecker
 } from '../util';
+import { AuthParams, AuthParamsGatherer } from './authParamsGatherer';
 import { ForceAuthLogoutAll } from './forceAuthLogout';
 
-import { AuthParams, AuthParamsGatherer } from './authParamsGatherer';
+interface DeviceCodeResponse {
+  user_code: string;
+  device_code: string;
+  interval: number;
+  verification_uri: string;
+}
 
-export class ForceAuthWebLoginExecutor extends SfdxCommandletExecutor<
+export class ForceAuthWebLoginContainerExecutor extends SfdxCommandletExecutor<
   AuthParams
 > {
-  protected showChannelOutput = isSFDXContainerMode();
+  protected showChannelOutput = false;
 
   public build(data: AuthParams): Command {
     const command = new SfdxCommandBuilder().withDescription(
       nls.localize('force_auth_web_login_authorize_org_text')
     );
-    if (isSFDXContainerMode()) {
-      command
-        .withArg('force:auth:device:login')
-        .withLogName('force_auth_device_login');
-    } else {
-      command
-        .withArg('force:auth:web:login')
-        .withLogName('force_auth_web_login');
-    }
+
     command
+      .withArg('force:auth:device:login')
+      .withLogName('force_auth_device_login')
+      .withFlag('--setalias', data.alias)
+      .withFlag('--instanceurl', data.loginUrl)
+      .withArg('--setdefaultusername')
+      .withJson();
+
+    return command.build();
+  }
+
+  public execute(response: ContinueResponse<AuthParams>): void {
+    const startTime = process.hrtime();
+    const cancellationTokenSource = new vscode.CancellationTokenSource();
+    const cancellationToken = cancellationTokenSource.token;
+    const execution = new CliCommandExecutor(this.build(response.data), {
+      cwd: getRootWorkspacePath(),
+      env: { SFDX_JSON_TO_STDOUT: 'true' }
+    }).execute(cancellationToken);
+    let deviceCodeReceived = false;
+
+    channelService.streamCommandStartStop(execution);
+
+    let stdOut = '';
+    execution.stdoutSubject.subscribe(cliResponse => {
+      stdOut += cliResponse.toString();
+
+      if (!deviceCodeReceived) {
+        const authUrl = this.parseAuthUrlFromStdOut(stdOut);
+
+        if (authUrl) {
+          deviceCodeReceived = true;
+          // open the default browser
+          vscode.env.openExternal(vscode.Uri.parse(authUrl, true));
+        }
+      }
+    });
+
+    execution.processExitSubject.subscribe(() => {
+      this.logMetric(execution.command.logName, startTime);
+    });
+
+    notificationService.reportCommandExecutionStatus(
+      execution,
+      cancellationToken
+    );
+
+    ProgressNotification.show(execution, cancellationTokenSource);
+    taskViewService.addCommandExecution(execution, cancellationTokenSource);
+  }
+
+  private parseAuthUrlFromStdOut(stdOut: string): string | undefined {
+    let authUrl;
+    try {
+      const response = JSON.parse(stdOut) as DeviceCodeResponse;
+      const verificationUrl = response.verification_uri;
+      const userCode = response.user_code;
+
+      if (verificationUrl && userCode) {
+        authUrl = `${verificationUrl}?user_code=${userCode}`;
+      }
+    } catch (error) {
+      channelService.appendLine(
+        nls.localize('force_auth_web_login_device_code_parse_error')
+      );
+      telemetryService.sendException(
+        'force_auth_web_container',
+        `There was an error when parsing the cli response ${error}`
+      );
+    }
+
+    return authUrl;
+  }
+}
+
+export class ForceAuthWebLoginExecutor extends SfdxCommandletExecutor<
+  AuthParams
+> {
+  protected showChannelOutput = false;
+
+  public build(data: AuthParams): Command {
+    const command = new SfdxCommandBuilder().withDescription(
+      nls.localize('force_auth_web_login_authorize_org_text')
+    );
+
+    command
+      .withArg('force:auth:web:login')
+      .withLogName('force_auth_web_login')
       .withFlag('--setalias', data.alias)
       .withFlag('--instanceurl', data.loginUrl)
       .withArg('--setdefaultusername');
+
     return command.build();
   }
 }
@@ -129,9 +217,14 @@ const workspaceChecker = new SfdxWorkspaceChecker();
 const parameterGatherer = new AuthParamsGatherer();
 
 export function createAuthWebLoginExecutor(): SfdxCommandletExecutor<{}> {
-  return isDemoMode()
-    ? new ForceAuthWebLoginDemoModeExecutor()
-    : new ForceAuthWebLoginExecutor();
+  switch (true) {
+    case isSFDXContainerMode():
+      return new ForceAuthWebLoginContainerExecutor();
+    case isDemoMode():
+      return new ForceAuthWebLoginDemoModeExecutor();
+    default:
+      return new ForceAuthWebLoginExecutor();
+  }
 }
 
 export async function forceAuthWebLogin() {
