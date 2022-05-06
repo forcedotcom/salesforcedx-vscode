27efc7b0696dbd31c6313b3d2cebfd2c4ca110ce
mohanraj-r <mrajamanickam@salesforce.com>
2022-05-05 19:59:05 -0700

    build: add rollup 3pp and config

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
+import json from '@rollup/plugin-json';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: './src/index.ts',
+  output: {
+    file: './out/src/bundle.js',
+    format: 'cjs',
+    sourcemap: false
+  },
+  external: [
+    'vscode'
+  ],
+  plugins: [
+    typescript({
+      tsconfigOverride: { 
+        compilerOptions: { 
+          module: 'es2015',
+      }, 
+      verbosity: 3 
+    },
+  }),
+  json(), // Process JSON imports
+  dynamicImportVars({
+    // Throw a warning on error and don't quit build
+    warnOnError: true,
+  }),
+  nodeResolve(),
+  commonjs(),
+  ]
+};
