399eb5cb213a9d4ecbe6ccfa23fe8b30227510a6
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2022-05-09 10:44:06 -0700

    build: disable plugin to handle dynamic imports
    as it is not working as expected and causing other errors

--- scripts/esbuild.js
+++ scripts/esbuild.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const esbuild = require('esbuild');
-const {filelocPlugin} = require('esbuild-plugin-fileloc')
+const { filelocPlugin } = require('esbuild-plugin-fileloc');
 const path = require('path');
 const cwd = process.cwd();
 
@@ -9,12 +9,13 @@ esbuild.build({
   entryPoints: [path.join(cwd, 'out/src/index.js')],
   bundle: true,
   outfile: path.join(cwd, 'out/src/bundle.js'),
-  format: 'cjs', 
+  format: 'cjs',
   platform: 'node',
-  mainFields: ['module','main'],
+  mainFields: ['module', 'main'],
   external: ['vscode'],
-  logLevel: 'info',
-  plugins: [
-    filelocPlugin()
-  ]
+  logLevel: 'info'
+  // TODO (debug): Doesn't fix the dynamic import issue as expected. Causes other errors.
+  // plugins: [
+  //   filelocPlugin()
+  // ]
 });
