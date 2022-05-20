39f3d1339cfb7642b165a1b739982592341222ff
flora lan <zhujun.lan@salesforce.com>
2022-05-20 16:17:16 -0700

    test: finsih up test

--- packages/salesforcedx-vscode-core/test/vscode-integration/commands/util/inputGuard.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/util/inputGuard.test.ts
@@ -1,6 +1,5 @@
 import { expect } from 'chai';
-import { format } from 'util';
-import { lwcComponentInputGuard } from '../../../../src/commands/util/inputGuard';
+import { auraComponentInputGuard, lwcComponentInputGuard } from '../../../../src/commands/util/inputGuard';
 import { nls } from '../../../../src/messages';
 
 const NOT_ALPHANUMERIC_OR_UNDERSCORE_ERROR = 'not_alphanumeric_or_underscore_error';
@@ -10,70 +9,119 @@ const HAS_TWO_CONSECUTIVE_UNDERSCORES_ERROR = 'has_two_consecutive_underscores_e
 const NOT_START_WITH_LETTER_ERROR = 'not_start_with_letter_error';
 
 describe('inputGuard Unite Tests', () => {
-  describe('LWC Component Name Input Guard', () => {
-    it('should not show the error message for a valid component name', () => {
-      let exceptionThrown: any;
-      try {
-        lwcComponentInputGuard('account_map');
-      } catch (e) {
-        exceptionThrown = e;
-      }
-      expect(exceptionThrown).to.equal(null);
-    });
+  it('should not show the error message for a valid component name for LWC and Aura', () => {
+    let exceptionThrownLwc: any;
+    let exceptionThrownAura: any;
 
-    it('should show the error message when component name does not start with a lowercase letter', () => {
-      let exceptionThrown: any;
-      const errorMessage = nls.localize(NOT_START_WITH_LOWERCASE_ERROR);
-      try {
-        lwcComponentInputGuard('_hello');
-      } catch (e) {
-        exceptionThrown = e;
-      }
-      expect(exceptionThrown.message).to.equal(errorMessage);
-    });
+    try {
+      lwcComponentInputGuard('hello');
+    } catch (e) {
+      exceptionThrownLwc = e;
+    }
+    try {
+      auraComponentInputGuard('Hello');
+    } catch (e) {
+      exceptionThrownAura = e;
+    }
+    expect(exceptionThrownLwc).to.equal(undefined);
+    expect(exceptionThrownAura).to.equal(undefined);
+  });
 
-    it('should show the error message when component name contains white space between letters', () => {
-      let exceptionThrown: any;
-      const errorMessage = nls.localize(NOT_ALPHANUMERIC_OR_UNDERSCORE_ERROR);
-      try {
-        lwcComponentInputGuard('hello  world');
-      } catch (e) {
-        exceptionThrown = e;
-      }
-      expect(exceptionThrown.message).to.equal(errorMessage);
-    });
+  it('should show the error message when component name does not start with a lowercase letter for LWC', () => {
+    let exceptionThrown: any;
+    const errorMessage = nls.localize(NOT_START_WITH_LOWERCASE_ERROR);
+    try {
+      lwcComponentInputGuard('Hello');
+    } catch (e) {
+      exceptionThrown = e;
+    }
+    expect(exceptionThrown.message).to.equal(errorMessage);
+  });
 
-    it('should show the error message when component name contains special characters other than underscore or alphanumeric', () => {
-      let exceptionThrown: any;
-      const errorMessage = nls.localize(NOT_ALPHANUMERIC_OR_UNDERSCORE_ERROR);
-      try {
-        lwcComponentInputGuard('hello%$world');
-      } catch (e) {
-        exceptionThrown = e;
-      }
-      expect(exceptionThrown.message).to.equal(errorMessage);
-    });
+  it('should show the error message when component name does not start with a letter for Aura', () => {
+    let exceptionThrown: any;
+    const errorMessage = nls.localize(NOT_START_WITH_LETTER_ERROR);
+    try {
+      auraComponentInputGuard('_hello');
+    } catch (e) {
+      exceptionThrown = e;
+    }
+    expect(exceptionThrown.message).to.equal(errorMessage);
+  });
 
-    it('should show the error message when component name contains two consecutive underscores', () => {
-      let exceptionThrown: any;
-      const errorMessage = nls.localize(HAS_TWO_CONSECUTIVE_UNDERSCORES_ERROR);
-      try {
-        lwcComponentInputGuard('hello__world');
-      } catch (e) {
-        exceptionThrown = e;
-      }
-      expect(exceptionThrown.message).to.equal(errorMessage);
-    });
+  it('should show the error message when component name contains white space between letters for LWC and Aura', () => {
+    let exceptionThrownLwc: any;
+    let exceptionThrownAura: any;
 
-    it('should show the error message when component name contains two consecutive underscores', () => {
-      let exceptionThrown: any;
-      const errorMessage = nls.localize(HAS_TWO_CONSECUTIVE_UNDERSCORES_ERROR);
-      try {
-        lwcComponentInputGuard('hello__world');
-      } catch (e) {
-        exceptionThrown = e;
-      }
-      expect(exceptionThrown.message).to.equal(errorMessage);
-    });
+    const errorMessage = nls.localize(NOT_ALPHANUMERIC_OR_UNDERSCORE_ERROR);
+    try {
+      lwcComponentInputGuard('hello  world');
+    } catch (e) {
+      exceptionThrownLwc = e;
+    }
+    try {
+      auraComponentInputGuard('hello  world');
+    } catch (e) {
+      exceptionThrownAura = e;
+    }
+    expect(exceptionThrownLwc.message).to.equal(errorMessage);
+    expect(exceptionThrownAura.message).to.equal(errorMessage);
+  });
+
+  it('should show the error message when component name contains special characters other than underscore or alphanumeric for LWC and Aura', () => {
+    let exceptionThrownLwc: any;
+    let exceptionThrownAura: any;
+
+    const errorMessage = nls.localize(NOT_ALPHANUMERIC_OR_UNDERSCORE_ERROR);
+    try {
+      lwcComponentInputGuard('hello%$world');
+    } catch (e) {
+      exceptionThrownLwc = e;
+    }
+    try {
+      auraComponentInputGuard('hello%$world');
+    } catch (e) {
+      exceptionThrownAura = e;
+    }
+    expect(exceptionThrownLwc.message).to.equal(errorMessage);
+    expect(exceptionThrownAura.message).to.equal(errorMessage);
+  });
+
+  it('should show the error message when component name contains two consecutive underscores for LWC and Aura', () => {
+    let exceptionThrownLwc: any;
+    let exceptionThrownAura: any;
+
+    const errorMessage = nls.localize(HAS_TWO_CONSECUTIVE_UNDERSCORES_ERROR);
+    try {
+      lwcComponentInputGuard('hello__world');
+    } catch (e) {
+      exceptionThrownLwc = e;
+    }
+    try {
+      auraComponentInputGuard('hello__world');
+    } catch (e) {
+      exceptionThrownAura = e;
+    }
+    expect(exceptionThrownLwc.message).to.equal(errorMessage);
+    expect(exceptionThrownAura.message).to.equal(errorMessage);
+  });
+
+  it('should show the error message when component name ends with an underscore for LWC and Aura', () => {
+    let exceptionThrownLwc: any;
+    let exceptionThrownAura: any;
+
+    const errorMessage = nls.localize(END_WITH_UNDERSCORE_ERROR);
+    try {
+      lwcComponentInputGuard('hello_');
+    } catch (e) {
+      exceptionThrownLwc = e;
+    }
+    try {
+      auraComponentInputGuard('hello_');
+    } catch (e) {
+      exceptionThrownAura = e;
+    }
+    expect(exceptionThrownLwc.message).to.equal(errorMessage);
+    expect(exceptionThrownAura.message).to.equal(errorMessage);
   });
 });
