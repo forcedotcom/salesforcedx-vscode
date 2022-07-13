e3eb8c10156bb74b6eb217f55c83589d7cb753f9
angela-young <angela.young@salesforce.com>
2022-07-12 17:03:08 -0700

    test: add additional tests

--- packages/salesforcedx-vscode-core/test/vscode-integration/commands/forceConfigSet.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/forceConfigSet.test.ts
@@ -6,6 +6,7 @@
  */
 
 import { Config } from '@salesforce/core';
+import { Table } from '@salesforce/salesforcedx-utils-vscode/out/src/output';
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import { channelService } from '../../../src/channels';
@@ -18,12 +19,14 @@ const sandbox = sinon.createSandbox();
 let channelSpy: sinon.SinonSpy;
 let configSetSpy: sinon.SinonSpy;
 let configWriteSpy: sinon.SinonSpy;
+let tableSpy: sinon.SinonSpy;
 
 describe('Force Config Set', () => {
   beforeEach(() => {
     channelSpy = sandbox.spy(channelService, 'appendLine');
     configSetSpy = sandbox.spy(Config.prototype, 'set');
     configWriteSpy = sandbox.spy(Config.prototype, 'write');
+    tableSpy = sandbox.spy(Table.prototype, 'createTable');
   });
 
   afterEach(() => {
@@ -32,13 +35,10 @@ describe('Force Config Set', () => {
 
   it('should set config with given username or alias', async () => {
     const usernameOrAlias = 'test-username1@gmail.com';
-    const expectedOutput = '';
-    sandbox.stub(ForceConfigSetExecutor.prototype as any, 'formatOutput').returns(expectedOutput);
     await forceConfigSet(usernameOrAlias);
     expect(configSetSpy.calledOnce);
     expect(configSetSpy.calledWith(CONFIG_KEY, usernameOrAlias)).to.equal(true);
     expect(configWriteSpy.calledOnce);
-    expect(channelSpy.calledWith(expectedOutput)).to.equal(true);
   });
 
   it('should set config with first alias', async () => {
@@ -47,5 +47,22 @@ describe('Force Config Set', () => {
     await forceConfigSet(aliases.join(','));
     expect(configSetSpy.callCount).to.equal(1);
     expect(configSetSpy.calledWith(CONFIG_KEY, expectedAlias)).to.equal(true);
+  });
+
+  it('should display return value in output channel', async () => {
+    const usernameOrAlias = 'test-username1@gmail.com';
+    const expectedOutput = 'Successful table row';
+    sandbox.stub(ForceConfigSetExecutor.prototype as any, 'formatOutput').returns(expectedOutput);
+    await forceConfigSet(usernameOrAlias);
+    expect(channelSpy.calledWith(expectedOutput)).to.equal(true);
+  });
+
+  it('should format output correctly', async () => {
+    const usernameOrAlias = 'test-username1@gmail.com';
+    const outputTableRow = { name: CONFIG_KEY, val: usernameOrAlias, success: String(true) };
+    const forceConfigSet = new ForceConfigSetExecutor(usernameOrAlias);
+    const formatOutput = (forceConfigSet as any).formatOutput(outputTableRow);
+    expect(tableSpy.calledOnce);
+    expect(formatOutput).to.equal('=== Set Config\nName             Value                     Success\n───────────────  ────────────────────────  ───────\ndefaultusername  test-username1@gmail.com  true   \n');
   });
 });
