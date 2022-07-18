02832cfb91a644036ed586d7b50fa298cba89f66
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-07-18 14:42:35 -0700

    chore: use keys for default org and dev hub from core v3

--- packages/salesforcedx-vscode-core/src/constants.ts
+++ packages/salesforcedx-vscode-core/src/constants.ts
@@ -16,8 +16,6 @@ export const SFDX_CONFIG_DISABLE_TELEMETRY = 'disableTelemetry';
 export const ENV_SFDX_DISABLE_TELEMETRY = 'SFDX_DISABLE_TELEMETRY';
 export const SFDX_CLI_DOWNLOAD_LINK =
   'https://developer.salesforce.com/tools/sfdxcli';
-export const DEFAULT_USERNAME_KEY = 'defaultusername';
-export const DEFAULT_DEV_HUB_USERNAME_KEY = 'defaultdevhubusername';
 export const PKG_ID_PREFIX = '04t';
 
 export const TELEMETRY_GLOBAL_VALUE = 'sfdxTelemetryMessage';
@@ -30,7 +28,8 @@ export const BETA_DEPLOY_RETRIEVE = 'experimental.deployRetrieve';
 export const CONFLICT_DETECTION_ENABLED = 'detectConflictsAtSync';
 export const INTERNAL_DEVELOPMENT_FLAG = 'internal-development';
 export const PUSH_OR_DEPLOY_ON_SAVE_ENABLED = 'push-or-deploy-on-save.enabled';
-export const PUSH_OR_DEPLOY_ON_SAVE_OVERRIDE_CONFLICTS = 'push-or-deploy-on-save.overrideConflictsOnPush';
+export const PUSH_OR_DEPLOY_ON_SAVE_OVERRIDE_CONFLICTS =
+  'push-or-deploy-on-save.overrideConflictsOnPush';
 export const RETRIEVE_TEST_CODE_COVERAGE = 'retrieve-test-code-coverage';
 export const SFDX_CORE_CONFIGURATION_NAME = 'salesforcedx-vscode-core';
 export const SHOW_CLI_SUCCESS_INFO_MSG = 'show-cli-success-msg';
