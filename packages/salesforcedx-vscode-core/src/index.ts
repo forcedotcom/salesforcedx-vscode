f45f5f4b0c6e97bd553938fe36ea22aafa922ede
Dehru Cromer <dehru.cromer@salesforce.com>
2022-07-26 10:35:38 -0600

    chore: fix lint

--- packages/salesforcedx-vscode-core/src/index.ts
+++ packages/salesforcedx-vscode-core/src/index.ts
@@ -4,8 +4,8 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import * as vscode from 'vscode';
 import { SFDX_CORE_CONFIGURATION_NAME } from '@salesforce/salesforcedx-utils-vscode/out/src';
+import * as vscode from 'vscode';
 import { channelService } from './channels';
 import {
   checkSObjectsAndRefresh,
@@ -92,7 +92,7 @@ import {
   setupConflictView
 } from './conflict';
 import {
-  ENABLE_SOBJECT_REFRESH_ON_STARTUP,
+  ENABLE_SOBJECT_REFRESH_ON_STARTUP
 } from './constants';
 import { getDefaultUsernameOrAlias } from './context';
 import { workspaceContext } from './context';
