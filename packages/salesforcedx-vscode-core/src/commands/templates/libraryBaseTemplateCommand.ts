538fe1f40f2597876fa48729f23d2db32e456080
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-07-28 15:57:36 -0700

    refactor: correct imports

--- packages/salesforcedx-vscode-core/src/commands/templates/libraryBaseTemplateCommand.ts
+++ packages/salesforcedx-vscode-core/src/commands/templates/libraryBaseTemplateCommand.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { OrgConfigProperties } from '@salesforce/core';
+import { ConfigAggregator, OrgConfigProperties } from '@salesforce/core';
 import {
   TemplateOptions,
   TemplateService,
@@ -23,7 +23,6 @@ import { channelService } from '../../channels';
 import { notificationService } from '../../notifications';
 import { telemetryService } from '../../telemetry';
 import {
-  ConfigUtil,
   getRootWorkspacePath,
   hasRootWorkspace,
   MetadataDictionary,
