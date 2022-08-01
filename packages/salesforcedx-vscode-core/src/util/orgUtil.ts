d1616a5e92f52e984a7369d05990d4f85e388745
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-08-01 12:14:43 -0700

    chore: catch up w develop

---
+++ packages/salesforcedx-vscode-core/src/util/orgUtil.ts
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { Aliases } from '@salesforce/core';
+import { channelService } from '../channels';
+import { nls } from '../messages';
+import { notificationService } from '../notifications';
+import { FileInfo, OrgList } from '../orgPicker';
+
+export async function setUpOrgExpirationWatcher(orgList: OrgList) {
+  // Run once to start off with.
+  await checkForExpiredOrgs(orgList);
+
+  /*
+  Comment this out for now.  For now, we are only going to check once on activation,
+  however it would be helpful if we also checked once a day.  If we decide to also
+  check once a day, uncomment the following code.
+
+  // And run again once every 24 hours.
+  setInterval(async () => {
+    await checkForExpiredOrgs(orgList);
+  }, 1000 * 60 * 60 * 24);
+  */
+}
+
+export async function checkForExpiredOrgs(orgList: OrgList) {
+  if (!orgList) {
+    return;
+  }
+
+  try {
+    const daysBeforeExpire = 5;
+    const today = new Date();
+    const daysUntilExpiration = new Date();
+    daysUntilExpiration.setDate(
+      daysUntilExpiration.getDate() + daysBeforeExpire
+    );
+
+    const authInfoObjects = await orgList.getAuthInfoObjects();
+    if (!authInfoObjects) {
+      return;
+    }
+
+    const orgsAboutToExpire = authInfoObjects!.filter((authInfoObject: FileInfo) => {
+      // Filter out the dev hubs.
+      if (authInfoObject.isDevHub) {
+        return false;
+      }
+
+      // Some dev hubs have isDevHub=false, but no expiration date, so filter them out.
+      if (!authInfoObject.expirationDate) {
+        return false;
+      }
+
+      // Filter out the expired orgs.
+      const expirationDate = new Date(authInfoObject.expirationDate);
+      if (expirationDate < today) {
+        return false;
+      }
+
+      // Now filter and only return the results that are within the 5 day window.
+      return expirationDate <= daysUntilExpiration;
+    });
+
+    if (!orgsAboutToExpire || orgsAboutToExpire.length < 1) {
+      return;
+    }
+
+    const defaultOptions = Aliases.getDefaultOptions();
+    const aliases = await Aliases.create(defaultOptions);
+
+    const formattedOrgsToDisplay = orgsAboutToExpire.map((orgAboutToExpire: any) => {
+      const alias = aliases.getKeysByValue(orgAboutToExpire.username);
+      const aliasName = alias.length > 0
+        ? alias.toString()
+        : orgAboutToExpire.username;
+
+      return nls.localize(
+        'pending_org_expiration_expires_on_message',
+        aliasName,
+        orgAboutToExpire.expirationDate
+      );
+    }).join('\n\n');
+
+    notificationService.showWarningMessage(
+      nls.localize(
+        'pending_org_expiration_notification_message',
+        daysBeforeExpire
+      )
+    );
+    channelService.appendLine(
+      nls.localize(
+        'pending_org_expiration_output_channel_message',
+        daysBeforeExpire,
+        formattedOrgsToDisplay
+      )
+    );
+    channelService.showChannelOutput();
+  } catch (err) {
+    console.error(err);
+  }
+}
---
+++ packages/salesforcedx-vscode-core/test/vscode-integration/util/orgUtil.test.ts
@@ -0,0 +1,194 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+// Imports for testing
+import { expect } from 'chai';
+import { createSandbox, SinonSandbox, SinonSpy } from 'sinon';
+import * as vscode from 'vscode';
+import { checkForExpiredOrgs } from '../../../src/util';
+
+// Imports from the target source file
+import { Aliases } from '@salesforce/core';
+import { channelService } from '../../../src/channels';
+import { OrgList } from '../../../src/orgPicker';
+
+describe('orgUtil tests', () => {
+  let sb: SinonSandbox;
+
+  beforeEach(() => {
+    sb = createSandbox();
+  });
+
+  afterEach(async () => {
+    sb.restore();
+  });
+
+  describe('checkForExpiredOrgs tests', () => {
+    let today: Date;
+    let showWarningMessageSpy: SinonSpy;
+    let appendLineSpy: SinonSpy;
+    let showChannelOutputSpy: SinonSpy;
+
+    beforeEach(() => {
+      today = new Date();
+      showWarningMessageSpy = sb.spy(vscode.window, 'showWarningMessage');
+      appendLineSpy = sb.spy(channelService, 'appendLine');
+      showChannelOutputSpy = sb.spy(channelService, 'showChannelOutput');
+    });
+
+    afterEach(async () => {
+      showWarningMessageSpy.restore();
+      appendLineSpy.restore();
+      showChannelOutputSpy.restore();
+    });
+
+    it('should not display a notification when no orgs are present', async () => {
+      const getAuthInfoObjectsStub = sb
+        .stub(OrgList.prototype, 'getAuthInfoObjects')
+        .resolves([]);
+
+      const orgList = new OrgList();
+      await checkForExpiredOrgs(orgList);
+
+      expect(showWarningMessageSpy.called).to.equal(false);
+      expect(appendLineSpy.called).to.equal(false);
+      expect(showChannelOutputSpy.called).to.equal(false);
+
+      getAuthInfoObjectsStub.restore();
+    });
+
+    it('should not display a notification when dev hubs are present', async () => {
+      const getAuthInfoObjectsStub = sb
+        .stub(OrgList.prototype, 'getAuthInfoObjects')
+        .resolves([
+          {
+            isDevHub: true
+          },
+          {
+            isDevHub: false,
+            expirationDate: undefined
+          }
+        ]);
+
+      const orgList = new OrgList();
+      await checkForExpiredOrgs(orgList);
+
+      expect(showWarningMessageSpy.called).to.equal(false);
+      expect(appendLineSpy.called).to.equal(false);
+      expect(showChannelOutputSpy.called).to.equal(false);
+
+      getAuthInfoObjectsStub.restore();
+    });
+
+    it('should not display a notification when the scratch org has already expired', async () => {
+      const getAuthInfoObjectsStub = sb
+        .stub(OrgList.prototype, 'getAuthInfoObjects')
+        .resolves([
+          {
+            isDevHub: false,
+            expirationDate: `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate() - 1}`
+          }
+        ]);
+
+      const orgList = new OrgList();
+      await checkForExpiredOrgs(orgList);
+
+      expect(showWarningMessageSpy.called).to.equal(false);
+      expect(appendLineSpy.called).to.equal(false);
+      expect(showChannelOutputSpy.called).to.equal(false);
+
+      getAuthInfoObjectsStub.restore();
+    });
+
+    it('should display a notification when the scratch org is about to expire', async () => {
+      const getAuthInfoObjectsStub = sb
+        .stub(OrgList.prototype, 'getAuthInfoObjects')
+        .resolves([
+          {
+            isDevHub: false,
+            expirationDate: `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate() + 3}`,
+            username: 'foo'
+          }
+        ]);
+      const getDefaultOptionsStub = sb
+        .stub(Aliases, 'getDefaultOptions')
+        .returns({
+          defaultGroup: 'orgs',
+          filename: 'alias.json',
+          isGlobal: true,
+          isState: true
+        });
+      const orgName = 'dreamhouse-org';
+      const createStub = sb
+        .stub(Aliases, 'create')
+        .resolves({
+          getKeysByValue: () => {
+            return orgName;
+          }
+        });
+
+      const orgList = new OrgList();
+      await checkForExpiredOrgs(orgList);
+
+      expect(showWarningMessageSpy.called).to.equal(true);
+      expect(appendLineSpy.called).to.equal(true);
+      expect(appendLineSpy.args[0][0]).to.contain(orgName);
+      expect(showChannelOutputSpy.called).to.equal(true);
+
+      getAuthInfoObjectsStub.restore();
+      getDefaultOptionsStub.restore();
+      createStub.restore();
+    });
+
+    it('should display multiple orgs in the output when there are several scratch orgs about to expire', async () => {
+      const getAuthInfoObjectsStub = sb
+        .stub(OrgList.prototype, 'getAuthInfoObjects')
+        .resolves([
+          {
+            isDevHub: false,
+            expirationDate: `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate() + 2}`,
+            username: 'foo'
+          },
+          {
+            isDevHub: false,
+            expirationDate: `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate() + 3}`,
+            username: 'bar'
+          }
+        ]);
+      const getDefaultOptionsStub = sb
+        .stub(Aliases, 'getDefaultOptions')
+        .returns({
+          defaultGroup: 'orgs',
+          filename: 'alias.json',
+          isGlobal: true,
+          isState: true
+        });
+      const orgName1 = 'dreamhouse-org';
+      const orgName2 = 'ebikes-lwc';
+      const createStub = sb
+        .stub(Aliases, 'create')
+        .resolves({
+          getKeysByValue: (key: string) => {
+            return (key === 'foo') ? orgName1 : orgName2;
+          }
+        });
+
+      const orgList = new OrgList();
+      await checkForExpiredOrgs(orgList);
+
+      expect(showWarningMessageSpy.called).to.equal(true);
+      expect(appendLineSpy.called).to.equal(true);
+      expect(appendLineSpy.args[0][0]).to.contain(orgName1);
+      expect(appendLineSpy.args[0][0]).to.contain(orgName2);
+      expect(showChannelOutputSpy.called).to.equal(true);
+
+      getAuthInfoObjectsStub.restore();
+      getDefaultOptionsStub.restore();
+      createStub.restore();
+    });
+  });
+});
