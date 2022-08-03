0c534a7120d528ec3bdc48d0417ac3b552951201
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-08-02 17:43:45 -0700

    chore: saved file to run formatter
    To get past CircleCI build error: 
    ERROR: C:/Users/circleci/project/packages/salesforcedx-utils-vscode/src/helpers/utils.ts[26, 1]: space indentation expected
    ERROR: C:/Users/circleci/project/packages/salesforcedx-utils-vscode/src/helpers/utils.ts[28, 1]: space indentation expected

--- packages/salesforcedx-utils-vscode/src/helpers/utils.ts
+++ packages/salesforcedx-utils-vscode/src/helpers/utils.ts
@@ -14,16 +14,13 @@ export function isNullOrUndefined(object: any): object is null | undefined {
 }
 
 export function extractJsonObject(str: string): any {
-  const jsonString = str.substring(
-    str.indexOf('{'),
-    str.lastIndexOf('}') + 1
-  );
+  const jsonString = str.substring(str.indexOf('{'), str.lastIndexOf('}') + 1);
 
   return JSON.parse(jsonString);
 }
 
 export async function asyncFilter<T>(arr: T[], predicate: any) {
-	const results = await Promise.all(arr.map(predicate));
+  const results = await Promise.all(arr.map(predicate));
 
-	return arr.filter((_v, index) => results[index]);
+  return arr.filter((_v, index) => results[index]);
 }
