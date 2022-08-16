90f4846ea5ef60e70ade5c38dc069401500e0326
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-08-16 13:36:14 -0700

    refactor: remove no longer used imports

--- packages/salesforcedx-vscode-core/test/vscode-integration/orgPicker/orgList.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/orgPicker/orgList.test.ts
@@ -5,10 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { AuthInfo, OrgAuthorization, StateAggregator } from '@salesforce/core';
-import { fail } from 'assert';
 import { expect } from 'chai';
-import * as fs from 'fs';
-import * as sinon from 'sinon';
 import { createSandbox, SinonStub } from 'sinon';
 import * as vscode from 'vscode';
 import { nls } from '../../../src/messages';
