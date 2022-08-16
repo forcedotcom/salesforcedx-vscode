65d4d5db526e5012e84c037162b33e2779855b58
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-08-16 13:58:03 -0700

    refactor: add return type

--- packages/salesforcedx-vscode-core/src/orgPicker/orgList.ts
+++ packages/salesforcedx-vscode-core/src/orgPicker/orgList.ts
@@ -110,7 +110,7 @@ export class OrgList implements vscode.Disposable {
     return authList;
   }
 
-  public async updateOrgList(): Promise<string[]> {
+  public async updateOrgList(): Promise<string[] | null> {
     const orgAuthorizations = await this.getOrgAuthorizations();
     if (orgAuthorizations && orgAuthorizations.length === 0) {
       return null;
