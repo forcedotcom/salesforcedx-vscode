3b86f4ea07863451e7f3c7283b53ab2c9f9d060b
Jeff Beeghly <jbeeghly@salesforce.com>
2022-08-24 09:48:25 -0700

    fix: dependabot-library-updates

--- packages/salesforcedx-vscode-apex/src/requirements.ts
+++ packages/salesforcedx-vscode-apex/src/requirements.ts
@@ -9,14 +9,15 @@
 // Original version licensed under the Eclipse Public License (EPL)
 
 import * as cp from 'child_process';
+import * as path from 'path';
+import * as pathExists from 'path-exists';
 import { workspace } from 'vscode';
 import { SET_JAVA_DOC_LINK } from './constants';
 import { nls } from './messages';
-import path = require('path');
-import pathExists = require('path-exists');
 
 // tslint:disable-next-line:no-var-requires
 const expandHomeDir = require('expand-home-dir');
+
 // tslint:disable-next-line:no-var-requires
 const findJavaHome = require('find-java-home');
 
