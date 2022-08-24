4dcd92ed392197597f69e50916c966db4932e776
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-08-23 20:55:54 -0700

    refactor: reload config aggregators on conf chg

--- packages/salesforcedx-utils-vscode/src/context/workspaceContextUtil.ts
+++ packages/salesforcedx-utils-vscode/src/context/workspaceContextUtil.ts
@@ -5,7 +5,12 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { AuthInfo, Connection } from '@salesforce/core';
+import {
+  AuthInfo,
+  ConfigAggregator,
+  Connection,
+  SfdxConfigAggregator
+} from '@salesforce/core';
 import { join } from 'path';
 import * as vscode from 'vscode';
 import { AuthUtil } from '..';
@@ -88,6 +93,7 @@ export class WorkspaceContextUtil {
   }
 
   protected async handleCliConfigChange() {
+    this.reloadConfigAggregators();
     const usernameOrAlias = await this.getAuthUtil().getDefaultUsernameOrAlias(
       false
     );
@@ -114,8 +120,96 @@ export class WorkspaceContextUtil {
   get alias(): string | undefined {
     return this._alias;
   }
+
+  private async reloadConfigAggregators() {
+    console.log(
+      'The .sfdx config file has changed.  Reloading ConfigAggregator values.'
+    );
+
+    const sfConfigAggregator = await VSCEConfigAggregator.create();
+    await sfConfigAggregator.reload();
+
+    const sfdxConfigAggregator = await VSCEConfigAggregator.create({
+      sfdx: true
+    });
+    await sfdxConfigAggregator.reload();
+  }
 }
 
 export function getLogDirPath(): string {
   return join(getRootWorkspacePath(), '.sfdx', 'tools', 'debug', 'logs');
+}
+
+type VSCEConfigAggregatorOptions = {
+  /*
+   *  The SfdxConfigAggregator is used only to get configuration
+   *  values that correspond with old/deprecated config keys.
+   *  Currently, the key used for the custom templates
+   *  directory is the only usage, since it is documented for use
+   *  here: https://developer.salesforce.com/tools/vscode/en/user-guide/byotemplate#set-default-template-location
+   */
+  sfdx?: boolean;
+  globalValuesOnly?: boolean;
+};
+
+/*
+ * The VSCEConfigAggregator class is used to abstract away
+ * some of the complexities around changing the process directory
+ * that are needed to accurately retrieve configuration values
+ * when using the ConfigAggregator in the VSCE context.
+ */
+class VSCEConfigAggregator {
+  public static async create(
+    options?: VSCEConfigAggregatorOptions
+  ): Promise<ConfigAggregator> {
+    return VSCEConfigAggregator.getConfigAggregator(options);
+  }
+
+  private static async getConfigAggregator(
+    options: VSCEConfigAggregatorOptions = {
+      sfdx: false,
+      globalValuesOnly: false
+    }
+  ): Promise<ConfigAggregator> {
+    let configAggregator;
+    const currentWorkingDirectory = process.cwd();
+    if (options.globalValuesOnly) {
+      VSCEConfigAggregator.ensureProcessIsRunningUnderUserHomeDir(
+        currentWorkingDirectory
+      );
+    } else {
+      // Change the current working directory to the project path,
+      // so that ConfigAggregator reads the local project values
+      VSCEConfigAggregator.ensureProcessIsRunningUnderProjectRoot(
+        currentWorkingDirectory
+      );
+    }
+    try {
+      configAggregator = options.sfdx
+        ? await SfdxConfigAggregator.create()
+        : await ConfigAggregator.create();
+    } finally {
+      // Change the current working directory back to what it was
+      // before returning.
+      // Wrapping this in a finally block ensures that the working
+      // directory is switched back to what it was before this method
+      // was called if SfdxConfigAggregator.create() throws an exception.
+      process.chdir(currentWorkingDirectory);
+    }
+    return configAggregator;
+  }
+
+  private static ensureProcessIsRunningUnderUserHomeDir(path: string) {
+    const userHomePath = '/';
+    if (path !== userHomePath) {
+      process.chdir(userHomePath);
+    }
+  }
+
+  private static ensureProcessIsRunningUnderProjectRoot(path: string) {
+    const rootWorkspacePath = getRootWorkspacePath();
+    if (path !== rootWorkspacePath) {
+      process.chdir(rootWorkspacePath);
+    }
+  }
 }
