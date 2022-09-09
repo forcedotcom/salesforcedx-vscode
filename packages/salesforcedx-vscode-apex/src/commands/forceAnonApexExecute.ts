4e54e66fe05c56ff14fd26b6f278866c69975b1f
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-09-09 01:21:56 -0700

    refactor: organization

--- packages/salesforcedx-vscode-apex/src/commands/forceAnonApexExecute.ts
+++ packages/salesforcedx-vscode-apex/src/commands/forceAnonApexExecute.ts
@@ -264,47 +264,6 @@ export class AnonApexLibraryExecuteExecutor extends LibraryCommandletExecutor<
   }
 }
 
-export class DebugLog {
-  private _log: string | undefined;
-  private _lines: string[] | undefined;
-  private userDebugEventIdentifier = 'USER_DEBUG';
-  // Todo: QA on Windows machines to validate/verify
-  // newLine aka '\n' works as expected
-  private newLineCharacter = '\n';
-
-  constructor(logOrLogLines: string | string[]) {
-    if (Array.isArray(logOrLogLines)) {
-      this._lines = logOrLogLines;
-    } else {
-      this._log = logOrLogLines;
-    }
-  }
-
-  public value(): string {
-    if (!this._log) {
-      this._log = this._lines!.join(this.newLineCharacter);
-    }
-    return this._log!;
-  }
-
-  public debugStatements(): string {
-    const userDebugStatements = this.lines(this.userDebugEventIdentifier);
-    const userDebugStatementsText = userDebugStatements.join(
-      this.newLineCharacter
-    );
-    return userDebugStatementsText;
-  }
-
-  private lines(filter?: string): string[] {
-    if (!this._lines) {
-      this._lines = this._log!.split(this.newLineCharacter);
-    }
-    return filter
-      ? this._lines.filter(line => line.includes(filter))
-      : this._lines;
-  }
-}
-
 export async function forceAnonApexExecute() {
   const commandlet = new SfdxCommandlet(
     new SfdxWorkspaceChecker(),
