f17e7b30677671765d5a37d332d620e14e5b1da7
Gordon Bockus <gbockus@salesforce.com>
2022-09-21 13:48:08 -0500

    chore: changes from review of PR 4428

--- packages/salesforcedx-apex-debugger/jest.integration.config.js
+++ packages/salesforcedx-apex-debugger/jest.integration.config.js
@@ -1,6 +1,5 @@
 const baseConfig =  require('../../config/jest.integration.config');
 
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = Object.assign({},
   baseConfig,
   {}
--- packages/salesforcedx-sobjects-faux-generator/jest.config.js
+++ packages/salesforcedx-sobjects-faux-generator/jest.config.js
@@ -1,6 +1,5 @@
 const baseConfig =  require('../../config/jest.base.config');
 
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = Object.assign({},
   baseConfig,
   {}
