b00f68eae7c5ebfe3c0cc5ac31919efd793e4067
gbockus-sf <76090802+gbockus-sf@users.noreply.github.com>
2022-09-27 18:24:10 -0500

    Chore: move the configAggregator to the utils package.  (#4449)
    * chore: move provider to utils
    
    * chore: fix lint issue
    
    * chore: fix authInfo core int tests
    
    * chore: fix more int tests
    
    * chore: lint fix
    
    * chore: update faux package to use configUtil in utils
    
    * chore: update getUsername to use the config agg provider
    
    * chore: removed no longer needed grouping module in core
    
    * chore: updates from review
    
    * chore: update int test for removal of projectDir

--- packages/salesforcedx-vscode-core/test/vscode-integration/util/configUtil.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/util/configUtil.test.ts
@@ -5,40 +5,38 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { ConfigAggregator } from '@salesforce/core';
+import { ConfigAggregatorProvider } from '@salesforce/salesforcedx-utils-vscode';
 import { expect } from 'chai';
-import { createSandbox, SinonSandbox, SinonStub } from 'sinon';
+import { createSandbox, SinonStub } from 'sinon';
 import { ConfigSource, ConfigUtil } from '../../../src/util';
 
 describe('getConfigSource', () => {
-  let sandboxStub: SinonSandbox;
-  let configAggregatorGetLocationStub: SinonStub;
+  const sandbox = createSandbox();
+  let getLocationStub: SinonStub;
 
   beforeEach(() => {
-    sandboxStub = createSandbox();
-    configAggregatorGetLocationStub = sandboxStub.stub(
-      ConfigAggregator.prototype,
-      'getLocation'
-    );
+    getLocationStub = sandbox.stub();
+    sandbox.stub(ConfigAggregatorProvider, 'getInstance').returns({
+      getConfigAggregator: sandbox.stub().resolves({
+        getLocation: getLocationStub
+      })
+    });
   });
   afterEach(() => {
-    sandboxStub.restore();
+    sandbox.restore();
   });
   it('should return ConfigSource.Local if the key/value is in the local config', async () => {
-    configAggregatorGetLocationStub
-      .onCall(0)
-      .returns(ConfigAggregator.Location.LOCAL);
+    getLocationStub.returns(ConfigAggregator.Location.LOCAL);
     const configSource = await ConfigUtil.getConfigSource('key');
     expect(configSource).to.be.eq(ConfigSource.Local);
   });
   it('should return ConfigSource.Global if the key/value is in the global config', async () => {
-    configAggregatorGetLocationStub
-      .onCall(0)
-      .returns(ConfigAggregator.Location.GLOBAL);
+    getLocationStub.returns(ConfigAggregator.Location.GLOBAL);
     const configSource = await ConfigUtil.getConfigSource('key');
     expect(configSource).to.be.eq(ConfigSource.Global);
   });
   it('should return ConfigSource.None if the key/value is not in the local or global config', async () => {
-    configAggregatorGetLocationStub.onCall(0).returns(undefined);
+    getLocationStub.returns(undefined);
     const configSource = await ConfigUtil.getConfigSource('key');
     expect(configSource).to.be.eq(ConfigSource.None);
   });
