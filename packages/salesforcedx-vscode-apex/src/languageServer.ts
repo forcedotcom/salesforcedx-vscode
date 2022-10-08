149e2c99afd20fcf823dba7f147eedd1e90b26eb
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-10-08 06:11:50 -0700

    chore: fix import

--- packages/salesforcedx-vscode-apex/src/languageServer.ts
+++ packages/salesforcedx-vscode-apex/src/languageServer.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { projectPaths } from '@salesforce/salesforcedx-utils-vscode/src';
+import { projectPaths } from '@salesforce/salesforcedx-utils-vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
