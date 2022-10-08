17faa84de48d1bf06bc7f286586268cd8b132653
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-10-08 06:57:38 -0700

    chore: remove unused

--- packages/salesforcedx-vscode-apex-replay-debugger/src/index.ts
+++ packages/salesforcedx-vscode-apex-replay-debugger/src/index.ts
@@ -5,7 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { Global } from '@salesforce/core';
 import {
   MetricError,
   MetricLaunch
