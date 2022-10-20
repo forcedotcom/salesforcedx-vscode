a5d6a79026ed8322326e8c89c98490a02ad351dd
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-10-20 12:13:18 -0700

    chore: update comments

--- packages/salesforcedx-utils-vscode/test/jest/config/configUtil.test.ts
+++ packages/salesforcedx-utils-vscode/test/jest/config/configUtil.test.ts
@@ -3,6 +3,11 @@ import { ConfigUtil } from '../../../src';
 describe('ConfigUtil', () => {
   describe('getProjectPackageNames', () => {
     it('should return project package directories listed in project config file', () => {
+      // Need to stub:
+      // getRootWorkspacePath
+      // SfProject.getInstance()
+      // project.getUniquePackageNames()
+
       const a = ConfigUtil.getProjectPackageNames();
 
       expect(a).toEqual('');
