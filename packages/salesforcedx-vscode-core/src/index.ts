857d1132414f2356e370645401df9c4cbe420268
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-10-20 11:30:13 -0700

    refactor: combine imports

--- packages/salesforcedx-vscode-core/src/index.ts
+++ packages/salesforcedx-vscode-core/src/index.ts
@@ -4,8 +4,10 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { ConfigUtil } from '@salesforce/salesforcedx-utils-vscode';
-import { SFDX_CORE_CONFIGURATION_NAME } from '@salesforce/salesforcedx-utils-vscode';
+import {
+  ConfigUtil,
+  SFDX_CORE_CONFIGURATION_NAME
+} from '@salesforce/salesforcedx-utils-vscode';
 import * as vscode from 'vscode';
 import { channelService } from './channels';
 import {
