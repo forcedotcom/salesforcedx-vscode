054e7ce8e2cc5a398421b3988b0944829584e691
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-10-20 12:27:45 -0700

    refactor: more comments

--- packages/salesforcedx-utils-vscode/test/jest/config/configUtil.test.ts
+++ packages/salesforcedx-utils-vscode/test/jest/config/configUtil.test.ts
@@ -8,6 +8,13 @@ describe('ConfigUtil', () => {
       // SfProject.getInstance()
       // project.getUniquePackageNames()
 
+      // In this test I want to assert that
+      // 1. getRootWorkspace was called first
+      // 2. SfProject.getInstance() was called, passing the value from getRootWorkspace
+      // 3. getUniquePackageNames() was called last
+      // I don't want to test the response from getUniquePackageNames()
+      // since that is already tested in the core lib here: https://github.com/forcedotcom/sfdx-core/blob/4b6f99b44b77989fc525460391b41da62deb62a8/test/unit/projectTest.ts#L516
+
       const a = ConfigUtil.getProjectPackageNames();
 
       expect(a).toEqual('');
