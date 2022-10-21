ae12a6d669cd101267a84d19457fc6b7f399546c
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-10-21 15:23:08 -0700

    chore: update jest config to align

--- packages/salesforcedx-vscode-apex-replay-debugger/jest.config.js
+++ packages/salesforcedx-vscode-apex-replay-debugger/jest.config.js
@@ -1,5 +1,5 @@
+// eslint:disable-next-line:no-var-requires
 const baseConfig = require('../../config/jest.base.config');
 
-module.exports = Object.assign({}, baseConfig, {
-  testMatch: ['**/unit/**/?(*.)+(spec|test).[t]s?(x)']
-});
+module.exports = Object.assign({}, baseConfig);
+
