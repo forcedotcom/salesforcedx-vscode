4fb945e5988460c62959c326fbebde2123fa730e
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-10-22 11:45:22 -0700

    chore: add copywrights

--- packages/salesforcedx-vscode-apex-replay-debugger/src/activation/getDialogStartingPath.ts
+++ packages/salesforcedx-vscode-apex-replay-debugger/src/activation/getDialogStartingPath.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import { LAST_OPENED_LOG_FOLDER_KEY } from '@salesforce/salesforcedx-apex-replay-debugger/out/src/constants';
 import {
   projectPaths,
