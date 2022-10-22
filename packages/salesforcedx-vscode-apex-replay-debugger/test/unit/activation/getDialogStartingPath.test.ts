87007e5a9cd67be6d5d0257d321d001bff92f17e
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-10-22 08:21:50 -0700

    refactor: mock underlying functions

--- packages/salesforcedx-vscode-apex-replay-debugger/test/unit/activation/getDialogStartingPath.test.ts
+++ packages/salesforcedx-vscode-apex-replay-debugger/test/unit/activation/getDialogStartingPath.test.ts
@@ -1,29 +1,25 @@
 import { LAST_OPENED_LOG_FOLDER_KEY } from '@salesforce/salesforcedx-apex-replay-debugger/src';
 import { workspaceUtils } from '@salesforce/salesforcedx-utils-vscode';
+import * as pathExists from 'path-exists';
 import * as vscode from 'vscode';
 import { dialogStartingPath } from '../../../src/activation/getDialogStartingPath';
 
 describe('getDialogStartingPath', () => {
-  let hasRootWorkspaceStub: jest.SpyInstance;
-  let getLastOpenedLogFolderMock: jest.SpyInstance;
-  let folderExistsMock: jest.SpyInstance;
-  let getUriForMock: jest.SpyInstance;
   const testPath = '/here/is/a/fake/path/to/';
+  let hasRootWorkspaceStub: jest.SpyInstance;
+  let pathExistsMock: jest.SpyInstance;
+  let vsCodeUriMock: jest.SpyInstance;
 
   beforeEach(() => {
     hasRootWorkspaceStub = jest.spyOn(workspaceUtils, 'hasRootWorkspace');
-    getLastOpenedLogFolderMock = jest.spyOn(
-      dialogStartingPath,
-      'getLastOpenedLogFolder'
-    );
-    folderExistsMock = jest.spyOn(dialogStartingPath, 'folderExists');
-    getUriForMock = jest.spyOn(dialogStartingPath, 'getUriFor');
+    pathExistsMock = jest.spyOn(pathExists, 'sync').mockReturnValue(true);
+    vsCodeUriMock = jest
+      .spyOn(vscode.Uri, 'file')
+      .mockReturnValue({ path: testPath } as vscode.Uri);
   });
 
   it('Should return last opened log folder', () => {
     hasRootWorkspaceStub.mockReturnValue(true);
-    folderExistsMock.mockReturnValue(true);
-    getUriForMock.mockReturnValue({ path: testPath } as vscode.Uri);
     const mockGet = jest.fn().mockReturnValue(testPath);
     const mockExtensionContext: any = { workspaceState: { get: mockGet } };
 
@@ -33,17 +29,30 @@ describe('getDialogStartingPath', () => {
     );
 
     expect(hasRootWorkspaceStub).toHaveBeenCalled();
-    expect(getLastOpenedLogFolderMock).toHaveBeenCalledWith(
-      mockExtensionContext
-    );
     expect(mockGet).toHaveBeenCalledWith(LAST_OPENED_LOG_FOLDER_KEY);
-    expect(folderExistsMock).toHaveBeenCalledWith(testPath);
+    expect(pathExistsMock).toHaveBeenCalledWith(testPath);
+    expect(vsCodeUriMock).toHaveBeenCalledWith(testPath);
     expect((dialogStartingPathUri as vscode.Uri).path).toEqual(testPath);
   });
 
-  // it('Should return log folder', async () => {});
+  it('Should return log folder', async () => {
+    hasRootWorkspaceStub.mockReturnValue(true);
+  });
 
-  // it('Should return state folder', async () => {});
+  it('Should return state folder', async () => {
+    hasRootWorkspaceStub.mockReturnValue(true);
+  });
 
-  // it('Should return undefined', async () => {});
+  it('Should return undefined when not in a project workspace', async () => {
+    hasRootWorkspaceStub.mockReturnValue(false);
+    const mockGet = jest.fn().mockReturnValue(testPath);
+    const mockExtensionContext: any = { workspaceState: { get: mockGet } };
+
+    // Act
+    const dialogStartingPathUri = dialogStartingPath.getDialogStartingPathUri(
+      mockExtensionContext
+    );
+
+    expect(dialogStartingPathUri as vscode.Uri).toEqual(undefined);
+  });
 });
