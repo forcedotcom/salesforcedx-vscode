c8a27690789269212b91fe3c50e1d822be44bbf0
CristiCanizales <cristina.canizales@salesforce.com>
2022-11-03 13:47:36 -0300

    chore: config for jest

--- scripts/setup-jest.ts
+++ scripts/setup-jest.ts
@@ -28,13 +28,18 @@ const getMockVSCode = () => {
       };
       public dispose = () => {};
     },
+    TreeItem: jest.fn(),
     commands: jest.fn(),
     Disposable: jest.fn(),
     env: {
       machineId: '12345534'
     },
+    // tslint:disable-next-line:object-literal-shorthand
     EventEmitter: EventEmitter,
     ExtensionMode: { Production: 1, Development: 2, Test: 3 },
+    languages: {
+      createDiagnosticCollection: jest.fn()
+    },
     Uri: {
       parse: jest.fn(),
       file: jest.fn()
@@ -63,7 +68,11 @@ const getMockVSCode = () => {
         };
       },
       onDidChangeConfiguration: jest.fn(),
-      createFileSystemWatcher: jest.fn(),
+      createFileSystemWatcher: jest.fn().mockReturnValue({
+        onDidChange: jest.fn(),
+        onDidCreate: jest.fn(),
+        onDidDelete: jest.fn()
+      }),
       workspaceFolders: []
     }
   };
