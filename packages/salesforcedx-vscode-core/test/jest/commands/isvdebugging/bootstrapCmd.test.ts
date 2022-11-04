d575ba8a62d845c3f2b511213d1a101b4819d313
Ritam Agrawal <72183558+RitamAgrawal@users.noreply.github.com>
2022-11-04 23:57:22 +0530

    update usage of '.sfdx' dir in isvdebugging  (#4521)
    * chore: add relativeToolsFolder
    
    * refactor: updated paths.test.ts
    
    * refactor: paths.test file
    
    * test: add jest test to core package
    
    * fix: lint error
    
    * test: updates for running jest unit test in core
    
    * test: add more jest test
    
    Co-authored-by: Gordon Bockus <gbockus@salesforce.com>
    Co-authored-by: gbockus-sf <76090802+gbockus-sf@users.noreply.github.com>

---
+++ packages/salesforcedx-vscode-core/test/jest/commands/isvdebugging/bootstrapCmd.test.ts
@@ -0,0 +1,37 @@
+import { projectPaths } from '@salesforce/salesforcedx-utils-vscode';
+import * as path from 'path';
+import { INSTALLED_PACKAGES, IsvDebugBootstrapExecutor, ISVDEBUGGER, PACKAGE_XML } from '../../../../src/commands/isvdebugging/bootstrapCmd';
+
+describe('isvdebugging unit test', () => {
+
+  const TOOLS_FOLDER = 'tools';
+  let relativeToolsFolderStub: jest.SpyInstance;
+
+  let isvDebugBootstrapExecutorInst: IsvDebugBootstrapExecutor;
+
+  beforeEach(() => {
+    relativeToolsFolderStub = jest.spyOn(projectPaths, 'relativeToolsFolder').mockReturnValue(TOOLS_FOLDER);
+  });
+
+  it('should be defined', () => {
+    isvDebugBootstrapExecutorInst = new IsvDebugBootstrapExecutor();
+    expect(isvDebugBootstrapExecutorInst).toBeDefined();
+  });
+
+  it('should test readonly relativeMetdataTempPath property', () => {
+    isvDebugBootstrapExecutorInst = new IsvDebugBootstrapExecutor();
+    expect(isvDebugBootstrapExecutorInst.relativeMetdataTempPath).toEqual(path.join(TOOLS_FOLDER, ISVDEBUGGER));
+    expect(relativeToolsFolderStub).toBeCalled();
+  });
+
+  it('should test readonly relativeApexPackageXmlPath property', () => {
+    isvDebugBootstrapExecutorInst = new IsvDebugBootstrapExecutor();
+    expect(isvDebugBootstrapExecutorInst.relativeApexPackageXmlPath).toEqual(path.join(isvDebugBootstrapExecutorInst.relativeMetdataTempPath, PACKAGE_XML));
+  });
+
+  it('should test readonly relativeInstalledPackagesPath property', () => {
+    isvDebugBootstrapExecutorInst = new IsvDebugBootstrapExecutor();
+    expect(isvDebugBootstrapExecutorInst.relativeInstalledPackagesPath).toEqual(path.join(TOOLS_FOLDER, INSTALLED_PACKAGES));
+    expect(relativeToolsFolderStub).toBeCalled();
+  });
+});
