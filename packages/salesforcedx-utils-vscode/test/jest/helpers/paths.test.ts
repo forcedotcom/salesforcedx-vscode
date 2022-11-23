d299ba0d6c63421dd29686c93cd1cac38dc6f586
CristiCanizales <cristina.canizales@salesforce.com>
2022-11-23 17:06:49 -0300

    test: increased coverage for paths

--- packages/salesforcedx-utils-vscode/test/jest/helpers/paths.test.ts
+++ packages/salesforcedx-utils-vscode/test/jest/helpers/paths.test.ts
@@ -1,4 +1,6 @@
+
 import * as path from 'path';
+import { WorkspaceContextUtil } from '../../../src';
 import { projectPaths, workspaceUtils } from '../../../src/';
 import { TOOLS } from '../../../src/helpers/paths';
 
@@ -14,7 +16,16 @@ jest.mock('@salesforce/core', () => {
 describe('test project paths', () => {
   const hasRootWorkspaceStub = jest.spyOn(workspaceUtils, 'hasRootWorkspace');
   const FAKE_WORKSPACE = '/here/is/a/fake/path/to/';
+  const FAKE_WORKSPACE_INSTANCE: any = {};
   const FAKE_STATE_FOLDER = '.sfdx';
+  const FAKE_METADATA = 'metadata';
+  const FAKE_ORGS = 'orgs';
+  const FAKE_TEST_RESULTS = 'testresults';
+  const FAKE_APEX = 'apex';
+  const FAKE_APEX_DB = 'apex.db';
+  const FAKE_DEBUG = 'debug';
+  const FAKE_LOGS = 'logs';
+  const FAKE_LWC = 'lwc';
   const FAKE_CONFIG_FILE = 'sfdx-config.json';
   const FAKE_PATH_TO_STATE_FOLDER = path.join(FAKE_WORKSPACE, FAKE_STATE_FOLDER);
 
@@ -76,6 +87,140 @@ describe('test project paths', () => {
     it('should return a path to the relative tools folder', () => {
       const relativeToolsFolder = projectPaths.relativeToolsFolder();
       expect(relativeToolsFolder).toEqual(path.join(FAKE_STATE_FOLDER, TOOLS));
+    });
+  });
+
+  describe('test toolsFolder', () => {
+    let stateFolderStub: jest.SpyInstance;
+
+    beforeEach(() => {
+      stateFolderStub = jest
+        .spyOn(projectPaths, 'stateFolder')
+        .mockReturnValue(FAKE_WORKSPACE);
+    });
+    it('should be defined', () => {
+      expect(projectPaths.toolsFolder).toBeDefined();
+    });
+
+    it('should return a path to the tools folder based on root workspace', () => {
+      const toolsFolder = projectPaths.toolsFolder();
+      expect(toolsFolder).toEqual(path.join(FAKE_WORKSPACE, TOOLS));
+    });
+  });
+
+  describe('test metadataFolder', () => {
+    jest.setTimeout(10000);
+    let stateFolderStub: jest.SpyInstance;
+    let usernameStub: jest.SpyInstance;
+    beforeEach(() => {
+      FAKE_WORKSPACE_INSTANCE.username = 'fakeUsername';
+      stateFolderStub = jest
+        .spyOn(projectPaths, 'stateFolder')
+        .mockReturnValue(FAKE_WORKSPACE);
+
+      usernameStub = jest
+      .spyOn(WorkspaceContextUtil, 'getInstance')
+      .mockReturnValue(FAKE_WORKSPACE_INSTANCE);
+    });
+    it('should be defined', () => {
+      expect(projectPaths.metadataFolder).toBeDefined();
+    });
+
+    it('should return a path to the metadata folder based on root workspace', () => {
+      // expect(usernameStub).toHaveBeenCalled();
+      const metadataFolder = projectPaths.metadataFolder();
+      const username = FAKE_WORKSPACE_INSTANCE.username;
+      expect(metadataFolder).toEqual(path.join(FAKE_WORKSPACE, FAKE_ORGS, username, FAKE_METADATA));
+    });
+  });
+
+  describe('test apexTestResultsFolder', () => {
+    let stateFolderStub: jest.SpyInstance;
+
+    beforeEach(() => {
+      stateFolderStub = jest
+        .spyOn(projectPaths, 'stateFolder')
+        .mockReturnValue(FAKE_WORKSPACE);
+    });
+    it('should be defined', () => {
+      expect(projectPaths.apexTestResultsFolder).toBeDefined();
+    });
+
+    it('should return a path to the apex test results folder based on root workspace', () => {
+      const apexTestResultsFolder = projectPaths.apexTestResultsFolder();
+      expect(apexTestResultsFolder).toEqual(path.join(FAKE_WORKSPACE, TOOLS, FAKE_TEST_RESULTS, FAKE_APEX));
+    });
+  });
+
+  describe('test apexLanguageServerDatabase', () => {
+    let stateFolderStub: jest.SpyInstance;
+
+    beforeEach(() => {
+      stateFolderStub = jest
+        .spyOn(projectPaths, 'stateFolder')
+        .mockReturnValue(FAKE_WORKSPACE);
+    });
+    it('should be defined', () => {
+      expect(projectPaths.apexLanguageServerDatabase).toBeDefined();
+    });
+
+    it('should return a path to the apex Language Server Database folder based on root workspace', () => {
+      const apexLanguageServerDatabase = projectPaths.apexLanguageServerDatabase();
+      expect(apexLanguageServerDatabase).toEqual(path.join(FAKE_WORKSPACE, TOOLS, FAKE_APEX_DB));
+    });
+  });
+
+  describe('test lwcTestResultsFolder', () => {
+    let stateFolderStub: jest.SpyInstance;
+
+    beforeEach(() => {
+      stateFolderStub = jest
+        .spyOn(projectPaths, 'stateFolder')
+        .mockReturnValue(FAKE_WORKSPACE);
+    });
+    it('should be defined', () => {
+      expect(projectPaths.lwcTestResultsFolder).toBeDefined();
+    });
+
+    it('should return a path to the lwc Test Results Folder based on root workspace', () => {
+      const lwcTestResultsFolder = projectPaths.lwcTestResultsFolder();
+      expect(lwcTestResultsFolder).toEqual(path.join(FAKE_WORKSPACE, TOOLS, FAKE_TEST_RESULTS, FAKE_LWC));
+    });
+  });
+
+  describe('test testResultsFolder', () => {
+    let stateFolderStub: jest.SpyInstance;
+
+    beforeEach(() => {
+      stateFolderStub = jest
+        .spyOn(projectPaths, 'stateFolder')
+        .mockReturnValue(FAKE_WORKSPACE);
+    });
+    it('should be defined', () => {
+      expect(projectPaths.testResultsFolder).toBeDefined();
+    });
+
+    it('should return a path to the  Test Results Folder based on root workspace', () => {
+      const testResultsFolder = projectPaths.testResultsFolder();
+      expect(testResultsFolder).toEqual(path.join(FAKE_WORKSPACE, TOOLS, FAKE_TEST_RESULTS));
+    });
+  });
+
+  describe('test debugLogsFolder', () => {
+    let stateFolderStub: jest.SpyInstance;
+
+    beforeEach(() => {
+      stateFolderStub = jest
+        .spyOn(projectPaths, 'stateFolder')
+        .mockReturnValue(FAKE_WORKSPACE);
+    });
+    it('should be defined', () => {
+      expect(projectPaths.debugLogsFolder).toBeDefined();
+    });
+
+    it('should return a path to the debug Logs folder based on root workspace', () => {
+      const debugLogsFolder = projectPaths.debugLogsFolder();
+      expect(debugLogsFolder).toEqual(path.join(FAKE_WORKSPACE, TOOLS, FAKE_DEBUG, FAKE_LOGS));
     });
   });
 });
