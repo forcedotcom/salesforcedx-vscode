20b335285942f7e0281a04603558f25fec2c8808
Gordon Bockus <gbockus@salesforce.com>
2022-12-09 09:43:13 -0600

    chore: fix int test regression

--- packages/salesforcedx-vscode-core/src/commands/util/parameterGatherers.ts
+++ packages/salesforcedx-vscode-core/src/commands/util/parameterGatherers.ts
@@ -162,7 +162,7 @@ export class SelectUsername
 }
 
 export class DemoModePromptGatherer implements ParametersGatherer<{}> {
-  private readonly LOGOUT_RESPONSE = CANCEL;
+  private readonly LOGOUT_RESPONSE = 'Cancel';
   private readonly DO_NOT_LOGOUT_RESPONSE = 'Authorize Org';
   private readonly prompt = nls.localize('demo_mode_prompt');
 
