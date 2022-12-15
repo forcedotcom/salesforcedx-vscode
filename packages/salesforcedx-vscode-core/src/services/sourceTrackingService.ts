b544732c8fbf5896202e8182775abebd95089ed8
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-12-15 14:40:31 -0800

    feat: use connection from workspaceCtxt
    to use the freshest data

--- packages/salesforcedx-vscode-core/src/services/sourceTrackingService.ts
+++ packages/salesforcedx-vscode-core/src/services/sourceTrackingService.ts
@@ -6,14 +6,12 @@
  */
 
 import { Org, SfProject } from '@salesforce/core';
-import {
-  ConfigUtil,
-  getRootWorkspacePath
-} from '@salesforce/salesforcedx-utils-vscode';
+import { getRootWorkspacePath } from '@salesforce/salesforcedx-utils-vscode';
 import {
   SourceTracking,
   SourceTrackingOptions
 } from '@salesforce/source-tracking';
+import { WorkspaceContext } from '../context/workspaceContext';
 
 export class SourceTrackingService {
   private _sourceTracking: SourceTracking | undefined;
@@ -25,9 +23,21 @@ export class SourceTrackingService {
   }
 
   public async createSourceTracking(): Promise<SourceTracking> {
+    const origCwd = process.cwd();
     const projectPath = getRootWorkspacePath();
-    const aliasOrUsername = await ConfigUtil.getDefaultUsernameOrAlias();
-    const org: Org = await Org.create({ aliasOrUsername });
+    if (process.cwd() !== projectPath) {
+      // Change the environment to get the node process to use
+      // the correct current working directory (process.cwd).
+      // Without this, process.cwd() returns "'/'" and SourceTracking.create() fails.
+      process.chdir(projectPath);
+    }
+
+    const connection = await WorkspaceContext.getInstance().getConnection();
+    // It is important to pass the connection from WorkspaceContext to
+    // Org.create() here.  Without this, core uses its cached version
+    // of State Aggregator and the "No auth info found" error can be
+    // thrown after creating a default scratch org.
+    const org: Org = await Org.create({ connection });
     const project = await SfProject.resolve(projectPath);
     const options: SourceTrackingOptions = {
       org,
@@ -37,14 +47,11 @@ export class SourceTrackingService {
       ignoreConflicts: false
     };
 
-    // Change the environment to get the node process to use
-    // the correct current working directory (process.cwd).
-    // Without this, process.cwd() returns "'/'" and SourceTracking.create() fails.
-    // const origCwd = process.cwd();
-    process.chdir(projectPath);
     const tracking = await SourceTracking.create(options);
-    // SourceTracking returns different results when process is changed back
-    // process.chdir(origCwd);
+    if (process.cwd() !== origCwd) {
+      // Change the directory back to the orig dir
+      process.chdir(origCwd);
+    }
     return tracking;
   }
 }
