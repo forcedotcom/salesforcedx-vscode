311648cd2da9121845e8b24ec5045b5972c7ecdd
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-12-15 10:23:39 -0800

    chore: test scaffold

--- packages/salesforcedx-vscode-core/test/vscode-integration/services/tracking/sourceTrackingService.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/services/tracking/sourceTrackingService.test.ts
@@ -5,12 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { SourceTracking } from '@salesforce/source-tracking';
-import { expect } from 'chai';
-import * as sinon from 'sinon';
-import { SourceTrackingService } from '../../../../src/services/sourceTrackingService';
-import { testData } from './testdata/sourceTracking';
-
 describe('createSourceTracking', () => {
   it('Should return an instance of SourceTracking', async () => {});
 });
