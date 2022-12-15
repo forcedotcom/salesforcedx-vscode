a71f11ce18f78256d05b8bcd866fe000cc528acf
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-12-15 15:57:22 -0800

    chore: align w other branch

--- packages/salesforcedx-vscode-core/src/services/sourceTrackingService.ts
+++ packages/salesforcedx-vscode-core/src/services/sourceTrackingService.ts
@@ -16,7 +16,7 @@ import {
   SourceTrackingOptions
 } from '@salesforce/source-tracking';
 import { StatusOutputRow } from '@salesforce/source-tracking';
-import { OrgAuthInfo } from '../util';
+import { WorkspaceContext } from '../context/workspaceContext';
 
 export class SourceTrackingService {
   private _sourceTracking: SourceTracking | undefined;
@@ -25,6 +25,40 @@ export class SourceTrackingService {
     if (sourceTracking !== undefined) {
       this._sourceTracking = sourceTracking;
     }
+  }
+
+  public async createSourceTracking(): Promise<SourceTracking> {
+    const origCwd = process.cwd();
+    const projectPath = getRootWorkspacePath();
+    if (process.cwd() !== projectPath) {
+      // Change the environment to get the node process to use
+      // the correct current working directory (process.cwd).
+      // Without this, process.cwd() returns "'/'" and SourceTracking.create() fails.
+      process.chdir(projectPath);
+    }
+
+    const connection = await WorkspaceContext.getInstance().getConnection();
+    // It is important to pass the connection from WorkspaceContext to
+    // Org.create() here.  Without this, core uses its cached version
+    // of State Aggregator and the "No auth info found" error can be
+    // thrown when deploying or retrieving immediately after creating a
+    // default scratch org.
+    const org: Org = await Org.create({ connection });
+    const project = await SfProject.resolve(projectPath);
+    const options: SourceTrackingOptions = {
+      org,
+      project,
+      ignoreLocalCache: true,
+      subscribeSDREvents: true,
+      ignoreConflicts: false
+    };
+
+    const tracking = await SourceTracking.create(options);
+    if (process.cwd() !== origCwd) {
+      // Change the directory back to the orig dir
+      process.chdir(origCwd);
+    }
+    return tracking;
   }
 
   public getSourceStatusSummary = async ({
@@ -46,30 +80,6 @@ export class SourceTrackingService {
       this._sourceTracking = await this.createSourceTracking();
     }
     return this._sourceTracking;
-  }
-
-  public async createSourceTracking(): Promise<SourceTracking> {
-    const projectPath = getRootWorkspacePath();
-    const aliasOrUsername = await OrgAuthInfo.getDefaultUsernameOrAlias(false);
-    const org: Org = await Org.create({ aliasOrUsername });
-    const project = await SfProject.resolve(projectPath);
-    const options: SourceTrackingOptions = {
-      org,
-      project,
-      ignoreLocalCache: true,
-      subscribeSDREvents: true,
-      ignoreConflicts: false
-    };
-
-    // Change the environment to get the node process to use
-    // the correct current working directory (process.cwd).
-    // Without this, process.cwd() returns "'/'" and SourceTracking.create() fails.
-    // const origCwd = process.cwd();
-    process.chdir(projectPath);
-    const tracking = await SourceTracking.create(options);
-    // SourceTracking returns different results when process is changed back
-    // process.chdir(origCwd);
-    return tracking;
   }
 }
 
