384953de753a3e088fc19909774d74b42ddf9c50
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-01-10 20:59:59 -0800

    chore: fix another

--- packages/salesforcedx-vscode-core/src/conflict/persistentStorageService.ts
+++ packages/salesforcedx-vscode-core/src/conflict/persistentStorageService.ts
@@ -7,7 +7,7 @@
 import { getRootWorkspacePath } from '@salesforce/salesforcedx-utils-vscode';
 import { FileProperties } from '@salesforce/source-deploy-retrieve';
 import { ExtensionContext, Memento } from 'vscode';
-import { workspaceContext } from '../context';
+import { WorkspaceContext } from '../context/workspaceContext';
 import { nls } from '../messages';
 
 interface ConflictFileProperties {
@@ -64,7 +64,7 @@ export class PersistentStorageService {
   }
 
   public makeKey(type: string, fullName: string): string {
-    const orgUserName = workspaceContext.username;
+    const orgUserName = WorkspaceContext.getInstance().username;
     const projectPath = getRootWorkspacePath();
     return `${orgUserName}#${projectPath}#${type}#${fullName}`;
   }
