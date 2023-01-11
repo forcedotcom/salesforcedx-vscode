410821ea77cc6ada1b1f384031bf458e32f10bd7
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-01-11 07:19:09 -0800

    chore: get around "Message is not a constructor"

--- packages/salesforcedx-vscode-core/test/jest/commands/deployExecutor2.test.ts
+++ packages/salesforcedx-vscode-core/test/jest/commands/deployExecutor2.test.ts
@@ -1,14 +1,34 @@
-import { ContinueResponse } from '@salesforce/salesforcedx-utils-vscode/src';
+import {
+  ContinueResponse,
+  getRootWorkspacePath
+} from '@salesforce/salesforcedx-utils-vscode';
+// import { nls } from '@salesforce/salesforcedx-utils-vscode/src/messages';
 import { ComponentSet } from '@salesforce/source-deploy-retrieve';
+import * as fs from 'fs';
 import { DeployExecutor } from '../../../src/commands/DeployExecutor';
 import { WorkspaceContext } from '../../../src/context/workspaceContext';
+import { nls } from '../../../src/messages';
 
 jest.mock('../../../src/context/workspaceContext', () => {
   return { getInstance: jest.fn() };
 });
+
+const dummyProjectPath = '/a/project/path';
+jest.mock('@salesforce/salesforcedx-utils-vscode', () => {
+  return { getRootWorkspacePath: () => dummyProjectPath };
+});
+// jest.mock('@salesforce/salesforcedx-utils-vscode/src/messages', () => {});
+jest.mock('../../../src/messages', () => {
+  return { loadMessageBundle: jest.fn() };
+});
+const n = nls;
+
 describe('Deploy Executor', () => {
+  const dummyProcessCwd = '/';
+
   beforeEach(async () => {
-    // jest.spyOn(WorkspaceContext, 'getInstance').mockReturnValue({} as any);
+    jest.spyOn(process, 'cwd').mockReturnValue(dummyProcessCwd);
+    jest.spyOn(fs, 'existsSync').mockReturnValue(true);
   });
 
   it('should create an instance of Source Tracking before deploying', async () => {
