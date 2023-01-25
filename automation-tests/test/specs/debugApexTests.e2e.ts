8be7c05be832acfd977e27569a62cbee229c6fae
CristiCanizales <cristina.canizales@salesforce.com>
2023-01-25 17:15:03 -0300

    test: update steps

--- automation-tests/test/specs/debugApexTests.e2e.ts
+++ automation-tests/test/specs/debugApexTests.e2e.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, salesforce.com, inc.
+ * Copyright (c) 2023, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
@@ -44,23 +44,23 @@ describe('Debug Apex Tests', async () => {
 
     /*
     if (successNotificationWasFound !== true) {
-      const failureNotificationWasFound = await utilities.notificationIsPresent(workbench, 'SFDX: Turn On Apex Debug Log for Replay Debugger... failed to run');
+      const failureNotificationWasFound = await utilities.notificationIsPresent(workbench, 'SFDX: Turn On Apex Debug Log for Replay Debugger failed to run');
       if (failureNotificationWasFound === true) {
         if (await utilities.textIsPresentInOutputPanel(workbench, 'sfdx-cli update available from')) {
-          // This is a known issue...
+          // This is a known issue
           utilities.log('Warning - Turning On Apex Debug Log for Replay Debugger failed, but the failure was due to the sfdx-cli being outdated');
         } else if (await utilities.textIsPresentInOutputPanel(workbench, 'The org cannot be found')) {
-          // This is a known issue...
+          // This is a known issue
           utilities.log('Warning - Verify that the org still exists');
           utilities.log('Warning - If your org is newly created, wait a minute and run your command again.');
           utilities.log('Warning - If you deployed or updated the org\'s My Domain, logout from the CLI and authenticate again.');
           utilities.log('Warning - If you are running in a CI environment with a DNS that blocks external IPs, try setting SFDX_DISABLE_DNS_CHECK=true.');
         } else {
-          // The failure notification is showing, but it's not due to a known reason.  What to do...?
-          utilities.log('Warning - Turning On Apex Debug Log for Replay Debugger failed... not sure why...');
+          // The failure notification is showing, but it's not due to a known reason.  What to do?
+          utilities.log('Warning - Turning On Apex Debug Log for Replay Debugger failed not sure why');
         }
       } else {
-        utilities.log('Warning - Turning On Apex Debug Log for Replay Debugger failed... neither the success notification or the failure notification was found.');
+        utilities.log('Warning - Turning On Apex Debug Log for Replay Debugger failed neither the success notification or the failure notification was found.');
       expect(failureNotificationWasFound).toBe(true);
       expect(successNotificationWasFound).toBe(false);
       }
@@ -86,11 +86,11 @@ describe('Debug Apex Tests', async () => {
       expect(failureNotificationWasFound).toBe(true);
     } else {
       // Select the "Standard" project type.
-      let quickPicks = await prompt.getQuickPicks();
+      const quickPicks = await prompt.getQuickPicks();
       expect(quickPicks).not.toBeUndefined();
       expect(quickPicks.length).toBeGreaterThanOrEqual(1);
       // expect(await quickPicks[0].getLabel()).toEqual('Standard');
-      await prompt.selectQuickPick('Standard');
+      // await prompt.selectQuickPick('Standard');
       expect(successNotificationWasFound).toBe(true);
     }
   });
@@ -98,21 +98,23 @@ describe('Debug Apex Tests', async () => {
   step('SFDX: Launch Apex Replay Debugger with Current File', async () => {
     const workbench = await (await browser.getWorkbench()).wait();
 
+    // set up: have a log file opened
+
     // Run SFDX: Launch Apex Replay Debugger with Current File
     prompt = await utilities.executeQuickPick(workbench, 'SFDX: Launch Apex Replay Debugger with Current File');
     await utilities.pause(1);
 
     // Wait for the command to execute
-    await utilities.waitForNotificationToGoAway(workbench, 'Running SFDX: Launch Apex Replay Debugger with Current File...', 5 * 60);
+    await utilities.waitForNotificationToGoAway(workbench, 'Running SFDX: Launch Apex Replay Debugger with Current File', 5 * 60);
     await utilities.pause(5);
 
-    const successNotificationWasFound = await utilities.notificationIsPresent(workbench, 'SFDX: Launch Apex Replay Debugger with Current File... successfully ran');
+    const successNotificationWasFound = await utilities.notificationIsPresent(workbench, 'SFDX: Launch Apex Replay Debugger with Current File successfully ran');
     if (successNotificationWasFound !== true) {
       const failureNotificationWasFound = await utilities.notificationIsPresent(workbench, 'You can only run this command with Anonymous Apex files, Apex Test files, or Apex Debug Log files.');
       if (failureNotificationWasFound === true) {
-        //TODO:
+        expect(successNotificationWasFound).toBe(false);
       } else {
-        utilities.log('Warning - creating the scratch org failed... neither the success notification or the failure notification was found.');
+        utilities.log('Warning - Launching Apex Replay Debugger with Current File failed, neither the success notification or the failure notification was found.');
       }
     }
     expect(successNotificationWasFound).toBe(true);
@@ -120,59 +122,51 @@ describe('Debug Apex Tests', async () => {
 
   step('SFDX: Launch Apex Replay Debugger with Last Log File', async () => {
     const workbench = await (await browser.getWorkbench()).wait();
+
+    // Run SFDX: Launch Apex Replay Debugger with Last Log File
     prompt = await utilities.executeQuickPick(workbench, 'SFDX: Launch Apex Replay Debugger with Last Log File');
     await utilities.pause(1);
-    await utilities.waitForNotificationToGoAway(workbench, 'Running SFDX: Launch Apex Replay Debugger with Last Log File...', 5 * 60);
 
-    const successNotificationWasFound = await utilities.notificationIsPresent(workbench, 'SFDX: Launch Apex Replay Debugger with Last Log File... successfully ran');
-    if (successNotificationWasFound !== true) {
-      const failureNotificationWasFound = await utilities.notificationIsPresent(workbench, 'SFDX: Launch Apex Replay Debugger with Last Log File... failed to run');
-      if (failureNotificationWasFound === true) {
-        //TODO:
-      } else {
-        utilities.log('Warning - creating the scratch org failed... neither the success notification or the failure notification was found.');
-      }
-    }
+    // Wait for the command to execute
+    await utilities.waitForNotificationToGoAway(workbench, 'Running SFDX: Launch Apex Replay Debugger with Last Log File', 5 * 60);
+    await utilities.pause(1);
+
+    const successNotificationWasFound = await utilities.notificationIsPresent(workbench, 'SFDX: Launch Apex Replay Debugger with Last Log File successfully ran');
     expect(successNotificationWasFound).toBe(true);
   });
 
   step('Run the Anonymous Apex Debugger using the Right-Click Menu', async () => {
-    // This is "SFDX: Execute Anonymous Apex with Currently Selected Text", using the Right-Click Menu.
     const workbench = await (await browser.getWorkbench()).wait();
+
+    // set up: file opened with some text selected
+
+    // Run SFDX: Launch Apex Replay Debugger with Currently Selected Text, using the Right-Click Menu.
     prompt = await utilities.executeQuickPick(workbench, 'SFDX: Execute Anonymous Apex with Currently Selected Text');
     await utilities.pause(1);
-    await utilities.waitForNotificationToGoAway(workbench, 'Running SFDX: Execute Anonymous Apex with Currently Selected Text...', 5 * 60);
 
-    const successNotificationWasFound = await utilities.notificationIsPresent(workbench, 'SFDX: Execute Anonymous Apex with Currently Selected Text... successfully ran');
-    if (successNotificationWasFound !== true) {
-      const failureNotificationWasFound = await utilities.notificationIsPresent(workbench, 'SFDX: Execute Anonymous Apex with Currently Selected Text... failed to run');
-      if (failureNotificationWasFound === true) {
-        //TODO:
-      } else {
-        utilities.log('Warning - creating the scratch org failed... neither the success notification or the failure notification was found.');
-      }
-    }
+    // Wait for the command to execute
+    await utilities.waitForNotificationToGoAway(workbench, 'Running Execute Anonymous Apex', 5 * 60);
+    await utilities.pause(1);
+
+    const successNotificationWasFound = await utilities.notificationIsPresent(workbench, 'Execute Anonymous Apex successfully ran');
     expect(successNotificationWasFound).toBe(true);
   });
 
   step('Run the Anonymous Apex Debugger using the Command Palette', async () => {
-    // This is "SFDX: Execute Anonymous Apex with Editor Contents", using the Command Palette.
     const workbench = await (await browser.getWorkbench()).wait();
+
+    // Open a file so the command can run
+    // await utilities.openFile(, 'force-app/main/default/classes/*.cls');
     await utilities.pause(1);
 
+   // Run SFDX: Launch Apex Replay Debugger with Editor Contents", using the Command Palette.
     prompt = await utilities.executeQuickPick(workbench, 'SFDX: Execute Anonymous Apex with Editor Contents');
     await utilities.pause(1);
-    await utilities.waitForNotificationToGoAway(workbench, 'Running SFDX: Execute Anonymous Apex with Editor Contents...', 5 * 60);
 
-    const successNotificationWasFound = await utilities.notificationIsPresent(workbench, 'SFDX: Execute Anonymous Apex with Editor Contents... successfully ran');
-    if (successNotificationWasFound !== true) {
-      const failureNotificationWasFound = await utilities.notificationIsPresent(workbench, 'SFDX: Execute Anonymous Apex with Editor Contents... failed to run');
-      if (failureNotificationWasFound === true) {
-        //TODO:
-      } else {
-        utilities.log('Warning - creating the scratch org failed... neither the success notification or the failure notification was found.');
-      }
-    }
+    // Wait for the command to execute
+    await utilities.waitForNotificationToGoAway(workbench, 'Running Execute Anonymous Apex', 5 * 60);
+    await utilities.pause(5);
+    const successNotificationWasFound = await utilities.notificationIsPresent(workbench, 'Execute Anonymous Apex successfully ran');
     expect(successNotificationWasFound).toBe(true);
   });
 
--- automation-tests/test/wdio.conf.ts
+++ automation-tests/test/wdio.conf.ts
@@ -80,9 +80,9 @@ export const config: Options.Testrunner = {
     // './test/specs/**/*.e2e.ts'
     [
       // Place inside the array to run sequentially.
-      './test/specs/**/*.e2e.ts'
+      // './test/specs/**/*.e2e.ts'
       // './test/specs/**/anInitialSuite.e2e.ts'
-      // './test/specs/**/debugApexTests.e2e.ts'
+      './test/specs/**/debugApexTests.e2e.ts'
       // './test/specs/**/functions.e2e.ts'
       // './test/specs/**/orgBrowser.e2e.ts'
       // './test/specs/**/orgCreationAndAuthentication.e2e.ts'
