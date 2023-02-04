31168474103cbbfd20babad064a858f16edff888
CristiCanizales <cristina.canizales@salesforce.com>
2023-02-04 00:36:57 -0300

    chore: let to const

--- packages/salesforcedx-vscode-apex/src/commands/forceLaunchApexReplayDebuggerWithCurrentFile.ts
+++ packages/salesforcedx-vscode-apex/src/commands/forceLaunchApexReplayDebuggerWithCurrentFile.ts
@@ -81,13 +81,14 @@ async function getApexTestClassName(
   }
 
   await testOutlineProvider.refresh();
-  let testClassName = testOutlineProvider.getTestClassName(sourceUri);
+  const testClassName = testOutlineProvider.getTestClassName(sourceUri);
   // This is a little bizarre.  Intellisense is reporting that getTestClassName() returns a string,
   // but it actually it returns string | undefined.  Well, regardless, since flushFilePath() takes
   // a string (and guards against empty strings) using the Non-null assertion operator
   // (https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator)
   // fixes the issue.
-  testClassName = fileUtils.flushFilePath(testClassName || '');
+  // cico
+  // testClassName = fileUtils.flushFilePath(testClassName || '');
 
   return testClassName;
 }
