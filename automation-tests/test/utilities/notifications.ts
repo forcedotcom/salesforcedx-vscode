2d38af6a5065eaf50253ba6ce4c1161937fbd4db
CristiCanizales <cristina.canizales@salesforce.com>
2023-02-20 16:08:19 -0300

    chore: unused import

--- automation-tests/test/utilities/notifications.ts
+++ automation-tests/test/utilities/notifications.ts
@@ -5,7 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { match } from 'assert';
 import {
   Workbench
 } from 'wdio-vscode-service';
