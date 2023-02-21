bcd7db3739d1b7a10064baeeb615d546bf9aafdf
Ritam Agrawal <ritam.kumari.27@gmail.com>
2023-02-21 15:34:48 -0800

    chore: add more tests

--- automation-tests/test/specs/sObjectsDefinitions.e2e.ts
+++ automation-tests/test/specs/sObjectsDefinitions.e2e.ts
@@ -22,25 +22,30 @@ describe('SObjects Definitions', async () => {
     await scratchOrg.setUp();
     const projectPath = scratchOrg.projectFolderPath;
     const source = '/Users/ritam.agrawal/workspace/salesforcedx-vscode/automation-tests/test/testData/CustomSObjects'
-    const destination = path.join(projectPath,'force-app/main/default/objects');
-    // Copy custom sobjects from source to destination
-    fs.cp(source, destination, { recursive: true }, (err) => {
-      if (err) {
-        console.error(err);
+
+    // @ts-ignore: Object is possibly 'undefined'
+    const destination = path.join(projectPath,'force-app', 'main', 'default', 'objects');
+
+    fs.cp(source, destination, { recursive: true }, async (error) => {
+      if (error) {
+        await scratchOrg.tearDown();
+        return error
       }
     });
   });
 
-  // Create a -meta.xml file through CP in force-app/main/default/objects
-  step('Custom Objects Customer and Product are within objects folder', async () => {
+  step('Check Custom Objects Customer and Product are within objects folder', async () => {
     const workbench = await browser.getWorkbench();
     const sidebar = workbench.getSideBar();
     const content = sidebar.getContent();
+
     const treeViewSection = await content.getSection(tempProjectName.toUpperCase());
     expect(treeViewSection).not.toEqual(undefined);
+
     const objectTreeItem = await treeViewSection.findItem('objects') as DefaultTreeItem;
     expect(objectTreeItem).not.toEqual(undefined);
     await objectTreeItem.select();
+  
     const customerObject = await objectTreeItem.findChildItem('Customer__c');
     expect(customerObject).not.toEqual(undefined);
     await customerObject?.expand();
@@ -53,81 +58,94 @@ describe('SObjects Definitions', async () => {
     const workbench = await browser.getWorkbench();
     await utilities.runCommandFromCommandPrompt(workbench, 'SFDX: Push Source to Default Scratch Org', 5);
     await utilities.pause(1);
+
     const successNotificationWasFound = await utilities.attemptToFindNotification(workbench, 'SFDX: Push Source to Default Scratch Org successfully ran', 10);
     expect(successNotificationWasFound).toBe(true);
+
+    const outputPanelText = await utilities.attemptToFindOutputPanelText('Salesforce CLI', 'Starting SFDX: Push Source to Default Scratch Org', 5);
+    expect(outputPanelText).not.toBeUndefined();
+    expect(outputPanelText).toMatch('Pushed Source');
   });
-  
+
   step('Refresh SObject Definitions for Custom SObjects', async () => {
-    // Type'Sfdx: Refresh SObject Definitions in the Command Pallette'
     const workbench = await browser.getWorkbench();
     const prompt = await utilities.runCommandFromCommandPrompt(workbench, 'SFDX: Refresh SObject Definitions', 2);
-    // Select "Custom SObjects" from the Drop down menu .
     await prompt.setText('Custom SObjects');
     await prompt.selectQuickPick('Custom SObjects');
     await utilities.pause(1);
+
     const successNotificationWasFound = await utilities.attemptToFindNotification(workbench, 'SFDX: Refresh SObject Definitions successfully ran', 10);
     expect(successNotificationWasFound).toBe(true);
+
     const outputPanelText = await utilities.attemptToFindOutputPanelText('Salesforce CLI', 'sObjects', 10);
     expect(outputPanelText).not.toBeUndefined();
 
     // Search for 'Processed xxx Custom sObjects'
     const matchedResults = outputPanelText?.match(/Processed [0-9]{1,} Custom sObjects/gm);
     expect(matchedResults).not.toBe(undefined);
-    // @ts-ignore: Object is possibly 'null'
-    expect(matchedResults.length).toBe(1);
-    // @ts-ignore: Object is possibly 'null'
-    const customObjectCount = parseInt(matchedResults[matchedResults.length - 1].match(/[0-9]{1,}/)[0]);
+    expect(matchedResults!.length).toBe(1);
+    const customObjectCount = parseInt(matchedResults![matchedResults!.length - 1].match(/[0-9]{1,}/)![0]);
+
+    // The total number of custom objects created is 2
     expect(customObjectCount).toBe(2);
 
     const sidebar = workbench.getSideBar();
     const content = sidebar.getContent();
     const treeViewSection = await content.getSection(tempProjectName.toUpperCase());
     expect(treeViewSection).not.toEqual(undefined);
+
     // Verify if '.sfdx' folder is in side panel
     const sfdxTreeItem = await treeViewSection.findItem('.sfdx') as DefaultTreeItem;
     expect(sfdxTreeItem).not.toEqual(undefined);
     await sfdxTreeItem.expand();
     expect(await sfdxTreeItem.isExpanded()).toBe(true);
     await utilities.pause(1);
+
     // Verify if 'tools' folder is within '.sfdx'
     const toolsTreeItem = await sfdxTreeItem.findChildItem('tools') as TreeItem;
     expect(toolsTreeItem).not.toEqual(undefined);
     await toolsTreeItem.expand();
     expect(await toolsTreeItem.isExpanded()).toBe(true);
     await utilities.pause(1);
+
     // Verify if 'sobjects' folder is within 'tools'
     const sobjectsTreeItem = await toolsTreeItem.findChildItem('sobjects') as TreeItem;
     expect(sobjectsTreeItem).not.toEqual(undefined);
     await sobjectsTreeItem.expand();
     expect(await sobjectsTreeItem.isExpanded()).toBe(true);
     await utilities.pause(1);
+
     // Verify if 'customObjects' folder is within 'sobjects'
     const customObjectsTreeItem = await sobjectsTreeItem.findChildItem('customObjects') as TreeItem;
     await customObjectsTreeItem.expand();
     expect(await customObjectsTreeItem.isExpanded()).toBe(true);
     await utilities.pause(1);
+
     expect(await treeViewSection.findItem('Customer__c.cls')).not.toBe(undefined);
     expect(await treeViewSection.findItem('Product__c.cls')).not.toBe(undefined);
   });
+
   step('Refresh SObject Definitions for Standard SObjects', async () => {
-    // Type'Sfdx: Refresh SObject Definitions in the Command Pallette'
     const workbench = await browser.getWorkbench();
     const prompt = await utilities.runCommandFromCommandPrompt(workbench, 'SFDX: Refresh SObject Definitions', 5);
-    // Select "Standard SObjects" from the Drop down menu .
     await prompt.setText('Standard SObjects');
     await prompt.selectQuickPick('Standard SObjects');
     await utilities.pause(1);
-    // Expectation: Look for notification with text 'Sfdx: Refresh SObject Definitions successfully ran'
+
     const successNotificationWasFound = await utilities.attemptToFindNotification(workbench, 'SFDX: Refresh SObject Definitions successfully ran', 10);
     expect(successNotificationWasFound).toBe(true);
-    // Search for 'Standard sObjects' to obtain the whole text in output panel'
+
+    // Search for 'sObjects' to obtain the whole text in output panel'
     const outputPanelText = await utilities.attemptToFindOutputPanelText('Salesforce CLI', 'sObjects', 10);
     expect(outputPanelText).not.toBeUndefined();
+
     // Search for 'Processed xxx Standard sObjects'
     const matchedResults = outputPanelText?.match(/Processed [0-9]{1,} Standard sObjects/gm);
     expect(matchedResults).not.toBe(undefined);
+
     // @ts-ignore: Object is possibly 'null'
     expect(matchedResults.length).toBeGreaterThanOrEqual(2)
+
     // @ts-ignore: Object is possibly 'null'
     const sObjectCount = parseInt(matchedResults[matchedResults.length - 1].match(/[0-9]{1,}/)[0]);
     expect(sObjectCount).toBeGreaterThan(100);
@@ -137,43 +155,58 @@ describe('SObjects Definitions', async () => {
     const content = sidebar.getContent();
     const treeViewSection = await content.getSection(tempProjectName.toUpperCase());
     expect(treeViewSection).not.toEqual(undefined);
+
     // Verify if 'standardObjects' folder is in side panel
     const standardObjectsTreeItem = await treeViewSection.findItem('standardObjects') as DefaultTreeItem;
     expect(standardObjectsTreeItem).not.toEqual(undefined);
     await standardObjectsTreeItem.expand();
     expect(await standardObjectsTreeItem.isExpanded()).toBe(true);
     await utilities.pause(1);
+
     expect(await treeViewSection.findItem('Account.cls')).not.toBe(undefined);
     expect(await treeViewSection.findItem('AccountCleanInfo.cls')).not.toBe(undefined);
     expect(await treeViewSection.findItem('AcceptedEventRelation.cls')).not.toBe(undefined);
   });
+
   step('Refresh SObject Definitions for All SObjects', async () => {
+
     // Clear the output for correct test validation.
     const outputView = await utilities.openOutputView();
     outputView.clearText();
-    // Type'Sfdx: Refresh SObject Definitions in the Command Pallette'
+
     const workbench = await browser.getWorkbench();
     const prompt = await utilities.runCommandFromCommandPrompt(workbench, 'SFDX: Refresh SObject Definitions', 5);
-    // Select "All SObjects" from the Drop down menu .
     await prompt.setText('All SObjects');
     await prompt.selectQuickPick('All SObjects');
     await utilities.pause(1);
-    // Expectation: Look for notification with text 'Sfdx: Refresh SObject Definitions successfully ran'
+
     const successNotificationWasFound = await utilities.attemptToFindNotification(workbench, 'SFDX: Refresh SObject Definitions successfully ran', 10);
     expect(successNotificationWasFound).toBe(true);
-    // Search for 'Standard sObjects' to obtain the whole text in output panel'
+
+    // Search for 'sObjects' to obtain the whole text in output panel'
     const outputPanelText = await utilities.attemptToFindOutputPanelText('Salesforce CLI', 'sObjects', 10);
     expect(outputPanelText).not.toBeUndefined();
+
     // Search for 'Processed xxx Standard sObjects'
-    const matchedResults = outputPanelText?.match(/Processed [0-9]{1,} [A-Za-z]{1,} sObjects/gm);
-    expect(matchedResults).not.toBe(undefined);
+    const matchedStandardResults = outputPanelText?.match(/Processed [0-9]{1,} Standard sObjects/gm);
+    expect(matchedStandardResults).not.toBe(undefined);
+
     // @ts-ignore: Object is possibly 'null'
-    expect(matchedResults.length).toBeGreaterThanOrEqual(2)
+    expect(matchedStandardResults.length).toBe(1);
+
     // @ts-ignore: Object is possibly 'null'
-    const sObjectCount = parseInt(matchedResults[0].match(/[0-9]{1,}/)[0]);
-    expect(sObjectCount).toBeGreaterThan(400);
+    const standardObjectCount = parseInt(matchedStandardResults[0].match(/[0-9]{1,}/)[0]);
+    expect(standardObjectCount).toBeGreaterThan(400);
+
+    // Search for 'Processed xxx Custom sObjects'
+    const matchedCustomResults = outputPanelText?.match(/Processed [0-9]{1,} Custom sObjects/gm);
+    expect(matchedCustomResults).not.toBe(undefined);
+
     // @ts-ignore: Object is possibly 'null'
-    const customObjectCount = parseInt(matchedResults[matchedResults.length - 1].match(/[0-9]{1,}/)[0]);
+    expect(matchedCustomResults.length).toBe(1);
+
+    // @ts-ignore: Object is possibly 'null'
+    const customObjectCount = parseInt(matchedCustomResults[0].match(/[0-9]{1,}/)[0]);
     expect(customObjectCount).toBe(2);
   });
 
