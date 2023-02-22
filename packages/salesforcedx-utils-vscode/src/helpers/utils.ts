505ff4afa26b38dcc865bc6db4a7741cf3561972
Jeff Beeghly <jbeeghly@salesforce.com>
2023-02-22 13:21:48 -0800

    fix: updated comments on expected filePath formats

--- packages/salesforcedx-utils-vscode/src/helpers/utils.ts
+++ packages/salesforcedx-utils-vscode/src/helpers/utils.ts
@@ -27,8 +27,8 @@ export function flushFilePath(filePath: string): string {
   // The filePath should be a file system file path and not a VS Code URI string.
   // (no scheme ("file:/") should be present)
   // Examples:
-  // filePath is in the format of "/Users/{user-name}/{path-to-apex-file.cls}" on Mac OSX
-  // filePath is in the format of "c:\\Users\\{user-name}\\{path-to-apex-file.cls}" on Windows
+  // "/Users/{user-name}/{path-to-apex-file.cls}" on Mac OSX
+  // "c:\\Users\\{user-name}\\{path-to-apex-file.cls}" on Windows
   if (filePath === '') {
     return filePath;
   }
--- packages/salesforcedx-vscode-apex/src/commands/forceLaunchApexReplayDebuggerWithCurrentFile.ts
+++ packages/salesforcedx-vscode-apex/src/commands/forceLaunchApexReplayDebuggerWithCurrentFile.ts
@@ -98,8 +98,8 @@ async function getApexTestClassName(
   const filePath = sourceUri.toString();
   // The filePath should be a VS Code URI string and not a file system file path.
   // ("file:/" should be present)
-  // filePath is in the format of "file:///Users/{user-name}/{path-to-apex-file.cls}" on Mac OSX
-  // filePath is in the format of "file:///c%3A/Users/{user-name}/{path-to-apex-file.cls}" on Windows
+  // "file:///Users/{user-name}/{path-to-apex-file.cls}" on Mac OSX
+  // "file:///c%3A/Users/{user-name}/{path-to-apex-file.cls}" on Windows
 
   const testClassName = testOutlineProvider.getTestClassName(filePath);
 
