c4c3af7073d5fc5d04c0c76b08e07bf46277ac5c
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-02-25 13:59:43 -0800

    feat: add stl serv

---
+++ packages/salesforcedx-utils-vscode/src/services/sourceTrackingService.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { Connection, Org, SfProject } from '@salesforce/core';
+import {
+  SourceTracking,
+  SourceTrackingOptions
+} from '@salesforce/source-tracking';
+
+export class SourceTrackingService {
+  public static async createSourceTracking(
+    projectPath: string,
+    connection: Connection
+  ): Promise<SourceTracking> {
+    const project = await SfProject.resolve(projectPath);
+    const org = await Org.create({ connection });
+    const options: SourceTrackingOptions = {
+      org,
+      project,
+      ignoreLocalCache: true,
+      subscribeSDREvents: true,
+      ignoreConflicts: false
+    };
+    const sourceTracking = await SourceTracking.create(options);
+    return sourceTracking;
+  }
+}
