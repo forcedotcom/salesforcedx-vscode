5dc486dde00e2d515236543bc958db152cadf6dc
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-03-14 15:03:12 -0700

    test: building out draft tests

---
+++ packages/salesforcedx-vscode-core/test/jest/commands/util/baseDeployRetrieve.test.ts
@@ -0,0 +1,6 @@
+describe('DeployRetrieveExecutor', () => {
+  describe('run', () => {
+    it('should handle a SourceConflictError', async () => {});
+    it('should not handle a non-SourceConflictError', async () => {});
+  });
+});
