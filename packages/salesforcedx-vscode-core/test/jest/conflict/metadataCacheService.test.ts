f5daae02a4bb440b869ee2614aaba947da8d0bfc
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-03-14 15:26:42 -0700

    test: draft tests

---
+++ packages/salesforcedx-vscode-core/test/jest/conflict/metadataCacheService.test.ts
@@ -0,0 +1,5 @@
+describe('MetadataCacheService', () => {
+  describe('createRetrieveOperation', () => {
+    it('should use the suppressEvents option when retrieving to be able to retrieve even when conflicts exist', async () => {});
+  });
+});
---
+++ packages/salesforcedx-vscode-core/test/jest/conflict/timestampConflictDetector.test.ts
@@ -0,0 +1,5 @@
+describe('TimestampConflictDetector', () => {
+  describe('createDiffs', () => {
+    it('should return diff results for all files passed in when the skipTimestampCheck option is used', async () => {});
+  });
+});
