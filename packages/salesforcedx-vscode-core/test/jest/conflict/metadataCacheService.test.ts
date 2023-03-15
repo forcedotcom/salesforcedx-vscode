0f38c249a16b284f3e5addefd332868fb1f3f0c8
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-03-14 21:23:14 -0700

    test: draft metadata cache serv test

--- packages/salesforcedx-vscode-core/test/jest/conflict/metadataCacheService.test.ts
+++ packages/salesforcedx-vscode-core/test/jest/conflict/metadataCacheService.test.ts
@@ -1,5 +1,52 @@
+import { ComponentSet } from '@salesforce/source-deploy-retrieve';
+import { MetadataCacheService } from '../../../src/conflict';
+import { WorkspaceContext } from '../../../src/context';
+import * as sdrUtils from '../../../src/services/sdr/componentSetUtils';
+
 describe('MetadataCacheService', () => {
   describe('createRetrieveOperation', () => {
-    it('should use the suppressEvents option when retrieving to be able to retrieve even when conflicts exist', async () => {});
+    const dummyComponentSet = new ComponentSet([
+      { fullName: 'Test', type: 'apexclass' },
+      { fullName: 'Test2', type: 'layout' }
+    ]);
+    let retrieveStub: jest.SpyInstance;
+    let getSourceComponentsStub: jest.SpyInstance;
+    let setApiVersionOnStub: jest.SpyInstance;
+    let setApiVersionOnStub2: jest.SpyInstance;
+    let componentSetRetrieveStub: jest.SpyInstance;
+
+    beforeEach(() => {
+      retrieveStub = jest
+        .spyOn(dummyComponentSet, 'retrieve')
+        .mockResolvedValue({} as any);
+      componentSetRetrieveStub = jest
+        .spyOn(ComponentSet.prototype, 'retrieve')
+        .mockResolvedValue({} as any);
+      setApiVersionOnStub = jest
+        .spyOn(sdrUtils, 'setApiVersionOn')
+        .mockImplementation(jest.fn());
+      setApiVersionOnStub2 = jest
+        .spyOn(WorkspaceContext, 'getInstance')
+        .mockReturnValue({
+          getConnection: async () => {
+            return {};
+          }
+        } as any);
+      getSourceComponentsStub = jest
+        .spyOn(MetadataCacheService.prototype, 'getSourceComponents')
+        .mockResolvedValue(dummyComponentSet);
+    });
+
+    it('should use the suppressEvents option to retrieve files with conflicts', async () => {
+      const m = new MetadataCacheService('');
+
+      const o = await m.createRetrieveOperation();
+
+      expect(setApiVersionOnStub).toHaveBeenCalledWith(dummyComponentSet);
+      const dummyRetrieveOptionsWithSuppressEvents = { suppressEvents: true };
+      expect(retrieveStub).toHaveBeenCalledWith(
+        expect.objectContaining(dummyRetrieveOptionsWithSuppressEvents)
+      );
+    });
   });
 });
