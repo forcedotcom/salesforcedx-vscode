6dc7bc6b4a8c6d407d8579a8bbbf1cff689365c5
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-03-31 16:24:01 -0700

    test: draft unit test for workspaceContext

---
+++ packages/salesforcedx-vscode-core/test/jest/context/workspaceContext.test.ts
@@ -0,0 +1,9 @@
+describe('workspaceContext', () => {
+  describe('handleCliConfigChange', () => {
+    it('should update context variables and UI elements when the config file changes', () => {});
+  });
+  describe('setIsAScratchOrg', () => {
+    it('should set the sfdx:is_a_scratch_org to true when connected to a scratch org', () => {});
+    it('should set the sfdx:is_a_scratch_org to false when NOT connected to a scratch org', () => {});
+  });
+});
