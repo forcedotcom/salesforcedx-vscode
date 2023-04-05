72b7af0930e9d43bbc69fda346146fa6112276dd
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-04-05 07:26:04 -0700

    feat: remove stl from deploy for this pr

--- packages/salesforcedx-vscode-core/test/vscode-integration/commands/baseDeployRetrieve.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/baseDeployRetrieve.test.ts
@@ -265,9 +265,6 @@ describe('Base Deploy Retrieve Commands', () => {
       setApiVersionOnStub = sb.stub(componentSetUtils, 'setApiVersionOn');
       const mockExtensionContext = new MockExtensionContext(false);
       PersistentStorageService.initialize(mockExtensionContext);
-      sb.stub(SourceTrackingService, 'createSourceTracking').resolves({
-        ensureLocalTracking: async () => {}
-      });
     });
 
     class TestDeploy extends DeployExecutor<{}> {
