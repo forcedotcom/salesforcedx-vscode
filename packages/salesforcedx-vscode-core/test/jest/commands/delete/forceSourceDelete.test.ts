6e63d6637d586c1f01d95163711c9f182ac87095
Ritam Agrawal <ritam.kumari.27@gmail.com>
2023-04-06 15:50:09 -0700

    feat: remove test

--- packages/salesforcedx-vscode-core/test/jest/commands/delete/forceSourceDelete.test.ts
+++ packages/salesforcedx-vscode-core/test/jest/commands/delete/forceSourceDelete.test.ts
@@ -9,21 +9,12 @@ import * as path from 'path';
 import { ForceSourceDeleteExecutor } from '../../../../src/commands';
 
 describe('Force Source Delete', () => {
-  it('Should build the source delete command', () => {
+  it('Should build the source delete command with --tracksource flag', () => {
     const executor = new ForceSourceDeleteExecutor();
     const sourcePath = path.join('example', 'path');
     const sourceDeleteCommand = executor.build({ filePath: sourcePath });
     expect(sourceDeleteCommand.toCommand()).toEqual(
       `sfdx force:source:delete --sourcepath ${sourcePath} --noprompt --tracksource`
-    );
-  });
-
-  it('Should not build the source delete command without --tracksource flag', () => {
-    const executor = new ForceSourceDeleteExecutor();
-    const sourcePath = path.join('example', 'path');
-    const sourceDeleteCommand = executor.build({ filePath: sourcePath });
-    expect(sourceDeleteCommand.toCommand()).not.toEqual(
-      `sfdx force:source:delete --sourcepath ${sourcePath} --noprompt`
     );
   });
 });
