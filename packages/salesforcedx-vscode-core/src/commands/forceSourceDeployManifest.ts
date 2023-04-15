241c9a6e2bb5c35046d651fd2e60124d016801fb
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-04-15 07:03:51 -0700

    refactor: remove unused

--- packages/salesforcedx-vscode-core/src/commands/forceSourceDeployManifest.ts
+++ packages/salesforcedx-vscode-core/src/commands/forceSourceDeployManifest.ts
@@ -4,10 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import {
-  ContinueResponse,
-  SfdxCommandBuilder
-} from '@salesforce/salesforcedx-utils-vscode';
+import { ContinueResponse } from '@salesforce/salesforcedx-utils-vscode';
 import { ComponentSet } from '@salesforce/source-deploy-retrieve';
 import { join } from 'path';
 import * as vscode from 'vscode';
