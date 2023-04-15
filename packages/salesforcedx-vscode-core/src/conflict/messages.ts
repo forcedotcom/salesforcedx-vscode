ff0d194340ad6da286509400e93b60bac4901b10
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-04-15 07:19:22 -0700

    refactor: replace logic at import time

--- packages/salesforcedx-vscode-core/src/conflict/messages.ts
+++ packages/salesforcedx-vscode-core/src/conflict/messages.ts
@@ -7,42 +7,55 @@
 import { SfdxCommandBuilder } from '@salesforce/salesforcedx-utils-vscode';
 import { ConflictDetectionMessages } from '../commands/util';
 
-const messagesByLogName: Map<string, ConflictDetectionMessages> = new Map();
-const warningMessageKey = 'conflict_detect_conflicts_during_deploy';
-
-messagesByLogName.set('force_source_deploy_with_sourcepath_beta', {
-  warningMessageKey,
-  commandHint: inputs => {
-    const commands: string[] = [];
-    (inputs as string[]).forEach(input => {
-      commands.push(
-        new SfdxCommandBuilder()
-          .withArg('force:source:deploy')
-          .withFlag('--sourcepath', input)
-          .build()
-          .toString()
-      );
-    });
-    const hints = commands.join('\n  ');
-
-    return hints;
-  }
-});
-
-messagesByLogName.set('force_source_deploy_with_manifest_beta', {
-  warningMessageKey,
-  commandHint: input => {
-    return new SfdxCommandBuilder()
-      .withArg('force:source:deploy')
-      .withFlag('--manifest', input as string)
-      .build()
-      .toString();
-  }
-});
-
 export function getConflictMessagesFor(
   logName: string
 ): ConflictDetectionMessages | undefined {
+  const warningMessageKey = 'conflict_detect_conflicts_during_deploy';
+  const messagesByLogName: Map<string, ConflictDetectionMessages> = new Map([
+    [
+      'force_source_deploy_with_sourcepath_beta',
+      {
+        warningMessageKey,
+        commandHint: inputs => {
+          const commands: string[] = [];
+          (inputs as string[]).forEach(input => {
+            commands.push(
+              new SfdxCommandBuilder()
+                .withArg('force:source:deploy')
+                .withFlag('--sourcepath', input)
+                .build()
+                .toString()
+            );
+          });
+          const hints = commands.join('\n  ');
+
+          return hints;
+        }
+      }
+    ],
+    [
+      'force_source_deploy_with_sourcepath_beta',
+      {
+        warningMessageKey,
+        commandHint: inputs => {
+          const commands: string[] = [];
+          (inputs as string[]).forEach(input => {
+            commands.push(
+              new SfdxCommandBuilder()
+                .withArg('force:source:deploy')
+                .withFlag('--sourcepath', input)
+                .build()
+                .toString()
+            );
+          });
+          const hints = commands.join('\n  ');
+
+          return hints;
+        }
+      }
+    ]
+  ]);
+
   const conflictMessages = messagesByLogName.get(logName);
   if (!conflictMessages) {
     throw new Error(`No conflict messages found for ${logName}`);
