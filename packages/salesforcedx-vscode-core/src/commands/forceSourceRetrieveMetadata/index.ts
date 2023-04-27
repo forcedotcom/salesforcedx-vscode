cc2e657f46cff9f385e666e8153b0761490d4ca6
Jeff Beeghly <jbeeghly@salesforce.com>
2023-04-27 16:16:37 -0700

    fix: split files up

--- packages/salesforcedx-vscode-core/src/commands/forceSourceRetrieveMetadata/index.ts
+++ packages/salesforcedx-vscode-core/src/commands/forceSourceRetrieveMetadata/index.ts
@@ -4,3 +4,8 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
+export { RetrieveDescriber } from './retrieveDescriber';
+export { forceSourceRetrieveCmp } from './forceSourceRetrieveCmp';
+export { RetrieveMetadataTrigger } from './retrieveMetadataTrigger';
+export { RetrieveDescriberFactory } from './retrieveDescriberFactory';
