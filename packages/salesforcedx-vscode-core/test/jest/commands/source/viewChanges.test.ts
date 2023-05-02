9d670e05e14b916be2262e2964092c83a9850151
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-05-02 16:27:05 -0700

    test: draft tests

---
+++ packages/salesforcedx-vscode-core/test/jest/commands/source/viewChanges.test.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+describe('viewChanges', () => {
+  describe('viewAllChanges', () => {
+    it('should get both local and remote changes', () => {});
+  });
+  describe('viewLocalChanges', () => {
+    it('should get local changes', () => {});
+  });
+  describe('viewRemoteChanges', () => {
+    it('should get remote changes', () => {});
+  });
+});
