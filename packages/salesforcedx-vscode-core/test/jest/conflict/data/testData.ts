0873ac0de8b7e2301306e79cc0c00a623865a60c
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-05-01 21:02:30 -0700

    chore: add back copywright

--- packages/salesforcedx-vscode-core/test/jest/conflict/data/testData.ts
+++ packages/salesforcedx-vscode-core/test/jest/conflict/data/testData.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 export const dummyLastModifiedDateCache = '2023-03-17T17:52:51.000Z';
 export const dummyLastModifiedDateLocal = '2023-03-16T17:52:51.000Z';
 export const testData = {
