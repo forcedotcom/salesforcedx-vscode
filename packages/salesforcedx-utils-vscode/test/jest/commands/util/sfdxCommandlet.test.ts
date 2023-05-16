814b2d5d0f36cd30aec0f187333b9686975ffb91
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-05-16 16:30:04 -0700

    test: more tests

--- packages/salesforcedx-utils-vscode/test/jest/commands/util/sfdxCommandlet.test.ts
+++
@@ -1,5 +0,0 @@
-describe('SfdxCommandletExecutor', () => {
-  describe('execute', () => {
-    it('should update the local cache for the components that were retrieved after a pull', () => {});
-  });
-});
---
+++ packages/salesforcedx-vscode-core/test/jest/commands/util/sfdxCommandlet.test.ts
@@ -0,0 +1,31 @@
+import { ChannelService } from '@salesforce/salesforcedx-utils-vscode/src/commands';
+import { ForceSourcePullExecutor } from '../../../../src/commands';
+import { CommandParams } from '../../../../src/commands/util';
+import { CONTINUE } from '../../../../src/commands/util/parameterGatherers';
+import { FORCE_SOURCE_PULL_LOG_NAME } from '../../../../src/constants';
+
+describe('SfdxCommandletExecutor', () => {
+  describe('execute', () => {
+    it('should update the local cache for the components that were retrieved after a pull', () => {
+      const flag = undefined;
+      const pullCommand: CommandParams = {
+        command: 'force:source:pull',
+        description: {
+          default: 'force_source_pull_default_org_text',
+          forceoverwrite: 'force_source_pull_force_default_org_text'
+        },
+        logName: { default: FORCE_SOURCE_PULL_LOG_NAME }
+      };
+      const executor = new ForceSourcePullExecutor(flag, pullCommand);
+      const updateCacheAfterPushPullMock = jest.spyOn(
+        executor as any,
+        'updateCacheAfterPushPull'
+      );
+      jest.spyOn(ChannelService.prototype, 'clear');
+
+      executor.execute({ type: 'CONTINUE', data: '' });
+
+      expect(updateCacheAfterPushPullMock).toHaveBeenCalled();
+    });
+  });
+});
