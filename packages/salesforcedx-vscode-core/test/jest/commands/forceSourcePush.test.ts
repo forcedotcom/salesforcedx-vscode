5742ee06f0f1229aea606cfe2fc6826fe1cd80af
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-05-19 18:13:27 -0700

    test: updating test outlines

--- packages/salesforcedx-vscode-core/test/jest/commands/forceSourcePush.test.ts
+++ packages/salesforcedx-vscode-core/test/jest/commands/forceSourcePush.test.ts
@@ -1,8 +1,5 @@
 describe('ForceSourcePushExecutor', () => {
-  describe('cacheLocalChanges', () => {
-    it('should get and store the local changes from source tracking', () => {});
-  });
-  describe('updateCacheAfterPushPull', () => {
-    it('should update the local cache for the changes that were deployed in the push operation', () => {});
+  describe('updateCache', () => {
+    it('should update the local cache for the pushed source components after push', async () => {});
   });
 });
--- packages/salesforcedx-vscode-core/test/jest/conflict/persistentStorageService.test.ts
+++ packages/salesforcedx-vscode-core/test/jest/conflict/persistentStorageService.test.ts
@@ -1,9 +1,5 @@
 describe('PersistentStorageService', () => {
-  describe('updateCacheAfterPushPull', () => {
-    it('should update the properties in the cache for the files that were pushed or pulled', () => {});
-  });
-
-  describe('addMetaFiles', () => {
-    it('should add metafiles for components that need them', () => {});
+  describe('setPropertiesForFilesPushPull', () => {
+    it('should update the properties in the cache for the files that are passed in', () => {});
   });
 });
