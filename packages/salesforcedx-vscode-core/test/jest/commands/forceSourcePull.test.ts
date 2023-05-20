19163d9032a656eacc8cd5a16a22a258a029cdc0
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-05-20 08:26:05 -0700

    test: add tests

--- packages/salesforcedx-vscode-core/test/jest/commands/forceSourcePull.test.ts
+++ packages/salesforcedx-vscode-core/test/jest/commands/forceSourcePull.test.ts
@@ -1,8 +1,25 @@
-import { FlagParameter } from '@salesforce/salesforcedx-utils-vscode/src';
-import { forceSourcePull } from '../../../src/commands';
+import { ForceSourcePullExecutor } from '../../../src/commands/forceSourcePull';
+import { PersistentStorageService } from '../../../src/conflict';
+import { dummyPullResult } from './data/testData';
 
 describe('ForceSourcePullExecutor', () => {
   describe('updateCache', () => {
-    it('should update the local cache for the pulled source components after pull', async () => {});
+    const setPropertiesForFilesPushPullMock = jest.fn();
+
+    beforeEach(() => {
+      jest.spyOn(PersistentStorageService, 'getInstance').mockReturnValue({
+        setPropertiesForFilesPushPull: setPropertiesForFilesPushPullMock
+      } as any);
+    });
+
+    it('should update the local cache for the pulled source components after pull', async () => {
+      const p = new ForceSourcePullExecutor();
+
+      (p as any).updateCache(dummyPullResult);
+
+      expect(setPropertiesForFilesPushPullMock).toHaveBeenCalledWith(
+        dummyPullResult.result.pulledSource
+      );
+    });
   });
 });
--- packages/salesforcedx-vscode-core/test/jest/commands/forceSourcePush.test.ts
+++ packages/salesforcedx-vscode-core/test/jest/commands/forceSourcePush.test.ts
@@ -1,5 +1,32 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { ForceSourcePushExecutor } from '../../../src/commands';
+import { PersistentStorageService } from '../../../src/conflict';
+import { dummyPushResult } from './data/testData';
+
 describe('ForceSourcePushExecutor', () => {
   describe('updateCache', () => {
-    it('should update the local cache for the pushed source components after push', async () => {});
+    const setPropertiesForFilesPushPullMock = jest.fn();
+
+    beforeEach(() => {
+      jest.spyOn(PersistentStorageService, 'getInstance').mockReturnValue({
+        setPropertiesForFilesPushPull: setPropertiesForFilesPushPullMock
+      } as any);
+    });
+
+    it('should update the local cache for the pulled source components after pull', async () => {
+      const p = new ForceSourcePushExecutor();
+
+      (p as any).updateCache(dummyPushResult);
+
+      expect(setPropertiesForFilesPushPullMock).toHaveBeenCalledWith(
+        dummyPushResult.result.pushedSource
+      );
+    });
   });
 });
