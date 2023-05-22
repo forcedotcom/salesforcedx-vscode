261f22e5d4d25ed98e98fa12cdb6422f38ecc9b0
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-05-22 10:38:33 -0700

    test: more tests

--- packages/salesforcedx-vscode-core/test/jest/commands/baseDeployCommand.test.ts
+++ packages/salesforcedx-vscode-core/test/jest/commands/baseDeployCommand.test.ts
@@ -12,6 +12,7 @@ import {
 } from '../../../src/commands/baseDeployCommand';
 import { CommandParams } from '../../../src/commands/util';
 import { nls } from '../../../src/messages';
+import { dummyStdOut } from './data/testData';
 
 jest.mock('../../../src/channels');
 jest.mock('../../../src/statuses');
@@ -48,8 +49,6 @@ describe('BaseDeployExecutor', () => {
       executorAsAny.updateCache = updateCacheMock;
       executorAsAny.getDeployType = jest.fn().mockReturnValue(DeployType.Push);
       executorAsAny.logMetric = jest.fn();
-      const dummyStdOut =
-        '{\n  "status": 0,\n  "result": {\n    "pushedSource": [\n      {\n        "state": "Changed",\n        "fullName": "D9",\n        "type": "ApexClass",\n        "filePath": "/Users/kenneth.lewis/scratchpad/NewProj1/force-app/main/default/classes/D9.cls"\n      },\n      {\n        "state": "Changed",\n        "fullName": "D9",\n        "type": "ApexClass",\n        "filePath": "/Users/kenneth.lewis/scratchpad/NewProj1/force-app/main/default/classes/D9.cls-meta.xml"\n      }\n    ]\n  },\n  "warnings": [\n    "We plan to deprecate this command in the future. Try using the \\"project deploy start\\" command instead.",\n    "The loglevel flag is no longer in use on this command. You may use it without error, but it will be ignored.\\nSet the log level using the `SFDX_LOG_LEVEL` environment variable."\n  ]\n}\n';
 
       // Act
       await (executor as any).exitProcessHandlerDeploy(
--- packages/salesforcedx-vscode-core/test/jest/commands/util/sfdxCommandlet.test.ts
+++ packages/salesforcedx-vscode-core/test/jest/commands/util/sfdxCommandlet.test.ts
@@ -1,8 +1,15 @@
 import { ChannelService } from '@salesforce/salesforcedx-utils-vscode/src/commands';
-import { ForceSourcePullExecutor } from '../../../../src/commands';
+import {
+  ForceSourcePullExecutor,
+  ForceSourcePushExecutor
+} from '../../../../src/commands';
 import { CommandParams } from '../../../../src/commands/util';
 import { dummyPullOutput } from '../../../../src/commands/util/testData';
 import { FORCE_SOURCE_PULL_LOG_NAME } from '../../../../src/constants';
+import { DeployType } from '../../../../src/commands/baseDeployCommand';
+import { notificationService } from '../../../../src/notifications';
+import { dummyStdOut } from '../data/testData';
+import { SfdxCommandlet } from '@salesforce/salesforcedx-utils-vscode/src';
 
 describe('SfdxCommandletExecutor', () => {
   describe('execute', () => {
@@ -34,6 +41,72 @@ describe('SfdxCommandletExecutor', () => {
       );
 
       expect(updateCacheAfterPushPullMock).toHaveBeenCalled();
+    });
+
+    describe('parseOutput', () => {
+      let showWarningMessageMock: jest.SpyInstance;
+      let parseSpy: jest.SpyInstance;
+
+      beforeEach(() => {
+        showWarningMessageMock = jest
+          .spyOn(notificationService, 'showWarningMessage')
+          .mockImplementation(jest.fn());
+
+        parseSpy = jest.spyOn(JSON, 'parse');
+      });
+
+      it('should parse well formatted response and return JSON', () => {
+        // Arrange
+        const pushCommand: CommandParams = {
+          command: 'force:source:push',
+          description: {
+            default: 'force_source_push_default_org_text',
+            forceoverwrite: 'force_source_push_force_default_org_text'
+          },
+          logName: { default: 'force_source_push_default_scratch_org' }
+        };
+        const flag = '';
+        const executor = new ForceSourcePushExecutor(flag, pushCommand);
+        const a = jest.spyOn(executor as any, 'parseOutput');
+
+        // Act
+        const parsed = (executor as any).parseOutput(dummyStdOut);
+
+        // Assert
+        expect(parseSpy).toHaveBeenCalledWith(dummyStdOut);
+        // expect(a).not.toThrow();
+      });
+
+      it('should show a message to the User if there is a parsing error', async () => {
+        // Arrange
+        const pushCommand: CommandParams = {
+          command: 'force:source:push',
+          description: {
+            default: 'force_source_push_default_org_text',
+            forceoverwrite: 'force_source_push_force_default_org_text'
+          },
+          logName: { default: 'force_source_push_default_scratch_org' }
+        };
+        const flag = '';
+        const executor = new ForceSourcePushExecutor(flag, pushCommand);
+        const updateCacheMock = jest.fn();
+        const executorAsAny = executor as any;
+        executorAsAny.updateCache = updateCacheMock;
+        executorAsAny.getDeployType = jest
+          .fn()
+          .mockReturnValue(DeployType.Push);
+        executorAsAny.logMetric = jest.fn();
+
+        try {
+          // Act
+          (executor as any).parseOutput('{abcdef}');
+        } catch (error) {
+          // Assert
+          expect(error).toBeInstanceOf(Error);
+          expect(updateCacheMock).not.toHaveBeenCalled();
+          expect(showWarningMessageMock).toHaveBeenCalled();
+        }
+      });
     });
   });
 });
