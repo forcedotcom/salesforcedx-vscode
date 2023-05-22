34ade40bbc112fbb672bcba451322f1684eedf38
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-05-22 12:00:02 -0700

    chore: add cp

--- packages/salesforcedx-vscode-core/test/jest/commands/forceSourcePull.test.ts
+++ packages/salesforcedx-vscode-core/test/jest/commands/forceSourcePull.test.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import { ForceSourcePullExecutor } from '../../../src/commands/forceSourcePull';
 import { PersistentStorageService } from '../../../src/conflict';
 import { dummyPullResult } from './data/testData';
@@ -13,9 +20,9 @@ describe('ForceSourcePullExecutor', () => {
     });
 
     it('should update the local cache for the pulled source components after pull', async () => {
-      const pullExecutor = new ForceSourcePullExecutor();
+      const p = new ForceSourcePullExecutor();
 
-      (pullExecutor as any).updateCache(dummyPullResult);
+      (p as any).updateCache(dummyPullResult);
 
       expect(setPropertiesForFilesPushPullMock).toHaveBeenCalledWith(
         dummyPullResult.result.pulledSource
