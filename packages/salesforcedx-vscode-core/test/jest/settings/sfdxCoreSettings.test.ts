0a94ea5657b89b1794bb630529b01275f8a90b56
Ritam Agrawal <ritam.kumari.27@gmail.com>
2023-05-30 14:37:00 -0700

    feat: add jest test

---
+++ packages/salesforcedx-vscode-core/test/jest/settings/sfdxCoreSettings.test.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { ENABLE_SOURCE_TRACKING_FOR_DEPLOY_RETRIEVE } from "../../../src/constants";
+import { SfdxCoreSettings } from "../../../src/settings/sfdxCoreSettings";
+
+describe('sfdxCoreSettings', () => {
+  let sfdxCoreSettingsInstance = SfdxCoreSettings.getInstance();
+  let getConfigValueSpy: jest.SpyInstance;
+  getConfigValueSpy = jest.spyOn((SfdxCoreSettings as any).prototype, 'getConfigValue');
+  describe('getEnableSourceTrackingForDeployAndRetrieve', () => {
+    it('should set the default value for enable source tracking to be true', () => {
+      sfdxCoreSettingsInstance.getEnableSourceTrackingForDeployAndRetrieve();
+      expect(sfdxCoreSettingsInstance).toBeInstanceOf(SfdxCoreSettings);
+      expect(getConfigValueSpy).toHaveBeenCalled();
+      expect(getConfigValueSpy).toBeCalledWith(ENABLE_SOURCE_TRACKING_FOR_DEPLOY_RETRIEVE, true);
+    });
+  });
+});
