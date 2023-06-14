2aff4b917f8bb2e24324e7bc8a24aef4f73475ed
Kenneth Lewis <kenneth.lewis@salesforce.com>
2023-06-14 09:56:43 -0700

    test: draft unit tests

---
+++ packages/salesforcedx-utils-vscode/test/jest/telemetry/telemetryReporter.test.ts
@@ -0,0 +1,13 @@
+describe('Telemetry Reporter', () => {
+  describe('sendTelemetryEvent', () => {
+    it('should send orgId to trackException', () => {});
+
+    it('should send orgId to logStream.write', () => {});
+  });
+
+  describe('sendExceptionEvent', () => {
+    it('should send orgId to trackEvent', () => {});
+
+    it('should send orgId to logStream.write', () => {});
+  });
+});
