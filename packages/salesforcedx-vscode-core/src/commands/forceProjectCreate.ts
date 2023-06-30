0f1b3eb0a08e1f66b831b051b1c41abe24d7f9e8
Jeff Beeghly <jbeeghly@salesforce.com>
2023-06-29 18:22:34 -0700

    fix: added debug code to forceSfdxProjectCreate()

--- packages/salesforcedx-vscode-core/src/commands/forceProjectCreate.ts
+++ packages/salesforcedx-vscode-core/src/commands/forceProjectCreate.ts
@@ -15,6 +15,7 @@ import { ProjectOptions, TemplateType } from '@salesforce/templates';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
+import { channelService } from '../channels';
 import { nls } from '../messages';
 import { notificationService } from '../notifications';
 import { InputUtils } from '../util/inputUtils';
@@ -64,13 +65,24 @@ export class LibraryForceProjectCreateExecutor extends LibraryBaseTemplateComman
     outputdir: string,
     fileName: string
   ) {
+
+    console.log('DEBUG: entering openCreatedTemplateInVSCode()');
+    channelService.appendLine('DEBUG: entering openCreatedTemplateInVSCode()');
+
     await vscode.commands.executeCommand(
       'vscode.openFolder',
       vscode.Uri.file(path.join(outputdir, fileName))
     );
+
+    console.log('DEBUG: leaving openCreatedTemplateInVSCode()');
+    channelService.appendLine('DEBUG: leaving openCreatedTemplateInVSCode()');
   }
 
   public constructTemplateOptions(data: ProjectNameAndPathAndTemplate) {
+
+    console.log('DEBUG: entering constructTemplateOptions()');
+    channelService.appendLine('DEBUG: entering constructTemplateOptions()');
+
     const templateOptions: ProjectOptions = {
       projectname: data.projectName,
       template: data.projectTemplate as ProjectOptions['template'],
@@ -81,6 +93,10 @@ export class LibraryForceProjectCreateExecutor extends LibraryBaseTemplateComman
       manifest: this.options.isProjectWithManifest
     };
     this.telemetryProperties = { projectTemplate: data.projectTemplate };
+
+    console.log('DEBUG: leaving constructTemplateOptions()');
+    channelService.appendLine('DEBUG: leaving constructTemplateOptions()');
+
     return templateOptions;
   }
 }
@@ -112,6 +128,10 @@ export class SelectProjectTemplate
   public async gather(): Promise<
     CancelResponse | ContinueResponse<ProjectTemplate>
   > {
+
+    console.log('DEBUG: entering SelectProjectTemplate.gather()');
+    channelService.appendLine('DEBUG: entering SelectProjectTemplate.gather()');
+
     const items: vscode.QuickPickItem[] = [
       new ProjectTemplateItem(
         'force_project_create_standard_template_display_text',
@@ -127,26 +147,57 @@ export class SelectProjectTemplate
       )
     ];
 
+    console.log('DEBUG: in SelectProjectTemplate.gather(), about to call vscode.window.showQuickPick()');
+    channelService.appendLine('DEBUG: in SelectProjectTemplate.gather(), about to call vscode.window.showQuickPick()');
+
     const selection = await vscode.window.showQuickPick(items);
+
+    console.log('DEBUG: in SelectProjectTemplate.gather(), finished calling vscode.window.showQuickPick()');
+    channelService.appendLine('DEBUG: in SelectProjectTemplate.gather(), finished calling vscode.window.showQuickPick()');
+
+    if (!selection) {
+      console.log('DEBUG: ERROR! in SelectProjectTemplate.gather(), showQuickPick() returned no selection');
+      channelService.appendLine('DEBUG: ERROR! in SelectProjectTemplate.gather(), showQuickPick() returned no selection');
+    } else {
+      console.log(`DEBUG: in SelectProjectTemplate.gather(), selection.label: ${selection.label}`);
+      channelService.appendLine(`DEBUG: in SelectProjectTemplate.gather(), selection.label: ${selection.label}`);
+    }
+
     let projectTemplate: string | undefined;
     switch (selection && selection.label) {
       case nls.localize('force_project_create_standard_template_display_text'):
         projectTemplate = projectTemplateEnum.standard;
+        console.log(`DEBUG: in SelectProjectTemplate.gather(), projectTemplate: ${projectTemplate}`);
+        channelService.appendLine(`DEBUG: in SelectProjectTemplate.gather(), projectTemplate: ${projectTemplate}`);
         break;
       case nls.localize('force_project_create_empty_template_display_text'):
         projectTemplate = projectTemplateEnum.empty;
+        console.log(`DEBUG: in SelectProjectTemplate.gather(), projectTemplate: ${projectTemplate}`);
+        channelService.appendLine(`DEBUG: in SelectProjectTemplate.gather(), projectTemplate: ${projectTemplate}`);
         break;
       case nls.localize('force_project_create_analytics_template_display_text'):
         projectTemplate = projectTemplateEnum.analytics;
+        console.log(`DEBUG: in SelectProjectTemplate.gather(), projectTemplate: ${projectTemplate}`);
+        channelService.appendLine(`DEBUG: in SelectProjectTemplate.gather(), projectTemplate: ${projectTemplate}`);
         break;
       default:
+        // What???
+        console.log('DEBUG: ERROR! in SelectProjectTemplate.gather(), unknown selection type');
+        channelService.appendLine('DEBUG: ERROR! in SelectProjectTemplate.gather(), unknown selection type');
+        console.log(`DEBUG: ERROR! in SelectProjectTemplate.gather(), selection.label: ${selection!.label}`);
+        channelService.appendLine(`DEBUG: ERROR! in SelectProjectTemplate.gather(), selection.label: ${selection!.label}`);
         break;
     }
+
+    console.log('DEBUG: leaving SelectProjectTemplate.gather()');
+    channelService.appendLine('DEBUG: leaving SelectProjectTemplate.gather()');
+
     return projectTemplate
       ? { type: 'CONTINUE', data: { projectTemplate } }
       : { type: 'CANCEL' };
   }
 }
+
 export class SelectProjectName implements ParametersGatherer<ProjectName> {
   private readonly prefillValueProvider?: () => string;
 
@@ -157,6 +208,9 @@ export class SelectProjectName implements ParametersGatherer<ProjectName> {
   public async gather(): Promise<
     CancelResponse | ContinueResponse<ProjectName>
   > {
+    console.log('DEBUG: entering SelectProjectName.gather()');
+    channelService.appendLine('DEBUG: entering SelectProjectName.gather()');
+
     const prompt = nls.localize('parameter_gatherer_enter_project_name');
     const prefillValue = this.prefillValueProvider
       ? this.prefillValueProvider()
@@ -165,6 +219,10 @@ export class SelectProjectName implements ParametersGatherer<ProjectName> {
       prompt,
       prefillValue
     );
+
+    console.log('DEBUG: leaving SelectProjectName.gather()');
+    channelService.appendLine('DEBUG: leaving SelectProjectName.gather()');
+
     return projectName
       ? { type: 'CONTINUE', data: { projectName } }
       : { type: 'CANCEL' };
@@ -175,12 +233,20 @@ export class SelectProjectFolder implements ParametersGatherer<ProjectURI> {
   public async gather(): Promise<
     CancelResponse | ContinueResponse<ProjectURI>
   > {
+
+    console.log('DEBUG: entering SelectProjectFolder.gather()');
+    channelService.appendLine('DEBUG: entering SelectProjectFolder.gather()');
+
     const projectUri = await vscode.window.showOpenDialog({
       canSelectFiles: false,
       canSelectFolders: true,
       canSelectMany: false,
       openLabel: nls.localize('force_project_create_open_dialog_create_label')
     } as vscode.OpenDialogOptions);
+
+    console.log('DEBUG: leaving SelectProjectFolder.gather()');
+    channelService.appendLine('DEBUG: leaving SelectProjectFolder.gather()');
+
     return projectUri && projectUri.length === 1
       ? { type: 'CONTINUE', data: { projectUri: projectUri[0].fsPath } }
       : { type: 'CANCEL' };
@@ -189,14 +255,23 @@ export class SelectProjectFolder implements ParametersGatherer<ProjectURI> {
 
 export class PathExistsChecker
   implements PostconditionChecker<ProjectNameAndPathAndTemplate> {
+
   public async check(
     inputs: ContinueResponse<ProjectNameAndPathAndTemplate> | CancelResponse
   ): Promise<ContinueResponse<ProjectNameAndPathAndTemplate> | CancelResponse> {
+
+    console.log('DEBUG: entering PathExistsChecker.check()');
+    channelService.appendLine('DEBUG: entering PathExistsChecker.check()');
+
     if (inputs.type === 'CONTINUE') {
       const pathExists = fs.existsSync(
         path.join(inputs.data.projectUri, `${inputs.data.projectName}/`)
       );
+
       if (!pathExists) {
+        console.log('DEBUG: leaving PathExistsChecker.check(ver1)');
+        channelService.appendLine('DEBUG: leaving PathExistsChecker.check(ver1)');
+
         return inputs;
       } else {
         const overwrite = await notificationService.showWarningMessage(
@@ -204,11 +279,19 @@ export class PathExistsChecker
           nls.localize('warning_prompt_overwrite'),
           nls.localize('warning_prompt_overwrite_cancel')
         );
+
         if (overwrite === nls.localize('warning_prompt_overwrite')) {
+          console.log('DEBUG: leaving PathExistsChecker.check(ver2)');
+          channelService.appendLine('DEBUG: leaving PathExistsChecker.check(ver2)');
+
           return inputs;
         }
       }
     }
+
+    console.log('DEBUG: leaving PathExistsChecker.check(ver3)');
+    channelService.appendLine('DEBUG: leaving PathExistsChecker.check(ver3)');
+
     return { type: 'CANCEL' };
   }
 }
@@ -222,6 +305,12 @@ const parameterGatherer = new CompositeParametersGatherer(
 const pathExistsChecker = new PathExistsChecker();
 
 export async function forceSfdxProjectCreate() {
+
+  // This is what gets called when "SFDX: Create Project" is called.
+  console.log('DEBUG: entering forceSfdxProjectCreate()');
+  channelService.showChannelOutput();
+  channelService.appendLine('DEBUG: entering forceSfdxProjectCreate()');
+
   const createTemplateExecutor = new LibraryForceProjectCreateExecutor();
   const sfdxProjectCreateCommandlet = new SfdxCommandlet(
     workspaceChecker,
@@ -230,6 +319,9 @@ export async function forceSfdxProjectCreate() {
     pathExistsChecker
   );
   await sfdxProjectCreateCommandlet.run();
+
+  console.log('DEBUG: leaving forceSfdxProjectCreate()');
+  channelService.appendLine('DEBUG: leaving forceSfdxProjectCreate()');
 }
 
 export async function forceProjectWithManifestCreate() {
