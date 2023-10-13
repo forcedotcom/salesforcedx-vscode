ad3c0a050f33d271d485833b9fcfdae882af884d
Gordon Bockus <gbockus@salesforce.com>
2023-10-13 15:32:57 -0500

    chore: turn off forceExit for some unit tests

--- packages/salesforcedx-utils-vscode/jest.config.js
+++ packages/salesforcedx-utils-vscode/jest.config.js
@@ -1,6 +1,4 @@
 // eslint:disable-next-line:no-var-requires
 const baseConfig = require('../../config/jest.base.config');
 
-module.exports = Object.assign({}, baseConfig, {
-  testMatch: ['**/(jest)/**/?(*.)+(spec|test).[t]s?(x)']
-});
+module.exports = Object.assign({}, baseConfig);
