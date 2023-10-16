8383de254aa41e74d6a7bc4952ca43a1917af60b
Gordon Bockus <gbockus@salesforce.com>
2023-10-16 10:11:41 -0500

    chore: remove collectCoverageFrom jest setting

--- config/jest.base.config.js
+++ config/jest.base.config.js
@@ -9,6 +9,12 @@ module.exports = {
     ['jest-junit', { outputName: 'junit-custom-unitTests.xml' }]
   ],
   coverageReporters: ['lcov', 'text'],
-  resetMocks: true,
-  collectCoverageFrom: ['src/**/*.ts']
+  resetMocks: true
+  // This collectCoverageFrom will show coverage for all files in a projects, but slows down calculating coverage results.
+  // Can be a good tool for measuring coverage of the project as a whole locally, but shouldn't be committed at this time.
+  // Off:
+  // npm run test:unit  1231.48s user 150.82s system 1092% cpu 2:06.55 total
+  // On:
+  // npm run test:unit  2461.94s user 244.14s system 1210% cpu 3:43.47 total
+  // collectCoverageFrom: ['src/**/*.ts']
 };
