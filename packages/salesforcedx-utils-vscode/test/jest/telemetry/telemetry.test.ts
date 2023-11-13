304de943f1871f4675344ecc87d5a085289ba52b
mingxuanzhang <mingxuanzhang@salesforce.com>
2023-11-13 13:49:28 -0800

    chore: header

--- packages/salesforcedx-utils-vscode/test/jest/telemetry/telemetry.test.ts
+++ packages/salesforcedx-utils-vscode/test/jest/telemetry/telemetry.test.ts
@@ -1,9 +1,9 @@
-/**
+/*
  * Copyright (c) 2023, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- **/
+ */
 import { TelemetryProvider, TelemetryService } from '../../../src';
 import {
   SFDX_CORE_EXTENSION_NAME,
