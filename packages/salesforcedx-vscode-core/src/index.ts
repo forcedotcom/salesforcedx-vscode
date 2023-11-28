c7b3c9ea515be9ed1068eacfe35b36aa460ad47b
Daphne Yang <daphne.yang@salesforce.com>
2023-11-28 10:21:19 -0500

    revert: revert copyright in index.ts

--- packages/salesforcedx-vscode-core/src/index.ts
+++ packages/salesforcedx-vscode-core/src/index.ts
@@ -1,9 +1,9 @@
-/**
- * Copyright (c) 2023, salesforce.com, inc.
+/*
+ * Copyright (c) 2017, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- **/
+ */
 import { CheckCliEnum, CheckCliVersion, ensureCurrentWorkingDirIsProjectPath } from '@salesforce/salesforcedx-utils';
 import {
   ChannelService,
