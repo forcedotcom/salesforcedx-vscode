bc23525df8d3281441f4239f800bbcda4b48b366
Daphne Yang <daphne.yang@salesforce.com>
2023-12-05 12:28:35 -0500

    fix: user-facing error messages after working with doc writer

--- packages/salesforcedx-vscode-core/src/messages/i18n.ja.ts
+++ packages/salesforcedx-vscode-core/src/messages/i18n.ja.ts
@@ -251,11 +251,11 @@ export const messages = {
   sfdx_cli_not_found:
     'Salesforce CLI がインストールされていません。[%s](%s) からインストールしてください。',
   sfdx_cli_not_supported:
-    'Your installed CLI version is no longer supported. Please uninstall your CLI and reinstall the latest version from [%s](%s)',
+    'Your installed Salesforce CLI version is no longer supported. Uninstall CLI and install the latest version from [%s](%s).',
   sf_v1_not_supported:
-    'Your installed CLI version is incompatible with our extensions. Please uninstall your CLI and reinstall the latest version from [%s](%s)',
+    'Your installed Salesforce CLI version is incompatible with all versions of our extensions. Uninstall Salesforce CLI and install the latest version from [%s](%s).',
   both_sfdx_and_sf:
-    'You have both SFDX v7 and SF v2 installed. Please uninstall SFDX v7 by running "npm uninstall sfdx-cli --global" in your Terminal.',
+    'You have both SFDX(v7) and SF(v2) of Salesforce CLI installed. Uninstall SFDX(v7) by running `npm uninstall sfdx-cli --global` in your terminal.',
   table_header_errors: 'ERRORS',
   table_header_project_path: 'PROJECT PATH',
   table_header_type: 'TYPE',
