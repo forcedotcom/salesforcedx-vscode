de67697e086cb112b38b9d8b12bbd52cbf08c1f2
Daphne Yang <daphne.yang@salesforce.com>
2023-12-17 07:41:15 -0500

    fix: make checkCliVersion.test.ts unit tests run and pass with new structure

--- packages/salesforcedx-utils/test/jest/cli/checkCliVersion.test.ts
+++ packages/salesforcedx-utils/test/jest/cli/checkCliVersion.test.ts
@@ -4,7 +4,8 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { CheckCliEnum, CheckCliVersion } from '../../../src';
+
+import { CheckCliEnum, CheckCliVersion } from '../../../src/cli/checkCliVersion';
 
 describe('CheckCliVersion unit tests.', () => {
 
@@ -15,12 +16,12 @@ describe('CheckCliVersion unit tests.', () => {
   const noSFDX_string = 'No SFDX CLI';
   const noSF_string = 'No SF CLI';
 
-  const sfV2_array = [2, 15, 9];
-  const sfdxV7_valid_array = [7, 209, 6];
-  const sfV1_array = [1, 87, 0];
-  const sfdxV7_outdated_array = [7, 183, 1];
-  const noSFDX_array = [-1];
-  const noSF_array = [-1];
+  const sfV2_parsed = '2.15.9';
+  const sfdxV7_valid_parsed = '7.209.6';
+  const sfV1_parsed = '1.87.0';
+  const sfdxV7_outdated_parsed = '7.183.1';
+  const noSFDX_parsed = '0.0.0';
+  const noSF_parsed = '0.0.0';
 
   it('Should create instance.', () => {
     const c = new CheckCliVersion();
@@ -28,54 +29,43 @@ describe('CheckCliVersion unit tests.', () => {
   });
 
   /*
-  Test cases for the parseSfdxCliVersion() function
+  Test cases for the parseCliVersion() function
   */
-  describe('Test cases for the parseSfdxCliVersion() function', () => {
-    it('Parse valid SFDX', async () => {
+  describe('Test cases for the parseCliVersion() function', () => {
+    it('Parse valid SFDX', () => {
       const c = new CheckCliVersion();
-      const result = await c.parseSfdxCliVersion(sfdxV7_valid_string);
-      expect(result).toStrictEqual(sfdxV7_valid_array);
+      const result = c.parseCliVersion(sfdxV7_valid_string);
+      expect(result).toStrictEqual(sfdxV7_valid_parsed);
     });
 
-    it('Parse outdated SFDX', async () => {
+    it('Parse outdated SFDX', () => {
       const c = new CheckCliVersion();
-      const result = await c.parseSfdxCliVersion(sfdxV7_outdated_string);
-      expect(result).toStrictEqual(sfdxV7_outdated_array);
+      const result = c.parseCliVersion(sfdxV7_outdated_string);
+      expect(result).toStrictEqual(sfdxV7_outdated_parsed);
     });
 
-    it('Parse valid SF v2', async () => {
+    it('Parse valid SF v2', () => {
       const c = new CheckCliVersion();
-      const result = await c.parseSfdxCliVersion(sfV2_string);
-      expect(result).toStrictEqual(sfV2_array);
+      const result = c.parseCliVersion(sfV2_string);
+      expect(result).toStrictEqual(sfV2_parsed);
     });
 
-    it('Parse SFDX not installed', async () => {
+    it('Parse SFDX not installed', () => {
       const c = new CheckCliVersion();
-      const result = await c.parseSfdxCliVersion(noSFDX_string);
-      expect(result).toStrictEqual(noSFDX_array);
-    });
-  });
-
-  /*
-  Test cases for the parseSfCliVersion() function
-  */
-  describe('Test cases for the parseSfCliVersion() function', () => {
-    it('Parse SF v1', async () => {
-      const c = new CheckCliVersion();
-      const result = await c.parseSfCliVersion(sfV1_string);
-      expect(result).toStrictEqual(sfV1_array);
+      const result = c.parseCliVersion(noSFDX_string);
+      expect(result).toStrictEqual(noSFDX_parsed);
     });
 
-    it('Parse SF v2', async () => {
+    it('Parse SF v1', () => {
       const c = new CheckCliVersion();
-      const result = await c.parseSfCliVersion(sfV2_string);
-      expect(result).toStrictEqual(sfV2_array);
+      const result = c.parseCliVersion(sfV1_string);
+      expect(result).toStrictEqual(sfV1_parsed);
     });
 
-    it('Parse SF not installed', async () => {
+    it('Parse SF not installed', () => {
       const c = new CheckCliVersion();
-      const result = await c.parseSfCliVersion(noSF_string);
-      expect(result).toStrictEqual(noSF_array);
+      const result = c.parseCliVersion(noSF_string);
+      expect(result).toStrictEqual(noSF_parsed);
     });
   });
 
@@ -83,57 +73,57 @@ describe('CheckCliVersion unit tests.', () => {
   Test cases for the validateCliInstallationAndVersion() function
   */
   describe('Test cases for the validateCliInstallationAndVersion() function', () => {
-    it('Case 1: No Salesforce CLI installed - should fail', async () => {
+    it('Case 1: No Salesforce CLI installed - should fail', () => {
       const c = new CheckCliVersion();
-      const result = await c.validateCliInstallationAndVersion(noSFDX_array, noSF_array);
+      const result = c.validateCliInstallationAndVersion(noSFDX_parsed, noSF_parsed);
       expect(result).toStrictEqual(CheckCliEnum.cliNotInstalled);
     });
 
-    it('Case 2: Only SF v1 installed - should fail', async () => {
+    it('Case 2: Only SF v1 installed - should fail', () => {
       const c = new CheckCliVersion();
-      const result = await c.validateCliInstallationAndVersion(noSFDX_array, sfV1_array);
+      const result = c.validateCliInstallationAndVersion(noSFDX_parsed, sfV1_parsed);
       expect(result).toStrictEqual(CheckCliEnum.onlySFv1);
     });
 
-    it('Case 3: Both SFDX v7 (outdated) and SF v2 installed - should fail', async () => {
+    it('Case 3: Both SFDX v7 (outdated) and SF v2 installed - should fail', () => {
       const c = new CheckCliVersion();
-      const result = await c.validateCliInstallationAndVersion(sfdxV7_outdated_array, sfV2_array);
+      const result = c.validateCliInstallationAndVersion(sfdxV7_outdated_parsed, sfV2_parsed);
       expect(result).toStrictEqual(CheckCliEnum.bothSFDXAndSFInstalled);
     });
 
-    it('Case 4: Both SFDX v7 (valid) and SF v2 installed - should fail', async () => {
+    it('Case 4: Both SFDX v7 (valid) and SF v2 installed - should fail', () => {
       const c = new CheckCliVersion();
-      const result = await c.validateCliInstallationAndVersion(sfdxV7_valid_array, sfV2_array);
+      const result = c.validateCliInstallationAndVersion(sfdxV7_valid_parsed, sfV2_parsed);
       expect(result).toStrictEqual(CheckCliEnum.bothSFDXAndSFInstalled);
     });
 
-    it('Case 5: Only SFDX v7 (outdated) installed - should fail', async () => {
+    it('Case 5: Only SFDX v7 (outdated) installed - should fail', () => {
       const c = new CheckCliVersion();
-      const result = await c.validateCliInstallationAndVersion(sfdxV7_outdated_array, noSF_array);
+      const result = c.validateCliInstallationAndVersion(sfdxV7_outdated_parsed, noSF_parsed);
       expect(result).toStrictEqual(CheckCliEnum.outdatedSFDXVersion);
     });
 
-    it('Case 6: Only SFDX v7 (valid) installed - should activate Core extension', async () => {
+    it('Case 6: Only SFDX v7 (valid) installed - should activate Core extension', () => {
       const c = new CheckCliVersion();
-      const result = await c.validateCliInstallationAndVersion(sfdxV7_valid_array, noSF_array);
+      const result = c.validateCliInstallationAndVersion(sfdxV7_valid_parsed, noSF_parsed);
       expect(result).toStrictEqual(CheckCliEnum.validCli);
     });
 
-    it('Case 7: SFDX v7 (outdated) and SF v1 installed - should fail', async () => {
+    it('Case 7: SFDX v7 (outdated) and SF v1 installed - should fail', () => {
       const c = new CheckCliVersion();
-      const result = await c.validateCliInstallationAndVersion(sfdxV7_outdated_array, sfV1_array);
+      const result = c.validateCliInstallationAndVersion(sfdxV7_outdated_parsed, sfV1_parsed);
       expect(result).toStrictEqual(CheckCliEnum.outdatedSFDXVersion);
     });
 
-    it('Case 8: SFDX v7 (valid) and SF v1 installed - should activate Core extension', async () => {
+    it('Case 8: SFDX v7 (valid) and SF v1 installed - should activate Core extension', () => {
       const c = new CheckCliVersion();
-      const result = await c.validateCliInstallationAndVersion(sfdxV7_valid_array, sfV1_array);
+      const result = c.validateCliInstallationAndVersion(sfdxV7_valid_parsed, sfV1_parsed);
       expect(result).toStrictEqual(CheckCliEnum.validCli);
     });
 
-    it('Case 9: Only SF v2 installed - should activate Core extension', async () => {
+    it('Case 9: Only SF v2 installed - should activate Core extension', () => {
       const c = new CheckCliVersion();
-      const result = await c.validateCliInstallationAndVersion(sfV2_array, sfV2_array);
+      const result = c.validateCliInstallationAndVersion(sfV2_parsed, sfV2_parsed);
       expect(result).toStrictEqual(CheckCliEnum.validCli);
     });
   });
