f512a879f6abb388ec01a00e06678ef02525a3c9
Daphne Yang <daphne.yang@salesforce.com>
2023-12-17 07:59:37 -0500

    chore: clean up checkCliVersion.ts

--- packages/salesforcedx-utils/src/cli/checkCliVersion.ts
+++ packages/salesforcedx-utils/src/cli/checkCliVersion.ts
@@ -5,10 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-// import { CliCommandExecutor } from './cliCommandExecutor';
-// import { CommandOutput } from './commandOutput';
-// import { SfCommandBuilder } from './sfCommandBuilder';
-// import { SfdxCommandBuilder } from './sfdxCommandBuilder';
 import { execSync } from 'child_process';
 import * as semver from 'semver';
 
@@ -39,52 +35,14 @@ export class CheckCliVersion {
     }
   }
 
-  // public async parseSfdxCliVersion(sfdxCliVersion: string): Promise<number[]> {
-  //   // Both SFDX and SF v2 can match a valid SFDX version, so there are 2 patterns to search for
-  //   const sfdxPattern = /sfdx-cli\/(\d+\.\d+\.\d+)/;
-  //   const sfdxMatch = sfdxPattern.exec(sfdxCliVersion);
-  //   const sfPattern = /@salesforce\/cli\/(\d+\.\d+\.\d+)/;
-  //   const sfMatch = sfPattern.exec(sfdxCliVersion);
-  //   if (sfdxMatch) {
-  //     const sfdxVersion = sfdxMatch[1];
-  //     const sfdxVersionNumber = sfdxVersion.split('.').map(Number);
-  //     return sfdxVersionNumber;
-  //   }
-  //   else if (sfMatch) {
-  //     const sfdxVersion = sfMatch[1];
-  //     const sfdxVersionNumber = sfdxVersion.split('.').map(Number);
-  //     return sfdxVersionNumber;
-  //   }
-  //   return [-1];
-  // }
-
-  // public async parseSfCliVersion(sfCliVersion: string): Promise<number[]> {
-  //   const sfPattern = /@salesforce\/cli\/(\d+\.\d+\.\d+)/;
-  //   const sfMatch = sfPattern.exec(sfCliVersion);
-  //   if (sfMatch) {
-  //     const sfVersion = sfMatch[1];
-  //     const sfVersionNumber = sfVersion.split('.').map(Number);
-  //     return sfVersionNumber;
-  //   }
-  //   return [-1];
-  // }
-
   public parseCliVersion(sfCliVersion: string): string {
     const pattern = /sfdx-cli\/(\d+\.\d+\.\d+)|@salesforce\/cli\/(\d+\.\d+\.\d+)/;
     const match = pattern.exec(sfCliVersion);
-    const y = match?.length;
-    console.log('y = ' + y);
-    for (let x = 0; x < y!; x++) {
-      console.log('match[' + x + '] = ' + match![x]);
-    }
     // SFDX v7 reports results in match[1], SF v2 reports results in match[2]
     return match ? (match[1] ? match[1] : match[2]) : '0.0.0';
   }
 
   public validateCliInstallationAndVersion(sfdxCliVersionString: string, sfCliVersionString: string): CheckCliEnum {
-
-    console.log('sfdxCliVersionString = ' + sfdxCliVersionString);
-    console.log('sfCliVersionString = ' + sfCliVersionString);
 
     // Case 1: Neither SFDX CLI nor SF CLI is installed
     if (semver.satisfies(sfdxCliVersionString, '0.0.0') && semver.satisfies(sfCliVersionString, '0.0.0')) {
