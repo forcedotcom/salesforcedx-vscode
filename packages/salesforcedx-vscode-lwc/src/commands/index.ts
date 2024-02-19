23a58ce0c37719d9de69bb55af6a5548af0206fd
CristiCanizales <cristina.canizales@salesforce.com>
2024-02-19 13:33:36 -0300

    fix: merge branches

--- packages/salesforcedx-vscode-lwc/src/commands/index.ts
+++ packages/salesforcedx-vscode-lwc/src/commands/index.ts
@@ -4,8 +4,3 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
-export { lightningLwcStart } from './lightningLwcStart';
-export { lightningLwcStop } from './lightningLwcStop';
-export { lightningLwcOpen } from './lightningLwcOpen';
-export { lightningLwcPreview } from './lightningLwcPreview';
