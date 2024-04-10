914a196444a1ed8373209fa8fba5df46b4d86d77
Daphne Yang <daphne.yang@salesforce.com>
2024-04-10 09:59:42 -0400

    chore: remove unused import that was commented out

--- packages/salesforcedx-vscode-apex-debugger/src/telemetry/telemetry.ts
+++ packages/salesforcedx-vscode-apex-debugger/src/telemetry/telemetry.ts
@@ -5,7 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-// import { isMetric } from '@salesforce/salesforcedx-apex-debugger/out/src';
 import * as util from 'util';
 import { DebugSessionCustomEvent } from 'vscode';
 import TelemetryReporter from 'vscode-extension-telemetry';
