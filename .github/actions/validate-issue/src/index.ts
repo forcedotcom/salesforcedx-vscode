bcb637102f3150ab0fdca4c15513d0d98a12256c
Daphne Yang <daphne.yang@salesforce.com>
2024-05-15 19:29:38 -0400

    test: print 1 to 50

--- .github/actions/validate-issue/src/index.ts
+++ .github/actions/validate-issue/src/index.ts
@@ -7,13 +7,62 @@
 
 import { getInput, setFailed } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
-import { execSync } from "child_process";
 import * as semver from "semver";
 import { readFileSync } from "fs";
 import * as path from "path";
 import { isAnyVersionValid } from "./nodeVersions";
 
 async function run() {
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+  console.log(5);
+  console.log(6);
+  console.log(7);
+  console.log(8);
+  console.log(9);
+  console.log(10);
+  console.log(11);
+  console.log(12);
+  console.log(13);
+  console.log(14);
+  console.log(15);
+  console.log(16);
+  console.log(17);
+  console.log(18);
+  console.log(19);
+  console.log(20);
+  console.log(21);
+  console.log(22);
+  console.log(23);
+  console.log(24);
+  console.log(25);
+  console.log(26);
+  console.log(27);
+  console.log(28);
+  console.log(29);
+  console.log(30);
+  console.log(31);
+  console.log(32);
+  console.log(33);
+  console.log(34);
+  console.log(35);
+  console.log(36);
+  console.log(37);
+  console.log(38);
+  console.log(39);
+  console.log(40);
+  console.log(41);
+  console.log(42);
+  console.log(43);
+  console.log(44);
+  console.log(45);
+  console.log(46);
+  console.log(47);
+  console.log(48);
+  console.log(49);
+  console.log(50);
   try {
     // Uncomment for local testing
     // const issue = JSON.parse(getFile("../mock/sample-context.json"));
