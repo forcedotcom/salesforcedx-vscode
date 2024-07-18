d637f649eb076e3e10b22be9db817a0f70c976b1
RitamAgrawal <ritam.kumari.27@gmail.com>
2024-07-18 16:47:12 -0700

    chore: add tests for getUserId method

--- packages/salesforcedx-utils-vscode/test/jest/telemetry/telemetry.test.ts
+++ packages/salesforcedx-utils-vscode/test/jest/telemetry/telemetry.test.ts
@@ -9,7 +9,7 @@ import { SFDX_CORE_EXTENSION_NAME } from '../../../src/constants';
 import { TelemetryServiceProvider } from '../../../src/services/telemetry';
 
 describe('Telemetry', () => {
-  describe('Telemetry Provider', () => {
+  describe('Telemetry Service Provider', () => {
     afterEach(() => {
       // Clear instances after each test to avoid state leakage.
       TelemetryServiceProvider.instances.clear();
@@ -56,7 +56,7 @@ describe('Telemetry', () => {
     });
   });
 
-  describe('Telemetry Service', () => {
+  describe('Telemetry Service - getInstance', () => {
     it('getInstance should return the core instance if no extension name provided', () => {
       const firstInstance = TelemetryService.getInstance();
       const secondInstance = TelemetryServiceProvider.getInstance(
@@ -89,6 +89,49 @@ describe('Telemetry', () => {
       telemetryService.extensionName = 'salesforcedx-einstein-gpt';
       const result = telemetryService.getTelemetryReporterName();
       expect(result).toBe(telemetryService.extensionName);
+    });
+  });
+
+  describe('Telemetry Service - getUserId', () => {
+    let telemetryService: TelemetryService;
+    let executeCliTelemetrySpy: jest.SpyInstance;
+    let getRandomUserIdSpy: jest.SpyInstance;
+
+    const fakeCliTelemetryData = {
+      result: {
+        enabled: true,
+        cliId: 'rdccvghuhbnjiokmnmklop'
+      }
+    };
+    const fakeCliIdUndefined = {
+      result: {
+        enabled: true
+      }
+    };
+    const randomId = 'sedtrfyv234dd';
+    beforeEach(() => {
+      telemetryService = new TelemetryService();
+      executeCliTelemetrySpy = jest.spyOn((telemetryService as any), 'executeCliTelemetry');
+      getRandomUserIdSpy = jest.spyOn((telemetryService as any), 'getRandomUserId');
+    });
+
+    it('should return cliId when UserId is undefined in global state', async () => {
+      executeCliTelemetrySpy.mockResolvedValueOnce(JSON.stringify(fakeCliTelemetryData));
+
+      const uId = await telemetryService.getUserId();
+
+      expect(uId).toBe(fakeCliTelemetryData.result.cliId);
+      expect(executeCliTelemetrySpy).toHaveBeenCalled();
+    });
+
+    it('should generate random userId when globalState userId as well as cliId is undefined', async () => {
+      executeCliTelemetrySpy.mockResolvedValueOnce(JSON.stringify(fakeCliIdUndefined));
+      getRandomUserIdSpy.mockResolvedValueOnce(randomId);
+
+      const uId = await telemetryService.getUserId();
+
+      expect(getRandomUserIdSpy).toHaveBeenCalled();
+      expect(uId).toBe(randomId);
     });
   });
 });
