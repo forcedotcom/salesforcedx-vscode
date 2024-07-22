18b703bb444e9e996df51e7ffcb3b32166b65dc1
RitamAgrawal <ritam.kumari.27@gmail.com>
2024-07-22 14:22:58 -0700

    chore: add more tests

--- packages/salesforcedx-utils-vscode/test/jest/telemetry/telemetry.test.ts
+++ packages/salesforcedx-utils-vscode/test/jest/telemetry/telemetry.test.ts
@@ -92,10 +92,12 @@ describe('Telemetry', () => {
     });
   });
 
-  describe('Telemetry Service - getUserId', () => {
+  describe('getUserId', () => {
     let telemetryService: TelemetryService;
     let executeCliTelemetrySpy: jest.SpyInstance;
     let getRandomUserIdSpy: jest.SpyInstance;
+    let fakeGet: jest.SpyInstance;
+    let fakeUpdate: jest.SpyInstance;
 
     const fakeCliTelemetryData = {
       result: {
@@ -109,13 +111,21 @@ describe('Telemetry', () => {
       }
     };
     const randomId = 'sedtrfyv234dd';
+    const globalTelemetryUserId = 'e45rdcfy7ygvhu8uhbnjiiugdrgb';
+
     beforeEach(() => {
       telemetryService = new TelemetryService();
+      fakeGet = jest.fn();
+      fakeUpdate = jest.fn();
+      (telemetryService as any).extensionContext = { globalState: { get: fakeGet, update: fakeUpdate } };
       executeCliTelemetrySpy = jest.spyOn((telemetryService as any), 'executeCliTelemetry');
       getRandomUserIdSpy = jest.spyOn((telemetryService as any), 'getRandomUserId');
     });
 
-    it('should return cliId when UserId is undefined in global state', async () => {
+    it('should return cliId when telemetryUserId is undefined in global state', async () => {
+      fakeGet.mockReturnValueOnce(undefined);
+      fakeUpdate.mockResolvedValueOnce(undefined);
+
       executeCliTelemetrySpy.mockResolvedValueOnce(JSON.stringify(fakeCliTelemetryData));
 
       const uId = await telemetryService.getUserId();
@@ -124,7 +134,7 @@ describe('Telemetry', () => {
       expect(executeCliTelemetrySpy).toHaveBeenCalled();
     });
 
-    it('should generate random userId when globalState userId as well as cliId is undefined', async () => {
+    it('should generate random userId when telemetryUserId as well as cliId is undefined', async () => {
       executeCliTelemetrySpy.mockResolvedValueOnce(JSON.stringify(fakeCliIdUndefined));
       getRandomUserIdSpy.mockResolvedValueOnce(randomId);
 
@@ -132,6 +142,17 @@ describe('Telemetry', () => {
 
       expect(getRandomUserIdSpy).toHaveBeenCalled();
       expect(uId).toBe(randomId);
+    });
+
+    it('should return telemetryUserId when defined in globalState', async () => {
+      fakeGet.mockReturnValueOnce(globalTelemetryUserId);
+
+      const uId = await telemetryService.getUserId();
+
+      expect(executeCliTelemetrySpy).not.toHaveBeenCalled();
+      expect(getRandomUserIdSpy).not.toHaveBeenCalled();
+      expect(uId).not.toBe(randomId);
+      expect(uId).toBe(globalTelemetryUserId);
     });
   });
 });
