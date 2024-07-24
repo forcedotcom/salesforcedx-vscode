bc0f7466196eb67fba3c196f81ed554c9ec6d198
James Tu <jmsjtu@gmail.com>
2024-07-24 15:27:56 -0700

    feat: add TypeScript template to create LWC command (#5687)
    * feat: add typescript support setting
    
    * feat: add typescript support for creating lwc
    
    * test: add tests
    
    * chore: remove japanese localization
    
    * chore: update lwc_extension_name reference
    
    * test: add unit tests
    
    * chore: fix package-lock.json
    
    * chore: hide config from ui
    
    ---------
    
    Co-authored-by: Daphne Yang <daphne.yang@salesforce.com>
    Co-authored-by: mingxuanzhang <mingxuanzhang@salesforce.com>
    Co-authored-by: Mingxuan Zhang <132491513+mingxuanzhangsfdx@users.noreply.github.com>

--- packages/salesforcedx-vscode-core/src/commands/templates/lightningGenerateLwc.ts
+++ packages/salesforcedx-vscode-core/src/commands/templates/lightningGenerateLwc.ts
@@ -22,6 +22,7 @@ import {
   SfWorkspaceChecker
 } from '../util';
 import { OverwriteComponentPrompt } from '../util/overwriteComponentPrompt';
+import { SelectLwcComponentType } from '../util/parameterGatherers';
 import {
   FileInternalPathGatherer,
   InternalDevWorkspaceChecker
@@ -34,34 +35,39 @@ export class LibraryLightningGenerateLwcExecutor extends LibraryBaseTemplateComm
   public telemetryName = 'lightning_generate_lwc';
   public metadataTypeName = LWC_TYPE;
   public templateType = TemplateType.LightningComponent;
+  private templateOptions: LightningComponentOptions | undefined;
+
   public getOutputFileName(data: DirFileNameSelection) {
     return data.fileName;
   }
+
   public constructTemplateOptions(data: DirFileNameSelection) {
     const internal = salesforceCoreSettings.getInternalDev();
-    const templateOptions: LightningComponentOptions = {
-      outputdir: data.outputdir,
-      componentname: data.fileName,
-      template: 'default',
+    const { outputdir, fileName: componentname, extension } = data;
+    this.templateOptions = {
+      outputdir,
+      componentname,
+      template: extension === 'TypeScript' ? 'typeScript' : 'default',
       type: 'lwc',
       internal
     };
-    return templateOptions;
+    return this.templateOptions;
+  }
+
+  public getFileExtension(): string {
+    return this.templateOptions?.template === 'typeScript' ? '.ts' : '.js';
   }
 }
-
-const fileNameGatherer = new SelectFileName();
-const outputDirGatherer = new SelectOutputDir(LWC_DIRECTORY, true);
-const metadataTypeGatherer = new MetadataTypeGatherer(LWC_TYPE);
 
 export const lightningGenerateLwc = (): void => {
   const createTemplateExecutor = new LibraryLightningGenerateLwcExecutor();
   const commandlet = new SfCommandlet(
     new SfWorkspaceChecker(),
     new CompositeParametersGatherer<LocalComponent>(
-      metadataTypeGatherer,
-      fileNameGatherer,
-      outputDirGatherer
+      new MetadataTypeGatherer(LWC_TYPE),
+      new SelectLwcComponentType(),
+      new SelectFileName(),
+      new SelectOutputDir(LWC_DIRECTORY, true)
     ),
     createTemplateExecutor,
     new OverwriteComponentPrompt()
@@ -74,7 +80,7 @@ export const internalLightningGenerateLwc = (sourceUri: Uri): void => {
   const commandlet = new SfCommandlet(
     new InternalDevWorkspaceChecker(),
     new CompositeParametersGatherer(
-      fileNameGatherer,
+      new SelectFileName(),
       new FileInternalPathGatherer(sourceUri)
     ),
     createTemplateExecutor
--- packages/salesforcedx-vscode-core/test/vscode-integration/commands/templates/lightningGenerateLwc.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/commands/templates/lightningGenerateLwc.test.ts
@@ -4,6 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import { SFDX_LWC_EXTENSION_NAME } from '@salesforce/salesforcedx-utils-vscode';
 import * as path from 'path';
 import * as shell from 'shelljs';
 import * as sinon from 'sinon';
@@ -15,6 +16,7 @@ import {
   internalLightningGenerateLwc,
   lightningGenerateLwc
 } from '../../../../src/commands/templates';
+import { LWC_PREVIEW_TYPESCRIPT_SUPPORT } from '../../../../src/commands/util/parameterGatherers';
 import { notificationService } from '../../../../src/notifications';
 import { SalesforceCoreSettings } from '../../../../src/settings/salesforceCoreSettings';
 import { workspaceUtils } from '../../../../src/util';
@@ -28,6 +30,8 @@ describe('Generate Lightning Web Component', () => {
   let showSuccessfulExecutionStub: SinonStub;
   let showFailedExecutionStub: SinonStub;
   let openTextDocumentStub: SinonStub;
+  let settingStub: SinonStub;
+  let getConfiguration: SinonStub;
 
   beforeEach(() => {
     getInternalDevStub = stub(
@@ -44,6 +48,9 @@ describe('Generate Lightning Web Component', () => {
     showSuccessfulExecutionStub.returns(Promise.resolve());
     showFailedExecutionStub = stub(notificationService, 'showFailedExecution');
     openTextDocumentStub = stub(vscode.workspace, 'openTextDocument');
+    getConfiguration = stub(vscode.workspace, 'getConfiguration').withArgs(SFDX_LWC_EXTENSION_NAME).returns({
+      get: settingStub
+    });
   });
 
   afterEach(() => {
@@ -54,11 +61,14 @@ describe('Generate Lightning Web Component', () => {
     showFailedExecutionStub.restore();
     appendLineStub.restore();
     openTextDocumentStub.restore();
+    getConfiguration.restore();
+    settingStub.restore();
   });
 
-  it('Should generate LWC', async () => {
+  it('Should generate LWC - JavaScript', async () => {
     // arrange
     getInternalDevStub.returns(false);
+    settingStub.withArgs(LWC_PREVIEW_TYPESCRIPT_SUPPORT).returns(false);
     const fileName = 'testLwc';
     const outputPath = 'force-app/main/default/lwc';
     const lwcHtmlPath = path.join(
@@ -102,9 +112,57 @@ describe('Generate Lightning Web Component', () => {
     );
   });
 
+  it('Should generate LWC - TypeScript', async () => {
+    // arrange
+    getInternalDevStub.returns(false);
+    settingStub.withArgs(LWC_PREVIEW_TYPESCRIPT_SUPPORT).returns(true);
+    const fileName = 'testLwc';
+    const outputPath = 'force-app/main/default/lwc';
+    const lwcHtmlPath = path.join(
+      workspaceUtils.getRootWorkspacePath(),
+      outputPath,
+      fileName,
+      'testLwc.html'
+    );
+    const lwcTsPath = path.join(
+      workspaceUtils.getRootWorkspacePath(),
+      outputPath,
+      fileName,
+      'testLwc.ts'
+    );
+    const lwcJsMetaPath = path.join(
+      workspaceUtils.getRootWorkspacePath(),
+      outputPath,
+      fileName,
+      'testLwc.js-meta.xml'
+    );
+    shell.rm(
+      '-rf',
+      path.join(workspaceUtils.getRootWorkspacePath(), outputPath, fileName)
+    );
+    assert.noFile([lwcHtmlPath, lwcTsPath, lwcJsMetaPath]);
+    showInputBoxStub.returns(fileName);
+    quickPickStub.returns(outputPath);
+
+    // act
+    await lightningGenerateLwc();
+
+    // assert
+    assert.file([lwcHtmlPath, lwcTsPath, lwcJsMetaPath]);
+    sinon.assert.calledOnce(openTextDocumentStub);
+    sinon.assert.calledWith(openTextDocumentStub, lwcTsPath);
+
+    // clean up
+    shell.rm(
+      '-rf',
+      path.join(workspaceUtils.getRootWorkspacePath(), outputPath, fileName)
+    );
+  });
+
   it('Should generate internal LWC', async () => {
     // arrange
     getInternalDevStub.returns(true);
+    settingStub.withArgs(LWC_PREVIEW_TYPESCRIPT_SUPPORT).returns(false);
     const fileName = 'testLwc';
     const outputPath = 'force-app/main/default/lwc';
     const lwcHtmlPath = path.join(
