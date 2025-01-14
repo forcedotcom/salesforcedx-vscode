e86539fec326448e88d91f6c1c56997312a5bc8b
mingxuanzhang <mingxuanzhang@salesforce.com>
2025-01-14 12:30:19 -0800

    fix: parameter

--- packages/salesforcedx-utils-vscode/src/telemetry/reporters/logStream.ts
+++ packages/salesforcedx-utils-vscode/src/telemetry/reporters/logStream.ts
@@ -4,6 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+/* eslint-disable prettier/prettier */
 'use strict';
 
 import { TelemetryReporter } from '@salesforce/vscode-service-provider';
