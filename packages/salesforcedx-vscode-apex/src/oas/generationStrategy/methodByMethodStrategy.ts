d51913e54e07e232bf43b418cb5fe078a70cddf0
mingxuanzhang <mingxuanzhang@salesforce.com>
2025-01-21 10:08:53 -0800

    chore: update after committing strategy

--- packages/salesforcedx-vscode-apex/src/oas/generationStrategy/methodByMethodStrategy.ts
+++ packages/salesforcedx-vscode-apex/src/oas/generationStrategy/methodByMethodStrategy.ts
@@ -44,29 +44,27 @@ export class MethodByMethodStrategy extends GenerationStrategy {
   }
   public generate(): PromptGenerationResult {
     const methodsMap = new Map();
-    for (const symbol of (this.metadata.symbols ?? []).filter(symbol => symbol.isApexOasEligible)) {
-      if (symbol.isApexOasEligible) {
-        const methodName = symbol.docSymbol.name;
-        methodsMap.set(methodName, symbol.docSymbol); // docSymbol might be useful for generating prompts
-        const input = this.generatePromptForMethod(methodName);
-        const tokenCount = this.getPromptTokenCount(input);
-        if (tokenCount <= PROMPT_TOKEN_MAX_LIMIT * IMPOSED_FACTOR) {
-          this.prompts.push(input);
-          this.callCounts++;
-          const currentBudget = Math.floor((PROMPT_TOKEN_MAX_LIMIT - tokenCount) * IMPOSED_FACTOR);
-          if (currentBudget < this.maxBudget) {
-            this.maxBudget = currentBudget;
-          }
-        } else {
-          // as long as there is one failure, the strategy will be considered failed
-          this.prompts = [];
-          this.callCounts = 0;
-          this.maxBudget = 0;
-          return {
-            maxBudget: 0,
-            callCounts: 0
-          };
+    for (const symbol of (this.metadata.symbols ?? []).filter(s => s.isApexOasEligible)) {
+      const methodName = symbol.docSymbol.name;
+      methodsMap.set(methodName, symbol.docSymbol); // docSymbol might be useful for generating prompts
+      const input = this.generatePromptForMethod(methodName);
+      const tokenCount = this.getPromptTokenCount(input);
+      if (tokenCount <= PROMPT_TOKEN_MAX_LIMIT * IMPOSED_FACTOR) {
+        this.prompts.push(input);
+        this.callCounts++;
+        const currentBudget = Math.floor((PROMPT_TOKEN_MAX_LIMIT - tokenCount) * IMPOSED_FACTOR);
+        if (currentBudget < this.maxBudget) {
+          this.maxBudget = currentBudget;
         }
+      } else {
+        // as long as there is one failure, the strategy will be considered failed
+        this.prompts = [];
+        this.callCounts = 0;
+        this.maxBudget = 0;
+        return {
+          maxBudget: 0,
+          callCounts: 0
+        };
       }
     }
     return {
