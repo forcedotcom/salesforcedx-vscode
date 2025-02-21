7dbbabfed58656f9ebbda732724bfe60b1b002cf
peternhale <peter.hale@salesforce.com>
2025-02-21 10:05:06 -0700

    fix: [W-17881383] add descriptions when missing
    @W-17881383@
    
    Added corrective actions to ensure descriptions appear where required.

--- packages/salesforcedx-vscode-apex/src/oas/documentProcessorPipeline/propertyCorrectionStep.ts
+++ packages/salesforcedx-vscode-apex/src/oas/documentProcessorPipeline/propertyCorrectionStep.ts
@@ -5,6 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import { JSONPath } from 'jsonpath-plus';
 import { OpenAPIV3 } from 'openapi-types';
 import { ProcessorInputOutput, ProcessorStep } from './processorStep';
 
@@ -12,17 +13,65 @@ export class PropertyCorrectionStep implements ProcessorStep {
   process(input: ProcessorInputOutput): Promise<ProcessorInputOutput> {
     let fixedOASDoc = this.ensureServersIsPresent(input.openAPIDoc);
     fixedOASDoc = this.ensureInfoVersionIsPresent(fixedOASDoc);
+    fixedOASDoc = this.ensurePathDescriptionIsPresent(fixedOASDoc);
+    fixedOASDoc = this.ensureResponseDescriptionsArePresent(fixedOASDoc);
+    fixedOASDoc = this.ensureParameterDescriptionsArePresent(fixedOASDoc);
+    fixedOASDoc = this.ensureRequestBodyDescriptionsArePresent(fixedOASDoc);
 
     return new Promise(resolve => {
       resolve({ ...input, openAPIDoc: fixedOASDoc });
     });
   }
 
-  private ensureInfoVersionIsPresent(yaml: OpenAPIV3.Document<{}>): OpenAPIV3.Document<{}> {
-    return { ...yaml, ...{ info: { ...yaml.info, ...{ version: '1.0.0' } } } };
+  private ensureInfoVersionIsPresent(oasDoc: OpenAPIV3.Document<{}>): OpenAPIV3.Document<{}> {
+    return { ...oasDoc, ...{ info: { ...oasDoc.info, ...{ version: '1.0.0' } } } };
   }
 
-  private ensureServersIsPresent(yaml: OpenAPIV3.Document<{}>): OpenAPIV3.Document<{}> {
-    return { ...yaml, ...{ servers: [{ url: '/services/apexrest' }] } };
+  private ensureServersIsPresent(oasDoc: OpenAPIV3.Document<{}>): OpenAPIV3.Document<{}> {
+    return { ...oasDoc, ...{ servers: [{ url: '/services/apexrest' }] } };
+  }
+
+  private ensurePathDescriptionIsPresent(oasDoc: OpenAPIV3.Document<{}>): OpenAPIV3.Document<{}> {
+    return this.ensureDescriptionsArePresent(oasDoc, '$.paths[*]', 'Default description for the endpoint.');
+  }
+
+  private ensureResponseDescriptionsArePresent(oasDoc: OpenAPIV3.Document<{}>): OpenAPIV3.Document<{}> {
+    return this.ensureDescriptionsArePresent(
+      oasDoc,
+      '$.paths[*][*].responses[*]',
+      'Default description for the response.'
+    );
+  }
+
+  private ensureParameterDescriptionsArePresent(oasDoc: OpenAPIV3.Document<{}>): OpenAPIV3.Document<{}> {
+    return this.ensureDescriptionsArePresent(
+      oasDoc,
+      '$.paths[*][*].parameters[*]',
+      'Default description for the parameter.'
+    );
+  }
+
+  private ensureRequestBodyDescriptionsArePresent(oasDoc: OpenAPIV3.Document<{}>): OpenAPIV3.Document<{}> {
+    return this.ensureDescriptionsArePresent(
+      oasDoc,
+      '$.paths[*][*].requestBody',
+      'Default description for the requestBody.'
+    );
+  }
+
+  private ensureDescriptionsArePresent(
+    oasDoc: OpenAPIV3.Document<{}>,
+    jsonPath: string,
+    defaultDescription: string
+  ): OpenAPIV3.Document<{}> {
+    const items = JSONPath({ path: jsonPath, json: oasDoc }) as { description?: string }[];
+
+    items.forEach(item => {
+      if (item && typeof item === 'object' && !item.description) {
+        item.description = defaultDescription;
+      }
+    });
+
+    return oasDoc;
   }
 }
---
+++ packages/salesforcedx-vscode-apex/test/jest/oas/propertyCorrectionStep.test.ts
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2025, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { JSONPath } from 'jsonpath-plus';
+import { OpenAPIV3 } from 'openapi-types';
+import { PropertyCorrectionStep } from '../../../src/oas/documentProcessorPipeline/propertyCorrectionStep';
+
+describe('PropertyCorrectionStep', () => {
+  let step: PropertyCorrectionStep;
+  let openAPIDoc: OpenAPIV3.Document;
+
+  beforeEach(() => {
+    step = new PropertyCorrectionStep();
+    openAPIDoc = {
+      openapi: '3.0.0',
+      info: {
+        title: 'Test API',
+        version: ''
+      },
+      paths: {
+        '/test': {
+          description: '',
+          get: {
+            responses: {
+              200: {
+                description: 'this one is ok'
+              }
+            },
+            parameters: [
+              {
+                name: 'testParam',
+                in: 'query',
+                description: ''
+              }
+            ],
+            requestBody: {
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+  }) as OpenAPIV3.Document;
+
+  it('should ensure info version is present', () => {
+    const result = step['ensureInfoVersionIsPresent'](openAPIDoc);
+    expect(result.info.version).toBe('1.0.0');
+  });
+
+  it('should ensure servers are present', () => {
+    const result = step['ensureServersIsPresent'](openAPIDoc);
+    expect(result.servers).toEqual([{ url: '/services/apexrest' }]);
+  });
+
+  it('should ensure path descriptions are present', () => {
+    const result = step['ensurePathDescriptionIsPresent'](openAPIDoc);
+    const pathDescriptions = JSONPath({ path: '$.paths[*].description', json: result });
+    expect(pathDescriptions).toContain('Default description for the endpoint.');
+  });
+
+  it('should ensure response descriptions are present', () => {
+    const result = step['ensureResponseDescriptionsArePresent'](openAPIDoc);
+    const responseDescriptions = JSONPath({ path: '$.paths[*][*].responses[*].description', json: result });
+    expect(responseDescriptions).toContain('this one is ok');
+    expect(responseDescriptions).not.toContain('Default description for the response.');
+  });
+
+  it('should ensure parameter descriptions are present', () => {
+    const result = step['ensureParameterDescriptionsArePresent'](openAPIDoc);
+    const parameterDescriptions = JSONPath({ path: '$.paths[*][*].parameters[*].description', json: result });
+    expect(parameterDescriptions).toContain('Default description for the parameter.');
+  });
+
+  it('should ensure request body descriptions are present', () => {
+    const result = step['ensureRequestBodyDescriptionsArePresent'](openAPIDoc);
+    const requestBodyDescriptions = JSONPath({ path: '$.paths[*][*].requestBody.description', json: result });
+    expect(requestBodyDescriptions).toContain('Default description for the requestBody.');
+  });
+
+  it('should ensure descriptions are present using generic method', () => {
+    const result = step['ensureDescriptionsArePresent'](
+      openAPIDoc,
+      '$.paths[*][*].parameters[*]',
+      'Default description for the parameter.'
+    );
+    const parameterDescriptions = JSONPath({ path: '$.paths[*][*].parameters[*].description', json: result });
+    expect(parameterDescriptions).toContain('Default description for the parameter.');
+  });
+});
