99a3dde1a584ae62d82175d75a26d842d6e29cb0
mingxuanzhang <mingxuanzhang@salesforce.com>
2025-02-26 10:32:51 -0800

    test: promptHandler

--- packages/salesforcedx-vscode-apex/test/jest/oas/generationStrategy/buildPromptUtils.test.ts
+++ packages/salesforcedx-vscode-apex/test/jest/oas/generationStrategy/buildPromptUtils.test.ts
@@ -13,12 +13,6 @@ import {
   getPromptForMethodContext
 } from '../../../../src/oas/generationStrategy/buildPromptUtils';
 
-/*
- * Copyright (c) 2025, salesforce.com, inc.
- * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- */
 describe('getMethodImplementation', () => {
   it('should return the correct method implementation', () => {
     const methodName = 'myMethod';
---
+++ packages/salesforcedx-vscode-apex/test/jest/oas/generationStrategy/promptsHandler.test.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2025, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import * as fs from 'fs';
+import { ensurePromptsExist } from '../../../../src/oas/generationStrategy/promptsHandler';
+
+jest.mock('fs');
+
+describe('ensurePromptsExist', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should create the prompts directory and file if they do not exist', () => {
+    (fs.existsSync as jest.Mock).mockReturnValueOnce(false).mockReturnValueOnce(false);
+
+    ensurePromptsExist();
+
+    expect(fs.mkdirSync).toHaveBeenCalledWith('.sfdx/oas_prompts', { recursive: true });
+    expect(fs.writeFileSync).toHaveBeenCalledWith('.sfdx/oas_prompts/prompts.yaml', expect.any(String), 'utf8');
+  });
+
+  it('should not create the prompts directory or file if they already exist', () => {
+    (fs.existsSync as jest.Mock).mockReturnValue(true);
+
+    ensurePromptsExist();
+
+    expect(fs.mkdirSync).not.toHaveBeenCalled();
+    expect(fs.writeFileSync).not.toHaveBeenCalled();
+  });
+});
