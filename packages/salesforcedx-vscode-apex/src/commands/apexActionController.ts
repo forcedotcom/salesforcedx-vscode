2b9859802f997dbc3051f6ac8428dc200d70b678
peternhale <peter.hale@salesforce.com>
2025-02-27 15:48:34 -0700

    chore: fix merge filename to remove colon

--- packages/salesforcedx-vscode-apex/src/commands/apexActionController.ts
+++ packages/salesforcedx-vscode-apex/src/commands/apexActionController.ts
@@ -302,7 +302,7 @@ export class ApexActionController {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    const formattedDate = `${month}${day}${year}_${hours}:${minutes}:${seconds}`;
+    const formattedDate = `${month}${day}${year}_${hours}${minutes}${seconds}`;
     return formattedDate;
   };
 
