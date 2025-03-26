9364f393f3533cc7e7c4097e0f5800bf08fdf623
Kyle Walker <kyle.walker+gh@salesforce.com>
2025-03-26 16:58:38 -0700

    fix: needed to also remove the remapping for the vscode mock

--- packages/salesforcedx-vscode-apex/jest.config.js
+++ packages/salesforcedx-vscode-apex/jest.config.js
@@ -1,12 +1,3 @@
 const baseConfig = require('../../config/jest.base.config');
 
-module.exports = Object.assign({}, baseConfig, {
-  moduleNameMapper: {
-    '^vscode$': '<rootDir>/test/jest/__mocks__/vscode.ts'
-  },
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['/node_modules/', '/out/']
-});
+module.exports = Object.assign({}, baseConfig);
