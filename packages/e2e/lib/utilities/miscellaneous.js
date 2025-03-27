ff4a78cd6a557732b9ea0e696943f2765a1488d2
madhur310 <madhur.shrivastava@salesforce.com>
2025-03-27 16:26:46 -0700

    chore: updating mocharc and custom reporter

--- packages/e2e/lib/utilities/miscellaneous.js
+++ packages/e2e/lib/utilities/miscellaneous.js
@@ -21,13 +21,23 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -220,16 +230,11 @@ async function sleep(duration) {
 async function openFolder(path) {
     const prompt = await (0, commandPrompt_1.executeQuickPick)('File: Open Folder...'); // use this cmd palette to open
     // Set the location of the project
-    console.log('open Folder A');
     await prompt.setText(path);
-    console.log('open Folder B');
     await pause(Duration.seconds(2));
     const projectName = path.substring(path.lastIndexOf('/') + 1);
-    console.log('open Folder C');
     await prompt.selectQuickPick(projectName);
-    console.log('open Folder D');
     await (0, commandPrompt_1.clickFilePathOkButton)();
-    console.log('open Folder E');
 }
 /**
  * An definite alternative of getTextEditor to open a file in text editor
