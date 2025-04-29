4eb0ada61deb18ca9de250ab5a84e0be8ae61e0a
mshanemc <shane.mclaughlin@salesforce.com>
2025-04-29 08:54:32 -0500

    refactor: single source of cancellation token

--- packages/salesforcedx-sobjects-faux-generator/src/transformer/sobjectTransformer.ts
+++ packages/salesforcedx-sobjects-faux-generator/src/transformer/sobjectTransformer.ts
@@ -4,6 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import type { CancellationToken } from '@salesforce/salesforcedx-utils';
 import { projectPaths } from '@salesforce/salesforcedx-utils-vscode';
 import { EventEmitter } from 'node:events';
 import * as fs from 'node:fs';
@@ -17,10 +18,6 @@ import {
   SObjectRefreshOutput as SObjectRefreshData,
   SObjectRefreshResult
 } from '../types';
-
-export type CancellationToken = {
-  isCancellationRequested: boolean;
-};
 
 type SObjectRefreshTransformData = SObjectRefreshData & {
   typeNames: SObjectShortDescription[];
