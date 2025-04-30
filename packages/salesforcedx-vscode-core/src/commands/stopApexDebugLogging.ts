6640327c70431720815981db3f9cf27334faa418
Daphne Yang <daphne.yang@salesforce.com>
2025-04-30 11:09:55 -0400

    try: second attempt, again it does not work

--- packages/salesforcedx-vscode-core/src/commands/stopApexDebugLogging.ts
+++ packages/salesforcedx-vscode-core/src/commands/stopApexDebugLogging.ts
@@ -6,64 +6,88 @@
  */
 
 import { CancelResponse, ContinueResponse, ParametersGatherer, TraceFlagsRemover } from '@salesforce/salesforcedx-utils-vscode';
-import { hideTraceFlagExpiration } from '../decorators';
 import { telemetryService } from '../telemetry';
 import { developerLogTraceFlag } from '.';
 import { SfCommandlet, SfWorkspaceChecker } from './util';
 import { WorkspaceContext } from '../context';
 
 export class StopApexDebugLoggingExecutor {
-  public execute(response: ContinueResponse<{}>): void {
-    console.log('Enter execute()');
-    const startTime = process.hrtime();
-
-    void (async () => {
-      try {
-        console.log('Enter try block');
-        await deleteTraceFlag();
-        developerLogTraceFlag.turnOffLogging();
-        hideTraceFlagExpiration();
-        telemetryService.sendCommandEvent('stop_apex_debug_logging', startTime);
-      } catch (error) {
-        console.log('Enter catch block');
-        telemetryService.sendException('stop_apex_debug_logging', error);
-      }
-      console.log('Exit execute()');
-    })();
+  public build(): Promise<boolean> {
+    return deleteTraceFlag();
   }
+
+  public execute(response: ContinueResponse<{}>): void { }
 }
+
+// export class StopApexDebugLoggingExecutor extends SfCommandletExecutor<{}> {
+//   public build(): Command {
+//     return deleteTraceFlag();
+//   }
+
+//   public execute(response: ContinueResponse<{}>): void {
+//     const startTime = process.hrtime();
+//     const cancellationTokenSource = new vscode.CancellationTokenSource();
+//     const cancellationToken = cancellationTokenSource.token;
+
+//     const execution = new CliCommandExecutor(this.build(), {
+//       cwd: workspaceUtils.getRootWorkspacePath()
+//     }).execute(cancellationToken);
+
+//     this.attachExecution(execution, cancellationTokenSource, cancellationToken);
+//     execution.processExitSubject.subscribe(async data => {
+//       this.logMetric(execution.command.logName, startTime);
+//       if (data !== undefined && String(data) === '0') {
+//         developerLogTraceFlag.turnOffLogging();
+//         hideTraceFlagExpiration();
+//       }
+//     });
+//   }
+// }
 
 export const turnOffLogging = async (): Promise<void> => {
   if (developerLogTraceFlag.isActive()) {
-    try {
-      await deleteTraceFlag();
-      telemetryService.sendCommandEvent('stop_apex_debug_logging');
+    // const execution = new CliCommandExecutor(deleteTraceFlag(), {
+    //   cwd: workspaceUtils.getRootWorkspacePath()
+    // }).execute();
+    const deleteTraceFlagResult = await deleteTraceFlag();
+    // telemetryService.sendCommandEvent(execution.command.logName);
+    telemetryService.sendCommandEvent('stop_apex_debug_logging');
+    // const resultPromise = new CommandOutput().getCmdResult(execution);
+    // const result = await resultPromise;
+    // const resultJson = JSON.parse(result);
+    // if (resultJson.status === 0) {
+    //   return Promise.resolve();
+    // } else {
+    //   return Promise.reject('Restoring the debug levels failed.');
+    // }
+    if (deleteTraceFlagResult) {
       return Promise.resolve();
-    } catch (e) {
+    } else {
       return Promise.reject('Restoring the debug levels failed.');
     }
   }
 };
 
-const deleteTraceFlag = async (): Promise<void> => {
-  console.log('Enter deleteTraceFlag()');
+const deleteTraceFlag = async (): Promise<boolean> => {
   const nonNullTraceFlag = developerLogTraceFlag.getTraceFlagId()!;
-  const connection = await WorkspaceContext.getInstance().getConnection();
-  await TraceFlagsRemover.getInstance(connection).removeTraceFlag(nonNullTraceFlag);
-  console.log('Exit deleteTraceFlag()');
+  try {
+    const connection = await WorkspaceContext.getInstance().getConnection();
+    await TraceFlagsRemover.getInstance(connection).removeTraceFlag(nonNullTraceFlag);
+    return true;
+  } catch (error) {
+    console.error('Error removing trace flag:', error);
+    return false;
+  }
 };
 
 class ActiveLogging implements ParametersGatherer<{}> {
   public async gather(): Promise<CancelResponse | ContinueResponse<{}>> {
-    console.log('Enter gather()');
-    console.log('developerLogTraceFlag.isActive()', developerLogTraceFlag.isActive());
     if (developerLogTraceFlag.isActive()) {
       return { type: 'CONTINUE', data: {} };
     }
     return { type: 'CANCEL' };
   }
 }
-
 const workspaceChecker = new SfWorkspaceChecker();
 const parameterGatherer = new ActiveLogging();
 const executor = new StopApexDebugLoggingExecutor();
