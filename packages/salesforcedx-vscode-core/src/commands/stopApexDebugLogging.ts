86a421052edf49c4a9e1445dca018a98a5241e37
Daphne Yang <daphne.yang@salesforce.com>
2025-05-01 17:23:09 -0400

    feat: notifications to make users aware that turnOffLogging() is running

--- packages/salesforcedx-vscode-core/src/commands/stopApexDebugLogging.ts
+++ packages/salesforcedx-vscode-core/src/commands/stopApexDebugLogging.ts
@@ -5,21 +5,44 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { TraceFlagsRemover } from '@salesforce/salesforcedx-utils-vscode';
+import { notificationService, TraceFlagsRemover } from '@salesforce/salesforcedx-utils-vscode';
+import * as vscode from 'vscode';
 import { WorkspaceContext } from '../context';
 import { telemetryService } from '../telemetry';
 import { developerLogTraceFlag } from '.';
 
 export const turnOffLogging = async (): Promise<void> => {
+  console.log('Enter turnOffLogging()');
   if (developerLogTraceFlag.isActive()) {
+    console.log('Developer log trace flag is active');
     try {
-      const nonNullTraceFlag = developerLogTraceFlag.getTraceFlagId()!;
-      const connection = await WorkspaceContext.getInstance().getConnection();
-      await TraceFlagsRemover.getInstance(connection).removeTraceFlag(nonNullTraceFlag);
+      await vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: 'SFDX: Turn Off Apex Debug Log for Replay Debugger',
+          cancellable: false
+        },
+        async progress => {
+          progress.report({ message: 'Running...' });
+          const nonNullTraceFlag = developerLogTraceFlag.getTraceFlagId()!;
+          const connection = await WorkspaceContext.getInstance().getConnection();
+          await TraceFlagsRemover.getInstance(connection).removeTraceFlag(nonNullTraceFlag);
+          telemetryService.sendCommandEvent('stop_apex_debug_logging');
+          return Promise.resolve();
+        });
+        await notificationService.showInformationMessage('SFDX: Turn Off Apex Debug Log for Replay Debugger successfully ran');
+      } catch (error) {
+        console.error('Error in turnOffLogging(): ', error);
+        telemetryService.sendException('stop_apex_debug_logging', error);
+        // await notificationService.showErrorMessage(error);
+        await notificationService.showErrorMessage('Failed to turn off Apex Debug Log for Replay Debugger');
+        return Promise.reject('Restoring the debug levels failed.');
+      }
+    } else {
+      console.log('Developer log trace flag is not active');
       telemetryService.sendCommandEvent('stop_apex_debug_logging');
+      await notificationService.showErrorMessage('No active trace flag found');
       return Promise.resolve();
-    } catch {
-      return Promise.reject('Restoring the debug levels failed.');
     }
-  }
-};
+    console.log('Exit turnOffLogging()');
+  };
