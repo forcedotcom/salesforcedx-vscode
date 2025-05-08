117491179ef0dea13bd268bbc7025d1ea8807b47
Daphne Yang <daphne.yang@salesforce.com>
2025-05-08 17:02:47 -0400

    refactor: use connection.tooling.delete instead of calling traceFlagsRemover.ts

--- packages/salesforcedx-vscode-core/src/commands/stopApexDebugLogging.ts
+++ packages/salesforcedx-vscode-core/src/commands/stopApexDebugLogging.ts
@@ -4,7 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { TraceFlagsRemover } from '@salesforce/salesforcedx-utils-vscode';
+
 import { WorkspaceContext } from '../context';
 import { handleStartCommand, handleFinishCommand } from '../utils/channelUtils';
 import { developerLogTraceFlag } from '.';
@@ -18,7 +18,7 @@ export const turnOffLogging = async (): Promise<void> => {
     try {
       const nonNullTraceFlag = developerLogTraceFlag.getTraceFlagId()!;
       const connection = await WorkspaceContext.getInstance().getConnection();
-      await TraceFlagsRemover.getInstance(connection).removeTraceFlag(nonNullTraceFlag);
+      await connection.tooling.delete('TraceFlag', nonNullTraceFlag);
       await handleFinishCommand(command, true);
     } catch (error) {
       console.error('Error in turnOffLogging(): ', error);
