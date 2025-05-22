77e375f6f55d42247381c2a3ceeca7f6db8daa5f
Daphne Yang <daphne.yang@salesforce.com>
2025-05-22 12:35:34 -0400

    try: put back changes in salesforcedx-vscode-apex-replay-debugger

--- packages/salesforcedx-vscode-apex-replay-debugger/src/adapter/debugConfigurationProvider.ts
+++ packages/salesforcedx-vscode-apex-replay-debugger/src/adapter/debugConfigurationProvider.ts
@@ -11,7 +11,7 @@ import { nls } from '../messages';
 
 export class DebugConfigurationProvider implements vscode.DebugConfigurationProvider {
   private salesforceApexExtension = vscode.extensions.getExtension('salesforce.salesforcedx-vscode-apex');
-  public static getConfig(logFile?: string, stopOnEntry: boolean = true) {
+  public static getConfig(logFile?: string, stopOnEntry: boolean = true): vscode.DebugConfiguration {
     return {
       name: nls.localize('config_name_text'),
       type: DEBUGGER_TYPE,
@@ -19,7 +19,7 @@ export class DebugConfigurationProvider implements vscode.DebugConfigurationProv
       logFile: logFile ? logFile : '${command:AskForLogFileName}',
       stopOnEntry,
       trace: true
-    } as vscode.DebugConfiguration;
+    };
   }
 
   public provideDebugConfigurations(
