4f82201f7b36846c291be31ea673c530f379dd6d
mshanemc <shane.mclaughlin@salesforce.com>
2025-06-03 08:50:11 -0500

    chore: correct typing on StatusBarItem

--- packages/salesforcedx-vscode-core/src/decorators/traceflagTimeDecorator.ts
+++ packages/salesforcedx-vscode-core/src/decorators/traceflagTimeDecorator.ts
@@ -10,29 +10,23 @@ import { StatusBarAlignment, StatusBarItem, window } from 'vscode';
 import { APEX_CODE_DEBUG_LEVEL } from '../constants';
 import { nls } from '../messages';
 
-let statusBarItem: StatusBarItem;
+let statusBarItem: StatusBarItem | undefined;
 
 export const showTraceFlagExpiration = (expirationDate: Date): void => {
-  if (!statusBarItem) {
-    statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left, 40);
-  }
-
-  statusBarItem.text = nls.localize(
-    'apex_debug_log_status_bar_text',
-    expirationDate.toLocaleTimeString(undefined, optionHHmm)
-  );
+  statusBarItem ??= window.createStatusBarItem(StatusBarAlignment.Left, 40);
+  const expirationHHmm = expirationDate.toLocaleTimeString(undefined, optionHHmm);
+  statusBarItem.text = nls.localize('apex_debug_log_status_bar_text', expirationHHmm);
 
   statusBarItem.tooltip = nls.localize(
     'apex_debug_log_status_bar_hover_text',
     APEX_CODE_DEBUG_LEVEL,
-    expirationDate.toLocaleTimeString(undefined, optionHHmm),
+    expirationHHmm,
     expirationDate.toLocaleDateString(undefined, optionMMddYYYY)
   );
   statusBarItem.show();
 };
 
 export const disposeTraceFlagExpiration = (): void => {
-  statusBarItem.dispose();
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/consistent-type-assertions
-  statusBarItem = undefined as any; // Resetting to undefined to allow re-creation
+  statusBarItem?.dispose();
+  statusBarItem = undefined; // Resetting to undefined to allow re-creation
 };
