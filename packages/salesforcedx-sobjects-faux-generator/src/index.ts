78a79fd7e96bd60d64a1441d64bba739727b1c71
mshanemc <shane.mclaughlin@salesforce.com>
2025-06-03 10:48:02 -0500

    refactor: unbarrel

--- packages/salesforcedx-sobjects-faux-generator/src/index.ts
+++ packages/salesforcedx-sobjects-faux-generator/src/index.ts
@@ -5,6 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 export { CLIENT_ID, SOBJECTS_DIR, STANDARDOBJECTS_DIR, CUSTOMOBJECTS_DIR, SOQLMETADATA_DIR } from './constants';
-export { SObjectTransformer, SObjectTransformerFactory } from './transformer';
 export * from './types';
 export * from './describe';
+export { SObjectTransformer } from './transformer/sobjectTransformer';
+export { SObjectTransformerFactory } from './transformer/transformerFactory';
