a1908ed1634b5f98965ecada119e5248fe53f6c8
mshanemc <shane.mclaughlin@salesforce.com>
2025-06-10 11:07:37 -0500

    refactor: less mutation

--- packages/salesforcedx-vscode-apex/src/oasUtils.ts
+++ packages/salesforcedx-vscode-apex/src/oasUtils.ts
@@ -13,7 +13,7 @@ import type { OpenAPIV3 } from 'openapi-types';
 import type { SalesforceVSCodeCoreApi } from 'salesforcedx-vscode-core';
 import * as vscode from 'vscode';
 import { URI } from 'vscode-uri';
-import * as yaml from 'yaml';
+import { parse as yamlParse } from 'yaml';
 import { SF_LOG_LEVEL_SETTING, VSCODE_APEX_EXTENSION_NAME } from './constants';
 import { nls } from './messages';
 import OasProcessor from './oas/documentProcessorPipeline';
@@ -148,7 +148,7 @@ export const parseOASDocFromJson = (doc: string): OpenAPIV3.Document => JSON.par
  * @param {string} doc - The YAML string representing the OAS document.
  * @returns {OpenAPIV3.Document} - The parsed OAS document.
  */
-export const parseOASDocFromYaml = (doc: string): OpenAPIV3.Document => yaml.parse(doc) as OpenAPIV3.Document;
+export const parseOASDocFromYaml = (doc: string): OpenAPIV3.Document => yamlParse(doc) as OpenAPIV3.Document;
 
 const PROMPT_TEMPLATES = {
   METHOD_BY_METHOD: path.join('resources', 'templates', 'methodByMethod.ejs')
