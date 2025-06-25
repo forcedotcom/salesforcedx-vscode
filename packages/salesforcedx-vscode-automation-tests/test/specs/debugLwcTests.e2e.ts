c39d22baf4fb0fd14179514dfaa9c0d86a83c2c2
madhur310 <madhur.shrivastava@salesforce.com>
2025-06-25 08:35:19 -0700

    fix: update debug lwc to scroll to top

--- packages/salesforcedx-vscode-automation-tests/test/specs/debugLwcTests.e2e.ts
+++ packages/salesforcedx-vscode-automation-tests/test/specs/debugLwcTests.e2e.ts
@@ -174,10 +174,15 @@ describe('Debug LWC Tests', () => {
     await verifyOutputPanelText(terminalText!, expectedTexts);
   });
 
-  it('Debug All Tests via Code Lens action', async () => {
+  it.only('Debug All Tests via Code Lens action', async () => {
     log(`${testSetup.testSuiteSuffixName} - Debug All Tests via Code Lens action`);
     const workbench = getWorkbench();
     const textEditor = await getTextEditor(workbench, 'lwc1.test.js');
+
+    // Go to the top of the file
+    const inputBox = await executeQuickPick('Go to Line/Column', Duration.seconds(3));
+    await inputBox.setText(':1');
+    await inputBox.confirm();
 
     // Click the "Debug" code lens at the top of the class
     const debugAllTestsOption = await waitForAndGetCodeLens(textEditor, 'Debug');
