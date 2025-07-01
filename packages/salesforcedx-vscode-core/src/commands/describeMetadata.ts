692b465c66bb3c3c6c904c29ef0bea2aa93898ca
Daphne Yang <daphne.yang@salesforce.com>
2025-07-01 16:12:10 -0400

    refactor: use createDirectory() and writeFile() instead of node:fs

--- packages/salesforcedx-vscode-core/src/commands/describeMetadata.ts
+++ packages/salesforcedx-vscode-core/src/commands/describeMetadata.ts
@@ -5,15 +5,15 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import * as fs from 'node:fs';
+import { createDirectory, writeFile } from '@salesforce/salesforcedx-utils-vscode';
 import * as path from 'node:path';
 import { WorkspaceContext } from '../context';
 
 export const describeMetadata = async (outputFolder: string): Promise<string> => {
-  await fs.promises.mkdir(outputFolder, { recursive: true });
+  await createDirectory(outputFolder);
   const filePath = path.join(outputFolder, 'metadataTypes.json');
   const connection = await WorkspaceContext.getInstance().getConnection();
   const result = await connection.metadata.describe();
-  fs.writeFileSync(filePath, JSON.stringify(result, null, 2));
+  await writeFile(filePath, JSON.stringify(result, null, 2));
   return JSON.stringify(result, null, 2);
 };
