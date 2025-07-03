aed1682bdb127b75b2dd5194186ed6ef5af6c9fe
madhur310 <madhur.shrivastava@salesforce.com>
2025-07-03 07:22:06 -0700

    fix: create scripts inline

--- .github/scripts/install-dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing npm dependencies..."
-cd salesforcedx-vscode
-npm install
-echo "Dependencies installed successfully"
--- .github/scripts/install-salesforce-cli.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing Salesforce CLI..."
-npm install -g @salesforce/cli
-echo "Salesforce CLI installed successfully"
