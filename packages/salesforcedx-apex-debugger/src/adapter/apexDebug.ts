48692184bd5e042413d1119ad4ef5896bcb629b3
mshanemc <shane.mclaughlin@salesforce.com>
2025-07-07 07:54:45 -0500

    chore: typo and lockfile

--- packages/salesforcedx-apex-debugger/src/adapter/apexDebug.ts
+++ packages/salesforcedx-apex-debugger/src/adapter/apexDebug.ts
@@ -1099,7 +1099,7 @@ export class ApexDebug extends LoggingDebugSession {
           );
         } else if (reference.type === 'map') {
           const mapContainer = new MapReferenceContainer(reference, requestId);
-          // explode all map entried so that we can drill down a map logically
+          // explode all map entries so that we can drill down a map logically
           if (reference.tuple) {
             reference.tuple.forEach(tuple => {
               const tupleContainer = new MapTupleContainer(tuple, requestId);
