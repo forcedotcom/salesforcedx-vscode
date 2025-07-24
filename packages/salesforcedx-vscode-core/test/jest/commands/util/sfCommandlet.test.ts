c267baae9716a7d7d1ed299869c9e609c67719de
Cristina Cañizales <113132642+CristiCanizales@users.noreply.github.com>
2025-07-24 14:27:30 -0300

    fix: launch extensions was not working (#6436)

--- packages/salesforcedx-vscode-core/test/jest/commands/util/sfCommandlet.test.ts
+++ packages/salesforcedx-vscode-core/test/jest/commands/util/sfCommandlet.test.ts
@@ -4,11 +4,11 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { ProjectRetrieveStartResultParser } from '@salesforce/salesforcedx-utils-vscode';
 import {
+  ProjectRetrieveStartResultParser,
   ProjectRetrieveStartErrorResponse,
   ProjectRetrieveStartSuccessResponse
-} from '@salesforce/salesforcedx-utils-vscode/src/cli/parsers/projectRetrieveStartResultParser';
+} from '@salesforce/salesforcedx-utils-vscode';
 import { channelService } from '../../../../src/channels';
 import { DeployType, ProjectDeployStartExecutor } from '../../../../src/commands/projectDeployStart';
 import { ProjectRetrieveStartExecutor } from '../../../../src/commands/projectRetrieveStart';
