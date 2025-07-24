9cb1532b304552c0f7917f835877c019eeb8e6c9
Cristina Isabel Cañizales <cristina.canizales@salesforce.com>
2025-07-24 12:22:45 -0300

    fix: launch extensions was not working

--- packages/salesforcedx-vscode-core/src/settings/pushOrDeployOnSave.ts
+++ packages/salesforcedx-vscode-core/src/settings/pushOrDeployOnSave.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { TimingUtils } from '@salesforce/salesforcedx-utils-vscode/src/helpers/timingUtils';
+import { TimingUtils } from '@salesforce/salesforcedx-utils-vscode';
 import * as path from 'node:path';
 import { setTimeout } from 'node:timers';
 import * as vscode from 'vscode';
