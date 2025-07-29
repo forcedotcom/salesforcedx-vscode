dc155446b0638a96d2712eb7266907baf78cf22e
Cristina Isabel Cañizales <cristina.canizales@salesforce.com>
2025-07-29 00:25:47 -0300

    chore: run push without ignore instead on e2e

--- packages/salesforcedx-vscode-automation-tests/test/specs/debugApexTests.e2e.ts
+++ packages/salesforcedx-vscode-automation-tests/test/specs/debugApexTests.e2e.ts
@@ -63,13 +63,10 @@ describe('Debug Apex Tests', () => {
     );
 
     // Push source to org
-    await executeQuickPick('SFDX: Push Source to Default Org and Ignore Conflicts', Duration.seconds(1));
+    await executeQuickPick('SFDX: Push Source to Default Org', Duration.seconds(1));
 
-    // Look for the success notification that appears which says, "SFDX: Push Source to Default Org and Ignore Conflicts successfully ran".
-    await verifyNotificationWithRetry(
-      /SFDX: Push Source to Default Org and Ignore Conflicts successfully ran/,
-      Duration.TEN_MINUTES
-    );
+    // Look for the success notification that appears which says, "SFDX: Push Source to Default Org successfully ran".
+    await verifyNotificationWithRetry(/SFDX: Push Source to Default Org successfully ran/, Duration.TEN_MINUTES);
   });
 
   beforeEach(function () {
