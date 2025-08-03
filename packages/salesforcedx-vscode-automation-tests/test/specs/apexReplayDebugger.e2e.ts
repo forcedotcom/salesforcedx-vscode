be8d1f4bda0e53717f8b24896dcabeffbda947ee
Daphne Yang <daphne.yang@salesforce.com>
2025-08-03 11:42:04 -0400

    Revert "fix: apexReplayDebugger.e2e.ts and trailApexReplayDebugger.e2e.ts should check for activation of Apex Replay Debugger extension"
    This reverts commit d8ffcc98eb385eb2cf4afdae022618df9d6bb445.

--- packages/salesforcedx-vscode-automation-tests/test/specs/apexReplayDebugger.e2e.ts
+++ packages/salesforcedx-vscode-automation-tests/test/specs/apexReplayDebugger.e2e.ts
@@ -34,7 +34,7 @@ import {
 import { expect } from 'chai';
 import * as path from 'node:path';
 import { InputBox, QuickOpenBox, TextEditor } from 'vscode-extension-tester';
-import { apexReplayDebuggerExtensionConfigs } from '../testData/constants';
+import { defaultExtensionConfigs } from '../testData/constants';
 import { tryToHideCopilot } from '../utils/copilotHidingHelper';
 import { logTestStart } from '../utils/loggingHelper';
 
@@ -50,7 +50,7 @@ describe('Apex Replay Debugger', () => {
     },
     isOrgRequired: true,
     testSuiteSuffixName: 'ApexReplayDebugger',
-    extensionConfigs: apexReplayDebuggerExtensionConfigs
+    extensionConfigs: defaultExtensionConfigs
   };
 
   before('Set up the testing environment', async () => {
--- packages/salesforcedx-vscode-automation-tests/test/specs/trailApexReplayDebugger.e2e.ts
+++ packages/salesforcedx-vscode-automation-tests/test/specs/trailApexReplayDebugger.e2e.ts
@@ -33,7 +33,7 @@ import {
 import { expect } from 'chai';
 import * as path from 'node:path';
 import { By, InputBox, QuickOpenBox, TextEditor } from 'vscode-extension-tester';
-import { apexReplayDebuggerExtensionConfigs } from '../testData/constants';
+import { defaultExtensionConfigs } from '../testData/constants';
 import { tryToHideCopilot } from '../utils/copilotHidingHelper';
 import { logTestStart } from '../utils/loggingHelper';
 
@@ -52,7 +52,7 @@ describe('"Find and Fix Bugs with Apex Replay Debugger" Trailhead Module', () =>
     },
     isOrgRequired: true,
     testSuiteSuffixName: 'TrailApexReplayDebugger',
-    extensionConfigs: apexReplayDebuggerExtensionConfigs
+    extensionConfigs: defaultExtensionConfigs
   };
 
   before('Set up the testing environment', async () => {
