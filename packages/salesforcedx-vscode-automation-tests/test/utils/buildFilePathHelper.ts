64882618518110e3d583d953e94c6b31208def4f
Daphne Yang <daphne.yang@salesforce.com>
2025-08-04 13:54:10 -0400

    fix: correct way to import path in buildFilePathHelper.ts

--- packages/salesforcedx-vscode-automation-tests/test/utils/buildFilePathHelper.ts
+++ packages/salesforcedx-vscode-automation-tests/test/utils/buildFilePathHelper.ts
@@ -4,6 +4,6 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import path from 'node:path';
+import * as path from 'node:path';
 
 export const getFolderPath = (projectPath: string, folder: string): string => path.join(projectPath, 'force-app', 'main', 'default', folder);
