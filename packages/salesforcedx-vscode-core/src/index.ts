aec8a210bf3093f6613ba6d4cc1aa45f60daec29
Daphne Yang <139700604+daphne-sfdc@users.noreply.github.com>
2025-08-21 10:06:19 -0400

    [W-19176017] refactor: telemetry display without CLI (#6482)
    * feat: use (orgId + userId) hashed in SHA256 for telemetry user id in AppInsights
    
    * fix: replace random userId with (orgId + userId) once an org is authorized
    
    * fix: all extensions share the telemetry ID from Core extension
    
    * fix: call onOrgChange() listener to update telemetry ID after each org change
    
    * fix: refresh telemetry reporters with the new user ID after switching orgs
    
    * fix: refresh telemetry reporters for all extensions, not just Core extension
    
    * chore: lint
    
    * chore: comment
    
    * fix: use anonymous telemetry ID instead of random telemetry ID when no org is authorized
    
    * refactor: put UNAUTHENTICATED_USER in a constant
    
    * chore: remove unnecessary console.log() statement
    
    * refactor: this.reporters.length = 0
    
    * refactor: move helper functions to new file telemetryUtils.ts
    
    * test: add unit tests for refreshReporters() in telemetry.test.ts
    
    * refactor: use dependency injection for infinite loop handling
    
    * refactor: move getTelemetryUserId() out of UserService class

--- packages/salesforcedx-vscode-core/src/index.ts
+++ packages/salesforcedx-vscode-core/src/index.ts
@@ -13,6 +13,7 @@ import {
   TelemetryService,
   TimingUtils,
   TraceFlags,
+  getTelemetryUserId,
   WorkspaceContextUtil,
   ensureCurrentWorkingDirIsProjectPath,
   getRootWorkspacePath,
@@ -475,6 +476,7 @@ export const activate = async (extensionContext: vscode.ExtensionContext): Promi
     WorkspaceContext,
     taskViewService,
     telemetryService,
+    getSharedTelemetryUserId: async () => await getTelemetryUserId(extensionContext, undefined),
     services: {
       RegistryAccess,
       ChannelService,
@@ -602,6 +604,7 @@ export type SalesforceVSCodeCoreApi = {
   WorkspaceContext: typeof WorkspaceContext;
   taskViewService: typeof taskViewService;
   telemetryService: typeof telemetryService;
+  getSharedTelemetryUserId: () => Promise<string>;
   services: {
     RegistryAccess: typeof RegistryAccess;
     ChannelService: typeof ChannelService;
