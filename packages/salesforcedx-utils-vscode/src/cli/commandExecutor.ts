4b3bf17e04e337a730419d3d9f1e3cf5730326f6
mshanemc <shane.mclaughlin@salesforce.com>
2025-08-22 12:38:39 -0500

    chore: remove dead classes

--- packages/salesforcedx-utils-vscode/src/cli/commandExecutor.ts
+++ packages/salesforcedx-utils-vscode/src/cli/commandExecutor.ts
@@ -6,11 +6,9 @@
  */
 import { Command, GlobalCliEnvironment, CancellationToken } from '@salesforce/salesforcedx-utils';
 import { ChildProcess, SpawnOptions } from 'node:child_process';
-import * as os from 'node:os';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/observable/interval';
 import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
 import { Subscription } from 'rxjs/Subscription';
 
 // Below two dependencies are not structured correctly for import unless require is used.
@@ -62,18 +60,6 @@ export class CliCommandExecutor {
   }
 }
 
-export class CompositeCliCommandExecutor {
-  private readonly command: Command;
-
-  constructor(commands: Command) {
-    this.command = commands;
-  }
-
-  public execute(cancellationToken?: CancellationToken): CompositeCliCommandExecution {
-    return new CompositeCliCommandExecution(this.command, cancellationToken);
-  }
-}
-
 /**
  * Represents a command execution (a process has already been spawned for it).
  * This is tightly coupled with the execution model (child_process).
@@ -88,69 +74,6 @@ export type CommandExecution = {
   readonly stdoutSubject: Observable<Buffer | string>;
   readonly stderrSubject: Observable<Buffer | string>;
 };
-
-export class CompositeCliCommandExecution implements CommandExecution {
-  public readonly command: Command;
-  public readonly cancellationToken?: CancellationToken;
-  public readonly processExitSubject: Observable<number | undefined>;
-  public readonly processErrorSubject: Observable<Error | undefined>;
-  public readonly stdoutSubject: Observable<string>;
-  public readonly stderrSubject: Observable<string>;
-  private readonly exitSubject: Subject<number | undefined>;
-  private readonly errorSubject: Subject<Error | undefined>;
-  private readonly stdout: Subject<string>;
-  private readonly stderr: Subject<string>;
-
-  constructor(command: Command, cancellationToken?: CancellationToken) {
-    this.exitSubject = new Subject();
-    this.errorSubject = new Subject();
-    this.stdout = new Subject();
-    this.stderr = new Subject();
-    this.command = command;
-    this.cancellationToken = cancellationToken;
-    this.processExitSubject = this.exitSubject.asObservable();
-    this.processErrorSubject = this.errorSubject.asObservable();
-    this.stdoutSubject = this.stdout.asObservable();
-    this.stderrSubject = this.stderr.asObservable();
-
-    let timerSubscriber: Subscription | null;
-    if (cancellationToken) {
-      const timer = Observable.interval(1000);
-      timerSubscriber = timer.subscribe(() => {
-        if (cancellationToken.isCancellationRequested) {
-          try {
-            this.exitSubject.next();
-          } catch (e) {
-            console.log(e);
-          }
-        }
-      });
-    }
-    this.processErrorSubject.subscribe(() => {
-      if (timerSubscriber) {
-        timerSubscriber.unsubscribe();
-      }
-    });
-
-    this.processExitSubject.subscribe(() => {
-      if (timerSubscriber) {
-        timerSubscriber.unsubscribe();
-      }
-    });
-  }
-
-  public successfulExit() {
-    this.exitSubject.next(0);
-  }
-
-  public failureExit(e?: {}) {
-    if (e) {
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      this.stderr.next(`${e}${os.EOL}`);
-    }
-    this.exitSubject.next(1);
-  }
-}
 
 export class CliCommandExecution implements CommandExecution {
   public readonly command: Command;
