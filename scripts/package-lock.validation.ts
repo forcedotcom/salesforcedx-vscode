598ade433b7914a935968f18bc6140bd5d913a55
mshanemc <shane.mclaughlin@salesforce.com>
2025-08-26 16:06:18 -0500

    test: faster output

--- scripts/package-lock.validation.ts
+++ scripts/package-lock.validation.ts
@@ -1,12 +1,12 @@
 import lockfile from '../package-lock.json';
+import assert from 'node:assert';
 
-Object.entries(lockfile.packages)
-  .filter(([key, value]) => key.startsWith('node_modules') && !('link' in value) && !('inBundle' in value))
-  .filter(([key, value]) => !('integrity' in value) || !('resolved' in value))
-  .map(([key], i, arr) => {
-    throw new Error(
-      `integrity/resolved property is missing for ${key} and ${arr.length - i} others.  see npm bug https://github.com/npm/cli/issues/4263.  copy the lockfile from the default branch and node_modules and re-run npm install.`
-    );
-  });
-
+assert.deepStrictEqual(
+  Object.entries(lockfile.packages)
+    .filter(([key]) => key.startsWith('node_modules'))
+    .filter(([, value]) => !('link' in value) && !('inBundle' in value))
+    .filter(([, value]) => !('integrity' in value) || !('resolved' in value)),
+  [],
+  `integrity/resolved property is missing in package-lock.json.  see npm bug https://github.com/npm/cli/issues/4263.  copy the lockfile from the default branch and node_modules and re-run npm install.`
+);
 console.log('Package lock file is valid');
