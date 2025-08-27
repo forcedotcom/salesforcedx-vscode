a8fd7468f19f01f89a2f53554d2e7e77e4ef46e3
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2025-08-27 10:41:14 -0500

    ci: core bundle without pino transforms W-19376651 (#6489)
    * ci: bundle without pino plugin
    
    * chore: ensuring dist
    
    * ci: compile + bundle before package, even more /dist creation just in case
    
    * ci: top-level await, less mkdirp
    
    * ci: unused import
    
    * chore: update date in copyright header (#6502)
    
    * chore: rebuild lockfile with optional peer dep ignored
    
    * chore: lockfile check
    
    * test: faster output
    
    ---------
    
    Co-authored-by: Cristina Cañizales <113132642+CristiCanizales@users.noreply.github.com>
    Co-authored-by: Cristina Isabel Cañizales <cristina.canizales@salesforce.com>

---
+++ packages/salesforcedx-vscode-lwc/esbuild.config.mjs
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { nodeConfig } from '../../scripts/bundling/node.mjs';
+import { build } from 'esbuild';
+
+await build({
+  ...nodeConfig,
+  loader: { '.node': 'file' },
+  external: [
+    'vscode',
+    'applicationinsights',
+    '@salesforce/lightning-lsp-common',
+    '@salesforce/lwc-language-server',
+    '@babel/preset-typescript/package.json',
+    'jest-editor-support'
+  ],
+  entryPoints: ['./src/index.ts'],
+  outdir: 'dist'
+});
---
+++ scripts/bundling/node.mjs
@@ -0,0 +1,17 @@
+export const nodeConfig = {
+  bundle: true,
+  format: 'cjs',
+  platform: 'node',
+  keepNames: true,
+  minify: true,
+  supported: {
+    'dynamic-import': false
+  },
+  logOverride: {
+    'unsupported-dynamic-import': 'error'
+  },
+  define: {
+    // this prevents the logger from writing to any files, obviating the need for pino-bundling stuff
+    'process.env.SF_DISABLE_LOG_FILE': "'true'"
+  }
+};
