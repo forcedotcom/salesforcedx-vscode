63be6b2fa747279c380fdcd6389c248f2dcf2590
Daphne Yang <daphne.yang@salesforce.com>
2025-10-08 11:53:30 -0400

    refactor: remove duplicate code

--- packages/salesforcedx-vscode-lwc/src/testSupport/codeLens/provideLwcTestCodeLens.ts
+++ packages/salesforcedx-vscode-lwc/src/testSupport/codeLens/provideLwcTestCodeLens.ts
@@ -4,7 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { parse } from 'jest-editor-support';
+import { parse, ItBlock, DescribeBlock } from 'jest-editor-support';
 import { CancellationToken, CodeLens, Command, Position, Range, TextDocument } from 'vscode';
 import { nls } from '../../messages';
 import { TestExecutionInfo, TestInfoKind, TestType } from '../types';
@@ -16,78 +16,54 @@ import { TestExecutionInfo, TestInfoKind, TestType } from '../types';
  * @param document text document
  * @param token cancellation token
  */
+/**
+ * Create code lenses for a test block (it or describe)
+ */
+const createCodeLensesForTestBlock = (testBlock: ItBlock | DescribeBlock, document: TextDocument): CodeLens[] => {
+  const { name, nameRange } = testBlock;
+  // VS Code position is zero-based
+  const range = new Range(
+    new Position(nameRange.start.line - 1, nameRange.start.column - 1),
+    new Position(nameRange.end.line - 1, nameRange.end.column - 1)
+  );
+
+  const testExecutionInfo: TestExecutionInfo = {
+    kind: TestInfoKind.TEST_CASE,
+    testType: TestType.LWC,
+    testUri: document.uri,
+    testName: name
+  };
+
+  const runTestTitle = nls.localize('run_test_title');
+  const runTestCaseCommand: Command = {
+    command: 'sf.lightning.lwc.test.case.run',
+    title: runTestTitle,
+    tooltip: runTestTitle,
+    arguments: [{ testExecutionInfo }]
+  };
+  const runTestCaseCodeLens = new CodeLens(range, runTestCaseCommand);
+
+  const debugTestTitle = nls.localize('debug_test_title');
+  const debugTestCaseCommand: Command = {
+    command: 'sf.lightning.lwc.test.case.debug',
+    title: debugTestTitle,
+    tooltip: debugTestTitle,
+    arguments: [{ testExecutionInfo }]
+  };
+  const debugTestCaseCodeLens = new CodeLens(range, debugTestCaseCommand);
+
+  return [runTestCaseCodeLens, debugTestCaseCodeLens];
+};
+
 export const provideLwcTestCodeLens = (document: TextDocument, _token: CancellationToken): CodeLens[] => {
   const fsPath = document.uri.fsPath;
   const parseResults = parse(fsPath, document.getText());
   const { itBlocks, describeBlocks } = parseResults;
 
-  const itBlockCodeLenses = itBlocks.flatMap(itBlock => {
-    const { name, nameRange } = itBlock;
-    // VS Code position is zero-based
-    const range = new Range(
-      new Position(nameRange.start.line - 1, nameRange.start.column - 1),
-      new Position(nameRange.end.line - 1, nameRange.end.column - 1)
-    );
-
-    const testExecutionInfo: TestExecutionInfo = {
-      kind: TestInfoKind.TEST_CASE,
-      testType: TestType.LWC,
-      testUri: document.uri,
-      testName: name
-    };
-    const runTestTitle = nls.localize('run_test_title');
-    const runTestCaseCommand: Command = {
-      command: 'sf.lightning.lwc.test.case.run',
-      title: runTestTitle,
-      tooltip: runTestTitle,
-      arguments: [{ testExecutionInfo }]
-    };
-    const runTestCaseCodeLens = new CodeLens(range, runTestCaseCommand);
-
-    const debugTestTitle = nls.localize('debug_test_title');
-    const debugTestCaseCommand: Command = {
-      command: 'sf.lightning.lwc.test.case.debug',
-      title: debugTestTitle,
-      tooltip: debugTestTitle,
-      arguments: [{ testExecutionInfo }]
-    };
-    const debugTestCaseCodeLens = new CodeLens(range, debugTestCaseCommand);
-    return [runTestCaseCodeLens, debugTestCaseCodeLens];
-  });
-
-  const describeBlockCodeLenses = describeBlocks.flatMap(describeBlock => {
-    const { name, nameRange } = describeBlock;
-    // VS Code position is zero-based
-    const range = new Range(
-      new Position(nameRange.start.line - 1, nameRange.start.column - 1),
-      new Position(nameRange.end.line - 1, nameRange.end.column - 1)
-    );
-
-    const testExecutionInfo: TestExecutionInfo = {
-      kind: TestInfoKind.TEST_CASE,
-      testType: TestType.LWC,
-      testUri: document.uri,
-      testName: name
-    };
-    const runTestTitle = nls.localize('run_test_title');
-    const runTestCaseCommand: Command = {
-      command: 'sf.lightning.lwc.test.case.run',
-      title: runTestTitle,
-      tooltip: runTestTitle,
-      arguments: [{ testExecutionInfo }]
-    };
-    const runTestCaseCodeLens = new CodeLens(range, runTestCaseCommand);
-
-    const debugTestTitle = nls.localize('debug_test_title');
-    const debugTestCaseCommand: Command = {
-      command: 'sf.lightning.lwc.test.case.debug',
-      title: debugTestTitle,
-      tooltip: debugTestTitle,
-      arguments: [{ testExecutionInfo }]
-    };
-    const debugTestCaseCodeLens = new CodeLens(range, debugTestCaseCommand);
-    return [runTestCaseCodeLens, debugTestCaseCodeLens];
-  });
+  const itBlockCodeLenses = itBlocks.flatMap(itBlock => createCodeLensesForTestBlock(itBlock, document));
+  const describeBlockCodeLenses = describeBlocks.flatMap(describeBlock =>
+    createCodeLensesForTestBlock(describeBlock, document)
+  );
 
   return [...itBlockCodeLenses, ...describeBlockCodeLenses];
 };
