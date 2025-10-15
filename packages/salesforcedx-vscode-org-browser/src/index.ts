437b09a5bfa25e42bf19a31354621f72d233e225
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2025-10-15 16:34:48 -0500

    feat: toggle between old and new orgBrowser W-19881349 (#6603)
    * feat: toggle between old and new orgBrowser
    
    * refactor: opt in the legacy orgBrowser, default false

--- packages/salesforcedx-vscode-org-browser/src/index.ts
+++ packages/salesforcedx-vscode-org-browser/src/index.ts
@@ -14,8 +14,17 @@ import { AllServicesLayer, ExtensionProviderService } from './services/extension
 import { MetadataTypeTreeProvider } from './tree/metadataTypeTreeProvider';
 import { OrgBrowserTreeItem } from './tree/orgBrowserNode';
 
-export const activate = async (context: vscode.ExtensionContext): Promise<void> =>
-  Effect.runPromise(Effect.provide(activateEffect(context), AllServicesLayer));
+export const activate = async (context: vscode.ExtensionContext): Promise<void> => {
+  const coreConfig = vscode.workspace.getConfiguration('salesforcedx-vscode-core');
+  const useLegacyOrgBrowser = coreConfig.get<boolean>('useLegacyOrgBrowser', false);
+
+  if (useLegacyOrgBrowser) {
+    console.log('Salesforce Org Browser extension disabled via setting (legacy org browser enabled)');
+    return;
+  }
+
+  return Effect.runPromise(Effect.provide(activateEffect(context), AllServicesLayer));
+};
 
 export const deactivate = async (): Promise<void> =>
   Effect.runPromise(Effect.provide(deactivateEffect, AllServicesLayer));
