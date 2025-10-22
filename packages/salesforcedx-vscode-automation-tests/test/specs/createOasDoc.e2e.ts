57a43a0ba30c6f58bc4852680e36aaebe505a350
Cristina Isabel Cañizales <cristina.canizales@salesforce.com>
2025-10-22 15:32:43 -0300

    chore: right problems

--- packages/salesforcedx-vscode-automation-tests/test/specs/createOasDoc.e2e.ts
+++ packages/salesforcedx-vscode-automation-tests/test/specs/createOasDoc.e2e.ts
@@ -227,7 +227,7 @@ describe('Create OpenAPI v3 Specifications', () => {
 
     it('Composed mode - Check for warnings and errors in the Problems Tab', async () => {
       logTestStart(testSetup, 'Check for warnings and errors in the Problems Tab');
-      await countProblemsInProblemsTab(0);
+      await countProblemsInProblemsTab(2);
     });
 
     it('Composed mode - Fix the OAS doc to get rid of the problems in the Problems Tab', async () => {
@@ -247,7 +247,7 @@ describe('Create OpenAPI v3 Specifications', () => {
         /Validated OpenAPI Document CaseManager.externalServiceRegistration-meta.xml successfully/
       );
 
-      const problems = await countProblemsInProblemsTab(2);
+      const problems = await countProblemsInProblemsTab(0);
       expect(await problems[0].getLabel()).to.equal('CaseManager.externalServiceRegistration-meta.xml');
       expect(await problems[1].getLabel()).to.equal('operations.responses.content should be application/json');
     });
