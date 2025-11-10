c4b9969010b3d938773f74e5718d27dc3a738c01
madhur310 <madhur.shrivastava@salesforce.com>
2025-11-09 21:46:50 -0800

    chore: unixify paths

--- packages/salesforcedx-lwc-language-server/jest.config.js
+++ packages/salesforcedx-lwc-language-server/jest.config.js
@@ -1,27 +1,29 @@
 module.exports = {
-    displayName: 'unit',
-    transform: {
-        '.ts': 'ts-jest',
-    },
-    testRegex: 'src/.*(\\.|/)(test|spec)\\.(ts|js)$',
-    testPathIgnorePatterns: ['/out/', '/lib/'],
-    moduleFileExtensions: ['ts', 'js', 'json'],
-    setupFilesAfterEnv: ['<rootDir>/jest/matchers.ts', 'jest-extended', '<rootDir>/jest.setup.js'],
-    testEnvironmentOptions: {
-        url: 'http://localhost/',
-    },
-    moduleNameMapper: {
-        '^vscode$': '<rootDir>/../../config/__mocks__/vscode.js',
-        '^@salesforce/salesforcedx-lightning-lsp-common/testUtils$': '<rootDir>/../salesforcedx-lightning-lsp-common/out/src/__tests__/testUtils',
-        '^@salesforce/salesforcedx-lightning-lsp-common/providers/fileSystemDataProvider$':
-            '<rootDir>/../salesforcedx-lightning-lsp-common/out/src/providers/fileSystemDataProvider',
-        '^@salesforce/salesforcedx-lightning-lsp-common/resources/sfdx/tsconfig-sfdx.base.json$':
-            '<rootDir>/../salesforcedx-lightning-lsp-common/out/src/resources/sfdx/tsconfig-sfdx.base.json',
-        '^@salesforce/salesforcedx-lightning-lsp-common/resources/sfdx/tsconfig-sfdx.json$':
-            '<rootDir>/../salesforcedx-lightning-lsp-common/out/src/resources/sfdx/tsconfig-sfdx.json',
-        '^@salesforce/salesforcedx-lightning-lsp-common$': '<rootDir>/../salesforcedx-lightning-lsp-common/out/src/index',
-    },
-    // Add common package's out/src to moduleDirectories so relative imports from baseContext.js resolve correctly
-    // This allows Jest to find JSON files when baseContext.js does require("./resources/...")
-    moduleDirectories: ['<rootDir>/../salesforcedx-lightning-lsp-common/out/src', 'node_modules'],
+  displayName: 'unit',
+  transform: {
+    '.ts': 'ts-jest'
+  },
+  testRegex: 'src/.*(\\.|/)(test|spec)\\.(ts|js)$',
+  // Use regex patterns that match both forward slashes and backslashes for cross-platform compatibility
+  testPathIgnorePatterns: ['[/\\\\]out[/\\\\]', '[/\\\\]lib[/\\\\]'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  setupFilesAfterEnv: ['<rootDir>/jest/matchers.ts', 'jest-extended', '<rootDir>/jest.setup.js'],
+  testEnvironmentOptions: {
+    url: 'http://localhost/'
+  },
+  moduleNameMapper: {
+    '^vscode$': '<rootDir>/../../config/__mocks__/vscode.js',
+    '^@salesforce/salesforcedx-lightning-lsp-common/testUtils$':
+      '<rootDir>/../salesforcedx-lightning-lsp-common/out/src/__tests__/testUtils',
+    '^@salesforce/salesforcedx-lightning-lsp-common/providers/fileSystemDataProvider$':
+      '<rootDir>/../salesforcedx-lightning-lsp-common/out/src/providers/fileSystemDataProvider',
+    '^@salesforce/salesforcedx-lightning-lsp-common/resources/sfdx/tsconfig-sfdx.base.json$':
+      '<rootDir>/../salesforcedx-lightning-lsp-common/out/src/resources/sfdx/tsconfig-sfdx.base.json',
+    '^@salesforce/salesforcedx-lightning-lsp-common/resources/sfdx/tsconfig-sfdx.json$':
+      '<rootDir>/../salesforcedx-lightning-lsp-common/out/src/resources/sfdx/tsconfig-sfdx.json',
+    '^@salesforce/salesforcedx-lightning-lsp-common$': '<rootDir>/../salesforcedx-lightning-lsp-common/out/src/index'
+  },
+  // Add common package's out/src to moduleDirectories so relative imports from baseContext.js resolve correctly
+  // This allows Jest to find JSON files when baseContext.js does require("./resources/...")
+  moduleDirectories: ['<rootDir>/../salesforcedx-lightning-lsp-common/out/src', 'node_modules']
 };
