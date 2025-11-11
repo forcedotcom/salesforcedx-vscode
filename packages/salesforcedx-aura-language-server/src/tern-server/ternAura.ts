84b52e13f636de4869557673f3a25ba2030de635
madhur310 <madhur.shrivastava@salesforce.com>
2025-11-11 08:42:01 -0800

    fix: ts-no-check to expect-error

--- packages/salesforcedx-aura-language-server/src/tern-server/ternAura.ts
+++ packages/salesforcedx-aura-language-server/src/tern-server/ternAura.ts
@@ -232,9 +232,9 @@ const connectModule = async (file: any, out: any): Promise<void> => {
               const grand = infer.parentNode(parent, file.ast);
               if (grand.type === 'Program') {
                 // add some jsdoc
-                // @ts-ignore - objType is a custom tern property
+                // @ts-expect-error - objType is a custom tern property
                 if (node.objType) {
-                  // @ts-ignore - objType is a custom tern property
+                  // @ts-expect-error - objType is a custom tern property
                   node.objType.doc =
                     'A helper resource contains functions that can be reused by your JavaScript code in the component bundle. ';
                 }
@@ -249,10 +249,10 @@ const connectModule = async (file: any, out: any): Promise<void> => {
                 //note: propogate calls addType on the target
                 // todo: this could be made more efficient with a custom propogation strategy
                 // similar to ForAllProps_Purgeable
-                // @ts-ignore - objType is a custom tern property
+                // @ts-expect-error - objType is a custom tern property
                 if (node.objType) {
                   try {
-                    // @ts-ignore - objType is a custom tern property
+                    // @ts-expect-error - objType is a custom tern property
                     node.objType.propagate(target);
                   } catch (err) {
                     console.error(err);
@@ -300,9 +300,9 @@ const connectModule = async (file: any, out: any): Promise<void> => {
         const grand = infer.parentNode(parent, file.ast);
         if (grand.type === 'Program') {
           for (const property of node.properties) {
-            // @ts-ignore - Property type narrowing
+            // @ts-expect-error - Property type narrowing
             if (property.value.type === 'FunctionExpression') {
-              // @ts-ignore - Property type narrowing
+              // @ts-expect-error - Property type narrowing
               const val = property.value;
               const fn = val?.scope?.fnType;
               if (!fn?.name) {
