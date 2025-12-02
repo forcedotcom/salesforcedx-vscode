0baf302540eb8d3b162e211f388628e59df0f598
mshanemc <shane.mclaughlin@salesforce.com>
2025-12-02 16:46:26 -0600

    ci: eslint rule to prevent missing/typo'd command ids

--- packages/eslint-local-rules/src/index.ts
+++ packages/eslint-local-rules/src/index.ts
@@ -5,11 +5,13 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import { commandMustBeInPackageJson } from './commandMustBeInPackageJson';
 import { noDuplicateI18nValues } from './noDuplicateI18nValues';
 import { noVscodeMessageLiterals } from './noVscodeMessageLiterals';
 
 const plugin = {
   rules: {
+    'command-must-be-in-package-json': commandMustBeInPackageJson,
     'no-duplicate-i18n-values': noDuplicateI18nValues,
     'no-vscode-message-literals': noVscodeMessageLiterals
   }
