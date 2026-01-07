73e0db48113ae4c6b962d431ed8c96ba2f9af368
Daphne Yang <daphne.yang@salesforce.com>
2026-01-07 16:46:15 -0500

    chore: cleaner jest.config.js

--- packages/salesforcedx-vscode-soql/jest.config.js
+++ packages/salesforcedx-vscode-soql/jest.config.js
@@ -3,5 +3,5 @@ const baseConfig = require('../../config/jest.base.config');
 
 module.exports = {
   ...baseConfig,
-  testMatch: ['<rootDir>/out/test/jest/**/*.test.js', '<rootDir>/out/soql-common/**/*.test.js']
+  testMatch: [...baseConfig.testMatch, '<rootDir>/out/soql-common/**/*.test.js']
 };
