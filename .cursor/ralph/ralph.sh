9968fc8e763515511cf4f0520869fa0620f02722
mshanemc <shane.mclaughlin@salesforce.com>
2026-01-13 11:45:10 -0600

    chore: ralph script

--- .cursor/ralph/ralph.sh
+++ .cursor/ralph/ralph.sh
@@ -13,15 +13,15 @@ fi
 for ((i=1; i<=$1; i++)); do
   result=$(agent --force -p \
 "@.cursor/plans/e2e_tests_for_playwright_vscode_extension_a7c36853.plan.md @.cursor/ralph/progress.txt \
-0. read this doc, remove any statements in progress.txt that contradict this script.
-0a. never edit this ralph.sh file.
-1. Decide which command to work next. \
-This should be the one YOU decide has the highest priority, \
-- not necessarily the first in the list. \
+0. read this doc, remove any statements in progress.txt that contradict this script.\
+0a. never edit this ralph.sh file.\
+1. Decide which command to work next.\
 some may already exist and may or may not be passing. \
- Start with any existing failing \
+ Start with any existing failing (locally or CI)\
+ Decide what to change to address the failure.  Review past commits to make sure you are not repeating the same mistake.
+ If there are no failures, choose the next highest priority command
 1a. make sure output channel tests completely clear the output channel, there should not be text left in it.  Use screenshots to verify this.
-2. never chagne branches, stay on ralph-e2e-meta-testing
+2. never change branches, stay on ralph-e2e-meta-testing
 3. get it to run locally on test:web.  Web tests should never be skipped
 4. get it to run locally using the :desktop tests (the local environment is macos) \
 5. makes sure org-browser tests are still passing locally (web and desktop) \
@@ -31,11 +31,13 @@ some may already exist and may or may not be passing. \
 8. monitor the e2e run via gh cli.  retry if connection errors happen \
 9. If failures, download logs/artifacts and update the progress.txt file \
 9a. if failure is provably happening, you may change code in playwright ext to fix it.
+9b. If a commit made the result much worse, revert the commit and try somethinge else.
 10. once a test is passing on github actions, remove fallbacks, waits, and <try another way> as long as they still pass on github actions \
 11. make sure code aligns with very carefully aligning with coding-playwright-tests.mdc rules and still passes3. make sure the org-browser tests are still passing (they share the playwright ext) \
 12. Append your progress to the progress.txt file. Do not make statements that are not supported by the code.\
 13. If tests flap, update the plan to fix it. \
 14. If, while implementing the feature, you notice that all work \
+14b. remove from plan.md anything that is complete.  Complete means it passes locally and on github actions and aligns with the rules in coding-playwright-tests.mdc
 is complete, output <promise>COMPLETE</promise>. \
 15. never exit unless complete.  do not disable tests or attempt to find ways to exit early.
 ")
