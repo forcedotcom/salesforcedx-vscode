cd12430eafd39ff824b774607d0d2f66560ee007
mshanemc <shane.mclaughlin@salesforce.com>
2026-01-13 22:02:16 -0600

    chore: ralph scripts

--- .cursor/ralph/ralph.sh
+++ .cursor/ralph/ralph.sh
@@ -13,11 +13,11 @@ fi
 for ((i=1; i<=$1; i++)); do
   result=$(agent --force -p \
 "@..cursor/plans/simplify_playwright_test_helpers_420ec698.plan.md @.cursor/ralph/progress.txt \
-0. read this doc, remove any statements in progress.txt that contradict this script.\
 0a. never edit this ralph.sh file.\
 1.  Review past commits and the plan to make sure you are not repeating the same mistake.\
  If there are no failures, choose the next highest priority item\
  only work on one item at a time, all the way through CI results.\
+ if a large simplificaiton doesn't work, try doing it in pieces.
 2. never change branches, stay on ralph-e2e-meta\
 2a. you may not bypass eslint rules.\
 3. must pass locally on test:web.  Web tests should never be skipped \
@@ -41,7 +41,7 @@ for ((i=1; i<=$1; i++)); do
 14. If all tasks are complete and the last CI run is passing on all platforms, output <promise>COMPLETE</promise>. \
 15. never exit unless complete and all tests are passing on all platforms locally and on CI. \
 16. Do not disable tests or attempt to find ways to exit early. \
-17. You may not ask a question, only respond with <promise>COMPLETE</promise> \
+17. You may not ask a question, only respond with <promise>COMPLETE</promise> when all work is done, or marked as impossible AND all github actions CI is passing \
 ")
 
   echo "$result"
