0b5ad910f7e6057ff7c0bae11eaf275917ed7574
Daphne Yang <daphne.yang@salesforce.com>
2026-01-14 18:24:46 -0500

    refactor: remove export from IndexableArrayItem and IndexableArray because they are not used outside lwcUtils.ts

--- packages/salesforcedx-vscode-soql/src/soql-builder-ui/modules/querybuilder/services/lwcUtils.ts
+++ packages/salesforcedx-vscode-soql/src/soql-builder-ui/modules/querybuilder/services/lwcUtils.ts
@@ -1,9 +1,9 @@
-export type IndexableArrayItem<T> = {
+type IndexableArrayItem<T> = {
   index: number;
   item: T;
 };
 
-export type IndexableArray<T> = Array<IndexableArrayItem<T>>;
+type IndexableArray<T> = Array<IndexableArrayItem<T>>;
 
 export function lwcIndexableArray<T>(arr: unknown[]): IndexableArray<T> {
   return arr.map((item: T, index: number): IndexableArrayItem<T> => {
