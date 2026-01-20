b1bda4ff3998a33754a14733ed4e755c8bff6c4b
Cristina Isabel Cañizales <cristina.canizales@salesforce.com>
2026-01-20 15:34:31 -0300

    test: fix import

--- packages/salesforcedx-vscode-automation-tests/test/utils/apexTestsHelper.ts
+++ packages/salesforcedx-vscode-automation-tests/test/utils/apexTestsHelper.ts
@@ -4,7 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { retryOperation } from '@salesforce/salesforcedx-vscode-test-tools/lib/src';
+import { retryOperation } from '@salesforce/salesforcedx-vscode-test-tools/lib/src/retryUtils';
 import { executeQuickPick, getWorkbench } from '@salesforce/salesforcedx-vscode-test-tools/lib/src/ui-interaction';
 import { expect } from 'chai';
 import { BottomBarPanel, By, InputBox, QuickOpenBox } from 'vscode-extension-tester';
