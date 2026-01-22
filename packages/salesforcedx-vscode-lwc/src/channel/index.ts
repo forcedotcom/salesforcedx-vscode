b1b599f2e476e491c206026a04310f37f135bc56
madhur310 <madhur.shrivastava@salesforce.com>
2026-01-22 10:39:26 -0800

    chore: wip

--- packages/salesforcedx-vscode-lwc/src/channel/index.ts
+++ packages/salesforcedx-vscode-lwc/src/channel/index.ts
@@ -8,7 +8,4 @@
 import { ChannelService } from '@salesforce/salesforcedx-utils-vscode';
 import { nls } from '../messages';
 
-// Create channel immediately when module loads - this ensures it exists even if activation fails
-const channelName = nls.localize('lwc_output_channel_name');
-console.log('[LWC] Creating output channel:', channelName);
-export const channelService = ChannelService.getInstance(channelName);
+export const channelService = ChannelService.getInstance(nls.localize('lwc_output_channel_name'));
