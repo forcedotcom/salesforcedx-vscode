7917445495d08c96bd6407b6c3e87d7bac935173
mshanemc <shane.mclaughlin@salesforce.com>
2026-01-25 20:04:41 -0600

    ci: circular deps check

--- packages/salesforcedx-vscode-services/src/observability/spans.ts
+++ packages/salesforcedx-vscode-services/src/observability/spans.ts
@@ -4,15 +4,12 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import type { SdkLayerConfig } from './sdkLayerConfig';
 import * as vscode from 'vscode';
 import { NodeSdkLayerFor } from './spansNode';
 import { WebSdkLayerFor } from './spansWeb';
 
-export type SdkLayerConfig = {
-  extensionName: string;
-  extensionVersion: string;
-  o11yEndpoint?: string;
-};
+export type { SdkLayerConfig } from './sdkLayerConfig';
 
 /**
  * Factory for per-extension SDK layers.
