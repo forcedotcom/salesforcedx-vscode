a65fe9e2571994c23d946c2e6e28642d9e7624b7
Daphne Yang <daphne.yang@salesforce.com>
2026-01-27 14:46:41 -0500

    fix: correct exports from soql-model index.ts for soql-builder-ui

--- packages/salesforcedx-vscode-soql/src/soql-model/index.ts
+++ packages/salesforcedx-vscode-soql/src/soql-model/index.ts
@@ -8,3 +8,8 @@
 export * as Impl from './model/impl';
 export * as Soql from './model/model';
 export { SelectAnalyzer, ColumnData } from './analyzers/selectAnalyzer';
+export { ModelDeserializer } from './serialization/deserializer';
+export { ModelSerializer } from './serialization/serializer';
+export { SoqlModelUtils } from './model/util';
+export { ValidatorFactory } from './validators/validatorFactory';
+export { splitMultiInputValues } from './validators/inputUtils';
