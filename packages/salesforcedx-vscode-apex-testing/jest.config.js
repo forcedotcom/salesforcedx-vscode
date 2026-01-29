c5d4256c6d834dd3f610eb81a684e57e2cc035f7
mshanemc <shane.mclaughlin@salesforce.com>
2026-01-29 12:28:22 -0600

    chore: conflict

--- packages/salesforcedx-vscode-apex-testing/jest.config.js
+++ packages/salesforcedx-vscode-apex-testing/jest.config.js
@@ -3,6 +3,7 @@ const baseConfig = require('../../config/jest.base.config.js');
 module.exports = {
   ...baseConfig,
   displayName: 'salesforcedx-vscode-apex-testing',
+
   // This package uses per-test-file mock functions with mockResolvedValue. With resetMocks=true those implementations
   // are wiped before each test, causing Thenable-based APIs (e.g. showInformationMessage) to return undefined.
   resetMocks: false,
