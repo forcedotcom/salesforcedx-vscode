32766250807bfd235aadcccde837c9e9c87bec29
mshanemc <shane.mclaughlin@salesforce.com>
2026-02-02 13:53:41 -0600

    fix: command overlap (core vs. metdata)

--- packages/salesforcedx-vscode-metadata/src/index.ts
+++ packages/salesforcedx-vscode-metadata/src/index.ts
@@ -5,11 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import {
-  closeExtensionScope,
-  ExtensionProviderService,
-  getExtensionScope
-} from '@salesforce/effect-ext-utils';
+import { closeExtensionScope, ExtensionProviderService, getExtensionScope } from '@salesforce/effect-ext-utils';
 import * as Effect from 'effect/Effect';
 import * as Scope from 'effect/Scope';
 import * as vscode from 'vscode';
@@ -26,6 +22,7 @@ import { projectRetrieveStart } from './commands/retrieveStart/projectRetrieveSt
 import { viewAllChanges, viewLocalChanges, viewRemoteChanges } from './commands/showSourceTrackingDetails';
 import { sourceDiff } from './commands/sourceDiff';
 import { DEPLOY_ON_SAVE_ENABLED, EXTENSION_NAME, METADATA_CONFIG_SECTION } from './constants';
+import { getShowSharedCommands, watchUseMetadataExtensionCommands } from './services/configWatcher';
 import { createDeployOnSaveService } from './services/deployOnSaveService';
 import { AllServicesLayer } from './services/extensionProvider';
 import { createSourceTrackingStatusBar } from './statusBar/sourceTrackingStatusBar';
@@ -38,60 +35,54 @@ export const activate = async (context: vscode.ExtensionContext): Promise<void> 
 export const deactivate = async (): Promise<void> =>
   Effect.runPromise(deactivateEffect().pipe(Effect.provide(AllServicesLayer)));
 
-/** Check if this extension should register shared commands (when core is not installed or config enables it) */
-const shouldRegisterSharedCommands = (): boolean => {
-  const coreExtension = vscode.extensions.getExtension('salesforce.salesforcedx-vscode-core');
-  const useMetadataCommands = vscode.workspace
-    .getConfiguration('salesforcedx-vscode-core')
-    .get<boolean>('useMetadataExtensionCommands', false);
-  return !coreExtension || useMetadataCommands;
-};
-
 /** Activate the metadata extension */
 export const activateEffect = Effect.fn(`activation:${EXTENSION_NAME}`)(function* (context: vscode.ExtensionContext) {
   const svc = yield* (yield* (yield* ExtensionProviderService).getServicesApi).services.ChannelService;
   yield* svc.appendToChannel('Salesforce Metadata extension activating');
 
-  // Register shared commands only if core extension is not installed or config enables it
-  if (shouldRegisterSharedCommands()) {
-    yield* Effect.promise(() =>
-      vscode.commands.executeCommand('setContext', 'salesforcedx-vscode-metadata.showSharedCommands', true)
-    );
+  // you don't have the core ext (ex: web) OR you have it and have the setting to use the metadata extension commands
+  yield* Effect.promise(() =>
+    vscode.commands.executeCommand('setContext', `${EXTENSION_NAME}.showSharedCommands`, getShowSharedCommands())
+  );
 
-    yield* svc.appendToChannel('Registering shared commands (core extension not present or config enabled)');
-    context.subscriptions.push(
-      vscode.commands.registerCommand('sf.project.deploy.start', async () => projectDeployStart(false)),
-      vscode.commands.registerCommand('sf.project.deploy.start.ignore.conflicts', async () => projectDeployStart(true)),
-      vscode.commands.registerCommand('sf.project.retrieve.start', async () => projectRetrieveStart(false)),
-      vscode.commands.registerCommand('sf.project.retrieve.start.ignore.conflicts', async () =>
-        projectRetrieveStart(true)
-      ),
-      vscode.commands.registerCommand('sf.view.all.changes', viewAllChanges),
-      vscode.commands.registerCommand('sf.view.local.changes', viewLocalChanges),
-      vscode.commands.registerCommand('sf.view.remote.changes', viewRemoteChanges),
-      vscode.commands.registerCommand('sf.source.tracking.reset.remote', resetRemoteTracking),
-      vscode.commands.registerCommand('sf.apex.generate.class', createApexClass),
-      vscode.commands.registerCommand('sf.delete.source', deleteSourcePaths),
-      vscode.commands.registerCommand('sf.delete.source.current.file', deleteSourcePaths),
-      vscode.commands.registerCommand('sf.deploy.source.path', deploySourcePaths),
-      vscode.commands.registerCommand('sf.deploy.active.editor', deployActiveEditor),
-      vscode.commands.registerCommand('sf.deploy.in.manifest', deployManifest),
-      vscode.commands.registerCommand('sf.retrieve.source.path', retrieveSourcePaths),
-      vscode.commands.registerCommand('sf.retrieve.current.source.file', retrieveSourcePaths),
-      vscode.commands.registerCommand('sf.retrieve.in.manifest', retrieveManifest),
-      vscode.commands.registerCommand('sf.project.generate.manifest', generateManifest),
-      vscode.commands.registerCommand('sf.source.diff', sourceDiff)
-    );
+  yield* svc.appendToChannel('Registering metadata commands');
+  context.subscriptions.push(
+    vscode.commands.registerCommand('sf.metadata.project.deploy.start', async () => projectDeployStart(false)),
+    vscode.commands.registerCommand('sf.metadata.project.deploy.start.ignore.conflicts', async () =>
+      projectDeployStart(true)
+    ),
+    vscode.commands.registerCommand('sf.metadata.project.retrieve.start', async () => projectRetrieveStart(false)),
+    vscode.commands.registerCommand('sf.metadata.project.retrieve.start.ignore.conflicts', async () =>
+      projectRetrieveStart(true)
+    ),
+    vscode.commands.registerCommand('sf.metadata.view.all.changes', viewAllChanges),
+    vscode.commands.registerCommand('sf.metadata.view.local.changes', viewLocalChanges),
+    vscode.commands.registerCommand('sf.metadata.view.remote.changes', viewRemoteChanges),
+    vscode.commands.registerCommand('sf.metadata.source.tracking.reset.remote', resetRemoteTracking),
+    vscode.commands.registerCommand('sf.metadata.apex.generate.class', createApexClass),
+    vscode.commands.registerCommand('sf.metadata.delete.source', deleteSourcePaths),
+    vscode.commands.registerCommand('sf.metadata.delete.source.current.file', deleteSourcePaths),
+    vscode.commands.registerCommand('sf.metadata.deploy.source.path', deploySourcePaths),
+    vscode.commands.registerCommand('sf.metadata.deploy.active.editor', deployActiveEditor),
+    vscode.commands.registerCommand('sf.metadata.deploy.in.manifest', deployManifest),
+    vscode.commands.registerCommand('sf.metadata.retrieve.source.path', retrieveSourcePaths),
+    vscode.commands.registerCommand('sf.metadata.retrieve.current.source.file', retrieveSourcePaths),
+    vscode.commands.registerCommand('sf.metadata.retrieve.in.manifest', retrieveManifest),
+    vscode.commands.registerCommand('sf.metadata.project.generate.manifest', generateManifest),
+    vscode.commands.registerCommand('sf.metadata.source.diff', sourceDiff)
+  );
 
-    if (process.env.ESBUILD_PLATFORM === 'web') {
-      vscode.workspace.getConfiguration(METADATA_CONFIG_SECTION).update(DEPLOY_ON_SAVE_ENABLED, true);
-    }
-    // Start deploy on save service only if this extension handles shared commands
-    yield* Effect.forkIn(createDeployOnSaveService(), yield* getExtensionScope());
-
-    // Register source tracking status bar
-    yield* Effect.forkIn(createSourceTrackingStatusBar(), yield* getExtensionScope());
+  if (process.env.ESBUILD_PLATFORM === 'web') {
+    vscode.workspace.getConfiguration(METADATA_CONFIG_SECTION).update(DEPLOY_ON_SAVE_ENABLED, true);
   }
+  // Start deploy on save service
+  yield* Effect.forkIn(createDeployOnSaveService(), yield* getExtensionScope());
+
+  // Register source tracking status bar
+  yield* Effect.forkIn(createSourceTrackingStatusBar(), yield* getExtensionScope());
+
+  // Watch for config changes to update showSharedCommands context
+  yield* Effect.forkIn(watchUseMetadataExtensionCommands(), yield* getExtensionScope());
 
   yield* svc.appendToChannel('Salesforce Metadata activation complete.');
 });
---
+++ packages/salesforcedx-vscode-metadata/src/services/configWatcher.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2025, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { ExtensionProviderService } from '@salesforce/effect-ext-utils';
+import * as Duration from 'effect/Duration';
+import * as Effect from 'effect/Effect';
+import * as PubSub from 'effect/PubSub';
+import * as Stream from 'effect/Stream';
+import * as vscode from 'vscode';
+import { CORE_CONFIG_SECTION, EXTENSION_NAME } from '../constants';
+
+const USE_METADATA_EXTENSION_COMMANDS_KEY = 'useMetadataExtensionCommands';
+const SHOW_SHARED_COMMANDS_CONTEXT = `${EXTENSION_NAME}.showSharedCommands`;
+
+export const getShowSharedCommands = (): boolean =>
+  !vscode.extensions.getExtension('salesforce.salesforcedx-vscode-core') ||
+  vscode.workspace.getConfiguration(CORE_CONFIG_SECTION).get<boolean>(USE_METADATA_EXTENSION_COMMANDS_KEY, false);
+
+const updateShowSharedCommandsContext = () =>
+  Effect.promise(() =>
+    vscode.commands.executeCommand('setContext', SHOW_SHARED_COMMANDS_CONTEXT, getShowSharedCommands())
+  );
+
+/** Watches for useMetadataExtensionCommands setting changes and updates context */
+export const watchUseMetadataExtensionCommands = () =>
+  Effect.gen(function* () {
+    const api = yield* (yield* ExtensionProviderService).getServicesApi;
+    const channelService = yield* api.services.ChannelService;
+
+    const pubsub = yield* PubSub.sliding<vscode.ConfigurationChangeEvent>(100);
+    const disposable = vscode.workspace.onDidChangeConfiguration(event => {
+      Effect.runSync(PubSub.publish(pubsub, event));
+    });
+
+    yield* Effect.addFinalizer(() =>
+      Effect.sync(() => {
+        disposable?.dispose();
+      })
+    );
+
+    yield* Stream.fromPubSub(pubsub).pipe(
+      Stream.filter(event =>
+        event.affectsConfiguration(`${CORE_CONFIG_SECTION}.${USE_METADATA_EXTENSION_COMMANDS_KEY}`)
+      ),
+      Stream.debounce(Duration.millis(100)),
+      Stream.tap(() => channelService.appendToChannel(`ConfigChanged: ${USE_METADATA_EXTENSION_COMMANDS_KEY}`)),
+      Stream.runForEach(() => updateShowSharedCommandsContext())
+    );
+  }).pipe(Effect.withSpan('watchUseMetadataExtensionCommands'));
