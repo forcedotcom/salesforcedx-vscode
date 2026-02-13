2ae10bf3360de653d4cfb05517e369ceba90949d
Daphne Yang <daphne.yang@salesforce.com>
2026-02-13 14:47:20 -0500

    feat: remove MessageServiceFactory and directly instantiate VscodeMessageService

--- packages/salesforcedx-vscode-soql/test/jest/soql-builder-ui/modules/querybuilder/app/app.test.ts
+++ packages/salesforcedx-vscode-soql/test/jest/soql-builder-ui/modules/querybuilder/app/app.test.ts
@@ -26,7 +26,6 @@ import {
   MessageType,
   SoqlEditorEvent
 } from '../../../../../../src/soql-builder-ui/modules/querybuilder/services/message/soqlEditorEvent';
-import { MessageServiceFactory } from '../../../../../../src/soql-builder-ui/modules/querybuilder/services/message/messageServiceFactory';
 import { IMessageService } from '../../../../../../src/soql-builder-ui/modules/querybuilder/services/message/iMessageService';
 import * as globals from '../../../../../../src/soql-builder-ui/modules/querybuilder/services/globals';
 
@@ -61,7 +60,6 @@ describe('App should', () => {
     payload: accountQuery
   };
   const querybuilderFromSelector = 'querybuilder-from';
-  let originalCreateFn;
   const createSoqlEditorEvent = (queryOverride = accountQuery, eventOverride?) => {
     const query = queryOverride;
     const event = { ...soqlEditorEvent, ...eventOverride };
@@ -70,11 +68,6 @@ describe('App should', () => {
   };
   beforeEach(() => {
     messageService = new TestMessageService() as unknown as IMessageService;
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    originalCreateFn = MessageServiceFactory.create;
-    MessageServiceFactory.create = () => {
-      return messageService;
-    };
     loadSObjectDefinitionsSpy = jest.spyOn(ToolingSDK.prototype, 'loadSObjectDefinitions');
     loadSObjectMetadataSpy = jest.spyOn(ToolingSDK.prototype, 'loadSObjectMetatada');
     jest.spyOn(globals, 'getBodyClass').mockReturnValue('vscode-dark');
@@ -82,11 +75,14 @@ describe('App should', () => {
     app = createElement('querybuilder-app', {
       is: TestApp
     });
+    // Inject the test message service
+    app.messageService = messageService;
+    app.toolingSDK = new ToolingSDK(messageService);
+    app.modelService = new ToolingModelService(messageService);
     document.body.appendChild(app);
   });
 
   afterEach(() => {
-    MessageServiceFactory.create = originalCreateFn;
     jest.clearAllMocks();
     while (document.body.firstChild) {
       document.body.removeChild(document.body.firstChild);
