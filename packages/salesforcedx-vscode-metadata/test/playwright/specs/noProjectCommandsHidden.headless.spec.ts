37b414be4f973f1ca76e589fcf9a5bac28edd959
mshanemc <shane.mclaughlin@salesforce.com>
2026-03-19 16:17:27 -0500

    test: make sure apex class create is hidden when not project

--- packages/salesforcedx-vscode-metadata/test/playwright/specs/noProjectCommandsHidden.headless.spec.ts
+++ packages/salesforcedx-vscode-metadata/test/playwright/specs/noProjectCommandsHidden.headless.spec.ts
@@ -13,6 +13,7 @@ import {
   assertWelcomeTabExists,
   closeWelcomeTabs,
   ensureSecondarySideBarHidden,
+  waitForEmptyWorkspaceReady,
   verifyCommandDoesNotExist,
   validateNoCriticalErrors,
   isDesktop
@@ -20,7 +21,7 @@ import {
 import packageNls from '../../../package.nls.json';
 
 (isDesktop() ? emptyWorkspaceDesktopTest : emptyWorkspaceDesktopTest.skip.bind(emptyWorkspaceDesktopTest))(
-  'No project: LWC create and deploy/retrieve/delete commands hidden',
+  'No project: LWC/Apex create and deploy/retrieve/delete commands hidden',
   async ({ page }) => {
     const consoleErrors = setupConsoleMonitoring(page);
     const networkErrors = setupNetworkMonitoring(page);
@@ -30,10 +31,12 @@ import packageNls from '../../../package.nls.json';
       await assertWelcomeTabExists(page);
       await closeWelcomeTabs(page);
       await ensureSecondarySideBarHidden(page);
+      await waitForEmptyWorkspaceReady(page);
     });
 
-    await emptyWorkspaceDesktopTest.step('verify LWC create does not exist', async () => {
+    await emptyWorkspaceDesktopTest.step('verify LWC and Create Apex Class do not exist', async () => {
       await verifyCommandDoesNotExist(page, packageNls.lightning_generate_lwc_text);
+      await verifyCommandDoesNotExist(page, packageNls.apex_generate_class_text);
     });
 
     await emptyWorkspaceDesktopTest.step('verify deploy/retrieve/delete/generate manifest do not exist', async () => {
