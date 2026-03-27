e6027c239381383ea36516007c889afd0583b032
madhur310 <madhur.shrivastava@salesforce.com>
2026-03-26 19:16:25 -0700

    Revert "fix: try fixing external deps"
    This reverts commit c3c1e5b8b8baee84d01b76f5015acbd25bfc8968.

--- packages/salesforcedx-vscode-lwc/esbuild.config.mjs
+++ packages/salesforcedx-vscode-lwc/esbuild.config.mjs
@@ -38,6 +38,7 @@ const htmlLsExternalList = [
   'applicationinsights',
   '@salesforce/lightning-lsp-common',
   'jest-editor-support'
+  // @babel/core and @babel/preset-typescript/package.json are bundled:
   // - @babel/core: all its require() calls are static; the dynamic import() for ESM config loading passes through
   // - @babel/preset-typescript/package.json: bundled as JSON via loader: { '.json': 'json' }
 ];
@@ -69,7 +70,7 @@ await build({
 await build({
   ...commonConfigBrowser,
   loader: { '.json': 'json' },
-  external: htmlLsExternalList,
+  external: htmlLsExternalList.filter(e => e !== '@babel/core'),
   entryPoints: ['../salesforcedx-lwc-language-server/out/src/server.js'],
   outfile: './dist/web/lwcServer.js',
   bundle: true,
