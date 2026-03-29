6cc1d176df60465734856f7d0f5fd5387d695203
Cristina Cañizales <113132642+CristiCanizales@users.noreply.github.com>
2026-03-29 03:00:04 -0300

    fix: only run refreshSObjects definitions on sfdx projects (#7095)

--- packages/salesforcedx-vscode-core/src/index.ts
+++ packages/salesforcedx-vscode-core/src/index.ts
@@ -263,12 +263,17 @@ export const activate = async (extensionContext: vscode.ExtensionContext): Promi
     CommandEventDispatcher.getInstance()
   );
 
-  if (metadataExtension && vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0) {
-    // Refresh SObject definitions if there aren't any faux classes (metadata ext registers the command)
+  if (
+    metadataExtension &&
+    salesforceProjectOpened &&
+    vscode.workspace.workspaceFolders &&
+    vscode.workspace.workspaceFolders.length > 0
+  ) {
+    // Refresh SObject definitions only for an open Salesforce project
+    // when faux classes are missing (metadata extension registers the command).
     const sobjectRefreshStartup: boolean = vscode.workspace
       .getConfiguration(SFDX_CORE_CONFIGURATION_NAME)
       .get<boolean>(ENABLE_SOBJECT_REFRESH_ON_STARTUP, false);
-
     await initSObjectDefinitions(vscode.workspace.workspaceFolders[0].uri.fsPath, sobjectRefreshStartup);
   }
 
