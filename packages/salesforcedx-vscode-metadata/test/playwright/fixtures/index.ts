e9c54a4ab852d7da9b202d2f5391ee0ac8cf63fc
mshanemc <shane.mclaughlin@salesforce.com>
2026-03-31 17:35:09 -0500

    test: e2e conflict testing for web

--- packages/salesforcedx-vscode-metadata/test/playwright/fixtures/index.ts
+++ packages/salesforcedx-vscode-metadata/test/playwright/fixtures/index.ts
@@ -6,7 +6,8 @@
  */
 
 import { test as webTest } from '@playwright/test';
-import { desktopTest, dreamhouseDesktopTest, nonTrackingDesktopTest } from './desktopFixtures';
+import { desktopTest, dreamhouseDesktopTest, nonTrackingDesktopTest, trackingConflictTest as trackingConflictDesktopTest } from './desktopFixtures';
+import { webTrackingConflictTest } from './webConflictFixtures';
 
 const isDesktop = process.env.VSCODE_DESKTOP === '1';
 
@@ -24,4 +25,5 @@ webTest.afterEach(async ({ page }, testInfo) => {
 export const test = isDesktop ? desktopTest : webTest;
 export const dreamhouseTest = isDesktop ? dreamhouseDesktopTest : webTest;
 export const nonTrackingTest = isDesktop ? nonTrackingDesktopTest : webTest;
+export const trackingConflictTest = isDesktop ? trackingConflictDesktopTest : webTrackingConflictTest;
 export { emptyWorkspaceDesktopTest } from './desktopFixtures';
--- packages/salesforcedx-vscode-metadata/test/playwright/specs-conflicts/tracking/deploy-conflict.spec.ts
+++ packages/salesforcedx-vscode-metadata/test/playwright/specs-conflicts/tracking/deploy-conflict.spec.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { trackingConflictTest as test } from '../../fixtures/desktopFixtures';
+import { trackingConflictTest as test } from '../../fixtures';
 import {
   createApexClass,
   deployCurrentSourceToOrg,
