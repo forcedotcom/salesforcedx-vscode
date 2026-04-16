b5998b7b9c5e82f972e795d284b0a31134c7d0e0
mshanemc <shane.mclaughlin@salesforce.com>
2026-04-16 13:07:40 -0500

    ci: playwrigt config location consistency

--- packages/playwright-vscode-ext/test/playwright/playwright.config.desktop.ts
+++ packages/playwright-vscode-ext/test/playwright/playwright.config.desktop.ts
@@ -4,6 +4,6 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { createDesktopConfig } from './src/config/createDesktopConfig';
+import { createDesktopConfig } from '../../src/config/createDesktopConfig';
 
-export default createDesktopConfig();
+export default createDesktopConfig({ testDir: './specs' });
--- packages/playwright-vscode-ext/test/playwright/playwright.config.web.ts
+++ packages/playwright-vscode-ext/test/playwright/playwright.config.web.ts
@@ -4,6 +4,6 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { createWebConfig } from './src/config/createWebConfig';
+import { createWebConfig } from '../../src/config/createWebConfig';
 
-export default createWebConfig();
+export default createWebConfig({ testDir: './specs' });
