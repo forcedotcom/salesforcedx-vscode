5046f50e7ee4bfff4f5c1fc6a83021a20083757a
peternhale <peter.hale@salesforce.com>
2026-06-24 08:16:10 -0600

    test(lwc): cover lwcTestDebugActiveTextEditorTest delegation

--- packages/salesforcedx-vscode-lwc/test/jest/testSupport/commands/lwcTestDebugAction.test.ts
+++ packages/salesforcedx-vscode-lwc/test/jest/testSupport/commands/lwcTestDebugAction.test.ts
@@ -13,22 +13,32 @@ jest.mock('../../../../src/testSupport/testExplorer/lwcTestController', () => ({
   getLwcTestController: () => ({ runByExecutionInfo, runActiveEditorFile })
 }));
 
-import { lwcTestFileDebug, lwcTestCaseDebug } from '../../../../src/testSupport/commands/lwcTestDebugAction';
+import {
+  lwcTestFileDebug,
+  lwcTestCaseDebug,
+  lwcTestDebugActiveTextEditorTest
+} from '../../../../src/testSupport/commands/lwcTestDebugAction';
 
 describe('lwcTestDebugAction routes through the controller', () => {
   beforeEach(() => {
     runByExecutionInfo.mockClear();
+    runActiveEditorFile.mockClear();
   });
 
   it('lwcTestFileDebug calls controller.runByExecutionInfo with isDebug=true', async () => {
-    const testExecutionInfo = { kind: 'testFile', testUri: URI.file('/a/foo.test.js') };
+    const testExecutionInfo = { kind: 'testFile' as const, testUri: URI.file('/a/foo.test.js') };
     await lwcTestFileDebug({ testExecutionInfo });
     expect(runByExecutionInfo).toHaveBeenCalledWith(testExecutionInfo, true);
   });
 
   it('lwcTestCaseDebug calls controller.runByExecutionInfo with isDebug=true', async () => {
-    const testExecutionInfo = { kind: 'testCase', testUri: URI.file('/a/foo.test.js'), testName: 'does x' };
+    const testExecutionInfo = { kind: 'testCase' as const, testUri: URI.file('/a/foo.test.js'), testName: 'does x' };
     await lwcTestCaseDebug({ testExecutionInfo });
     expect(runByExecutionInfo).toHaveBeenCalledWith(testExecutionInfo, true);
+  });
+
+  it('lwcTestDebugActiveTextEditorTest calls controller.runActiveEditorFile with isDebug=true', async () => {
+    await lwcTestDebugActiveTextEditorTest();
+    expect(runActiveEditorFile).toHaveBeenCalledWith(true);
   });
 });
