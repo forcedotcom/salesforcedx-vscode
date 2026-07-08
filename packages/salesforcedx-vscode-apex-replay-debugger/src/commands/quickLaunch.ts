e02c93798ecde1c42a409416752026ba91a85981
Daphne Yang <daphne.yang@salesforce.com>
2026-07-08 13:22:13 -0400

    fix: add back "Running" to the progress notification

--- packages/salesforcedx-vscode-apex-replay-debugger/src/commands/quickLaunch.ts
+++ packages/salesforcedx-vscode-apex-replay-debugger/src/commands/quickLaunch.ts
@@ -132,7 +132,11 @@ class QuickLaunch {
 
 export const setupAndDebugTests = async (className: string, methodName?: string): Promise<void> => {
   await vscode.window.withProgress(
-    { location: vscode.ProgressLocation.Notification, title: nls.localize('debug_test_exec_name'), cancellable: false },
+    {
+      location: vscode.ProgressLocation.Notification,
+      title: `Running ${nls.localize('debug_test_exec_name')}`,
+      cancellable: false
+    },
     () => new QuickLaunch().debugTest(className, methodName)
   );
 };
