daf10f6e6614e2f157cf52b3fa47dc8d7f86eaa0
mshanemc <shane.mclaughlin@salesforce.com>
2026-07-30 10:26:06 -0500

    fix: repair PR-7920 [opencode:ci-repair-W-23557549-7920-26f8d4938fb1-adf0db880654]

---
+++ packages/salesforcedx-vscode-metadata/test/playwright/specs/taggedErrorChannelOutput.headless.spec.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2026, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { expect } from '@playwright/test';
+import {
+  closeAllEditors,
+  closeWelcomeTabs,
+  ensureSecondarySideBarHidden,
+  executeCommandWithCommandPalette,
+  selectOutputChannel,
+  verifyCommandExists,
+  waitForNotification,
+  waitForOutputChannelText,
+  waitForVSCodeWorkbench
+} from '@salesforce/playwright-vscode-ext';
+import packageNls from '../../../package.nls.json';
+import { messages } from '../../../src/messages/i18n';
+import { test } from '../fixtures';
+
+test('tagged command errors include the tag only in channel output', async ({ page }) => {
+  test.setTimeout(120_000);
+  await waitForVSCodeWorkbench(page);
+  await closeWelcomeTabs(page);
+  await ensureSecondarySideBarHidden(page);
+  await verifyCommandExists(page, packageNls.deploy_in_manifest_text, 60_000);
+  await closeAllEditors(page);
+
+  await executeCommandWithCommandPalette(page, packageNls.deploy_in_manifest_text);
+
+  const notification = await waitForNotification(page, new RegExp(`^${messages.deploy_select_manifest}$`));
+  await expect(notification, 'Error notification should remain unprefixed').toHaveText(messages.deploy_select_manifest);
+
+  await selectOutputChannel(page, 'Salesforce Metadata');
+  await waitForOutputChannelText(page, {
+    expectedText: `[ManifestSelectionRequiredError] ${messages.deploy_select_manifest}`
+  });
+});
