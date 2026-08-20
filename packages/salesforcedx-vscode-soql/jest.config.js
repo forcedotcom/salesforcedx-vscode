3fd8b4c79ab536d44c1cceac5f0b611b2f95da58
peternhale <peternhale@users.noreply.github.com>
2026-08-20 10:08:16 -0600

    test(soql): establish UI migration baselines (#8021)

--- packages/salesforcedx-vscode-soql/jest.config.js
+++ packages/salesforcedx-vscode-soql/jest.config.js
@@ -2,7 +2,11 @@
 const baseConfig = require('../../config/jest.base.config');
 
 module.exports = Object.assign({}, baseConfig, {
-  testPathIgnorePatterns: [...(baseConfig.testPathIgnorePatterns || []), '/test/jest/soql-builder-ui/'],
+  testPathIgnorePatterns: [
+    ...(baseConfig.testPathIgnorePatterns || []),
+    '/test/jest/soql-builder-ui/',
+    '/test/jest/queryDataView/queryDataViewController.test.ts'
+  ],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { isolatedModules: true }]
   }
