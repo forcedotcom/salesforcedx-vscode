bdd18274b4fb282e4a9798d3ed4a81e7fac464b8
peternhale <peter.hale@salesforce.com>
2026-08-24 14:06:57 -0600

    refactor(services): add canonical Transmogrifier boundary - W-23973850

--- packages/salesforcedx-vscode-services/src/index.ts
+++ packages/salesforcedx-vscode-services/src/index.ts
@@ -310,7 +310,12 @@ export type {
   ListMetadataError,
   SObjectGlobalDescribeItem
 } from './core/metadataDescribeService';
-export type { DescribeSObjectResult, TransmogrifierService } from './core/transmogrifierService';
+export type {
+  DescribeSObjectResult,
+  RestSObjectDescribeTransmogrifierInput,
+  TransmogrifierInput,
+  TransmogrifierService
+} from './core/transmogrifierService';
 export type { SObject, SObjectField, ChildRelationship } from './core/schemas/sObject';
 export {
   SObjectSchema,
@@ -318,6 +323,7 @@ export {
   ChildRelationshipSchema,
   PicklistValueSchema
 } from './core/schemas/sObject';
+export { TransmogrifierError } from './core/transmogrifierService';
 export type { ExecuteAnonymousResult } from './core/executeAnonymousService';
 export type { ExecuteAnonymousError } from './errors/executeAnonymousErrors';
 export type { ApexLogBodyFetchError, ApexLogQueryError } from './errors/apexLogErrors';
